63852d5fbebe964cc3845358c2d1a77aff6e1e64
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-28 19:22:44 +0200

    [oracle] refactoring

--- computationOracle/test/Oracle.test.js
+++ computationOracle/test/Oracle.test.js
@@ -4,7 +4,7 @@ require('chai')
 
 const Oracle = artifacts.require('Oracle');
 
-contract('Oracle', accounts => {
+contract('Oracle', (accounts) => {
     it('correct deployed', async () => {
         const oracle = await Oracle.new();
         oracle.should.not.equal(null);
