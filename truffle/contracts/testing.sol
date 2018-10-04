57218950a7b0627f053860d332c60085f768a179
Toni <toni.mateos@gmail.com>
2018-10-04 21:06:23 +0200

    moving wrappers from public to just external

--- truffle/contracts/testing.sol
+++ truffle/contracts/testing.sol
@@ -16,43 +16,43 @@ import "./games.sol";
 contract Testing is GameEngine {
 
     // Wrappers for helpers:
-    function test_encode(uint8 nElem, uint16[] nums, uint bits) public pure returns(uint result) {
+    function test_encode(uint8 nElem, uint16[] nums, uint bits) external pure returns(uint result) {
         return encode(nElem, nums, bits);
     }
 
-    function test_decode(uint8 nNumbers, uint longState, uint bits) public pure returns(uint16[] result) {
+    function test_decode(uint8 nNumbers, uint longState, uint bits) external pure returns(uint16[] result) {
         return decode(nNumbers, longState, bits);
     }
 
-    function test_getNumAtIndex(uint longState, uint8 index, uint bits) public pure returns(uint) {
+    function test_getNumAtIndex(uint longState, uint8 index, uint bits) external pure returns(uint) {
         return getNumAtIndex(longState, index, bits);
     }
 
-    function test_setNumAtIndex(uint value, uint longState, uint8 index, uint bits) public pure returns(uint) {
+    function test_setNumAtIndex(uint value, uint longState, uint8 index, uint bits) external pure returns(uint) {
         return setNumAtIndex(value, longState, index, bits);
     }
 
-    function test_computeKeccak256ForNumber(uint n) public pure returns(uint)
+    function test_computeKeccak256ForNumber(uint n) external pure returns(uint)
     {
         return computeKeccak256ForNumber(n);
     }
 
-    function test_computeKeccak256(string s, uint n1, uint n2) public pure returns(uint) {
+    function test_computeKeccak256(string s, uint n1, uint n2) external pure returns(uint) {
         return computeKeccak256(s, n1, n2);
     }
 
-    function test_throwDice(uint weight1, uint weight2, uint rndNum, uint factor) public pure returns(uint8) {
+    function test_throwDice(uint weight1, uint weight2, uint rndNum, uint factor) external pure returns(uint8) {
         return throwDice(weight1, weight2, rndNum, factor);
     }
 
-    function test_throwDiceArray(uint[] memory weights, uint rndNum, uint factor) public pure returns(uint8) {
+    function test_throwDiceArray(uint[] weights, uint rndNum, uint factor) external pure returns(uint8) {
         return throwDiceArray(weights, rndNum, factor);
     }
 
 
     // Wrappers for Players
 
-    function test_createRandomPlayer(string _playerName, uint _teamIdx, uint16 _userChoice, uint8 _playerNumberInTeam, uint8 _playerRole) public {
+    function test_createRandomPlayer(string _playerName, uint _teamIdx, uint16 _userChoice, uint8 _playerNumberInTeam, uint8 _playerRole) external {
         return createRandomPlayer(_playerName, _teamIdx, _userChoice, _playerNumberInTeam, _playerRole);
     }
 
@@ -68,7 +68,7 @@ contract Testing is GameEngine {
         uint _endurance,
         uint _role
         )
-        public {
+        external {
         return createPlayer(
             _playerName,
             _teamIdx, 
@@ -83,7 +83,7 @@ contract Testing is GameEngine {
         );
     }
 
-    function test_getRole(uint idx, uint8 first, uint8 second) public pure returns(uint8) {
+    function test_getRole(uint idx, uint8 first, uint8 second) external pure returns(uint8) {
         return  getRole(idx, first, second);
     }
 
@@ -93,16 +93,16 @@ contract Testing is GameEngine {
 
     // WRAPPERS FOR TEAMS
 
-    function test_getNCreatedTeams() public view returns(uint) { return getNCreatedTeams(); }
-    function test_getTeamName(uint idx) public view returns(string) { return getTeamName(idx); }
-    function test_createTeam(string _teamName) public { return createTeam(_teamName); }
-    function test_getSkill(uint _teamIdx, uint8 _playerIdx) public view returns(uint) { return getSkill(_teamIdx, _playerIdx); }
+    function test_getNCreatedTeams() external view returns(uint) { return getNCreatedTeams(); }
+    function test_getTeamName(uint idx) external view returns(string) { return getTeamName(idx); }
+    function test_createTeam(string _teamName) external { return createTeam(_teamName); }
+    function test_getSkill(uint _teamIdx, uint8 _playerIdx) external view returns(uint) { return getSkill(_teamIdx, _playerIdx); }
 
 
     // WRAPPERS FOR GAMES
 
     function test_playGame(uint teamIdx1, uint teamIdx2, uint[] rndNum1, uint[] rndNum2, uint[] rndNum3, uint[] rndNum4)
-        public
+        external
         view
         returns (uint16[2] memory teamGoals) {
             return playGame(teamIdx1, teamIdx2, rndNum1, rndNum2, rndNum3, rndNum4);
