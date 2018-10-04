be1b1748f4b1fefa95d0386c71ec1875ae9a93f7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-10-05 00:15:20 +0200

    migrate CryptoTeams

--- truffle-loom/truffle-ethereum/migrations/2_deploy_mainnet_contracts.js
+++ truffle-loom/truffle-ethereum/migrations/2_deploy_mainnet_contracts.js
@@ -1,6 +1,7 @@
 const { writeFileSync } = require('fs')
 
 const CryptoPlayers = artifacts.require('CryptoPlayers')
+const CryptoTeams = artifacts.require('CryptoTeams');
 const Gateway = artifacts.require('Gateway')
 
 module.exports = (deployer, _network, accounts) => {
@@ -17,11 +18,20 @@ module.exports = (deployer, _network, accounts) => {
     console.log(`CryptoPlayers deployed at address: ${cryptoPlayersInstance.address}`)
     console.log(`CryptoPlayers transaction at hash: ${cryptoPlayersContract.transactionHash}`)
 
+    const cryptoTeamsContract = await deployer.deploy(CryptoTeams, gatewayInstance.address)
+    const cryptoTeamsInstance = await CryptoTeams.deployed()
+
+    console.log(`CryptoTeams deployed at address: ${cryptoTeamsInstance.address}`)
+    console.log(`CryptoTeams transaction at hash: ${cryptoTeamsContract.transactionHash}`)
+
     await gatewayInstance.toggleToken(cryptoPlayersInstance.address, { from: validator })
+    await gatewayInstance.toggleToken(cryptoTeamsInstance.address, { from: validator })
     await cryptoPlayersInstance.register(user)
 
     writeFileSync('../gateway_address', gatewayInstance.address)
     writeFileSync('../crypto_players_address', cryptoPlayersInstance.address)
     writeFileSync('../crypto_players_tx_hash', cryptoPlayersContract.transactionHash)
+    writeFileSync('../crypto_teams_address', cryptoTeamsInstance.address)
+    writeFileSync('../crypto_teams_tx_hash', cryptoTeamsContract.transactionHash)
   })
 }
