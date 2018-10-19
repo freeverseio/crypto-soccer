d8f31ac79949e0e08dd5e501f330a12ef8fbb247
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-10-19 19:24:07 +0200

    removed provider log ... TODO add next time

--- webclient/src/components/connection.jsx
+++ webclient/src/components/connection.jsx
@@ -3,15 +3,12 @@ import { Header, Segment } from 'semantic-ui-react'
 
 class Connection extends Component {
     render() {
-        const { provider, ethLeagueManager } = this.props;
+        const { testingFacade } = this.props;
 
         return (
             <Segment clearing>
                 <Header as='h2' floated='right'>
-                    {ethLeagueManager ? "connected" : "disconnected "}
-                </Header>
-                <Header as='h2' floated='left'>
-                    {provider.host}
+                    {testingFacade ? "connected" : "disconnected "}
                 </Header>
             </Segment>
         )
