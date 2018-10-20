14553299ab51875cf993236aec999abae573c008
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-10-20 16:54:15 +0200

    removed warnings

--- webclient/src/components/team_list.jsx
+++ webclient/src/components/team_list.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Card, Item, Image } from 'semantic-ui-react'
+import { Item } from 'semantic-ui-react'
 import TeamPlayerTable from '../components/team_players_table';
 
 export default props => {
-    const { teams, onChange } = props;
+    const { teams } = props;
 
     const cardList = teams.map(team => (
-        <Item>
+        <Item key={team.index}>
             <Item.Image size='tiny' src='https://upload.wikimedia.org/wikipedia/it/0/07/Fc_barcelona.png' />
             <Item.Content>
                 <Item.Header as='a'>{team.name}</Item.Header>
--- webclient/src/views/teams.jsx
+++ webclient/src/views/teams.jsx
@@ -1,25 +1,17 @@
 import React, { PureComponent } from 'react';
 import { Segment, Divider } from 'semantic-ui-react'
-import TeamPlayerTable from '../components/team_players_table';
 import TeamCreator from '../components/team_creator';
 import TeamList from '../components/team_list';
 
 class Teams extends PureComponent {
-    constructor(props){
-        super(props);
-
-        this.state = {};
-    }
-
     render() {
         const { testingFacade, teams } = this.props;
-        const { team } = this.state;
 
         return (
             <Segment>
                 <TeamCreator contract={testingFacade}/>
                 <Divider />
-                <TeamList teams={teams} onChange={team => this.setState({team})} />
+                <TeamList teams={teams} />
             </Segment>
         )
     }
