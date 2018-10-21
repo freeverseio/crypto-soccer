4424f43d554e6ce329ccba8cd6d71c85cfb73b87
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-10-21 11:53:49 +0200

    removed meta description

--- webclient/src/components/team_list.jsx
+++ webclient/src/components/team_list.jsx
@@ -10,7 +10,6 @@ export default props => {
             <Item.Image size='tiny' src='https://upload.wikimedia.org/wikipedia/it/0/07/Fc_barcelona.png' />
             <Item.Content>
                 <Item.Header as='a'>{team.name}</Item.Header>
-                <Item.Meta>TODO Description</Item.Meta>
                 <Item.Description>
                     <TeamPlayerTable team={team} />
                 </Item.Description>
