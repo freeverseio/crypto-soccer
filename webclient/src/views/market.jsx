45d436b3dadf296a81b42b5cbef5d583671098ed
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-10-23 18:53:58 +0200

    static image in market

--- webclient/src/views/market.jsx
+++ webclient/src/views/market.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react'
 
 export default props => (
-    <iframe src="https://opensea.io/assets?query=cryptosoccr" width="100%" height="800px" />
+    <Image src="market/market.png" fluid />
 )
+
