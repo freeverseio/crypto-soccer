64e62e0015d2a225693444ebe4d1d80d0a499acb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-13 12:58:00 +0100

    created struct Props

--- truffle-assets/contracts/CryptoPlayers.sol
+++ truffle-assets/contracts/CryptoPlayers.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 contract CryptoPlayers is ERC721Full {
+    struct Props {
+
+    }
+
+    // Mapping from token ID to owner
+    mapping (uint256 => Props) private _tokenProps;
+
     constructor(string name, string symbol) public 
     ERC721Full(name, symbol)
     {
     }
+
+
 }
