a0f2078b061e4ca43483fcdd090671877363f2d9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-13 08:51:37 +0100

    added ERC721 Contracts

---
+++ truffle-assets/contracts/CryptoTeams.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract CryptoTeams is ERC721, Ownable {
+}
+
---
+++ truffle-assets/test/CryptoPlayers.test.js
@@ -0,0 +1,11 @@
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
+const CryptoPlayers = artifacts.require('CryptoPlayers');
+
+contract('CryptoPlayers', (accounts) => {
+    it('test the test', () => {
+
+    });
+});
