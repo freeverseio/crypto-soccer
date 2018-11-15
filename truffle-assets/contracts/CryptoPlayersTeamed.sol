20d2c254122109244f1cc41a3f1fbf6baa6cd285
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-15 18:16:56 +0100

    test: add Team Mint

--- truffle-assets/contracts/CryptoPlayersTeamed.sol
+++ truffle-assets/contracts/CryptoPlayersTeamed.sol
@@ -3,4 +3,16 @@ pragma solidity ^0.4.24;
 import "./CryptoPlayers.sol";
 
 contract CryptoPlayersTeamed is CryptoPlayers {
+    // Mapping from player ID to its team
+    mapping (uint256 => uint256) private _playerTeam;
+
+    function getTeam(uint256 playerId) public view returns (uint256) {
+        require(_exists(playerId), "unexistent player");
+        return _playerTeam[playerId];
+    }
+
+    function _setTeam(uint256 playerId, uint256 teamId) internal {
+        require(_exists(playerId), "unexistent player");
+        _playerTeam[playerId] = teamId;
+    }
 }
--- truffle-assets/test/CryptoTeams.test.js
+++ truffle-assets/test/CryptoTeams.test.js
@@ -13,6 +13,12 @@ contract('CryptoTeams', (accounts) => {
     });
 
     it('mint a team', async () => {
-
+        const contract = await CryptoTeams.new(name, symbol).should.be.fulfilled;
+        let supply = await contract.totalSupply().should.be.fulfilled;
+        supply.toNumber().should.be.equal(0);
+        const tokenId = 1;
+        await contract.mint(accounts[0], tokenId).should.be.fulfilled;
+        supply = await contract.totalSupply().should.be.fulfilled;
+        supply.toNumber().should.be.equal(1);
     });
 });
