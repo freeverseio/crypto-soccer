eb4f1f66eb94d5a90bd1440f1786a5892fe1738f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-16 09:41:15 +0100

    team can be minted

--- truffle-assets/test/CryptoTeams.test.js
+++ truffle-assets/test/CryptoTeams.test.js
@@ -22,4 +22,13 @@ contract('CryptoTeams', (accounts) => {
         supply = await contract.totalSupply().should.be.fulfilled;
         supply.toNumber().should.be.equal(1);
     });
+
+    it('check name of minted team', async () => {
+        const contract = await CryptoTeams.new(name, symbol).should.be.fulfilled;
+        const id = 1;
+        const teamName = "panzerotto";
+        await contract.mint(accounts[0], id, teamName).should.be.fulfilled;
+        const name = await contract.getName(id).should.be.fulfilled;
+        name.should.be.equal(teamName);
+    });
 });
