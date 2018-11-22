d876d6751fadbf608444979a57e218388354a236
Toni <toni.mateos@gmail.com>
2018-11-22 23:57:51 +0100

    adding a regression test for pythonSimulator

--- pythonSimulator/test.py
+++ pythonSimulator/test.py
@@ -15,7 +15,7 @@ def test2EqualTeams():
         total1 += goals1
         total2 += goals2
     print "The aggregated Result should be close to equal: %s - %s, goals per game: %s - %s " % (total1, total2, total1*1.0/nGames, total2*1.0/nGames)
-
+    return total1,total2
 
 def test2DifferentRandomTeams():
     barca = createRandomTeam(roles433)
@@ -32,9 +32,27 @@ def test2DifferentRandomTeams():
         total1 += goals1
         total2 += goals2
     print "The aggregated Result should be close to equal: %s - %s, goals per game: %s - %s " % (total1, total2, total1*1.0/nGames, total2*1.0/nGames)
+    return total1,total2
 
-# np.random.seed(200)
-test2EqualTeams()
+
+np.random.seed(0)
+
+# Testing equal teams games:
+total1, total2 = test2EqualTeams()
+success = ( total1 == 1250 and total2 == 1232 )
+if not success:
+    print "TEST FAILED"
+else:
+    print "TEST PASSED"
+
 print "\n\n"
 
-test2DifferentRandomTeams()
+# Testing different teams games:
+total1, total2 = test2DifferentRandomTeams()
+success = (total1 == 758 and total2 == 940)
+if not success:
+    print "TEST FAILED"
+else:
+    print "TEST PASSED"
+
+
