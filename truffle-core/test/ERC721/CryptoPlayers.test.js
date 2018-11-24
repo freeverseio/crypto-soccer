89a7a19e00f4e89883ac51363883e1cd1e957879
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-24 07:26:43 +0100

    CryptoPlayer behavior test created

--- truffle-core/test/ERC721/CryptoPlayers.test.js
+++ truffle-core/test/ERC721/CryptoPlayers.test.js
@@ -1,20 +1,10 @@
-const { shouldBehaveLikeERC721 } = require('openzeppelin-solidity/test/token/ERC721/ERC721.behavior');
-const BigNumber = web3.BigNumber;
-
 require('chai')
     .use(require('chai-as-promised'))
-    .use(require('chai-bignumber')(BigNumber))
     .should();
 
 const CryptoPlayers = artifacts.require('CryptoPlayersMock');
 
-contract('CryptoPlayers', ([_, creator, ...accounts]) => {
-    beforeEach(async function () {
-        this.token = await CryptoPlayers.new({ from: creator });
-    });
-
-    shouldBehaveLikeERC721(creator, creator, accounts);
-
+contract('CryptoPlayers', (accounts) => {
     it('deployment', async () => {
         await CryptoPlayers.new().should.be.fulfilled;
     });
