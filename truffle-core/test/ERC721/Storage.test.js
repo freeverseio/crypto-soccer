206e64b579832fa72f8854935b8d38726135ddf9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-27 15:57:18 +0100

    added storage test

---
+++ truffle-core/test/ERC721/Storage.test.js
@@ -0,0 +1,23 @@
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
+const Storage = artifacts.require('Storage');
+
+contract('CryptoTeams', (accounts) => {
+    let contract = null;
+
+    beforeEach(async () => {
+        contract = await Storage.new().should.be.fulfilled;
+    });
+
+    it('no initial players', async () =>{
+        const count = await contract.getNCreatedPlayers().should.be.fulfilled;
+        count.toNumber().should.be.equal(0);
+    });
+
+    it('no initial teams', async () =>{
+        const count = await contract.getNCreatedTeams().should.be.fulfilled;
+        count.toNumber().should.be.equal(0);
+    })
+});
