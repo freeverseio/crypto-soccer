fe520bd0649e883418bcb59e0c7c4c927b295def
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-28 16:58:26 +0100

    refactoring

--- truffle-core/contracts/ERC721/CryptoTeams.sol
+++ truffle-core/contracts/ERC721/CryptoTeams.sol
@@ -7,41 +7,41 @@ contract CryptoTeams is ERC721 {
     /// @dev So userChoice allows the user to inspect lots of teams compatible with his chosen name
     /// @dev and select his favourite one.
     /// @dev playerIdx serializes each player idx, allowing 20 bit for each (>1M players possible)
-    struct Team {
+    struct Props {
         string name;
         uint256 playersIdx;
     }
 
-    mapping(uint256 => Team) private teams;
-    mapping(bytes32 => uint256) private teamToOwnerAddr;
-    uint256 private teamsCount = 1;
+    mapping(uint256 => Props) private _teamProps;
+    mapping(bytes32 => uint256) private _nameHashTeam;
+    uint256 private _nextTeamId = 1;
 
     function addTeam(string memory name, address owner) public {
         bytes32 nameHash = keccak256(abi.encodePacked(name));
-        require(teamToOwnerAddr[nameHash] == 0);
-        teams[teamsCount] = Team({name: name, playersIdx: 0});
-        teamToOwnerAddr[nameHash] = teamsCount;
-        _mint(owner, teamsCount);
-        teamsCount++;
+        require(_nameHashTeam[nameHash] == 0);
+        _teamProps[_nextTeamId] = Props({name: name, playersIdx: 0});
+        _nameHashTeam[nameHash] = _nextTeamId;
+        _mint(owner, _nextTeamId);
+        _nextTeamId++;
     }
 
     function getTeamName(uint idx) public view returns(string) { 
         require(_exists(idx));
-        return teams[idx].name;
+        return _teamProps[idx].name;
     }
 
     function getNCreatedTeams() public view returns(uint) {
-        return teamsCount - 1;
+        return _nextTeamId - 1;
     }
 
     function setTeamPlayersIdx(uint256 team, uint256 playersIdx) public {
         require(_exists(team));
-        teams[team].playersIdx = playersIdx;
+        _teamProps[team].playersIdx = playersIdx;
     }
 
     function getTeamPlayersIdx(uint256 team) public returns (uint256) {
         require(_exists(team));
-        return teams[team].playersIdx;
+        return _teamProps[team].playersIdx;
     }
 }
 
