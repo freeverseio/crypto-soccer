3ff36ee291a5b623769838f50fa6ecc0da70a087
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-28 16:48:59 +0100

    refactoring

--- truffle-core/contracts/ERC721/CryptoTeams.sol
+++ truffle-core/contracts/ERC721/CryptoTeams.sol
@@ -3,44 +3,23 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
 contract CryptoTeams is ERC721 {
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
-    }
-
-    function burn(uint256 tokenId) public {
-        _burn(ownerOf(tokenId), tokenId);
-    }
-
-    function _mint(address to, uint256 tokenId) internal {
-        require(tokenId != 0);
-        super._mint(to, tokenId);
-    }
-
-       /// @dev The player skills in each team are obtained from hashing: name + userChoice
+    /// @dev The player skills in each team are obtained from hashing: name + userChoice
     /// @dev So userChoice allows the user to inspect lots of teams compatible with his chosen name
     /// @dev and select his favourite one.
     /// @dev playerIdx serializes each player idx, allowing 20 bit for each (>1M players possible)
     struct Team {
         string name;
         uint256 playersIdx;
-        address owner;
     }
 
-    /// @dev An array containing the Team struct for all teams in existence. 
-    /// @dev The ID of each team is actually his index in this array.
     mapping(uint256 => Team) private teams;
-   
-    // Team[] private teams;a
-    uint256 private teamsCount = 1;
-
-    /// @dev A mapping from team hash(name) to the owner's address.
-    /// @dev Facilitates checking if a teamName already exists.
     mapping(bytes32 => uint256) private teamToOwnerAddr;
+    uint256 private teamsCount = 1;
 
     function addTeam(string memory name, address owner) public {
         bytes32 nameHash = keccak256(abi.encodePacked(name));
         require(teamToOwnerAddr[nameHash] == 0);
-        teams[teamsCount] = Team({name: name, playersIdx: 0, owner: owner});
+        teams[teamsCount] = Team({name: name, playersIdx: 0});
         teamToOwnerAddr[nameHash] = teamsCount;
         _mint(owner, teamsCount);
         teamsCount++;
