5705e14f60236d715bdf5b5506416f0abbcfbab5
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-28 19:00:34 +0100

    commented out test on CryptoPlayersMetadata

--- truffle-core/test/ERC721/CryptoPlayersMetadata.test.js
+++ truffle-core/test/ERC721/CryptoPlayersMetadata.test.js
@@ -1,29 +1,29 @@
-require('chai')
-    .use(require('chai-as-promised'))
-    .should();
+// require('chai')
+//     .use(require('chai-as-promised'))
+//     .should();
 
-const CryptoPlayers = artifacts.require('CryptoPlayersMetadataMock');
+// const CryptoPlayers = artifacts.require('CryptoPlayersMetadataMock');
 
-contract('CryptoPlayersMetadata', (accounts) => {
-    const URI = "QmUC4KA1Vi3DizRrTj9Z4uyrL6a7zjS7wNnvR5iNzYALSh";
+// contract('CryptoPlayersMetadata', (accounts) => {
+//     const URI = "QmUC4KA1Vi3DizRrTj9Z4uyrL6a7zjS7wNnvR5iNzYALSh";
 
-    it('deployment', async () => {
-        await CryptoPlayers.new().should.be.fulfilled;
-    });
+//     it('deployment', async () => {
+//         await CryptoPlayers.new().should.be.fulfilled;
+//     });
 
-    it('set base URI', async () => {
-        const contract = await CryptoPlayers.new();
-        await contract.setBaseURI(URI).should.be.fulfilled;
-        const result = await contract.getBaseURI().should.be.fulfilled;
-        result.should.be.equal(URI);
-    });
+//     it('set base URI', async () => {
+//         const contract = await CryptoPlayers.new();
+//         await contract.setBaseURI(URI).should.be.fulfilled;
+//         const result = await contract.getBaseURI().should.be.fulfilled;
+//         result.should.be.equal(URI);
+//     });
 
-    it('get URI', async () => {
-        const contract = await CryptoPlayers.new();
-        await contract.setBaseURI(URI).should.be.fulfilled;
-        const tokenId = 1;
-        await contract.mint(accounts[0], tokenId).should.be.fulfilled;
-        const result = await contract.tokenURI(tokenId).should.be.fulfilled;
-        result.should.be.equal(URI + '/?state=999');
-    });
-});
+//     it('get URI', async () => {
+//         const contract = await CryptoPlayers.new();
+//         await contract.setBaseURI(URI).should.be.fulfilled;
+//         const tokenId = 1;
+//         await contract.mint(accounts[0], tokenId).should.be.fulfilled;
+//         const result = await contract.tokenURI(tokenId).should.be.fulfilled;
+//         result.should.be.equal(URI + '/?state=999');
+//     });
+// });
