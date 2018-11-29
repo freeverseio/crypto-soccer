c6c725f78695a30483a5117911b4d2f804017763
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-29 09:04:24 +0100

    removed onlyMinter modifier

--- truffle-core/contracts/ERC721/CryptoTeamsBase.sol
+++ truffle-core/contracts/ERC721/CryptoTeamsBase.sol
@@ -17,7 +17,7 @@ contract CryptoTeamsBase is ERC721, ERC721Enumerable, MinterRole {
     mapping(uint256 => Props) internal _teamProps;
     mapping(bytes32 => uint256) internal _nameHashTeam;
 
-    function _mint(address to, uint256 tokenId, string memory name) internal onlyMinter {
+    function _mint(address to, uint256 tokenId, string memory name) internal {
         require(to != address(0));
         bytes32 nameHash = keccak256(abi.encodePacked(name));
         require(_nameHashTeam[nameHash] == 0);
