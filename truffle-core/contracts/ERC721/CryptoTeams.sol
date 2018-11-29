f8fd9fa53768002c2010a22512d90e31cabcc4ba
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-11-29 08:35:06 +0100

    CryptoTeam has Metadata

--- truffle-core/contracts/ERC721/CryptoTeams.sol
+++ truffle-core/contracts/ERC721/CryptoTeams.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 import "./CryptoTeamsBase.sol";
 import "./CryptoTeamsMetadata.sol";
 
-contract CryptoTeams is CryptoTeamsBase {
+contract CryptoTeams is CryptoTeamsBase, CryptoTeamsMetadata {
     function addTeam(string memory name, address owner) public {
         uint256 nextTeamId = totalSupply() + 1;
         _mint(owner, nextTeamId, name);
--- truffle-core/contracts/ERC721/CryptoTeamsMetadata.sol
+++ truffle-core/contracts/ERC721/CryptoTeamsMetadata.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
 
-contract CryptoTeamsMetadata is ERC721Metadata {
-    
+contract CryptoTeamsMetadata is ERC721Metadata("CryptoSoccerTeams", "CST") {
 }
 
