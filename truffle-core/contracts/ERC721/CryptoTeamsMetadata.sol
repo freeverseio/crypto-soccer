a1eb2c1b5b559d48538de964cdbc0cb7cecd4b36
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-12-05 23:33:26 +0100

    CryptoTeamsMetadata inherits from ERC721Metadata

--- truffle-core/contracts/ERC721/CryptoTeamsMetadata.sol
+++ truffle-core/contracts/ERC721/CryptoTeamsMetadata.sol
@@ -2,49 +2,10 @@ pragma solidity ^0.4.24;
 
 import "./CryptoTeamsBase.sol";
 import "../helpers.sol";
-import "openzeppelin-solidity/contracts/introspection/ERC165.sol";
-import "openzeppelin-solidity/contracts/token/ERC721/IERC721Metadata.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
 
-contract CryptoTeamsMetadata is ERC165, CryptoTeamsBase, IERC721Metadata, HelperFunctions  {
-    // Token name
-    string constant private _name = "CryptoSoccerTeams";
-
-    // Token symbol
-    string constant private _symbol = "CST";
-
+contract CryptoTeamsMetadata is ERC721Metadata("CryptoSoccerTeams", "CST"), CryptoTeamsBase, HelperFunctions  {
     string private _teamsURI;
-
-    bytes4 private constant InterfaceId_ERC721Metadata = 0x5b5e139f;
-    /**
-     * 0x5b5e139f ===
-     *     bytes4(keccak256('name()')) ^
-     *     bytes4(keccak256('symbol()')) ^
-     *     bytes4(keccak256('tokenURI(uint256)'))
-     */
-
-    /**
-     * @dev Constructor function
-     */
-    constructor () public {
-        // register the supported interfaces to conform to ERC721 via ERC165
-        _registerInterface(InterfaceId_ERC721Metadata);
-    }
-
-    /**
-     * @dev Gets the token name
-     * @return string representing the token name
-     */
-    function name() external view returns (string) {
-        return _name;
-    }
-
-    /**
-     * @dev Gets the token symbol
-     * @return string representing the token symbol
-     */
-    function symbol() external view returns (string) {
-        return _symbol;
-    }
 
     /**
      * @dev Returns an URI for a given token ID
