6129fcc2397d8cea684bea9b7d3506709c7d4b97
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-12-06 17:11:23 +0100

    onlyMinter cna addTeam and setPlayersIds

--- truffle-core/contracts/ERC721/CryptoTeams.sol
+++ truffle-core/contracts/ERC721/CryptoTeams.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.4.24;
 import "./CryptoTeamsMetadata.sol";
 
 contract CryptoTeams is CryptoTeamsMetadata {
-    function addTeam(string memory name, address owner) public {
+    function addTeam(string memory name, address owner) public onlyMinter {
         uint256 nextTeamId = totalSupply() + 1;
         mintWithName(owner, nextTeamId, name);
     }
 
-    function setPlayersIds(uint256 tokenId, uint256 playersIdx) public {
+    function setPlayersIds(uint256 tokenId, uint256 playersIdx) public onlyMinter {
         _setPlayersIds(tokenId, playersIdx);
     }
 }
