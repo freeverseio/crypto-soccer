1743432e1460f49326b35367a05e01078da0a3fd
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-12-06 17:40:47 +0100

    moving modifier usage into base class

--- truffle-core/contracts/ERC721/CryptoTeamsBase.sol
+++ truffle-core/contracts/ERC721/CryptoTeamsBase.sol
@@ -22,7 +22,7 @@ contract CryptoTeamsBase is ERC721, ERC721Enumerable, MinterRole {
         return _teamProps[tokenId].name;
     }
 
-    function _setPlayersIds(uint256 tokenId, uint256 playersIdx) internal {
+    function _setPlayersIds(uint256 tokenId, uint256 playersIdx) internal onlyMinter {
         require(_exists(tokenId));
         _teamProps[tokenId].playersIdx = playersIdx;
     }
