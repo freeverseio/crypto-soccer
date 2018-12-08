9e626016c8e11fd6ec27bfef86d42fc72181715f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-12-08 17:59:52 +0100

    team set to 0 when ownership changes

--- truffle-core/contracts/ERC721/CryptoPlayersLink.sol
+++ truffle-core/contracts/ERC721/CryptoPlayersLink.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.4.24;
 import "./CryptoPlayersBase.sol";
 
 contract CryptoPlayersLink is CryptoPlayersBase {
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        super.transferFrom(from, to, tokenId);
+        _setTeam(tokenId, 0);
+    }
 }
