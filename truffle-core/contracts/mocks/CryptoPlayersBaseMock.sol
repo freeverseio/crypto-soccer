b67c727857a49ec5f0a67a8877edb46ea1af7cad
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-12-30 09:46:56 +0100

    test PlayersBase setters tested

--- truffle-core/contracts/mocks/CryptoPlayersBaseMock.sol
+++ truffle-core/contracts/mocks/CryptoPlayersBaseMock.sol
@@ -11,6 +11,14 @@ contract CryptoPlayersBaseMock is CryptoPlayersBase {
         _mint(to, tokenId);
     }
 
+    function setState(uint256 playerId, uint256 state) public {
+        _setState(playerId, state);
+    }
+
+    function setName(uint256 playerId, string name) public {
+        _setName(playerId, name);
+    }
+
     function setTeam(uint256 playerId, uint256 teamId) public {
         _setTeam(playerId, teamId);
     }
--- truffle-core/test/ERC721/CryptoPlayersBase.test.js
+++ truffle-core/test/ERC721/CryptoPlayersBase.test.js
@@ -11,10 +11,48 @@ contract('CryptoPlayersBase', (accounts) => {
         contract = await CryptoPlayers.new().should.be.fulfilled;
     });
 
-    it('get name', async () => {
+    it('default name', async () => {
         const id = 1;
         await contract.mint(accounts[0], id).should.be.fulfilled;
         const name = await contract.getName(id).should.be.fulfilled;
         name.should.be.equal("");
+    });
+
+    it('default team', async () => {
+        const id = 1;
+        await contract.mint(accounts[0], id).should.be.fulfilled;
+        const team = await contract.getTeam(id).should.be.fulfilled;
+        team.toNumber().should.be.equal(0);
+    });
+    
+    it('default state', async () => {
+        const id = 1;
+        await contract.mint(accounts[0], id).should.be.fulfilled;
+        const state = await contract.getState(id).should.be.fulfilled;
+        state.toNumber().should.be.equal(0);
+    });
+
+    it('set name', async () => {
+        const id = 1;
+        await contract.mint(accounts[0], id).should.be.fulfilled;
+        await contract.setName(id, "player").should.be.fulfilled;
+        const name = await contract.getName(id).should.be.fulfilled;
+        name.should.be.equal("player");
+    });
+    
+    it('set team', async () => {
+        const id = 1;
+        await contract.mint(accounts[0], id).should.be.fulfilled;
+        await contract.setTeam(id, 1);
+        const team = await contract.getTeam(id).should.be.fulfilled;
+        team.toNumber().should.be.equal(1);
+    });
+
+    it('set state', async () => {
+        const id = 1;
+        await contract.mint(accounts[0], id).should.be.fulfilled;
+        await contract.setState(id, 1).should.be.fulfilled;
+        const state = await contract.getState(id).should.be.fulfilled;
+        state.toNumber().should.be.equal(1);
     });
 });
