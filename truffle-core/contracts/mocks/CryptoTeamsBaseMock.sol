dc6cbb31686615bdd7f109d77dffa56e53151e2e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-12-30 08:59:48 +0100

    refactoring

--- truffle-core/contracts/mocks/CryptoTeamsBaseMock.sol
+++ truffle-core/contracts/mocks/CryptoTeamsBaseMock.sol
@@ -7,6 +7,10 @@ import "../ERC721/CryptoTeamsBase.sol";
  * This mock just provides a public mint and burn functions for testing purposes
  */
 contract CryptoTeamsBaseMock is CryptoTeamsBase {
+    function setName(uint256 teamId, string name) public {
+        _setName(teamId, name);
+    }
+
     function addPlayer(uint256 teamId, uint256 playerId) public {
         _addPlayer(teamId, playerId);
     }
