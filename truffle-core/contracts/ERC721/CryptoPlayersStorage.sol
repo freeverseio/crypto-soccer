ce5ff5114dd04b7fabadbc77c6ffcb4a4d422af8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-03 07:35:14 +0100

    refactoring: function orden

--- truffle-core/contracts/ERC721/CryptoPlayersStorage.sol
+++ truffle-core/contracts/ERC721/CryptoPlayersStorage.sol
@@ -22,34 +22,6 @@ contract CryptoPlayersStorage is ERC721, ERC721Enumerable {
         return _playerProps[playerId].name;
     }
 
-    /**
-     * @dev sets name of existing player
-     */
-    function _setName(uint256 playerId, string name) internal {
-        require(_exists(playerId));
-        _playerProps[playerId].name = name;
-    }
-
-    function _setGenome(
-        uint256 playerId,
-        uint16 birth,
-        uint16 defence,
-        uint16 speed,
-        uint16 pass,
-        uint16 shoot,
-        uint16 endurance
-    ) internal {
-        require(_exists(playerId));
-        uint88 genome;
-        genome |= birth;
-        genome |= uint88(defence) << 14;
-        genome |= uint88(speed) << 14 * 2;
-        genome |= uint88(pass) << 14 * 3;
-        genome |= uint88(shoot) << 14 * 4;
-        genome |= uint88(endurance) << 14 * 5;
-        _playerProps[playerId].genome = genome;
-    }
-
     function getGenome(uint256 playerId) public view returns (uint88){
         require(_exists(playerId));
         return _playerProps[playerId].genome;
@@ -78,4 +50,33 @@ contract CryptoPlayersStorage is ERC721, ERC721Enumerable {
     function getEndurance(uint256 playerId) external view returns (uint16) {
         return 0x3fff & uint16(_playerProps[playerId].genome >> 14 * 5);
     }
+
+    /**
+     * @dev sets name of existing player
+     */
+    function _setName(uint256 playerId, string name) internal {
+        require(_exists(playerId));
+        _playerProps[playerId].name = name;
+    }
+
+    function _setGenome(
+        uint256 playerId,
+        uint16 birth,
+        uint16 defence,
+        uint16 speed,
+        uint16 pass,
+        uint16 shoot,
+        uint16 endurance
+    ) internal {
+        require(_exists(playerId));
+        uint88 genome;
+        genome |= birth;
+        genome |= uint88(defence) << 14;
+        genome |= uint88(speed) << 14 * 2;
+        genome |= uint88(pass) << 14 * 3;
+        genome |= uint88(shoot) << 14 * 4;
+        genome |= uint88(endurance) << 14 * 5;
+        _playerProps[playerId].genome = genome;
+    }
 }
+
