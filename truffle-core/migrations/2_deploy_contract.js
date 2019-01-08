33d55bd379ab483397a4955aa00f12ed99451fa2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-08 08:10:57 +0100

    deploying Horizon

--- truffle-core/migrations/2_deploy_contract.js
+++ truffle-core/migrations/2_deploy_contract.js
@@ -1,15 +1,22 @@
 const CryptoPlayers = artifacts.require("CryptoPlayers");
 const CryptoTeams = artifacts.require("CryptoTeams");
+const Horizon = artifacts.require("Horizon");
 
 module.exports = function (deployer) {
   deployer.deploy(CryptoPlayers)
     .then(cryptoPlayers => {
       console.log(`CryptoPlayers deployed at address: ${cryptoPlayers.address}`);
       console.log(`CryptoPlayers transaction at hash: ${cryptoPlayers.transactionHash}`);
-      deployer.deploy(CryptoTeams)
+      return deployer.deploy(CryptoTeams)
         .then(cryptoTeams => {
           console.log(`CryptoTeams deployed at address: ${cryptoTeams.address}`);
           console.log(`CryptoTeams transaction at hash: ${cryptoTeams.transactionHash}`);
+          return deployer.deploy(Horizon, cryptoPlayers.address, cryptoTeams.address)
+            .then(horizon => {
+              console.log(`Horizon deployed at address: ${horizon.address}`);
+              console.log(`Horizon transaction at hash: ${horizon.transactionHash}`);
+            })
+            .catch(console.error);
         })
         .catch(console.error);
     })
