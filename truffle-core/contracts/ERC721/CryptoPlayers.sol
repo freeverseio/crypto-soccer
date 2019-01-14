fb8c857e21254314dc717ce1840ed4c53b9bfbe3
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-14 20:07:15 +0100

    CryptoSoccer -> Crypto

--- truffle-core/contracts/ERC721/CryptoPlayers.sol
+++ truffle-core/contracts/ERC721/CryptoPlayers.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.4.24;
 import "./CryptoPlayersTeam.sol";
 import "./ERC721MetadataBaseURI.sol";
 
-contract CryptoPlayers is CryptoPlayersTeam, ERC721MetadataBaseURI("CryptoSoccerPlayers", "CSP") {
+contract CryptoPlayers is CryptoPlayersTeam, ERC721MetadataBaseURI("CryptoPlayers", "CP") {
 }
--- truffle-core/contracts/ERC721/CryptoTeams.sol
+++ truffle-core/contracts/ERC721/CryptoTeams.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.24;
 import "./CryptoTeamsPlayers.sol";
 import "./ERC721MetadataBaseURI.sol";
 
-contract CryptoTeams is CryptoTeamsPlayers, ERC721MetadataBaseURI("CryptoSoccerTeams", "CST") {
+contract CryptoTeams is CryptoTeamsPlayers, ERC721MetadataBaseURI("CryptoTeams", "CT") {
     constructor (address cryptoPlayers) CryptoTeamsPlayers(cryptoPlayers) public {}
 }
 
