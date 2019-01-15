3cb23593dc7fa796e955422a5626ba2afd13dbd4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-15 19:12:21 +0100

    added OpenSea attributes

--- metadata-server/routes/playersJSON.js
+++ metadata-server/routes/playersJSON.js
@@ -1,9 +1,40 @@
+const config = require('../config.json');
 
 module.exports = async (instance, id) => {
     const name = await instance.methods.getName(id).call();
+    const image = config.players_image_base_URL + id;
+    const speed = await instance.methods.getSpeed(id).call();
+    const defence = await instance.methods.getDefence(id).call();
+    const endurance = await instance.methods.getEndurance(id).call();
+    const shoot = await instance.methods.getShoot(id).call();
+    const pass = await instance.methods.getPass(id).call();
 
     const schema = {
-        "name": name
+        "name": name,
+        "description": "put a description",
+        "image": image,
+        "attributes": [
+            {
+                "trait_type": "speed",
+                "value": speed
+            },
+            {
+                "trait_type": "defence",
+                "value": defence
+            },
+            {
+                "trait_type": "endurance",
+                "value": endurance
+            },
+            {
+                "trait_type": "shoot",
+                "value": shoot
+            },
+            {
+                "trait_type": "pass",
+                "value": pass
+            }
+        ]
     };
 
     return schema;
--- metadata-server/test/players.test.js
+++ metadata-server/test/players.test.js
@@ -5,6 +5,7 @@ const EthCrypto = require('eth-crypto');
 const contractJSON = require('../../truffle-core/build/contracts/CryptoPlayers.json')
 const ganache = require("ganache-cli");
 const playersJSON = require('../routes/playersJSON');
+const config = require('../config.json');
 
 // Configure chai
 chai.use(require('chai-http'));
@@ -12,23 +13,24 @@ chai.use(require('chai-as-promised'));
 chai.should();
 
 describe('player', () => {
-    const identity = EthCrypto.createIdentity();
-    const provider = ganache.provider({
-        accounts: [{
-            secretKey: identity.privateKey, 
-            balance: Web3.utils.toWei('100', 'ether') 
-        }]
-    })
-    const web3 = new Web3(provider);
-    const contract = new web3.eth.Contract(contractJSON.abi);
-    const sendOptions = {
-        from: identity.address,
-        gas: 4712388,
-        gasPrice: provider.gasPrice
-    };
     let instance = null;
 
-    beforeEach(async () => {
+    before(async () => {
+        const identity = EthCrypto.createIdentity();
+        const provider = ganache.provider({
+            accounts: [{
+                secretKey: identity.privateKey,
+                balance: Web3.utils.toWei('100', 'ether')
+            }]
+        })
+        const web3 = new Web3(provider);
+        const contract = new web3.eth.Contract(contractJSON.abi);
+        const sendOptions = {
+            from: identity.address,
+            gas: 4712388,
+            gasPrice: provider.gasPrice
+        };
+
         instance = await contract.deploy({
             data: contractJSON.bytecode
         })
@@ -37,13 +39,36 @@ describe('player', () => {
             // .on('transactionHash', transactionHash => console.log("(II) transactionHash: " + transactionHash))
             // .on('receipt', receipt => console.log("(II) address: ", receipt.contractAddress)) // contains the new contract address
             // .on('confirmation', (confirmationNumber, receipt) => console.log("(II) confirmation: " + confirmationNumber))
-            .catch(console.error)
+            .catch(console.error);
+        await instance.methods.mint(identity.address, "player").send(sendOptions).should.be.fulfilled;
     });
 
     it('check ERC721 metadata', async () => {
-        await instance.methods.mint(identity.address, "player").send(sendOptions).should.be.fulfilled;
         const id = await instance.methods.getPlayerId("player").call().should.be.fulfilled;
         const schema = await playersJSON(instance, id).should.be.fulfilled;
         schema.name.should.be.equal("player");
+        schema.description.should.be.equal("put a description");
+        schema.image.should.be.equal(config.players_image_base_URL + id);
+    });
+
+    it('check OpenSea metadata', async () => {
+        const id = await instance.methods.getPlayerId("player").call().should.be.fulfilled;
+        const schema = await playersJSON(instance, id).should.be.fulfilled;
+        schema.attributes.length.should.be.equal(5);
+        const speed = await instance.methods.getSpeed(id).call().should.be.fulfilled;
+        schema.attributes[0].trait_type.should.be.equal('speed');
+        schema.attributes[0].value.should.be.equal(speed);
+        const defence = await instance.methods.getDefence(id).call().should.be.fulfilled;
+        schema.attributes[1].trait_type.should.be.equal('defence');
+        schema.attributes[1].value.should.be.equal(defence);
+        const endurance = await instance.methods.getEndurance(id).call().should.be.fulfilled;
+        schema.attributes[2].trait_type.should.be.equal('endurance');
+        schema.attributes[2].value.should.be.equal(endurance);
+        const shoot = await instance.methods.getShoot(id).call().should.be.fulfilled;
+        schema.attributes[3].trait_type.should.be.equal('shoot');
+        schema.attributes[3].value.should.be.equal(shoot);
+        const pass = await instance.methods.getPass(id).call().should.be.fulfilled;
+        schema.attributes[4].trait_type.should.be.equal('pass');
+        schema.attributes[4].value.should.be.equal(pass);
     });
 });
