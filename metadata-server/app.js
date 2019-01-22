5ec271490998f85d7ed3b3404732860335a2b2d7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-22 09:37:32 +0100

    preparing for production

--- metadata-server/app.js
+++ metadata-server/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var playersRouter = require('./routes/players');
 var teamsRouter = require('./routes/teams');
@@ -17,6 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/players', playersRouter);
