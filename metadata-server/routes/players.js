650ecad0ee08b4a09f8e3faefe8b4632cf12c0ed
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-26 10:26:34 +0100

    working with infura

--- metadata-server/routes/players.js
+++ metadata-server/routes/players.js
@@ -12,7 +12,7 @@ const instance = new web3.eth.Contract(jsonInterface, config.crypto_player_addre
 /* GET JSON schema for players with id. */
 router.get('/:id', async (req, res, next) => {
   const id = req.params.id;
-  const schema = playersJSON(instance, id);
+  const schema = await playersJSON(instance, id);
   res.send(schema);
 });
 
