8b8c5e3be68fccca830eb76a03afa934f1bf7897
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-01-27 22:04:06 +0100

    running the schema generator for teams

--- metadata-server/routes/teams.js
+++ metadata-server/routes/teams.js
@@ -1,11 +1,20 @@
 var express = require('express');
-const schema = require('./example_schema.json');
+const Web3 = require('web3');
+const jsonInterface = require('../../truffle-core/build/contracts/CryptoPlayers.json').abi;
+const teamsJSONInterface = require('../../truffle-core/build/contracts/CryptoTeams.json').abi;
+const teamsJSON = require('./teamsJSON');
+const config = require('../config.json');
 
 var router = express.Router();
 
+const web3 = new Web3(config.provider);
+const playersContract = new web3.eth.Contract(jsonInterface, config.crypto_player_address);
+const teamsContract = new web3.eth.Contract(teamsJSONInterface, config.crypto_teams_contract);
+
 /* GET JSON schema for teams with id. */
-router.get('/:id', function (req, res, next) {
-    const id = req.params.id;
+router.get('/:id', async (req, res, next) => {
+    const teamId = req.params.id;
+    const schema = await teamsJSON({ playersContract, teamsContract, teamId });
     res.send(schema);
 });
 
--- metadata-server/routes/teamsJSON.js
+++ metadata-server/routes/teamsJSON.js
@@ -13,18 +13,13 @@ module.exports = async ({playersContract, teamsContract, teamId}) => {
 
     const schema = {
         "name": name,
-        "description": "It's a team... a good one for sure.",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
         "image": "http://www.monkers.net/fotos/lucasartsDead.jpg",
         "external_url": "https://www.freeverse.io/",
         "attributes": [
             {
                 "trait_type": "team",
-                "value": teamName
-            },
-            {
-                "display_type": "number",
-                "trait_type": "generation",
-                "value": 0
+                "value": name
             }
         ]
     };
