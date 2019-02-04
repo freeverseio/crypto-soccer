cdf6766db601c3cdcd67851e82febc85551c1175
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-04 07:44:30 +0100

    Engine needs a Leagues address in construction

--- truffle-core/contracts/core/Engine.sol
+++ truffle-core/contracts/core/Engine.sol
@@ -1,4 +1,22 @@
 pragma solidity ^ 0.4.24;
 
+import "./Leagues.sol";
+
 contract Engine {
+    Leagues private _leagues;
+
+    constructor(address leagues) public {
+        _leagues = Leagues(leagues);
+    }
+
+    function getLeaguesContract() external view returns (address) {
+        return address(_leagues);
+    }
+
+    function computeLeagueFinalState (
+        uint256 initBlock,
+        uint256 step,
+        uint256 initPlayerState
+        ) public view {
+    }
 }
--- truffle-core/test/core/Engine.test.js
+++ truffle-core/test/core/Engine.test.js
@@ -2,12 +2,20 @@ require('chai')
     .use(require('chai-as-promised'))
     .should();
 
+const Leagues = artifacts.require('Leagues');
 const Engine = artifacts.require('Engine');
 
 contract('Engine', (accounts) => {
+    let leagues = null;
     let engine = null;
 
     beforeEach(async () => {
-        engine = await Engine.new().should.be.fulfilled;
+        leagues = await Leagues.new().should.be.fulfilled;
+        engine = await Engine.new(leagues.address).should.be.fulfilled;
+    });
+
+    it('Leagues contract', async () => {
+        const address = await engine.getLeaguesContract().should.be.fulfilled;
+        address.should.be.equal(leagues.address);
     });
 });
