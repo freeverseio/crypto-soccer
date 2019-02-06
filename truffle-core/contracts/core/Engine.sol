bcfaff383b57c7196fea8a9f40d349a0dcacdbf2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-06 18:58:49 +0100

    refactoring

--- truffle-core/contracts/core/Engine.sol
+++ truffle-core/contracts/core/Engine.sol
@@ -6,28 +6,28 @@ contract Engine {
     /**
      * @dev playMatch returns the result of a match
      * @param seed the pseudo-random number to use as a seed for the match
-     * @param stateTeam0 a vector with the state of the players of team 0
-     * @param stateTeam1 a vector with the state of the players of team 1
-     * @param tacticsTeam0 a vector[3] with the tactic (ex. [4,4,3]) of team 0 
-     * @param tacticsTeam0 a vector[3] with the tactic (ex. [4,4,3]) of team 1
+     * @param state0 a vector with the state of the players of team 0
+     * @param state1 a vector with the state of the players of team 1
+     * @param tactics0 a vector[3] with the tactic (ex. [4,4,3]) of team 0 
+     * @param tactics0 a vector[3] with the tactic (ex. [4,4,3]) of team 1
      * @return the score of the match
      */
     function playMatch(
         bytes32 seed,
-        uint256[] memory stateTeam0,
-        uint256[] memory stateTeam1, 
-        uint256[3] memory tacticsTeam0, 
-        uint256[3] memory tacticsTeam1
+        uint256[] memory state0,
+        uint256[] memory state1, 
+        uint256[3] memory tactics0, 
+        uint256[3] memory tactics1
     ) 
         public 
         pure 
         returns (uint256, uint256) 
     {
-        require(stateTeam0.length >= 11, "Team 0 needs at least 11 players");
-        require(stateTeam1.length >= 11, "Team 1 needs at least 11 players");
+        require(state0.length >= 11, "Team 0 needs at least 11 players");
+        require(state1.length >= 11, "Team 1 needs at least 11 players");
 
-        uint256 hash0 = uint256(seed) + stateTeam0[0] + tacticsTeam0[0];
-        uint256 hash1 = uint256(seed) + stateTeam1[0] + tacticsTeam1[0];
+        uint256 hash0 = uint256(seed) + state0[0] + tactics0[0];
+        uint256 hash1 = uint256(seed) + state1[0] + tactics1[0];
         return (hash0 % 4, hash1 % 4);
     }
 }
