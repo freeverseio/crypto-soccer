6afaf2afa826c17049e96bee01515d64d00eeef4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-08 08:16:32 +0100

    test refactoring

--- truffle-core/test/core/LeaguesComputer.test.js
+++ truffle-core/test/core/LeaguesComputer.test.js
@@ -14,6 +14,10 @@ contract('LeaguesComputer', (accounts) => {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // Team 0
         10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0  // Team 1
     ];
+    const tactics = [
+        [4,4,3],  // Team 0
+        [5,4,2]   // Team 1
+    ];
     const blocksToInit = 3;
     const step = 1
     const teamIds = [1, 2];
@@ -21,6 +25,7 @@ contract('LeaguesComputer', (accounts) => {
     beforeEach(async () => {
         engine = await Engine.new().should.be.fulfilled;
         leagues = await Leagues.new(engine.address).should.be.fulfilled;
+        await leagues.create(id, blocksToInit, step, teamIds).should.be.fulfilled;
     });
 
     it('Engine contract', async () => {
@@ -30,19 +35,15 @@ contract('LeaguesComputer', (accounts) => {
 
     it('compute unexistent league', async () => {
         const id = 532;
-        await leagues.computeLeagueFinalState(id).should.be.rejected;
+        await leagues.computeLeagueFinalState(id, initPlayerState, tactics).should.be.rejected;
     });
 
     it('compute league', async () => {
-        const tactics = [[4,4,3], [5,4,2]];
-        await leagues.create(id, blocksToInit, step, teamIds).should.be.fulfilled;
         const scores = await leagues.computeLeagueFinalState(id, initPlayerState, tactics).should.be.fulfilled;
         scores.length.should.be.equal(teamIds.length * (teamIds.length - 1));
     });
 
     it('compute league 2 times gives the same result', async () => {
-        const tactics = [[4,4,3], [5,4,2]];
-        await leagues.create(id, blocksToInit, step, teamIds).should.be.fulfilled;
         const scores0 = await leagues.computeLeagueFinalState(id, initPlayerState, tactics).should.be.fulfilled;       
         const scores1 = await leagues.computeLeagueFinalState(id, initPlayerState, tactics).should.be.fulfilled;   
         const finalHash0 = await leagues.calculateFinalHash(scores0).should.be.fulfilled;
@@ -51,8 +52,6 @@ contract('LeaguesComputer', (accounts) => {
     });
 
     it('compute league and update', async () => {
-        const tactics = [[4,4,3], [5,4,2]];
-        await leagues.create(id, blocksToInit, step, teamIds).should.be.fulfilled;
         const before = await leagues.getHash(id).should.be.fulfilled;
         await leagues.computeLeagueAndUpdate(id, initPlayerState, tactics).should.be.fulfilled;
         const after = await leagues.getHash(id).should.be.fulfilled;
@@ -60,8 +59,6 @@ contract('LeaguesComputer', (accounts) => {
     });
 
     it('external league final hash is equal to bc calculated one', async () => {
-        const tactics = [[4,4,3], [5,4,2]];
-        await leagues.create(id, blocksToInit, step, teamIds).should.be.fulfilled;
         const scores = await leagues.computeLeagueFinalState(id, initPlayerState, tactics).should.be.fulfilled;       
         const finalHash = await leagues.calculateFinalHash(scores).should.be.fulfilled;
         await leagues.computeLeagueAndUpdate(id, initPlayerState, tactics).should.be.fulfilled;
