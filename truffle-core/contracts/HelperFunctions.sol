0ccf5ca248b53efdd31dcf04f28f97b148d3a42c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-15 20:12:41 +0100

    fix uint2str function

--- truffle-core/contracts/HelperFunctions.sol
+++ truffle-core/contracts/HelperFunctions.sol
@@ -170,20 +170,21 @@ contract HelperFunctions {
         return decode(roundsPerGame, computeKeccak256ForNumber(seed), bitsPerRndNum);
     }
 
-    function uint2str(uint i) internal pure returns (string memory){
-        if (i == 0) return "0";
-        uint j = i;
-        uint length;
-        while (j != 0){
-            length++;
+    function uint2str(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
             j /= 10;
         }
-        bytes memory bstr = new bytes(length);
-        uint k = length - 1;
-        uint tmp = i;
-        while (tmp != 0){
-            bstr[k--] = byte(48 + tmp % 10);
-            tmp /= 10;
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
         }
         return string(bstr);
     }
