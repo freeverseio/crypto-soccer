a7cf4e98858d15c5a37883af1b55698e5c380509
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-17 16:08:20 +0100

    LeaguesStorageMock

---
+++ truffle-core/contracts/mocks/LeaguesStorageMock.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+import "../core/LeaguesStorage.sol";
+
+contract LeaguesStorageMock is LeaguesStorage {
+    function setScores(uint256 id, uint256[2][] memory scores) public {
+        _setScores(id, scores);
+    }
+}
