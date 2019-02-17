f71a05fd3c54ccc672cc15350dbe984958c9fd63
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-17 21:23:20 +0100

    removed redundand test

--- truffle-core/test/core/LeaguesComputer.test.js
+++ truffle-core/test/core/LeaguesComputer.test.js
@@ -145,19 +145,4 @@ contract('LeaguesComputer', (accounts) => {
         const result = await leagues.countTeamsStatus([3, 4, 0, 454, 0, 6, 0]).should.be.fulfilled;
         result.toNumber().should.be.equal(3);
     });
-
-    it('append team to league state', async () => {
-        await leagues.appendTeamToLeagueState([], []).should.be.rejected;
-        let state = await leagues.appendTeamToLeagueState([], [2]).should.be.fulfilled;
-        state.length.should.be.equal(2);
-        state[0].toNumber().should.be.equal(2);
-        state[1].toNumber().should.be.equal(0);
-        state = await leagues.appendTeamToLeagueState(state, [3, 4]).should.be.fulfilled;
-        state.length.should.be.equal(5);
-        state[0].toNumber().should.be.equal(2);
-        state[1].toNumber().should.be.equal(0);
-        state[2].toNumber().should.be.equal(3);
-        state[3].toNumber().should.be.equal(4);
-        state[4].toNumber().should.be.equal(0);
-    });
 });
