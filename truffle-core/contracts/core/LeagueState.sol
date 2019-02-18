490e9029b454d1d623ea73f210b04300f7d82547
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-18 11:52:42 +0100

    name refactoring

--- truffle-core/contracts/core/LeagueState.sol
+++ truffle-core/contracts/core/LeagueState.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.4.25;
 contract LeagueState {
     uint256 constant public DIVIDER = 0;
 
-    function appendTeamToLeagueState(uint256[] memory leagueState, uint256[] memory teamState) public pure returns (uint256[] memory) {
-        require(isValid(leagueState), "invalid league state");
-        require(isValid(teamState), "invalid team state");
+    function appendTeamToLeagueState(uint256[] memory leagueState, uint256[] memory state) public pure returns (uint256[] memory) {
+        require(isValid(leagueState), "invalid league result");
+        require(isValid(state), "invalid team result");
 
         if(leagueState.length == 0)
-            return teamState;
-        if(teamState.length == 0)
+            return state;
+        if(state.length == 0)
             return leagueState;
 
-        uint256[] memory state = new uint256[](leagueState.length + teamState.length + 1);
+        uint256[] memory result = new uint256[](leagueState.length + state.length + 1);
         uint256 i;
         for (i = 0; i < leagueState.length ; i++)
-            state[i] = leagueState[i];
-        state[leagueState.length] = DIVIDER;
-        for (i = 0 ; i < teamState.length ; i++)
-            state[leagueState.length + 1 + i] = teamState[i];
+            result[i] = leagueState[i];
+        result[leagueState.length] = DIVIDER;
+        for (i = 0 ; i < state.length ; i++)
+            result[leagueState.length + 1 + i] = state[i];
 
-        return state;        
+        return result;        
     }
 
     function countTeams(uint256[] memory leagueState) public pure returns (uint256) {
