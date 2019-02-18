c7119b8c9e3fc255a3166954442dea58203f1262
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-18 12:20:44 +0100

    name refactoring

--- truffle-core/test/core/LeagueState.test.js
+++ truffle-core/test/core/LeagueState.test.js
@@ -47,18 +47,18 @@ contract('LeagueState', (accounts) => {
     });
 
     it('count team states into league state', async () => {
-        let count = await instance.countTeams([]).should.be.fulfilled;
+        let count = await instance.countLeagueStateTeams([]).should.be.fulfilled;
         count.toNumber().should.be.equal(0);
-        count = await instance.countTeams([2]).should.be.fulfilled;
+        count = await instance.countLeagueStateTeams([2]).should.be.fulfilled;
         count.toNumber().should.be.equal(1);
-        count = await instance.countTeams([2, 3, 4, 5, 0, 5, 4, 0, 2]).should.be.fulfilled;
+        count = await instance.countLeagueStateTeams([2, 3, 4, 5, 0, 5, 4, 0, 2]).should.be.fulfilled;
         count.toNumber().should.be.equal(3);
     });
 
     it('count team states into invalid league state', async () => {
-        await instance.countTeams([0]).should.be.rejected;
-        await instance.countTeams([0,3]).should.be.rejected;
-        await instance.countTeams([3,0]).should.be.rejected;
+        await instance.countLeagueStateTeams([0]).should.be.rejected;
+        await instance.countLeagueStateTeams([0,3]).should.be.rejected;
+        await instance.countLeagueStateTeams([3,0]).should.be.rejected;
     });
 
     it('count players in team', async () => {
