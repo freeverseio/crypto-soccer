ab7c685e8f679874d78e13ff5232fe1a4a173def
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-02-27 21:40:44 +0100

    create LeaguesTactics

--- truffle-core/contracts/core/LeaguesStorage.sol
+++ truffle-core/contracts/core/LeaguesStorage.sol
@@ -8,8 +8,6 @@ contract LeaguesStorage {
         uint256 initBlock;
         // step blocks of the league
         uint256 step;
-        // hash of tactics
-        bytes32 tacticsHash;
     }
 
     mapping(uint256 => League) private _leagues;
@@ -27,13 +25,7 @@ contract LeaguesStorage {
         require(teamIds.length > 1, "minimum 2 teams per league");
         require(teamIds.length % 2 == 0, "odd teams count");
         require(!_exists(id), "league already created");
-        bytes32 tacticsHash = 0;
-        _leagues[id] = League(
-            teamIds, 
-            initBlock, 
-            step, 
-            tacticsHash
-        );
+        _leagues[id] = League(teamIds, initBlock, step);
     }
 
     function getInitBlock(uint256 id) public view returns (uint256) {
---
+++ truffle-core/contracts/core/LeaguesTactics.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.0;
+
+contract LeaguesTactics {
+    // hash of tactics
+    mapping(uint256 => bytes32) private _tactics;
+}
