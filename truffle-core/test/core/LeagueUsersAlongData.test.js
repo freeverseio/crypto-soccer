e6cee8709bc5675449e9d9ee80e5dde56b9c9833
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-03-08 17:50:47 +0100

    tests for LeagueUsersAlongData

--- truffle-core/test/core/LeagueUsersAlongData.test.js
+++ truffle-core/test/core/LeagueUsersAlongData.test.js
@@ -7,22 +7,42 @@ require('chai')
 const League = artifacts.require('LeagueUsersAlongData');
 
 contract('LeagueUsersAlongData', (accounts) => {
-    const id = 0;
-
     beforeEach(async () => {
         league = await League.new().should.be.fulfilled;
-        const initBlock = 1;
-        const step = 1;
-        const teamIds = [1, 2];
-        await league.create(id, initBlock, step, teamIds).should.be.fulfilled;
     });
 
     it('initial hash of unexistent league', async () => {
-        await league.getUsersAlongDataHash(3).should.be.rejected;
+        await league.getUsersAlongDataHash(id = 3).should.be.rejected;
     });
 
     it('initial hash of existing league', async () => {
+        await league.create(id = 0, initBlock = 1, step = 1, teamIds = [1, 2]).should.be.fulfilled;
         const hash = await league.getUsersAlongDataHash(id).should.be.fulfilled;
         hash.should.be.equal('0x0000000000000000000000000000000000000000000000000000000000000000');
+    });
+
+    it('update unexistent league', async () => {
+        await league.updateUsersAlongDataHash(id = 0, teamIdx = 0, tactic = [4, 4, 3]).should.be.rejected;
+    })
+
+    it('update finished league', async () => {
+        const id = 0;
+        await league.create(id, initBlock = 1, step = 1, teamIds = [1, 2]).should.be.fulfilled;
+        const finished = await league.hasFinished(id).should.be.fulfilled;
+        finished.should.be.equal(true);
+        await league.updateUsersAlongDataHash(id, teamIdx = 0, tactic = [4, 4, 2]).should.be.rejected;
+    });
+
+    it('update', async () => {
+        const id = 0;
+        await league.create(id, initBlock = 1, step = 100000, teamIds = [1, 2]).should.be.fulfilled;
+        const finished = await league.hasFinished(id).should.be.fulfilled;
+        finished.should.be.equal(false);
+        await league.updateUsersAlongDataHash(id, teamIdx = 0, tactic = [4, 4, 2]).should.be.fulfilled;
+        let hash = await league.getUsersAlongDataHash(id).should.be.fulfilled;
+        hash.should.be.equal('0xd0c6de7df5b6f49e55dab2d876310253a8da499e94d6de0e3a50fc437fd9d75d');
+        await league.updateUsersAlongDataHash(id, teamIdx = 0, tactic = [4, 4, 2]).should.be.fulfilled;
+        hash = await league.getUsersAlongDataHash(id).should.be.fulfilled;
+        hash.should.be.equal('0x3d829cedc91a56c3fc097e80a4a70ee30d63ede10f0356d0565741208fcb8cb6');
     });
 }) 
