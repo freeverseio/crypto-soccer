b08748b734962ac1f9b24c196a4faf674049f63a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-03-08 06:41:27 +0100

    added test for challenge period

--- truffle-core/contracts/core/LeagueChallengeable.sol
+++ truffle-core/contracts/core/LeagueChallengeable.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.5.0;
 import "./LeagueUpdatable.sol";
 
 contract LeagueChallengeable is LeagueUpdatable {
+    uint256 constant private CHALLENGING_PERIOD_BLKS = 60;
+
+    function getChallengePeriod() external view returns (uint256) {
+        return CHALLENGING_PERIOD_BLKS;
+    }
+
+    function challengeMatchdayStates(uint256 id) public {
+        require(_isUpdated(id), "not updated league. No challenge allowed");
+    }
 }
---
+++ truffle-core/test/core/LeagueChallengeable.test.js
@@ -0,0 +1,19 @@
+const BN = require('bn.js');
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bn')(BN))
+    .should();
+
+const League = artifacts.require('LeagueChallengeable');
+
+contract('LeaguesScheduler', (accounts) => {
+    beforeEach(async () => {
+        league = await League.new().should.be.fulfilled;
+    });
+ 
+    it('challenge period', async () => {
+        const period = await league.getChallengePeriod().should.be.fulfilled;
+        period.should.be.a.bignumber.that.equals('60');
+    });
+})
