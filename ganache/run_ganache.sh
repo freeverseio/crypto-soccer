8f26a604e57dbe95b15ea7706a4989be13fee0a7
eaylon <eaylon@gmail.com>
2019-03-10 18:40:30 +0100

    * more options to run ganache

--- ganache/run_ganache.sh
+++ ganache/run_ganache.sh
@@ -6,7 +6,11 @@ NETWORKID=5777
 
 mkdir -p ${DATABASE}
 ganache-cli \
+--deterministic \
 --mnemonic "much repair shock carbon improve miss forget sock include bullet interest solution" \
 --port ${PORT} \
 --networkId ${NETWORKID} \
---db ${DATABASE}
+--db ${DATABASE} \
+--blockTime 1 \
+--verbose
+#--noVMErrorsOnRPCResponse \
