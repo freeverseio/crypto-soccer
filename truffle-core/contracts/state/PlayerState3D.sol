e876804055f640be1fd12bd7158b1e078c607c0b
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-03-15 17:03:57 +0100

    create playerState3D test

--- truffle-core/contracts/state/PlayerState3D.sol
+++ truffle-core/contracts/state/PlayerState3D.sol
@@ -5,18 +5,33 @@ import "./LeagueState.sol";
 contract PlayerState3D is LeagueState {
     uint256 constant private DIMENSION_2_END = 1;
 
-    function PlayerState3DCreate() public pure returns (uint256[] memory) {
+    function playerState3DCreate() public pure returns (uint256[] memory) {
 
     }
 
-    function PlayerState3DAppend(
-        uint256[] memory playerState2D, 
-        uint256[] memory teamState
+    function playerState3DAppend(
+        uint256[] memory playerState3D, 
+        uint256[] memory playerState2D
     ) 
         public 
         pure 
         returns (uint256[] memory state) 
     {
-        // state = new u
+        require(isValidPlayerState3D(playerState3D), "invalid playerState3D");
+        require(isValidTeamState(playerState2D), "invalid playerState2D");
+        state = new uint256[](playerState2D.length + playerState2D.length + 1);
+        for (uint256 i = 0 ; i < playerState2D.length ; i++)
+            state[i] = playerState2D[i];
+        for (uint256 i = 0 ; i < playerState2D.length ; i++) 
+            state[playerState2D.length + i] = playerState2D[i];
+        state[playerState2D.length + playerState2D.length] = DIMENSION_2_END;
+    }
+
+    function isValidPlayerState3D(uint256[] memory state) public pure returns (bool) {
+        if (state.length == 0)
+            return true;
+        if (state[state.length - 1] != DIMENSION_2_END)
+            return false;
+        return true;
     }
 }
--- truffle-core/test/state/PlayerState3D.test.js
+++ truffle-core/test/state/PlayerState3D.test.js
@@ -4,10 +4,16 @@ require('chai')
     .use(require('chai-bn')(BN))
     .should();
 
-const League = artifacts.require('PlayerState3D');
+const Contract = artifacts.require('PlayerState3D');
 
 contract('PlayerState3D', (accounts) => {
     beforeEach(async () => {
-        league = await League.new().should.be.fulfilled;
+        instance = await Contract.new().should.be.fulfilled;
     });
+
+    it('create player state 3D', async () => {
+        const state = await instance.playerState3DCreate().should.be.fulfilled;
+        const valid = await instance.isValidPlayerState3D(state).should.be.fulfilled;
+        valid.should.be.equal(true);
+    })
 }) 
