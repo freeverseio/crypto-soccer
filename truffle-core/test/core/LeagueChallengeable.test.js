290aa7543d7de1cb5774cfad7d7ce579ef808cc0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-03-18 17:56:21 +0100

    every time the league is resetted, challenge period is resetted

--- truffle-core/test/core/LeagueChallengeable.test.js
+++ truffle-core/test/core/LeagueChallengeable.test.js
@@ -5,11 +5,15 @@ require('chai')
     .should();
 
 const League = artifacts.require('LeagueChallengeable');
+const Cronos = artifacts.require('Cronos');
 
 contract('LeagueChallengeable', (accounts) => {
+    let league = null;
+    let crosos = null;
     const id = 0;
     const teamIds = [1, 2];
     const tactics = [[4, 4, 3], [4, 5, 2]];
+    let challengePeriod = null;
 
     beforeEach(async () => {
         league = await League.new().should.be.fulfilled;
@@ -20,11 +24,65 @@ contract('LeagueChallengeable', (accounts) => {
             teamIds,
             tactics
         ).should.be.fulfilled;
+        const result = await league.getChallengePeriod().should.be.fulfilled;
+        challengePeriod = result.toNumber();
+        cronos = await Cronos.new().should.be.fulfilled;
     });
+    
+    const advanceToBlock = async (block) => {
+        let current = await web3.eth.getBlockNumber().should.be.fulfilled;
+        while (current.toString() < block) {
+            await cronos.wait().should.be.fulfilled;
+            current = await web3.eth.getBlockNumber().should.be.fulfilled;
+        }
+        // console.log("current block: " + current);
+    }
  
     it('challenge period', async () => {
-        const period = await league.getChallengePeriod().should.be.fulfilled;
-        period.should.be.a.bignumber.that.equals('60');
+        challengePeriod.should.be.equal(60);
+    });
+
+    it('last challenge block', async () => {
+        await league.getLastChallengeBlock(id).should.be.rejected;
+        const result = await league.updateLeague(
+            id, 
+            initStateHash = '0x54564', 
+            dayStateHashes = ['0x24353', '0x5434432'],
+            scores = ['0x12', '0x3']
+        ).should.be.fulfilled;
+        const lastChallengeBlock = await league.getLastChallengeBlock(id).should.be.fulfilled;
+        lastChallengeBlock.toNumber().should.be.equal(result.receipt.blockNumber + challengePeriod);
+    });
+
+    it('reset league makes invalid last challenge block', async () => {
+        await league.updateLeague(
+            id, 
+            initStateHash = '0x54564', 
+            dayStateHashes = ['0x24353', '0x5434432'],
+            scores = ['0x12', '0x3']
+        ).should.be.fulfilled;
+        await league.resetUpdater(id).should.be.fulfilled;
+        await league.getLastChallengeBlock(id).should.be.rejected;
+    });
+
+    it('is verified', async () => {
+        let verified = await league.isVerified(id).should.be.fulfilled;
+        verified.should.be.equal(false);
+        let result = await league.updateLeague(
+            id, 
+            initStateHash = '0x54564', 
+            dayStateHashes = ['0x24353', '0x5434432'],
+            scores = ['0x12', '0x3']
+        ).should.be.fulfilled;
+        verified = await league.isVerified(id).should.be.fulfilled;
+        verified.should.be.equal(false);
+        const updateBlockNumber = result.receipt.blockNumber;
+        await advanceToBlock(updateBlockNumber + challengePeriod - 1).should.be.fulfilled;
+        verified = await league.isVerified(id).should.be.fulfilled;
+        verified.should.be.equal(false);
+        await advanceToBlock(updateBlockNumber + challengePeriod).should.be.fulfilled;
+        verified = await league.isVerified(id).should.be.fulfilled;
+        verified.should.be.equal(true);
     });
 
     it('challenge init state', async () => {
