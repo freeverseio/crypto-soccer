53862871cc209b694c089f6536debff664c163be
eaylon <eaylon@gmail.com>
2019-03-19 09:10:27 +0100

    * break functionality into different files

---
+++ relay/server/storage.go
@@ -0,0 +1,40 @@
+package relay
+
+// TODO: use go-soccer/storage
+
+import (
+	"errors"
+	"time"
+)
+
+var db = make(map[string]*UserEntry)
+
+// Action - ...
+type Action struct {
+	Value interface{}
+}
+
+// UserEntry - ...
+type UserEntry struct {
+	ID     int64
+	Nonce  uint64
+	Action Action
+}
+
+// AddUserEntry - adds user to db
+func AddUserEntry(account string) error {
+	_, ok := db[account]
+	if ok {
+		return errors.New("User already exist")
+	}
+	db[account] = &UserEntry{ID: time.Now().Unix(), Nonce: 0}
+	return nil
+}
+
+// GetUserEntry - adds user to db
+func GetUserEntry(account string) *UserEntry {
+	if entry, ok := db[account]; ok == true {
+		return entry
+	}
+	return nil
+}
