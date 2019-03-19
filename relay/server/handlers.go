666fc393f34aa370d669a88425ba761177fbb5ad
eaylon <eaylon@gmail.com>
2019-03-19 09:55:11 +0100

    * implement verifySigEthMsg

--- relay/server/handlers.go
+++ relay/server/handlers.go
@@ -1,11 +1,13 @@
 package relay
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"net/http"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/gin-gonic/gin"
 )
 
@@ -80,25 +82,25 @@ func dbGET(c *gin.Context) {
 func verifyAction(userAddr string, atype string, value string, r string, s string, v int) bool {
 	hexUserAddr := common.HexToAddress(userAddr)
 
-	typeBytes, err := str2byte(atype)
+	typeBytes, err := str2bytes(atype)
 	if err != nil {
 		fmt.Println("Error decoding type: ", err)
 		return false
 	}
 
-	valueBytes, err := str2byte(value)
+	valueBytes, err := str2bytes(value)
 	if err != nil {
 		fmt.Println("Error decoding type: ", err)
 		return false
 	}
 
-	rBytes, err := str2byte(r)
+	rBytes, err := str2bytes(r)
 	if err != nil {
 		fmt.Println("Error decoding R: ", err)
 		return false
 	}
 
-	sBytes, err := str2byte(s)
+	sBytes, err := str2bytes(s)
 	if err != nil {
 		fmt.Println("Error decoding S: ", err)
 		return false
@@ -108,14 +110,10 @@ func verifyAction(userAddr string, atype string, value string, r string, s strin
 	sig := append(rBytes[:], sBytes[:]...)
 	sig = append(sig[:], byte(v))
 
-	_ = hexUserAddr
-	_ = msg
-	// TODO: ask adria about msg. I also get an error when installing iden3/go-iden3/utils (error loading module requirements)
-	//return common3.VerifySigEthMsg(hexUserAddr, sig, msgBytes)
-	return true
+	return verifySigEthMsg(hexUserAddr, sig, msg)
 }
 
-func str2byte(s string) ([32]byte, error) {
+func str2bytes(s string) ([32]byte, error) {
 	bytes, err := hex.DecodeString(s)
 	var result [32]byte
 	if err != nil {
@@ -123,4 +121,51 @@ func str2byte(s string) ([32]byte, error) {
 	}
 	copy(result[:], bytes)
 	return result, nil
+}
+
+func bytes2str(data []byte) string {
+	return fmt.Sprintf("0x%s", hex.EncodeToString(data[:]))
+}
+
+func hashBytes(b ...[]byte) (hash [32]byte) {
+	h := crypto.Keccak256(b...)
+	copy(hash[:], h)
+	return hash
+}
+
+// Signature is a secp256k1 ecdsa signature.
+type Signature [65]byte
+
+// SignatureEthMsg is a secp256k1 ecdsa signature of an ethereum message:
+// https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_sig://github.com/ethereum/wiki/wiki/JSON-RPC#eth_sign
+type SignatureEthMsg [65]byte
+
+// Hash - alias for [32]byte
+type Hash [32]byte
+
+// ethHash is the hashing function used before signing ethereum messages.
+func ethHash(b []byte) Hash {
+	const web3SignaturePrefix = "\x19Ethereum Signed Message:\n"
+	header := fmt.Sprintf("%s%d", web3SignaturePrefix, len(b))
+	return hashBytes([]byte(header), b)
+}
+
+func verifySigEthMsg(addr common.Address, sig []byte, msg []byte) bool {
+	hash := ethHash(msg)
+	var _sig SignatureEthMsg
+	copy(_sig[:], sig[:])
+	_sig[64] -= 27
+	return verifySig(addr, (*Signature)(&_sig), hash[:])
+}
+
+// verifySig verifies a given signature and the msgHash with the expected address
+func verifySig(addr common.Address, sig *Signature, msgHash []byte) bool {
+	recoveredPub, err := crypto.Ecrecover(msgHash, sig[:])
+	if err != nil {
+		fmt.Printf("ECRecover error: %s\n", err)
+		return false
+	}
+	pubKey, _ := crypto.UnmarshalPubkey(recoveredPub)
+	recoveredAddr := crypto.PubkeyToAddress(*pubKey)
+	return bytes.Equal(addr.Bytes(), recoveredAddr.Bytes())
 }
