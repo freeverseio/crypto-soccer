f7c29c42845038e55966ba01ff932fb6bc67ddcb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-09 15:07:12 +0200

    createTeam works

---
+++ truffle-core/contracts/assets/Storage.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.0;
+
+contract Storage {
+    /// @dev The player skills in each team are obtained from hashing: name + userChoice
+    /// @dev So userChoice allows the user to inspect lots of teams compatible with his chosen name
+    /// @dev and select his favourite one.
+    /// @dev playerIdx serializes each player idx, allowing 20 bit for each (>1M players possible)
+    struct Team {
+        string name;
+    }
+
+    /// @dev An array containing the Team struct for all teams in existence.
+    /// @dev The ID of each team is actually his index in this array.
+    Team[] private teams;
+
+    function _countTeams() internal view returns (uint256){
+        return teams.length;
+    }
+
+    function _addTeam(string memory name) internal {
+        teams.push(Team(name));
+    }
+
+    // function _getTeamName();
+
+    // function _getPlayersIds();
+}
--- truffle-core/contracts/assets/Teams.sol
+++ truffle-core/contracts/assets/Teams.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.5.0;
 
-contract Teams {
+import "./Storage.sol";
 
+contract Teams is Storage {
+    function countTeams() public view returns (uint256) {
+        return _countTeams();
+    }
+
+    function createTeam(string memory name) public {
+        _addTeam(name);
+    }
 }
