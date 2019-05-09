0b96ea770748a964404db35f2d858cef5ec94115
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-09 19:09:22 +0200

    failing test on getPlayerTeam

--- truffle-core/test/assets/Players.test.js
+++ truffle-core/test/assets/Players.test.js
@@ -18,5 +18,20 @@ contract('Players', (accounts) => {
     it('query non created player id', async () => {
         await players.getPlayerTeam(1).should.be.rejected;
     });
+
+    it('get player team of existing player', async () => {
+        await players.addTeam("Barca").should.be.fulfilled;
+        await players.getPlayerTeam(1).should.be.fulfilled;
+        await players.getPlayerTeam(2).should.be.fulfilled;
+        return;
+        await players.getPlayerTeam(3).should.be.fulfilled;
+        await players.getPlayerTeam(4).should.be.fulfilled;
+        await players.getPlayerTeam(5).should.be.fulfilled;
+        await players.getPlayerTeam(6).should.be.fulfilled;
+        await players.getPlayerTeam(7).should.be.fulfilled;
+        await players.getPlayerTeam(8).should.be.fulfilled;
+        await players.getPlayerTeam(9).should.be.fulfilled;
+        await players.getPlayerTeam(10).should.be.fulfilled;
+    })
 });
  
