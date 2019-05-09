8d2860cefd4c078b064543554cab6895016164f9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-09 19:01:06 +0200

    addTeam is public

--- truffle-core/contracts/mocks/PlayersMock.sol
+++ truffle-core/contracts/mocks/PlayersMock.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.21 <0.6.0;
 import "../assets/Players.sol";
 
 contract PlayersMock is Players {
-    function addTeam(string memory name) internal returns (uint256) {
+    function addTeam(string memory name) public returns (uint256) {
         _addTeam(name);
     }
 }
