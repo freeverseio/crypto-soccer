7bfab47c88b88cd0e4da55ad801097e953ce504c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-09 14:42:50 +0200

    moving all to legacy (temporary) and bootstraping

---
+++ truffle-core/test/assets/Teams.test.js
@@ -0,0 +1,24 @@
+require('chai')
+    .use(require('chai-as-promised'))
+    .should();
+
+const Assets = artifacts.require('Teams');
+
+contract('Assets', (accounts) => {
+    let assets = null;
+
+    beforeEach(async () => {
+        assets = await Assets.new().should.be.fulfilled;
+    });
+
+    it('initial number of team', async () => {
+        const count = await assets.countTeams().should.be.fulfilled;
+        count.toNumber().should.be.equal(0);
+    });
+
+    it('create team', async () => {
+        await assets.createTeam(name = "Barca").should.be.fulfilled;
+        const count = await assets.countTeams().should.be.fulfilled;
+        count.toNumber().should.be.equal(1);
+    });
+})
