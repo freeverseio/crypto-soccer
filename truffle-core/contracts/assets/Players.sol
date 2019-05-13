20628c093641ff953dc969e9428ba68cd336fbd2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-13 11:43:37 +0200

    getPlayerTeam is in Storage

--- truffle-core/contracts/assets/Players.sol
+++ truffle-core/contracts/assets/Players.sol
@@ -19,14 +19,6 @@ contract Players is Storage {
         // TODO: check playerId
     }
 
-    /// this function uses the inverse of the following formula
-    /// playerId = playersPerTeam * (teamId -1) + 1 + posInTeam;
-    function getPlayerTeam(uint256 playerId) public view returns (uint256) {
-        require(_playerExists(playerId), "unexistent player");
-        uint256 teamId = 1 + (playerId - 1) / PLAYERS_PER_TEAM;
-        return teamId;
-    }
-
     function getPlayerPosInTeam(uint256 playerId) public view returns (uint256) {
         uint256 teamId = getPlayerTeam(playerId);
         return playerId - PLAYERS_PER_TEAM * (teamId - 1) - 1;
