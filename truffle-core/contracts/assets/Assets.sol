8d2f54afcc6b897937d6af72b8b1391343935db4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-14 22:07:12 +0200

    refactoring

--- truffle-core/contracts/assets/Assets.sol
+++ truffle-core/contracts/assets/Assets.sol
@@ -2,7 +2,6 @@ pragma solidity >=0.4.21 <0.6.0;
 
 import "../state/PlayerState.sol";
 
-
 /// teamId == 0 is invalid and represents the null team
 /// TODO: fix the playerPos <=> playerShirt doubt
 contract Assets {
@@ -41,12 +40,6 @@ contract Assets {
         teams.push(Team("_", 0, 0, 0, 0, playerIds));
     }
 
-    // TODO: exception when not existent team
-    function getTeamOwner(string memory name) public view returns (address) {
-        bytes32 nameHash = keccak256(abi.encode(name));
-        return _teamNameHashToOwner[nameHash];
-    }
-
     /// Get the skills of a player
     function getPlayerSkills(uint256 playerId) external view returns (uint16[NUM_SKILLS] memory) {
         require(_playerExists(playerId), "unexistent player");
@@ -67,6 +60,65 @@ contract Assets {
             teams[teamId].posInCurrentLeague,
             teams[teamId].prevLeagueId,
             teams[teamId].posInPrevLeague);
+    }
+
+    // TODO: exchange fails on playerId0 & playerId1 of the same team
+    function exchangePlayersTeams(uint256 playerId0, uint256 playerId1) public {
+        // TODO: check ownership address
+        require(_playerExists(playerId0) && _playerExists(playerId1), "unexistent playerId");
+        uint256 state0 = getPlayerState(playerId0);
+        uint256 state1 = getPlayerState(playerId1);
+        uint256 newState0 = state0;
+        uint256 teamId0 = _playerState.getCurrentTeamId(state0);
+        uint256 teamId1 = _playerState.getCurrentTeamId(state1);
+        uint256 playerShirt0 = _playerState.getCurrentShirtNum(state0);
+        uint256 playerShirt1 = _playerState.getCurrentShirtNum(state1);
+        newState0 = _playerState.setCurrentTeamId(newState0, _playerState.getCurrentTeamId(state1));
+        newState0 = _playerState.setCurrentShirtNum(newState0, _playerState.getCurrentShirtNum(state1));
+        state1 = _playerState.setCurrentTeamId(state1,_playerState.getCurrentTeamId(state0));
+        state1 = _playerState.setCurrentShirtNum(state1,_playerState.getCurrentShirtNum(state0));
+        newState0 = _playerState.setLastSaleBlock(newState0, block.number);
+        state1 = _playerState.setLastSaleBlock(state1, block.number);
+
+        teams[teamId0].playerIds[playerShirt0] = playerId1;
+        teams[teamId1].playerIds[playerShirt1] = playerId0;
+
+        // TODO
+        // if getBlockNumForLastLeagueOfTeam(teamIdx1, ST) > state1.getLastSaleBlocknum():
+        //     state1.prevLeagueIdx = ST.teams[teamIdx1].currentLeagueIdx
+        //     state1.prevTeamPosInLeague = ST.teams[teamIdx1].teamPosInCurrentLeague
+
+        // if getBlockNumForLastLeagueOfTeam(teamIdx2, ST) > state2.getLastSaleBlocknum():
+        //     state2.prevLeagueIdx = ST.teams[teamIdx2].currentLeagueIdx
+        //     state2.prevTeamPosInLeague = ST.teams[teamIdx2].teamPosInCurrentLeague
+
+        _setPlayerState(newState0);
+        _setPlayerState(state1);
+    }
+
+    function createTeam(string memory teamName, address owner) public {
+        uint256 teamId = _addTeam(teamName, owner);
+        emit TeamCreation(teamName, teamId);
+    }
+
+    function signToLeague(
+        uint256 teamId,
+        uint256 leagueId,
+        uint8 posInLeague
+    )
+    public
+    {
+        require(_teamExists(teamId), "invalid team id");
+        teams[teamId].prevLeagueId = teams[teamId].currentLeagueId;
+        teams[teamId].posInPrevLeague = teams[teamId].posInCurrentLeague;
+        teams[teamId].currentLeagueId = leagueId;
+        teams[teamId].posInCurrentLeague = posInLeague;
+    }
+
+    // TODO: exception when not existent team
+    function getTeamOwner(string memory name) public view returns (address) {
+        bytes32 nameHash = keccak256(abi.encode(name));
+        return _teamNameHashToOwner[nameHash];
     }
 
     function getPlayerPosInTeam(uint256 playerId) public view returns (uint256) {
@@ -139,6 +191,15 @@ contract Assets {
             return _playerIdToState[playerId];
     }
 
+    function _addTeam(string memory name, address owner) internal returns (uint256) {
+        bytes32 nameHash = keccak256(abi.encode(name));
+        require(_teamNameHashToOwner[nameHash] == address(0), "team already exists");
+        _teamNameHashToOwner[nameHash] = owner;
+        uint256[PLAYERS_PER_TEAM] memory playerIds;
+        teams.push(Team(name, 0, 0, 0, 0, playerIds));
+        return teams.length - 1;
+    }
+
     function _setPlayerState(uint256 state) internal {
         uint256 playerId = _playerState.getPlayerId(state);
         require(_playerExists(playerId), "unexistent player");
@@ -151,29 +212,6 @@ contract Assets {
         uint256 saleBlock = _playerState.getLastSaleBlock(state);
         require(saleBlock != 0 && saleBlock <= block.number, "invalid sale block");
         _playerIdToState[playerId] = state;
-    }
-
-    function signToLeague(
-        uint256 teamId,
-        uint256 leagueId,
-        uint8 posInLeague
-    )
-    public
-    {
-        require(_teamExists(teamId), "invalid team id");
-        teams[teamId].prevLeagueId = teams[teamId].currentLeagueId;
-        teams[teamId].posInPrevLeague = teams[teamId].posInCurrentLeague;
-        teams[teamId].currentLeagueId = leagueId;
-        teams[teamId].posInCurrentLeague = posInLeague;
-    }
-
-    function _addTeam(string memory name, address owner) internal returns (uint256) {
-        bytes32 nameHash = keccak256(abi.encode(name));
-        require(_teamNameHashToOwner[nameHash] == address(0), "team already exists");
-        _teamNameHashToOwner[nameHash] = owner;
-        uint256[PLAYERS_PER_TEAM] memory playerIds;
-        teams.push(Team(name, 0, 0, 0, 0, playerIds));
-        return teams.length - 1;
     }
 
     function _teamExists(uint256 teamId) internal view returns (bool) {
@@ -237,47 +275,6 @@ contract Assets {
             skills[i]++;
 
         return skills;
-    }
-
-    // TODO: exchange fails on playerId0 & playerId1 of the same team
-    function exchangePlayersTeams(uint256 playerId0, uint256 playerId1) public {
-        // TODO: check ownership address
-        require(_playerExists(playerId0) && _playerExists(playerId1), "unexistent playerId");
-        uint256 state0 = getPlayerState(playerId0);
-        uint256 state1 = getPlayerState(playerId1);
-        uint256 newState0 = state0;
-        uint256 teamId0 = _playerState.getCurrentTeamId(state0);
-        uint256 teamId1 = _playerState.getCurrentTeamId(state1);
-        uint256 playerShirt0 = _playerState.getCurrentShirtNum(state0);
-        uint256 playerShirt1 = _playerState.getCurrentShirtNum(state1);
-        newState0 = _playerState.setCurrentTeamId(newState0, _playerState.getCurrentTeamId(state1));
-        newState0 = _playerState.setCurrentShirtNum(newState0, _playerState.getCurrentShirtNum(state1));
-        state1 = _playerState.setCurrentTeamId(state1,_playerState.getCurrentTeamId(state0));
-        state1 = _playerState.setCurrentShirtNum(state1,_playerState.getCurrentShirtNum(state0));
-        newState0 = _playerState.setLastSaleBlock(newState0, block.number);
-        state1 = _playerState.setLastSaleBlock(state1, block.number);
-
-        teams[teamId0].playerIds[playerShirt0] = playerId1;
-        teams[teamId1].playerIds[playerShirt1] = playerId0;
-
-        // TODO
-        // if getBlockNumForLastLeagueOfTeam(teamIdx1, ST) > state1.getLastSaleBlocknum():
-        //     state1.prevLeagueIdx = ST.teams[teamIdx1].currentLeagueIdx
-        //     state1.prevTeamPosInLeague = ST.teams[teamIdx1].teamPosInCurrentLeague
-
-        // if getBlockNumForLastLeagueOfTeam(teamIdx2, ST) > state2.getLastSaleBlocknum():
-        //     state2.prevLeagueIdx = ST.teams[teamIdx2].currentLeagueIdx
-        //     state2.prevTeamPosInLeague = ST.teams[teamIdx2].teamPosInCurrentLeague
-
-        _setPlayerState(newState0);
-        _setPlayerState(state1);
-    }
-
-    
-
-    function createTeam(string memory teamName, address owner) public {
-        uint256 teamId = _addTeam(teamName, owner);
-        emit TeamCreation(teamName, teamId);
     }
 
     /// @return hashed arg casted to uint256
