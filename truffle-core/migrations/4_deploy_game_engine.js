01b6af8a32c389d428a7f1be2be070f710495280
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-17 18:01:20 +0200

    refactoring log deploy

--- truffle-core/migrations/4_deploy_game_engine.js
+++ truffle-core/migrations/4_deploy_game_engine.js
@@ -1,10 +1,8 @@
-const fs = require('fs');
 const Engine = artifacts.require('Engine');
 
 module.exports = function (deployer) {
   deployer.then(async () => {
-      const engine = await deployer.deploy(Engine);
-      fs.appendFileSync('deploy_addresses.txt', "engine : " + engine.address + "\n");
+      await deployer.deploy(Engine);
     })
     .catch(console.error);
 };
---
+++ truffle-core/migrations/99_log.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const States = artifacts.require('LeagueState');
+const Engine = artifacts.require('Engine');
+const GameController = artifacts.require("GameController");
+const Leagues = artifacts.require('Leagues');
+const Stakers = artifacts.require('Stakers');
+
+module.exports = function (deployer) {
+  deployer.then(async () => {
+      let log = "--------------------------------" + "\n";
+      log += "States:         " + States.address + "\n";
+      log += "Engine:         " + Engine.address + "\n";
+      log += "GameController: " + GameController.address + "\n";
+      log += "Leagues:        " + Leagues.address + "\n";
+      log += "Stakers:        " + Stakers.address + "\n";
+      log += "--------------------------------";
+
+      fs.writeFileSync('deploy_addresses.txt', log);
+      console.log(log);
+    })
+    .catch(console.error);
+};
+
