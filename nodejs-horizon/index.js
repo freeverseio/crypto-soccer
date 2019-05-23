4b29218e51e422d27f2d9e17659b81a54c172251
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-23 22:49:47 +0200

    graphql added Assets contract

--- nodejs-horizon/index.js
+++ nodejs-horizon/index.js
@@ -1,59 +1,67 @@
 const Web3 = require('web3');
-const { GraphQLServer } = require('graphql-yoga')
-
+const {
+  GraphQLServer
+} = require('graphql-yoga');
+const assetsContractJSON = require('../truffle-core/build/contracts/Assets.json');
+const assetsContractAddress = '0x14ebD51cD831f8B371b19ad571FaCDa3655004a4';
 const providerURL = 'ws://localhost:8545';
 const web3 = new Web3(providerURL, null, {});
 
 web3.eth.net.isListening()
   .then(connected => {
-    console.log('provider ' + providerURL + ' is connected: ' + connected);
+      console.log('provider ' + providerURL + ' is connected: ' + connected);
+      const assetsContract = new web3.eth.Contract(assetsContractJSON.abi, assetsContractAddress);
+
+      const typeDefs = `
+        type Query {
+          hello(name : String): String!
+          team: Team!
+          getProvider: Provider!
+          getAssetsContractAddress: String
+        }
+
+        type Provider {
+          url: String
+          isListening: Boolean!
+        }
+
+        type Mutation {
+          setProviderUrl(url: String!): Provider!
+        }
+
+        type Team {
+          name: String!
+        }
+      `
+    const resolvers = {
+      Query: {
+        hello: (_, {
+          name
+        }) => `Hello ${name || 'World'}`,
+        team: (_) => (''),
+        getProvider: async () => ({
+          url: web3.currentProvider.connection._url,
+          isListening: await web3.eth.net.isListening()
+        }),
+        getAssetsContractAddress: () => (assetsContract.address)
+      },
+      Mutation: {
+        setProviderUrl: () => (console.log("setProviderUrl"))
+      },
+      Provider: {
+        url: () => (web3.currentProvider.connection._url),
+        isListening: async () => (await web3.eth.net.isListening()),
+      },
+      Team: {
+        name: () => 'Ciao qui'
+      }
+    }
+
+    const server = new GraphQLServer({
+      typeDefs,
+      resolvers
+    })
+
+    server.start(() => console.log('Server is running on localhost:4000'))
   })
   .catch(console.log);
-
-const typeDefs = `
-  type Query {
-    hello(name : String): String!
-    team: Team!
-    getProvider: Provider!
-    getAssetsContractAddress: String
-  }
-
-  type Provider {
-    url: String
-    isListening: Boolean!
-  }
-
-  type Mutation {
-    setProviderUrl(url: String!): Provider!
-  }
-
-  type Team {
-    name: String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-    team: (_) =>  (''),
-    getProvider: async () => ({
-      url: web3.currentProvider.connection._url,
-      isListening: await web3.eth.net.isListening()
-    }),
-    getAssetsContractAddress: () => (assetsContractAddress)
-  },
-  Mutation: {
-    setProviderUrl: () => (console.log("setProviderUrl"))
-  },
-  Provider: {
-    url: () => (web3.currentProvider.connection._url),
-    isListening: async () => (await web3.eth.net.isListening()),
-  },
-  Team: {
-    name: () => 'Ciao qui'
-  }
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
-
-server.start(() => console.log('Server is running on localhost:4000'))
