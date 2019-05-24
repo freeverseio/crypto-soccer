2ece4c1280f367bd55479e3093de89a9f63f9955
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-05-24 16:23:18 +0200

    refactoring

--- nodejs-horizon/index.js
+++ nodejs-horizon/index.js
@@ -2,47 +2,47 @@ const Web3 = require('web3');
 const {
   GraphQLServer
 } = require('graphql-yoga');
+
 const assetsContractJSON = require('../truffle-core/build/contracts/Assets.json');
+
+const providerUrl = 'ws://localhost:8545';
 const assetsContractAddress = '0x14ebD51cD831f8B371b19ad571FaCDa3655004a4';
-const providerURL = 'ws://localhost:8545';
-const web3 = new Web3(providerURL, null, {});
+const from = '0x9C33497cEc1E9603Ba65D3A8d5e59F543950d6Ef';
+const gas = 6721975;
 
-// web3.eth.net.isListening()
-//   .then(connected => {
-//     console.log('provider ' + providerURL + ' is connected: ' + connected);
-//     const assetsContract = new web3.eth.Contract(assetsContractJSON.abi, assetsContractAddress);
-
-//   })
-//   .catch(console.log);
+const web3 = new Web3(providerUrl, null, {});
+const assetsContract = new web3.eth.Contract(assetsContractJSON.abi, assetsContractAddress);
 
 const typeDefs = `
-        type Query {
-          getSettings: Settings!
-          getAssetsContractAddress: String
-          getTeamCount: String!
-        }
+  type Query {
+    getSettings: Settings!
+    getTeamCount: String!
+  }
 
-        type Mutation {
-          createTeam(name: String!, owner: String!): String
-        }
+  type Mutation {
+    createTeam(name: String!, owner: String!): String
+  }
 
-        type Settings {
-          providerUrl: String
-          assetsContractAddress: String
-        }
+  type Settings {
+    providerUrl: String
+    assetsContractAddress: String
+    from: String
+    gas: String
+  }
 
-        type Provider {
-          url: String
-          isListening: Boolean!
-        }
-      `
+  type Provider {
+    url: String
+    isListening: Boolean!
+  }
+`
 const resolvers = {
   Query: {
     getSettings: () => ({
       providerUrl: web3.currentProvider.connection._url,
-      assetsContractAddress: assetsContractAddress
+      assetsContractAddress: assetsContractAddress,
+      from,
+      gas
     }),
-    getAssetsContractAddress: () => (assetsContract.address),
     getTeamCount: async () => {
       const count = await assetsContract.methods.countTeams().call()
       return count.toString();
@@ -51,8 +51,8 @@ const resolvers = {
   Mutation: {
     createTeam: (_, params) => {
       assetsContract.methods.createTeam(params.name, params.owner).send({
-        from: '0x9C33497cEc1E9603Ba65D3A8d5e59F543950d6Ef',
-        gas: 6721975
+        from,
+        gas
       });
     }
   }
