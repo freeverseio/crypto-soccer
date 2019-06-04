0c7f320f1ff4e5400c622ea9b47471c0616d31a8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-04 16:27:58 +0200

    refactoring

--- nodejs-horizon/src/resolvers.js
+++ nodejs-horizon/src/resolvers.js
@@ -1,15 +1,42 @@
 module.exports = function Resolvers(universe) {
   this.Query = {
-    countTeams: () => universe.countTeams(),
-    allTeams: () => universe.getTeamIds(),
+    countTeams: () => universe.assets.methods.countTeams().call(),
+    allTeams: async () => {
+      const count = await universe.assets.methods.countTeams().call();
+      let ids = [];
+      for (let i = 1; i <= count; i++)
+        ids.push(i);
+      return ids;
+    },
     getTeam: (_, { id }) => id,
     getPlayer: (_, { id }) => id,
     countLeagues: () => universe.leagues.methods.leaguesCount().call(),
   };
 
   this.Mutation = {
-    createTeam: (_, { name, owner }) => universe.createTeam(name, owner),
-    createLeague: (_, { initBlock, step, teamIds, tactics }) => universe.createLeague(initBlock, step, teamIds, tactics),
+    createTeam: async (_, { name, owner }) => {
+      const gas = await universe.assets.methods.createTeam(name, owner).estimateGas();
+      await universe.assets.methods.createTeam(name, owner).send({ from: universe.from, gas });
+    },
+    createLeague: async (_, { initBlock, step, teamIds, tactics }) => {
+      const { leagues, from } = universe;
+      const count = await leagues.methods.leaguesCount().call();
+      const id = count + 1;
+      const gas = await leagues.methods.create(
+        id,
+        initBlock,
+        step,
+        teamIds,
+        tactics
+      ).estimateGas();
+      await leagues.methods.create(
+        id,
+        initBlock,
+        step,
+        teamIds,
+        tactics
+      ).send({ from, gas });
+    },
   };
 
   this.Subscription = {
@@ -26,13 +53,31 @@ module.exports = function Resolvers(universe) {
 
   this.Player = {
     id: (id) => id,
-    name: (id) => universe.getPlayerName(id),
-    defence: (id) => universe.getPlayerDefence(id),
-    speed: (id) => universe.getPlayerSpeed(id),
-    pass: (id) => universe.getPlayerPass(id),
-    shoot: (id) => universe.getPlayerShoot(id),
-    endurance: (id) => universe.getPlayerEndurance(id),
-    team: (id) => universe.getPlayerTeamId(id),
+    name: (id) => "player_" + id,
+    defence: async (id) => {
+      const state = await universe.assets.methods.getPlayerState(id).call();
+      return await universe.playerState.methods.getDefence(state).call();
+    },
+    speed: async (id) => {
+      const state = await universe.assets.methods.getPlayerState(id).call();
+      return await universe.playerState.methods.getSpeed(state).call();
+    },
+    pass: async (id) => {
+      const state = await universe.assets.methods.getPlayerState(id).call();
+      return await universe.playerState.methods.getPass(state).call();
+    },
+    shoot: async (id) => {
+      const state = await universe.assets.methods.getPlayerState(id).call();
+      return await universe.playerState.methods.getShoot(state).call();
+    },
+    endurance: async (id) => {
+      const state = await universe.assets.methods.getPlayerState(id).call();
+      return await universe.playerState.methods.getEndurance(state).call();
+    },
+    team: async (id) => {
+      const state = await universe.assets.methods.getPlayerState(id).call();
+      return await universe.playerState.methods.getCurrentTeamId(state).call();
+    },
   };
 
   this.League = {
