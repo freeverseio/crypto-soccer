58cbf98d41728538382e31cb322dd792903e97a7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-05 16:40:21 +0200

    removed old code

--- truffle-core/deprecated/contracts_legacy/ERC721/Teams.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.5.0;
-
-import "./TeamsPlayers.sol";
-import "./ERC721MetadataBaseURI.sol";
-
-contract Teams is TeamsPlayers, ERC721MetadataBaseURI("Teams", "CT") {
-    constructor (address Players) TeamsPlayers(Players) public {}
-}
-
