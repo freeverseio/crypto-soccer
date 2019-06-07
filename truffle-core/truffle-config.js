c9aee440beb0fb2163968fbcadf96fcdcbb8da3f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-07 14:20:58 +0200

    trying removing the nets

--- truffle-core/truffle-config.js
+++ truffle-core/truffle-config.js
@@ -25,15 +25,15 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545
     },
-    infura_ropsten: {
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
-      network_id: 3
-    },
-    devnet: {
-      // public key 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
-      provider: new HDWalletProvider("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54", "https://devnet.busyverse.com/web3"),
-      network_id: 63819
-    }
+    // infura_ropsten: {
+    //   provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+    //   network_id: 3
+    // },
+    // devnet: {
+    //   // public key 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
+    //   provider: new HDWalletProvider("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54", "https://devnet.busyverse.com/web3"),
+    //   network_id: 63819
+    // }
   },
   // mocha: {
   //   reporter: 'eth-gas-reporter',
