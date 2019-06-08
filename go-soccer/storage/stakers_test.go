a920763505904c30a76a2b1b95e94cd5b7c6544a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-08 18:41:23 +0200

    [go-soccer] staker test completed

--- go-soccer/storage/stakers_test.go
+++ go-soccer/storage/stakers_test.go
@@ -42,3 +42,42 @@ func TestHasStaker(t* testing.T) {
 	}
 
 }
+
+func TestSetStaker(t* testing.T) {
+	s, err := New("./test")
+	defer os.RemoveAll("./test")
+	if err != nil{
+		t.Error(err)
+	}
+
+	entry := StakerEntry{45}
+	address := common.HexToAddress("0x44")
+	err = s.SetStaker(address, &entry)
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, err := s.Staker(address)
+	if err != nil {
+		t.Error(err)
+	}
+	if *result != entry {
+		t.Error("Expected is ", entry, " and result is ", *result)
+	}
+
+	entry2 := StakerEntry{1}
+	err = s.SetStaker(address, &entry2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	result, err = s.Staker(address)
+	if err != nil {
+		t.Error(err)
+	}
+	if *result != entry2 {
+		t.Error("Expected is ", entry, " and result is ", *result)
+	}
+
+
+}
