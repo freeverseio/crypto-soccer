2365ef210de6ee512d638b87d63642a114d3d9f1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-09 09:13:31 +0200

    [core] truffle migration outputs on json file

--- truffle-core/truffle-config.js
+++ truffle-core/truffle-config.js
@@ -1,47 +1,26 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
 const HDWalletProvider = require("truffle-hdwallet-provider");
  
 const mnemonic = "twelve_words";
 const infura_apikey = "XXXXXX"; // infura api key
 
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
     ganache: {
       network_id: '*',
       host: '127.0.0.1',
       port: 8545
     },
+    devnet: {
+      // public key 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
+      provider: new HDWalletProvider("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54", "https://devnet.busyverse.com/web3"),
+      network_id: 63819
+    }
     // infura_ropsten: {
     //   provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
     //   network_id: 3
     // },
-    // devnet: {
-    //   // public key 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
-    //   provider: new HDWalletProvider("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54", "https://devnet.busyverse.com/web3"),
-    //   network_id: 63819
-    // }
+    
   },
-  // mocha: {
-  //   reporter: 'eth-gas-reporter',
-  //   reporterOptions: {
-  //     currency: 'EUR',
-  //     gasPrice: 21
-  //   }
-  // },
   compilers: {
     solc: {
       version: "0.5.9"
