3a5716cd0b0a0aa41c24cf71c557bd1ab72e47ad
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-13 07:08:17 +0200

    [synchronized] integration tests using docker

--- go-synchronizer/storage/team_test.go
+++ go-synchronizer/storage/team_test.go
@@ -5,31 +5,31 @@ import (
 )
 
 func TestCreateTeam(t *testing.T) {
-	url := "postgres://freeverse:freeverse@localhost/cryptosoccer"
+	url := "postgres://freeverse:freeverse@localhost/cryptosoccer?sslmode=disable"
 	err := Init(url)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	err = CreateTeam(1, "Barca")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	rows, err := db.Query("SELECT * FROM teams WHERE id= '1';")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
 	if(!rows.Next()){
-		t.Error("team not created")
+		t.Fatal("team not created")
 	}
 
 	var id int
 	var name string
 	err = rows.Scan(&id, &name)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if id != 1 {
 		t.Error("wrong id:", id)
