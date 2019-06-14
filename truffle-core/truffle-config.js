aff3a53133b346f2efdbdb49d9720e1633d0d719
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-14 12:44:03 +0200

    [core] docker mines

--- truffle-core/truffle-config.js
+++ truffle-core/truffle-config.js
@@ -10,14 +10,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545
     },
-    // dockertest: {
-    //   provider: new HDWalletProvider(
-    //     "FE058D4CE3446218A7B4E522D9666DF5042CF582A44A9ED64A531A81E7494A85",
-    //     "http://localhost:8545"
-    //   ),
-    //   network_id: 63819,
-    //   gas: 
-    // }
+    dockertest: { // 83a909262608c650bd9b0ae06e29d90d0f67ac5e
+      network_id: 63819,
+      provider: new HDWalletProvider(
+        "FE058D4CE3446218A7B4E522D9666DF5042CF582A44A9ED64A531A81E7494A85",
+        "http://localhost:8545"
+      ),
+      // gasLimit: 2000000
+    }
     // infura_ropsten: {
     //   provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
     //   network_id: 3
