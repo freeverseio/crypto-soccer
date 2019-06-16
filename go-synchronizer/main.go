74b8834d21a026e1d09e0ade3006f2920b913398
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-16 09:27:25 +0200

    [synchronizer] creating the pipe

--- go-synchronizer/main.go
+++ go-synchronizer/main.go
@@ -3,10 +3,21 @@ package main
 import (
 	_ "github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
 	_ "github.com/freeverseio/crypto-soccer/go-synchronizer/process"
-	_ "github.com/sirupsen/logrus"
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/fifo"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	log.Info("Start ...")
+
+	bufferSize := 1000;
+
+	log.Info("Creating the fifo with buffer size ", bufferSize)
+
+	fifo := fifo.FifoNew(bufferSize)
+
+	log.Info(fifo)
+	
 	// connStr := "user=postgres dbname=cryptosoccer"
 	// err := storage.Init(connStr)
 	// if err != nil {
