8e165b1ab29d8d15b54e1f0da940714fbe630a4e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-18 10:57:50 +0200

    [synchronizer] abigen process the bin too

--- scripts/deploy_go_contracts_bind.py
+++ scripts/deploy_go_contracts_bind.py
@@ -4,25 +4,28 @@ import json
 import os
 
 with open('../truffle-core/build/contracts/Assets.json', 'r') as fp:
-    abi = json.load(fp)['abi']
-
+    contract = json.load(fp)
 with open('./Assets.abi', 'w', encoding='utf-8') as outfile:
-    json.dump(abi, outfile, ensure_ascii=False, indent=2)
+    json.dump(contract['abi'], outfile, ensure_ascii=False, indent=2)
+with open('./Assets.bin', 'w', encoding='utf-8') as outfile:
+    json.dump(contract['bytecode'], outfile, ensure_ascii=False, indent=2)
 
-os.system('abigen --abi ./Assets.abi --pkg assets -out ../go-synchronizer/contracts/assets/assets.go')
+os.system('abigen --abi ./Assets.abi --bin ./Assets.bin --pkg assets -out ../go-synchronizer/contracts/assets/assets.go')
 
 with open('../truffle-core/build/contracts/TeamState.json', 'r') as fp:
-    abi = json.load(fp)['abi']
-
+    contract = json.load(fp)
 with open('./States.abi', 'w', encoding='utf-8') as outfile:
-    json.dump(abi, outfile, ensure_ascii=False, indent=2)
+    json.dump(contract['abi'], outfile, ensure_ascii=False, indent=2)
+with open('./States.bin', 'w', encoding='utf-8') as outfile:
+    json.dump(contract['bytecode'], outfile, ensure_ascii=False, indent=2)
 
-os.system('abigen --abi ./States.abi --pkg states -out ../go-synchronizer/contracts/states/states.go')
+os.system('abigen --abi ./States.abi --bin ./States.bin --pkg states -out ../go-synchronizer/contracts/states/states.go')
 
 with open('../truffle-core/build/contracts/Leagues.json', 'r') as fp:
-    abi = json.load(fp)['abi']
-
+    contract = json.load(fp)
 with open('./Leagues.abi', 'w', encoding='utf-8') as outfile:
-    json.dump(abi, outfile, ensure_ascii=False, indent=2)
+    json.dump(contract['abi'], outfile, ensure_ascii=False, indent=2)
+with open('./Leagues.bin', 'w', encoding='utf-8') as outfile:
+    json.dump(contract['bytecode'], outfile, ensure_ascii=False, indent=2)
 
-os.system('abigen --abi ./Leagues.abi --pkg leagues -out ../go-synchronizer/contracts/leagues/leagues.go')
+os.system('abigen --abi ./Leagues.abi --bin ./Leagues.bin --pkg leagues -out ../go-synchronizer/contracts/leagues/leagues.go')
