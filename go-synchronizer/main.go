0713ac448a7520ee7f0d3dd3a5125a0089ae7553
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-18 08:57:25 +0200

    [synchronizer] connection to devnet

--- go-synchronizer/main.go
+++ go-synchronizer/main.go
@@ -5,9 +5,43 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/assets"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/process"
 	log "github.com/sirupsen/logrus"
 )
+
+var ethereumClient = "https://devnet.busyverse.com/web3"
+var assetsContractAddress = "0x05Fdd4d2340bcA823802849c75F385561278c3aB"
+
+func main() {
+	log.Info("Setup ...")
+	log.Info("Dial the Ethereum client ", ethereumClient)
+	conn, err := ethclient.Dial(ethereumClient)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	log.Info("Creating Assets bindings to ", assetsContractAddress)
+	assetsContract, err := assets.NewAssets(common.HexToAddress(assetsContractAddress), conn)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	process := process.BackgroundProcessNew(assetsContract)
+
+	log.Info("Start to process events ...")
+	process.Start()
+
+	log.Info("Press 'ctrl + c' to interrupt")
+	waitForInterrupt()
+
+	log.Info("Stop to process events ...")
+	process.StopAndJoin()
+
+	log.Info("... exiting")
+}
 
 func waitForInterrupt() {
 	sigs := make(chan os.Signal, 1)
@@ -21,21 +55,4 @@ func waitForInterrupt() {
 	}()
 
 	<-done
-}
-
-func main() {
-	log.Info("Setup the synchronizer ...")
-
-	process := process.BackgroundProcessNew()
-
-	log.Info("Start to process events ...")
-	process.Start()
-
-	log.Info("Press 'ctrl + c' to interrupt")
-	waitForInterrupt()
-
-	log.Info("Stop to process events ...")
-	process.StopAndJoin()
-
-	log.Info("... exiting")
 }
