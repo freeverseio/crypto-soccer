b92f50355f7c0ac706c2cd45205cda4d672af186
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-18 07:17:53 +0200

    [synchronizer] infrastructure to interrupt

--- go-synchronizer/main.go
+++ go-synchronizer/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	_ "github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
-	_ "github.com/freeverseio/crypto-soccer/go-synchronizer/process"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/process"
 	log "github.com/sirupsen/logrus"
 )
+
+func waitForInterrupt() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	log.Info("ctrl + c to interrupt")
+	<-done
+}
 
 func main() {
 	log.Info("Setup the synchronizer ...")
 
 	process := process.BackgroundProcessNew()
 
-	log.Info("Start to process events ...")
-	process.Start()	
+	// log.Info("Start to process events ...")
+	process.Start()
+
+	waitForInterrupt()
 
 	log.Info("Stop to process events ...")
 	process.StopAndJoin()
-	
+
 	// connStr := "user=postgres dbname=cryptosoccer"
 	// err := storage.Init(connStr)
 	// if err != nil {
