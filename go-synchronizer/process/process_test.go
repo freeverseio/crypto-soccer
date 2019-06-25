f9e1bab37fd60ba06b7734613e3c23117cb4e9dc
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-25 15:30:35 +0200

    [synchronizer] process test refactoring

--- go-synchronizer/process/process_test.go
+++ go-synchronizer/process/process_test.go
@@ -2,55 +2,22 @@ package process
 
 import (
 	"testing"
-	log "github.com/sirupsen/logrus"
-	"github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
+
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/storage/memory"
-	"math/big"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/testutils"
 )
 
-// TODO : this is pattern to mocking contract !!!!!!
+func TestSyncTeam(t *testing.T) {
+	storage := memory.New()
+	blockchain := testutils.CryptosoccerNew(t)
 
+	Process(blockchain.AssetsContract, storage)
 
-type AssetsMock struct {
-
-}
-
-func AssetsMockNew() *AssetsMock {
-	return &AssetsMock{}
-}
-
-	func (m *AssetsMock) CountTeams(opts *bind.CallOpts) (*big.Int, error) {
-		return big.NewInt(1), nil
-	}
-
-func assert(t *testing.T,err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func XTestInitBase(sto storage.Storage, t *testing.T) {
-	assetsContract := AssetsMockNew()
-	
-	Process(assetsContract, sto)
-
-	count, err := sto.TeamCount()
+	count, err := storage.TeamCount()
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	countTeams, err := assetsContract.CountTeams(nil)
-	if err != nil {
-		log.Fatalf("Failed to retrieve token name: %v", err)
+	if count != 0 {
+		t.Fatalf("Expected 0 received %v", count)
 	}
-
-	if count != countTeams.Uint64() {
-		t.Fatal("team in bc", countTeams, " team in Database", count)
-	}
-}
-
-func TestSyncTeam(t *testing.T) {
-	mem := memory.New()
-	XTestInitBase(mem,t)
 }
