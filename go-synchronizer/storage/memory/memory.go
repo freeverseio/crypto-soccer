8384854073494d91e4b6ab64ee90399cc40e4009
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-25 17:24:48 +0200

    [synchronizer] Team is in storage package

--- go-synchronizer/storage/memory/memory.go
+++ go-synchronizer/storage/memory/memory.go
@@ -1,23 +1,23 @@
 package memory
 
-import "errors"
+import (
+	"errors"
 
-type Team struct {
-	Name string
-}
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
+)
 
 type MemoryStorage struct {
-	teams map[uint64]Team
+	teams map[uint64]storage.Team
 }
 
 func New() *MemoryStorage {
 	return &MemoryStorage{
-		teams: make(map[uint64]Team),
+		teams: make(map[uint64]storage.Team),
 	}
 }
 
 func (m *MemoryStorage) TeamAdd(ID uint64, name string) error {
-	m.teams[ID] = Team{name}
+	m.teams[ID] = storage.Team{name}
 	return nil
 }
 
@@ -25,7 +25,7 @@ func (m *MemoryStorage) TeamCount() (uint64, error) {
 	return uint64(len(m.teams)), nil
 }
 
-func (m *MemoryStorage) GetTeam(id uint64) (Team, error) {
+func (m *MemoryStorage) GetTeam(id uint64) (storage.Team, error) {
 	team := m.teams[id]
 	if team.Name == "" {
 		return team, errors.New("unexistent team")
--- go-synchronizer/storage/storage.go
+++ go-synchronizer/storage/storage.go
@@ -1,6 +1,10 @@
 package storage
 
+type Team struct {
+	Name string
+}
+
 type Storage interface {
 	TeamAdd(ID uint64, name string) error
-	TeamCount() (uint64,error)
+	TeamCount() (uint64, error)
 }
