4eea646dd32089f99bb6244fe3cbec4d6e5dcee2
eaylon <eaylon@gmail.com>
2019-06-28 01:02:36 +0200

    set blocktime to 0 so no waiting for transactions is needed

--- go-synchronizer/scanners/run_ganache.sh
+++ go-synchronizer/scanners/run_ganache.sh
@@ -10,7 +10,7 @@ ganache-cli \
 --mnemonic "much repair shock carbon improve miss forget sock include bullet interest solution" \
 --port ${PORT} \
 --networkId ${NETWORKID} \
---blockTime 1 \
+--blockTime 0 \
 --gasLimit 200000000000 \
 --verbose \
 --account="0xf1b3f8e0d52caec13491368449ab8d90f3d222a3e485aa7f02591bbceb5efba5,1000000000000000000000"
