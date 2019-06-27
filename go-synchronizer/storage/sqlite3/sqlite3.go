4be1807551ea8068f78e0ec57be3a28dea85d4f0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-06-27 13:31:06 +0200

    [synchronizer] ping on creation  sqllite database

--- go-synchronizer/storage/sqlite3/sqlite3.go
+++ go-synchronizer/storage/sqlite3/sqlite3.go
@@ -2,6 +2,7 @@ package sqlite3
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -13,9 +14,12 @@ type Sqlite3 struct {
 func New() (*Sqlite3, error) {
 	var err error
 	storage := Sqlite3{}
-	storage.db, err = sql.Open("sqlite3", "file:test.db?cache=shared&mode=memory")
+	storage.db, err = sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		return nil, err
+	}
+	if err := storage.db.Ping(); err != nil {
+		log.Fatalf("could not ping DB... %v", err)
 	}
 	return &storage, nil
 }
