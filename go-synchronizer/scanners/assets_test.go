1853119ae4030937fc4b6ca587b1316b05408d17
eaylon <eaylon@gmail.com>
2019-06-28 00:21:20 +0200

    Add Ganache.go which encapsulates calls to an eth.client

--- go-synchronizer/scanners/assets_test.go
+++ go-synchronizer/scanners/assets_test.go
@@ -26,3 +26,29 @@ func TestCreateTeam(t *testing.T) {
 		t.Fatalf("Scanning Assets contract with 1 team returned %v events", len(events))
 	}
 }
+
+/*
+func TestGanache(t *testing.T) {
+	ganache := testutils.NewGanache()
+	bob := ganache.CreateAccountWithBalance("1000000000000000000") // 1 eth
+	balance := ganache.GetBalance(testutils.CommonAddressFromPrivateKey(bob))
+	if balance.String() != "1000000000000000000" {
+		t.Fatalf("Funds were not transferred correctly. Transferred balance is %v", balance.Int64())
+	}
+	blocknum := ganache.GetLastBlockNumber()
+	if blocknum == 0 {
+		t.Fatalf("Block number should not be zero")
+	}
+	fmt.Println("block:", blocknum)
+	ganache.DeployContracts(bob)
+
+	alice := ganache.CreateAccountWithBalance("40000000000000000000") // 400 eth
+	ganache.CreateTeam("Barca", alice)
+
+	//events, err := ScanTeamCreated(ganache.Assets, nil)
+	//testutils.AssertNoErr(err)
+	//if len(events) != 1 {
+	//	t.Fatalf("Scanning Assets contract with 1 team returned %v events", len(events))
+	//}
+}
+*/
