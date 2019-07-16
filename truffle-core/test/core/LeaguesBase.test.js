48dff11676cbea179cce83cfcd1fd372c518e9a2
Toni <toni.mateos@gmail.com>
2019-07-16 16:16:06 +0200

    all working up to hashhhh

--- truffle-core/test/core/LeaguesBase.test.js
+++ truffle-core/test/core/LeaguesBase.test.js
@@ -57,19 +57,16 @@ contract('LeaguesBase', (accounts) => {
         counter.toNumber().should.be.equal(2);
     });
 
-return;    
-    
     it('step == 0 is invalid', async () => {
-        const step = 0;
-        await leagues.create(id, initBlock, step, teamIds, tactics).should.be.rejected;
+        await leagues.create(nTeams = 2, initBlock, thisStep = 0).should.be.rejected;
     });
-
+    
     it('count teams', async () => {
-        await leagues.create(id, initBlock, step, teamIds, tactics).should.be.fulfilled;
-        const count = await leagues.getNTeams(id).should.be.fulfilled;
+        await leagues.create(nTeams = 2, initBlock, step).should.be.fulfilled;
+        const count = await leagues.getNTeams(id = 1).should.be.fulfilled;
         count.toNumber().should.be.equal(2);
     });
-
+return;
     it('hash users init data', async () => {
         const hash = await leagues.hashUsersInitData(teamIds, tactics).should.be.fulfilled;
         hash.should.be.equal('0xf8a82ba6630ed0305c4d7718ec5f87567f404ebffc7ddd22a344831368bf4537');
