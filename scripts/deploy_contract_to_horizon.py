1f731faa2bbf131858686a8c3d394063f92c8218
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-08-01 12:02:45 +0200

    [scripts] removed deploy to horizon

--- scripts/deploy_contract_to_horizon.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-os.system('mkdir ../nodejs-horizon/contracts')
-os.system('cp ../truffle-core/build/contracts/Assets.json ../nodejs-horizon/contracts')
-os.system('cp ../truffle-core/build/contracts/Leagues.json ../nodejs-horizon/contracts')
-os.system('cp ../truffle-core/build/contracts/PlayerState.json ../nodejs-horizon/contracts')
