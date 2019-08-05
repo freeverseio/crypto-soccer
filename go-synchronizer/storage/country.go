9fbcf65b062d063c67d2388393373302f70d9817
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-08-05 11:39:15 +0200

    [synchronmizer] implemented CountryAdd and GetCountry

--- go-synchronizer/storage/country.go
+++ go-synchronizer/storage/country.go
@@ -1,6 +1,10 @@
 package storage
 
-import log "github.com/sirupsen/logrus"
+import (
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+)
 
 type Country struct {
 	Id          uint64
@@ -25,5 +29,34 @@ func (b *Storage) CountryCount() (uint64, error) {
 
 func (b *Storage) CountryAdd(country Country) error {
 	log.Infof("[DBMS] Adding team %v", country)
+	_, err := b.db.Exec("INSERT INTO countries (id, name, timezoneUTC) VALUES ($1, $2, $3);",
+		country.Id,
+		country.Name,
+		country.TimezoneUTC,
+	)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func (b *Storage) GetCountry(id uint64) (Country, error) {
+	country := Country{}
+	rows, err := b.db.Query("SELECT id, name, timezoneUTC FROM countries WHERE (id = $1);", id)
+	if err != nil {
+		return country, err
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return country, errors.New("Unexistent country")
+	}
+	err = rows.Scan(
+		&country.Id,
+		&country.Name,
+		&country.TimezoneUTC,
+	)
+	if err != nil {
+		return country, err
+	}
+	return country, nil
 }
--- go-synchronizer/storage/country_test.go
+++ go-synchronizer/storage/country_test.go
@@ -19,3 +19,49 @@ func TestCountryCount(t *testing.T) {
 		t.Fatalf("Expected 0 result %v", count)
 	}
 }
+
+func TestCountryAdd(t *testing.T) {
+	sto, err := storage.NewSqlite3("../sql/00_schema.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	country := storage.Country{
+		Id:          1,
+		Name:        "Spain",
+		TimezoneUTC: 4,
+	}
+	err = sto.CountryAdd(country)
+	if err != nil {
+		t.Fatal(err)
+	}
+	count, err := sto.CountryCount()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 1 {
+		t.Fatalf("Expected 1 result %v", count)
+	}
+}
+
+func TestGetCountry(t *testing.T) {
+	sto, err := storage.NewSqlite3("../sql/00_schema.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	country := storage.Country{
+		Id:          1,
+		Name:        "Spain",
+		TimezoneUTC: 4,
+	}
+	err = sto.CountryAdd(country)
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err := sto.GetCountry(country.Id)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if result != country {
+		t.Fatalf("Expected %v got %v", country, result)
+	}
+}
