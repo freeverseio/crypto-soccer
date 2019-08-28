6e69d0f8cc82d623cba617651dbf1a139c3213c5
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-08-28 16:31:22 +0200

    [market] add player buy order create/delete

--- market/nodejs-market-api/src/mutations.js
+++ market/nodejs-market-api/src/mutations.js
@@ -7,11 +7,18 @@ const MyPlugin = makeExtendSchemaPlugin(build => {
   return {
     typeDefs: gql`
       input PlayerSaleOrderInput {
-        playerId: String!
+        playerId: ID!
+      }
+
+      input PlayerBuyOrderInput {
+        playerId: ID!
       }
 
       extend type Mutation {
-        createPlayerSaleOrder(input: PlayerSaleOrderInput!): Int!
+        createPlayerSaleOrder(input: PlayerSaleOrderInput!): ID
+        deletePlayerSaleOrder(playerId: ID!): ID
+        createPlayerBuyOrder(input: PlayerBuyOrderInput!): ID
+        deletePlayerBuyOrder(playerId: ID!): ID
       }
     `,
     resolvers: {
@@ -22,7 +29,26 @@ const MyPlugin = makeExtendSchemaPlugin(build => {
           const {text, values} = sql.compile(query);
           await context.pgClient.query(text, values);
           return playerId;
-        }
+        },
+        deletePlayerSaleOrder: async (_, {playerId}, context) => {
+          const query = sql.query`DELETE FROM playerSaleOrders WHERE playerId=${sql.value(playerId)}`;
+          const {text, values} = sql.compile(query);
+          await context.pgClient.query(text, values);
+          return playerId;
+        },
+        createPlayerBuyOrder: async (_, {input}, context) => {
+          const { playerId } = input;
+          const query = sql.query`INSERT INTO playerBuyOrders (playerId) VALUES (${sql.value(playerId)})`;
+          const {text, values} = sql.compile(query);
+          await context.pgClient.query(text, values);
+          return playerId;
+        },
+        deletePlayerBuyOrder: async (_, {playerId}, context) => {
+          const query = sql.query`DELETE FROM playerBuyOrders WHERE playerId=${sql.value(playerId)}`;
+          const {text, values} = sql.compile(query);
+          await context.pgClient.query(text, values);
+          return playerId;
+        },
       }
     }
   };
