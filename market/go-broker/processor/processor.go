701b5c8629b1f7873b7a8f3243550b85db9169e7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-01 13:32:01 +0200

    [market] processor has a reference to storage

--- market/go-broker/processor/processor.go
+++ market/go-broker/processor/processor.go
@@ -1,16 +1,19 @@
 package processor
 
 import (
+	"github.com/freeverseio/crypto-soccer/market/go-broker/storage"
 	log "github.com/sirupsen/logrus"
 )
 
 type Processor struct {
+	db *storage.Storage
 }
 
-func NewProcessor() *Processor {
-	return &Processor{}
+func NewProcessor(db *storage.Storage) *Processor {
+	return &Processor{db}
 }
 
 func (b *Processor) Process() {
 	log.Info("Processing")
+
 }
--- market/go-broker/processor/processor_test.go
+++ market/go-broker/processor/processor_test.go
@@ -4,9 +4,14 @@ import (
 	"testing"
 
 	"github.com/freeverseio/crypto-soccer/market/go-broker/processor"
+	"github.com/freeverseio/crypto-soccer/market/go-broker/storage"
 )
 
 func TestProcess(t *testing.T) {
-	processor := processor.NewProcessor()
+	sto, err := storage.NewSqlite3("../../sql/00_schema.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	processor := processor.NewProcessor(sto)
 	processor.Process()
 }
