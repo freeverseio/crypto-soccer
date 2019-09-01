02932044b4c29a2a65399185db9f5b7b749a23a5
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-01 10:41:26 +0200

    [broker] added functions to create sell orders

--- market/go-broker/storage/selloffer.go
+++ market/go-broker/storage/selloffer.go
@@ -1,8 +1,19 @@
 package storage
 
+import log "github.com/sirupsen/logrus"
+
 type SellOffer struct {
 	PlayerId uint64
 	Price    uint64
+}
+
+func (b *Storage) CreateSellOffer(offer SellOffer) error {
+	log.Infof("[DBMS] + create sell offer %v", offer)
+	_, err := b.db.Exec("INSERT INTO player_sell_orders (playerId, price) VALUES ($1, $2);",
+		offer.PlayerId,
+		offer.Price,
+	)
+	return err
 }
 
 func (b *Storage) GetSellOfferts() ([]SellOffer, error) {
@@ -24,4 +35,12 @@ func (b *Storage) GetSellOfferts() ([]SellOffer, error) {
 		offers = append(offers, offer)
 	}
 	return offers, nil
+}
+
+func (b *Storage) DeleteSellOffer(playerId uint64) error {
+	log.Infof("[DBMS] Delete sell offer %v", playerId)
+	_, err := b.db.Exec("DELETE FROM player_sell_orders WHERE (playerId == '$1');",
+		playerId,
+	)
+	return err
 }
--- market/go-broker/storage/selloffer_test.go
+++ market/go-broker/storage/selloffer_test.go
@@ -18,4 +18,15 @@ func TestGetSellOffers(t *testing.T) {
 	if len(result) != 0 {
 		t.Fatalf("Expected 0 got %v", len(result))
 	}
+	err = sto.CreateSellOffer(storage.SellOffer{1, 100})
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err = sto.GetSellOfferts()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(result) != 1 {
+		t.Fatalf("Expected 1 got %v", len(result))
+	}
 }
