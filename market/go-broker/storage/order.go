f8326e6fd7a95f92c1f155b703cf8c5da4dc96d2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-01 11:58:44 +0200

    [market] added storage orders

---
+++ market/go-broker/storage/order.go
@@ -0,0 +1,41 @@
+package storage
+
+import log "github.com/sirupsen/logrus"
+
+type Order struct {
+	SellOrder SellOrder
+	BuyOrder  BuyOrder
+}
+
+func (b *Storage) GetOrders() ([]Order, error) {
+	var orders []Order
+	rows, err := b.db.Query("SELECT player_sell_orders.playerId, player_sell_orders.price, player_buy_orders.playerId, player_buy_orders.price FROM player_sell_orders LEFT JOIN player_buy_orders;")
+	if err != nil {
+		return orders, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var order Order
+		err = rows.Scan(
+			&order.SellOrder.PlayerId,
+			&order.SellOrder.Price,
+			&order.BuyOrder.PlayerId,
+			&order.BuyOrder.Price,
+		)
+		if err != nil {
+			return orders, err
+		}
+		orders = append(orders, order)
+	}
+	return orders, nil
+}
+
+func (b *Storage) DeleteOrder(playerId uint64) error {
+	log.Infof("[DBMS] + create order %v", playerId)
+	err := b.DeleteBuyOrder(playerId)
+	if err != nil {
+		return err
+	}
+	err = b.DeleteSellOrder(playerId)
+	return err
+}
---
+++ market/go-broker/storage/order_test.go
@@ -0,0 +1,37 @@
+package storage_test
+
+import (
+	"testing"
+
+	"github.com/freeverseio/crypto-soccer/market/go-broker/storage"
+)
+
+func TestGetOrders(t *testing.T) {
+	sto, err := storage.NewSqlite3("../../sql/00_schema.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err := sto.GetOrders()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(result) != 0 {
+		t.Fatalf("Expected 0 got %v", len(result))
+	}
+
+	err = sto.CreateSellOrder(storage.SellOrder{1, 1000})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = sto.CreateBuyOrder(storage.BuyOrder{1, 1000})
+	if err != nil {
+		t.Fatal(err)
+	}
+	result, err = sto.GetOrders()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(result) != 1 {
+		t.Fatalf("Expected 1 got %v", len(result))
+	}
+}
