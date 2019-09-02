947be0b3ef22378919c5f0c8f6b2bbffaf18df90
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-02 15:17:52 +0200

    [market] processing loop and cli params

--- market/go-broker/main.go
+++ market/go-broker/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"time"
 
+	"github.com/freeverseio/crypto-soccer/market/go-broker/processor"
+	"github.com/freeverseio/crypto-soccer/market/go-broker/storage"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	inMemoryDatabase := flag.Bool("memory", false, "use in memory database")
+	postgresURL := flag.String("postgres", "postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable", "postgres url")
+	ethereumClient := flag.String("ethereum_client", "https://devnet.busyverse.com/web3", "ethereum node")
+	assetsContractAddress := flag.String("assets_address", "0x12c3430840788f3a0660445D2fE1269A2E0C188A", "assets contract address")
+	debug := flag.Bool("debug", false, "print debug logs")
+	flag.Parse()
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	var err error
+	var sto *storage.Storage
+	if *inMemoryDatabase {
+		log.Warning("Using in memory DBMS (no persistence)")
+		sto, err = storage.NewSqlite3("../sql/00_schema.sql")
+	} else {
+		log.Info("Connecting to DBMS: ", *postgresURL)
+		sto, err = storage.NewPostgres(*postgresURL)
+	}
+	if err != nil {
+		log.Fatalf("Failed to connect to DBMS: %v", err)
+	}
+
+	processor, err := processor.NewProcessor(sto, *ethereumClient, *assetsContractAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		processor.Process()
+		time.Sleep(2 * time.Second)
+	}
 }
