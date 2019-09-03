88677a93efa3d5dd5c89f19a03b59330818ebce4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-03 09:55:25 +0200

    [market] new assets address

--- market/go-broker/main.go
+++ market/go-broker/main.go
@@ -14,7 +14,7 @@ func main() {
 	inMemoryDatabase := flag.Bool("memory", false, "use in memory database")
 	postgresURL := flag.String("postgres", "postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable", "postgres url")
 	ethereumClient := flag.String("ethereum_client", "https://devnet.busyverse.com/web3", "ethereum node")
-	assetsContractAddress := flag.String("assets_address", "0x12c3430840788f3a0660445D2fE1269A2E0C188A", "assets contract address")
+	assetsContractAddress := flag.String("assets_address", "0x893D66E9eBd8Db7cb4d565f4e135fc87D2547696", "assets contract address")
 	debug := flag.Bool("debug", false, "print debug logs")
 	flag.Parse()
 
