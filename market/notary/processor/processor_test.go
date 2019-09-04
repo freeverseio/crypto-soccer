153e70fe80afa52a4a759079f47cd80da970c528
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-04 19:58:31 +0200

    [market.notary] test of ownership is failing

--- market/notary/processor/processor_test.go
+++ market/notary/processor/processor_test.go
@@ -6,8 +6,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/freeverseio/crypto-soccer/market/notary/processor"
-	"github.com/freeverseio/crypto-soccer/market/notary/storage"
 	"github.com/freeverseio/crypto-soccer/market/notary/testutils"
 )
 
@@ -17,7 +15,7 @@ func TestChangeOwnership(t *testing.T) {
 	ganache.DeployContracts(owner)
 
 	alice := ganache.CreateAccountWithBalance("50000000000000000000") // 50 eth
-	// bob := ganache.CreateAccountWithBalance("50000000000000000000")   // 50 eth
+	bob := ganache.CreateAccountWithBalance("50000000000000000000")   // 50 eth
 
 	_, err := ganache.Assets.CreateTeam(
 		bind.NewKeyedTransactor(owner),
@@ -36,7 +34,7 @@ func TestChangeOwnership(t *testing.T) {
 	_, err = ganache.Assets.CreateTeam(
 		bind.NewKeyedTransactor(owner),
 		"Madrid",
-		crypto.PubkeyToAddress(alice.PublicKey))
+		crypto.PubkeyToAddress(bob.PublicKey))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -49,7 +47,7 @@ func TestChangeOwnership(t *testing.T) {
 	}
 
 	_, err = ganache.Assets.TransferPlayer(
-		nil,
+		bind.NewKeyedTransactor(alice),
 		big.NewInt(1),
 		big.NewInt(2))
 	if err != nil {
@@ -57,31 +55,31 @@ func TestChangeOwnership(t *testing.T) {
 	}
 }
 
-func TestProcess(t *testing.T) {
-	sto, err := storage.NewSqlite3("../../db/00_schema.sql")
-	if err != nil {
-		t.Fatal(err)
-	}
-	ganache := testutils.NewGanache()
-	owner := ganache.CreateAccountWithBalance("1000000000000000000") // 1 eth
-	ganache.DeployContracts(owner)
+// func TestProcess(t *testing.T) {
+// 	sto, err := storage.NewSqlite3("../../db/00_schema.sql")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	ganache := testutils.NewGanache()
+// 	owner := ganache.CreateAccountWithBalance("1000000000000000000") // 1 eth
+// 	ganache.DeployContracts(owner)
 
-	processor, err := processor.NewProcessor(sto, ganache.Client, ganache.Assets, owner)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	processor, err := processor.NewProcessor(sto, ganache.Client, ganache.Assets, owner)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	alice := ganache.CreateAccountWithBalance("50000000000000000000") // 50 eth
-	bob := ganache.CreateAccountWithBalance("50000000000000000000")   // 50 eth
+// 	alice := ganache.CreateAccountWithBalance("50000000000000000000") // 50 eth
+// 	bob := ganache.CreateAccountWithBalance("50000000000000000000")   // 50 eth
 
-	_, err = ganache.Assets.CreateTeam(
-		bind.NewKeyedTransactor(owner),
-		"Barca",
-		crypto.PubkeyToAddress(alice.PublicKey))
-	// ganache.CreateTeam("Barca", alice)
-	ganache.CreateTeam("Madrid", bob)
+// 	_, err = ganache.Assets.CreateTeam(
+// 		bind.NewKeyedTransactor(owner),
+// 		"Barca",
+// 		crypto.PubkeyToAddress(alice.PublicKey))
+// 	// ganache.CreateTeam("Barca", alice)
+// 	ganache.CreateTeam("Madrid", bob)
 
-	sto.CreateSellOrder(storage.SellOrder{1, 100})
-	sto.CreateBuyOrder(storage.BuyOrder{1, 100, 2})
-	processor.Process()
-}
+// 	sto.CreateSellOrder(storage.SellOrder{1, 100})
+// 	sto.CreateBuyOrder(storage.BuyOrder{1, 100, 2})
+// 	processor.Process()
+// }
