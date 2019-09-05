e5fd5f7be299ee24bdb0d04e5a58968d40e4b165
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-05 13:49:29 +0200

    [deploy] new contracts

--- market/notary/main.go
+++ market/notary/main.go
@@ -18,7 +18,7 @@ func main() {
 	inMemoryDatabase := flag.Bool("memory", false, "use in memory database")
 	postgresURL := flag.String("postgres", "postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable", "postgres url")
 	ethereumClient := flag.String("ethereum_client", "https://devnet.busyverse.com/web3", "ethereum node")
-	assetsContractAddress := flag.String("assets_address", "0x893D66E9eBd8Db7cb4d565f4e135fc87D2547696", "assets contract address")
+	assetsContractAddress := flag.String("assets_address", "0x83f7082312A668C6da08f9608C378D4726aB582D", "assets contract address")
 	debug := flag.Bool("debug", false, "print debug logs")
 	flag.Parse()
 
