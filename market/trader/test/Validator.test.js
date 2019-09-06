20c5fb74ee27c5e8a3eb27e1b8b269c56a598823
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-06 11:53:35 +0200

    [market.trader] deterministic signatire in tests

--- market/trader/test/Validator.test.js
+++ market/trader/test/Validator.test.js
@@ -6,9 +6,11 @@ require('chai')
 const Accounts = require('web3-eth-accounts');
 
 describe('Validator', () => {
+    const privateKey = '0x0a69684608770d018143dd70dc5dc5b6beadc366b87e45fcb567fc09407e7fe5';
+
     it('get the signer address by signatureObject', () => {
         var accounts = new Accounts();
-        const account = accounts.create("iamaseller");
+        const account = accounts.privateKeyToAccount(privateKey);
         const msg = "ciao";
         const hashedMsg = account.sign(msg);
         var address = accounts.recover(hashedMsg);
@@ -17,10 +19,11 @@ describe('Validator', () => {
 
     it('get the signer address by message and signature', () => {
         var accounts = new Accounts();
-        const account = accounts.create("iamaseller");
+        const account = accounts.privateKeyToAccount(privateKey);
         const msg = "ciao";
         const hashedMsg = account.sign(msg);
         address = accounts.recover(msg, hashedMsg.signature);
         address.should.be.equal(account.address);
+        hashedMsg.signature.should.be.equal('0xcf0a59da3b50f2827d9b15fc83391cd5feaf9b25131c2f4f20e7ae2d4fba811b41f35b6b17ba566c38a5c3737a759018be1f9064b7c8f56daaf4c00e51c7df281b');
     });
 }) 
