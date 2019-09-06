dc94c37985c32dcecbc35c134a92030024ebc405
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-06 09:34:59 +0200

    [market.trader] added test infrastructure

---
+++ market/trader/test/Validator.test.js
@@ -0,0 +1,11 @@
+const BN = require('bn.js');
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bn')(BN))
+    .should();
+
+describe('Validator', () => {
+    it('create signature of "ciao"', async () => {
+
+    });
+}) 
