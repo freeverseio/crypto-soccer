426f80d1a0f2302443719757dd09fd1cb50c8aa4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-06 12:05:32 +0200

    [market.trader] validator can recover signer's address

--- market/trader/src/Validator.js
+++ market/trader/src/Validator.js
@@ -1,3 +1,13 @@
+const Accounts = require('web3-eth-accounts');
 
 class Validator {
+    constructor() {
+        this.accounts = new Accounts();
+    }
+
+    recoverSignerAddress(msg, signature) {
+        return this.accounts.recover(msg, signature);
+    }
 }
+
+module.exports = Validator;
--- market/trader/test/Validator.test.js
+++ market/trader/test/Validator.test.js
@@ -4,26 +4,37 @@ require('chai')
     .use(require('chai-bn')(BN))
     .should();
 const Accounts = require('web3-eth-accounts');
+const Validator = require('../src/Validator');
 
 describe('Validator', () => {
     const privateKey = '0x0a69684608770d018143dd70dc5dc5b6beadc366b87e45fcb567fc09407e7fe5';
 
     it('get the signer address by signatureObject', () => {
-        var accounts = new Accounts();
+        const accounts = new Accounts();
         const account = accounts.privateKeyToAccount(privateKey);
         const msg = "ciao";
         const hashedMsg = account.sign(msg);
-        var address = accounts.recover(hashedMsg);
+        const address = accounts.recover(hashedMsg);
         address.should.be.equal(account.address);
     });
 
     it('get the signer address by message and signature', () => {
-        var accounts = new Accounts();
+        const accounts = new Accounts();
         const account = accounts.privateKeyToAccount(privateKey);
         const msg = "ciao";
         const hashedMsg = account.sign(msg);
         address = accounts.recover(msg, hashedMsg.signature);
         address.should.be.equal(account.address);
         hashedMsg.signature.should.be.equal('0xcf0a59da3b50f2827d9b15fc83391cd5feaf9b25131c2f4f20e7ae2d4fba811b41f35b6b17ba566c38a5c3737a759018be1f9064b7c8f56daaf4c00e51c7df281b');
+    });
+
+    it('get signer address', () => {
+        const validator = new Validator();
+        const accounts = new Accounts();
+        const account = accounts.privateKeyToAccount(privateKey);
+        const msg = "ciao";
+        const hashedMsg = account.sign(msg);
+        const address = validator.recoverSignerAddress(msg, hashedMsg.signature);
+        address.should.be.equal(account.address);
     });
 }) 
