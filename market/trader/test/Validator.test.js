49ee61df823d335e379caa4b3f55f62b44d3567b
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-06 11:37:11 +0200

    [market.trader] checke signing worflow

--- market/trader/test/Validator.test.js
+++ market/trader/test/Validator.test.js
@@ -3,9 +3,14 @@ require('chai')
     .use(require('chai-as-promised'))
     .use(require('chai-bn')(BN))
     .should();
+const Accounts = require('web3-eth-accounts');
 
 describe('Validator', () => {
     it('create signature of "ciao"', async () => {
-        
+        var accounts = new Accounts();
+        const account = await accounts.create("iamaseller");
+        const hashedMsg = account.sign("ciao");
+        const address = accounts.recover(hashedMsg);
+        address.should.be.equal(account.address);
     });
 }) 
