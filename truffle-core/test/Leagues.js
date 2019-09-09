2afce57fc9ca8f5fd907b3ad866c871422eb66bd
Toni <toni.mateos@gmail.com>
2019-09-09 11:08:32 +0200

    first league scheduler complete

--- truffle-core/test/Leagues.js
+++ truffle-core/test/Leagues.js
@@ -9,7 +9,6 @@ const Leagues = artifacts.require('Leagues');
 
 contract('Leagues', (accounts) => {
     let leagues = null;
-    let encoding = null;
     let TEAMS_PER_LEAGUE = null;
     let MATCHDAYS = null;
     let MATCHES_PER_DAY = null;
@@ -27,6 +26,34 @@ contract('Leagues', (accounts) => {
         MATCHDAYS.toNumber().should.be.equal(14);
         MATCHES_PER_DAY.toNumber().should.be.equal(4);
         TEAMS_PER_LEAGUE.toNumber().should.be.equal(8);
+    });
+
+    it('get teams for match in wrong day', async () => {
+        matchIdxInDay = 0; 
+        day = MATCHDAYS-1; 
+        await leagues.getTeamsInMatch(day, matchIdxInDay).should.be.fulfilled;
+        day = MATCHDAYS; 
+        await leagues.getTeamsInMatch(day, matchIdxInDay).should.be.rejected;
+    });
+
+    it('get teams for match in wrong match in day', async () => {
+        day = 0;
+        matchIdxInDay = MATCHES_PER_DAY-1;
+        await leagues.getTeamsInMatch(day, matchIdxInDay).should.be.fulfilled;
+        matchIdxInDay = MATCHES_PER_DAY;
+        await leagues.getTeamsInMatch(day, matchIdxInDay).should.be.rejected;
+    });
+
+    it('get teams for match in league day', async () => {
+        day = 0;
+        matchIdxInDay = 0;
+        teams = await leagues.getTeamsInMatch(day, matchIdxInDay).should.be.fulfilled;
+        teams[0].toNumber().should.be.equal(0);
+        teams[1].toNumber().should.be.equal(1);
+        day = Math.floor(MATCHDAYS/2);
+        teams = await leagues.getTeamsInMatch(day, matchIdxInDay).should.be.fulfilled;
+        teams[0].toNumber().should.be.equal(1);
+        teams[1].toNumber().should.be.equal(0);
     });
 
 });
