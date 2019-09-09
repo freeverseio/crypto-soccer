f799d83b138fbd97e4931787fd23bdffab47f4db
Toni <toni.mateos@gmail.com>
2019-09-09 10:59:12 +0200

    starting for fill League

--- truffle-core/contracts/Leagues.sol
+++ truffle-core/contracts/Leagues.sol
@@ -3,6 +3,38 @@ pragma solidity >=0.4.21 <0.6.0;
 import "./Assets.sol";
 
 contract Leagues is Assets {
+    
+    uint8 constant public MATCHDAYS = 14;
+    uint8 constant public MATCHES_PER_DAY = 4;
+    
+    function getTeamsInMatch(uint8 matchday, uint8 matchIdxInDay) public pure returns (uint8 homeIdx, uint8 visitorIdx) 
+    {
+        require(matchday < MATCHDAYS, "wrong match day");
+        require(matchIdxInDay < MATCHES_PER_DAY, "wrong match");
+        if (matchday < (TEAMS_PER_LEAGUE - 1))
+            (homeIdx, visitorIdx) = _getTeamsInMatchFirstHalf(matchday, matchIdxInDay);
+        else
+            (visitorIdx, homeIdx) = _getTeamsInMatchFirstHalf(matchday - (TEAMS_PER_LEAGUE - 1), matchIdxInDay);
+    }
 
+    function _shiftBack(uint8 t) private pure returns (uint8)
+    {
+        if (t < TEAMS_PER_LEAGUE)
+            return t;
+        else
+            return t-(TEAMS_PER_LEAGUE-1);
+    }
 
+    function _getTeamsInMatchFirstHalf(uint8 matchday, uint8 matchIdxInDay) private pure returns (uint8, uint8) 
+    {
+        uint8 team1 = 0;
+        if (matchIdxInDay > 0)
+            team1 = _shiftBack(TEAMS_PER_LEAGUE-matchIdxInDay+matchday);
+
+        uint8 team2 = _shiftBack(matchIdxInDay+1+matchday);
+        if ( (matchday % 2) == 0)
+            return (team1, team2);
+        else
+            return (team2, team1);
+    }
 }
--- truffle-core/test/Leagues.js
+++ truffle-core/test/Leagues.js
@@ -10,38 +10,23 @@ const Leagues = artifacts.require('Leagues');
 contract('Leagues', (accounts) => {
     let leagues = null;
     let encoding = null;
-    let PLAYERS_PER_TEAM_MAX = null;
-    let PLAYERS_PER_TEAM_INIT = null;
-    let LEAGUES_PER_DIV = null;
     let TEAMS_PER_LEAGUE = null;
-    let FREE_PLAYER_ID = null;
-    let NULL_ADDR = null;
-    const ALICE = accounts[1];
-    const BOB = accounts[2];
-    const CAROL = accounts[3];
-    const N_SKILLS = 5;
+    let MATCHDAYS = null;
+    let MATCHES_PER_DAY = null;
 
     beforeEach(async () => {
         leagues = await Leagues.new().should.be.fulfilled;
         encoding = leagues;
         await leagues.init().should.be.fulfilled;
-        PLAYERS_PER_TEAM_INIT = await leagues.PLAYERS_PER_TEAM_INIT().should.be.fulfilled;
-        PLAYERS_PER_TEAM_MAX = await leagues.PLAYERS_PER_TEAM_MAX().should.be.fulfilled;
-        LEAGUES_PER_DIV = await leagues.LEAGUES_PER_DIV().should.be.fulfilled;
         TEAMS_PER_LEAGUE = await leagues.TEAMS_PER_LEAGUE().should.be.fulfilled;
-        FREE_PLAYER_ID = await leagues.FREE_PLAYER_ID().should.be.fulfilled;
-        NULL_ADDR = await leagues.NULL_ADDR().should.be.fulfilled;
-        PLAYERS_PER_TEAM_INIT = PLAYERS_PER_TEAM_INIT.toNumber();
-        PLAYERS_PER_TEAM_MAX = PLAYERS_PER_TEAM_MAX.toNumber();
-        LEAGUES_PER_DIV = LEAGUES_PER_DIV.toNumber();
-        TEAMS_PER_LEAGUE = TEAMS_PER_LEAGUE.toNumber();
+        MATCHDAYS = await leagues.MATCHDAYS().should.be.fulfilled;
+        MATCHES_PER_DAY = await leagues.MATCHES_PER_DAY().should.be.fulfilled;
         });
 
-    it('check initial and max number of players per team', async () =>  {
-        PLAYERS_PER_TEAM_INIT.should.be.equal(18);
-        PLAYERS_PER_TEAM_MAX.should.be.equal(25);
-        LEAGUES_PER_DIV.should.be.equal(16);
-        TEAMS_PER_LEAGUE.should.be.equal(8);
+    it('check initial constants', async () =>  {
+        MATCHDAYS.toNumber().should.be.equal(14);
+        MATCHES_PER_DAY.toNumber().should.be.equal(4);
+        TEAMS_PER_LEAGUE.toNumber().should.be.equal(8);
     });
 
 });
