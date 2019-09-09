8e4b91eee3a8dce5f62b1c722588a68cff05c498
Toni <toni.mateos@gmail.com>
2019-09-09 11:42:02 +0200

    adding tests to Engine

--- truffle-core/test/Engine.js
+++ truffle-core/test/Engine.js
@@ -9,17 +9,53 @@ const Engine = artifacts.require('Engine');
 
 contract('Engine', (accounts) => {
     let engine = null;
+    let encoding = null;
+    let teamStateAll50 = null;
+    const seed = 610106;
+    const tactic0 = 0; // 442
+    const tactic1 = 1; // 541
+
+    const createTeamStateFromSinglePlayer = async (skills, encoding) => {
+        const playerStateTemp = await encoding.encodePlayerSkills(
+            skills, 
+            monthOfBirth = 0, 
+            playerId = 1
+        ).should.be.fulfilled;
+
+        teamState = []
+        for (player = 0; player < 11; player++) {
+            teamState.push(playerStateTemp)
+        }
+        return teamState;
+    };
+    
 
     beforeEach(async () => {
         engine = await Engine.new().should.be.fulfilled;
         encoding = engine;
         await engine.init().should.be.fulfilled;
+        teamStateAll50 = await createTeamStateFromSinglePlayer([50, 50, 50, 50, 50], encoding);
         });
 
-    it('check initial constants', async () =>  {
-        // MATCHDAYS.toNumber().should.be.equal(14);
-        // MATCHES_PER_DAY.toNumber().should.be.equal(4);
-        // TEAMS_PER_LEAGUE.toNumber().should.be.equal(8);
+    it('teams get tired', async () => {
+        const result = await engine.teamsGetTired([10,20,30,40,100], [20,40,60,80,50]).should.be.fulfilled;
+        result[0][0].toNumber().should.be.equal(10);
+        result[0][1].toNumber().should.be.equal(20);
+        result[0][2].toNumber().should.be.equal(30);
+        result[0][3].toNumber().should.be.equal(40);
+        result[0][4].toNumber().should.be.equal(100);
+        result[1][0].toNumber().should.be.equal(10);
+        result[1][1].toNumber().should.be.equal(20);
+        result[1][2].toNumber().should.be.equal(30);
+        result[1][3].toNumber().should.be.equal(40);
+        result[1][4].toNumber().should.be.equal(50);
+    });
+
+    it('play a match', async () => {
+        let teamStateAll1 = await createTeamStateFromSinglePlayer([1,1,1,1,1], encoding);
+        const result = await engine.playMatch(seed, teamStateAll50, teamStateAll1, tactic0, tactic1).should.be.fulfilled;
+        result[0].toNumber().should.be.equal(17);
+        result[1].toNumber().should.be.equal(0);
     });
 
 
