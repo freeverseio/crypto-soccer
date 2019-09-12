44324d9fe04d8647cf51cc736928d5b712cf9952
Toni <toni.mateos@gmail.com>
2019-09-12 16:44:17 +0200

    adding first updateTZ test

---
+++ truffle-core/contracts/Updates.sol
@@ -0,0 +1,136 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "./Encoding.sol";
+import "./Assets.sol";
+ 
+/// teamId == 0 is invalid and represents the null team
+/// TODO: fix the playerPos <=> playerShirt doubt
+contract Updates is Encoding {
+    event TeamTransfer(uint256 teamId, address to);
+    event ActionsSubmission(uint8 timeZone, bytes32 seed, uint256 submissionTime);
+    event TimeZoneUpdate(uint8 timeZone, bytes32 root, uint256 submissionTime);
+
+    uint16 constant public SECS_BETWEEN_VERSES = 900; // 15 mins
+    uint8 constant VERSES_PER_DAY = 96; // 24 * 4
+    uint16 constant VERSES_PER_ROUND = 1536; // 96 * 16
+    uint8 constant NULL_TIMEZONE = 0;
+    uint8 constant CHALLENGE_TIME = 60; // in secs
+    
+    uint256 public nextVerseTimestamp;
+    uint8 public timeZoneForRound1;
+    uint256 public currentVerse;
+    bytes32 private _currentVerseSeed;
+    bool private _needsInitUpdates = true;
+
+    Assets private _assets;
+
+    function initUpdates(address addr) public {
+        require(_needsInitUpdates == true, "cannot initialize twice");
+        _setAssetsAdress(addr);
+        // the game starts at verse = 0. The transition to verse = 1 will be at the next exact hour.
+        // that will be the begining of Round = 1. So Round 1 starts at some timezone that depends on
+        // the call to the contract init() function.
+        uint256 secsOfDay   = now % (3600 * 24);
+        uint256 hour        = secsOfDay / 3600;  // 0, ..., 23
+        uint256 minute      = (secsOfDay - hour * 3600) / 60; // 0, ..., 59
+        uint256 secs        = (secsOfDay - hour * 3600 - minute * 60); // 0, ..., 59
+        if (minute < 42) {
+            timeZoneForRound1 = 1 + uint8(hour);
+            nextVerseTimestamp = now + (44-minute)*60 + (60 - secs);
+        } else {
+            timeZoneForRound1 = 2 + uint8(hour);
+            nextVerseTimestamp = now + (44-minute)*60 + (60 - secs) + 3600;
+        }
+        _needsInitUpdates = false;
+    }
+ 
+    function _setAssetsAdress(address addr) private {
+        _assets = Assets(addr);
+    }
+
+    function getNow() public view returns(uint256) {
+        return now;
+    }
+
+    function incrementVerse() private {
+        currentVerse += 1;
+        nextVerseTimestamp += SECS_BETWEEN_VERSES;
+    }
+    
+    function submitActionsRoot(bytes32 actionsRoot) public {
+        require(now > nextVerseTimestamp, "too early to accept actions root");
+        (uint8 newTZ,,) = nextTimeZoneToUpdate();
+        (uint8 prevTz,,) = prevTimeZoneToUpdate();
+        // make sure the last verse is settled
+        if (prevTz != NULL_TIMEZONE) {
+            require(now > _assets.getLastUpdateTime(prevTz)+ CHALLENGE_TIME, "last verse is still under challenge period");
+        }
+        // if we are precisely at a moment where nothing needs to be done, just move ahead
+        if (newTZ == NULL_TIMEZONE) {
+            incrementVerse() ;
+            emit ActionsSubmission(NULL_TIMEZONE, 0, now);
+            return;
+        }
+        _assets.setActionsRoot(newTZ, actionsRoot);
+        incrementVerse() ;
+        setCurrentVerseSeed(blockhash(block.number-1)); 
+        emit ActionsSubmission(newTZ, blockhash(block.number-1), now);
+    }
+
+    function updateTZ(bytes32 root) public {
+        (uint8 tz,,) = prevTimeZoneToUpdate();
+        require(tz != NULL_TIMEZONE, "nothing to update in the current timeZone");
+        uint256 lastUpdate = _assets.getLastUpdateTime(tz);
+        uint256 lastActionsSubmissionTime = _assets.getLastActionsSubmissionTime(tz);
+        if (lastUpdate > lastActionsSubmissionTime) {
+            require(now < lastUpdate + CHALLENGE_TIME, "challenging period is already over for this timezone");
+        } else {
+            require(now < lastActionsSubmissionTime + CHALLENGE_TIME, "challenging period is already over for this timezone");
+        }
+        _assets.setSkillsRoot(tz, root);
+        emit TimeZoneUpdate(tz, root, now);
+    }
+    
+    // each day has 24 hours, each with 4 verses => 96 verses per day.
+    // day = 1,..16
+    // turnInDay = 0, 1, 2, 3
+    // so for each TZ, we go from (day, turn) = (1, 0) ... (15,3) => a total of 16*4 = 64 turns per timeZone
+    // from these, all map easily to timeZones
+    function nextTimeZoneToUpdate() public view returns (uint8 timeZone, uint8 day, uint8 turnInDay) {
+        return _timeZoneToUpdatePure(currentVerse, timeZoneForRound1);
+    }
+
+    function prevTimeZoneToUpdate() public view returns (uint8 timeZone, uint8 day, uint8 turnInDay) {
+        if (currentVerse == 0) {
+            return (NULL_TIMEZONE, 0, 0);
+        }
+        return _timeZoneToUpdatePure(currentVerse - 1, timeZoneForRound1);
+    }
+
+    function _timeZoneToUpdatePure(uint256 verse, uint8 TZForRound1) public pure returns (uint8 timeZone, uint8 day, uint8 turnInDay) {
+        // if currentVerse = 0, we should be updating timeZoneForRound1
+        // recall that timeZones range from 1...24 (not from 0...24)
+        uint16 verseInRound = uint16(verse % VERSES_PER_ROUND);
+        if (verseInRound < VERSES_PER_DAY) {
+            timeZone = 1 + uint8((TZForRound1 - 1 + (verseInRound / 4))% 24);
+            day = 1;
+            turnInDay = uint8(verseInRound % 4);
+        } else if (verseInRound == VERSES_PER_DAY) {
+            timeZone = NULL_TIMEZONE;
+        } else {
+            timeZone = 1 + uint8((TZForRound1 - 1 + ((verseInRound - 1) / 4))% 24);
+            day = 1 + uint8((verseInRound - 1) / VERSES_PER_DAY);
+            turnInDay = uint8((verseInRound - 1) % 4);
+        }
+    }
+
+    function setCurrentVerseSeed(bytes32 seed) public {
+        _currentVerseSeed = seed;
+    }
+        
+    function getCurrentVerseSeed() public view returns (bytes32) {
+        return _currentVerseSeed;
+    }
+        
+
+}
---
+++ truffle-core/test/Updates.js
@@ -0,0 +1,164 @@
+const BN = require('bn.js');
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bn')(BN))
+    .should();
+const truffleAssert = require('truffle-assertions');
+const timeTravel = require('../utils/TimeTravel.js');
+
+const Assets = artifacts.require('Assets');
+const Updates = artifacts.require('Updates');
+
+contract('Updates', (accounts) => {
+    const VERSES_PER_DAY = 96;
+    const VERSES_PER_ROUND = 96*16;
+   
+    beforeEach(async () => {
+        assets = await Assets.new().should.be.fulfilled;
+        encoding = assets;
+        await assets.init().should.be.fulfilled;
+        updates = await Updates.new().should.be.fulfilled;
+        await updates.initUpdates(assets.address).should.be.fulfilled;
+        snapShot = await timeTravel.takeSnapshot();
+        snapshotId = snapShot['result'];
+        });
+
+    afterEach(async() => {
+        await timeTravel.revertToSnapShot(snapshotId);
+    });
+            
+
+    it('check BC has the correct time', async () =>  {
+        nextVerseTimestamp = await updates.nextVerseTimestamp().should.be.fulfilled;
+        timeZoneForRound1 = await updates.timeZoneForRound1().should.be.fulfilled;
+        nextVerse = new Date(nextVerseTimestamp.toNumber() * 1000)
+        now = new Date()
+        if (now.getUTCMinutes() < 42) {
+            expectedHour = now.getUTCHours();
+        } else {
+            expectedHour = now.getUTCHours() + 1;
+        }
+        nextVerse.getUTCFullYear().should.be.equal(now.getUTCFullYear())
+        nextVerse.getUTCMonth().should.be.equal(now.getUTCMonth())
+        nextVerse.getUTCDate().should.be.equal(now.getUTCDate())
+        nextVerse.getUTCHours().should.be.equal(expectedHour)
+        nextVerse.getUTCMinutes().should.be.equal(45)
+        nextVerse.getUTCSeconds().should.be.equal(0)
+        timeZoneForRound1.toNumber().should.be.equal(1 + expectedHour)
+    });
+    
+    it('wait some minutes', async () =>  {
+        now = await updates.getNow().should.be.fulfilled;
+        block = await web3.eth.getBlockNumber().should.be.fulfilled;
+        extraTime = 3*60
+        await timeTravel.advanceTime(extraTime).should.be.fulfilled;
+        await timeTravel.advanceBlock().should.be.fulfilled;
+        newNow = await updates.getNow().should.be.fulfilled;
+        newBlock = await web3.eth.getBlockNumber().should.be.fulfilled;
+        newBlock.should.be.equal(block+1);
+        newNow.toNumber().should.be.equal(now.toNumber()+extraTime);
+        await timeTravel.revertToSnapShot(snapshotId);
+        newNow = await updates.getNow().should.be.fulfilled;
+        newNow.toNumber().should.be.equal(now.toNumber());
+    });
+
+    it('updsubmitActions to timezone too early', async () =>  {
+        await updates.submitActionsRoot(actionsRoot =  web3.utils.keccak256("hiboy")).should.be.rejected;
+    });
+
+    it('submitActions to timezone', async () =>  {
+        timeZoneToUpdateBefore = await updates.nextTimeZoneToUpdate().should.be.fulfilled;
+        verseBefore = await updates.currentVerse().should.be.fulfilled;
+        seed0 = await updates.getCurrentVerseSeed().should.be.fulfilled;
+        now = await updates.getNow().should.be.fulfilled;
+        nextTime = await updates.nextVerseTimestamp().should.be.fulfilled;
+        (nextTime-now > 0).should.be.equal(true);
+        await timeTravel.advanceTime(nextTime-now);
+        await timeTravel.advanceBlock().should.be.fulfilled;
+        await updates.submitActionsRoot(actionsRoot =  web3.utils.keccak256("hiboy")).should.be.rejected;
+        await timeTravel.advanceTime(1);
+        await timeTravel.advanceBlock().should.be.fulfilled;
+        tx = await updates.submitActionsRoot(actionsRoot =  web3.utils.keccak256("hiboys")).should.be.fulfilled;
+        timeZoneToUpdate = await updates.nextTimeZoneToUpdate().should.be.fulfilled;
+        verse = await updates.currentVerse().should.be.fulfilled;
+        verse.toNumber().should.be.equal(verseBefore.toNumber() + 1); 
+        timeZoneToUpdate[0].toNumber().should.be.equal(timeZoneToUpdateBefore[0].toNumber()); // tz to update does not change during the first 4 verses
+        seed1 = await updates.getCurrentVerseSeed().should.be.fulfilled;
+        seed1.should.not.be.equal(seed0);
+        now = await updates.getNow().should.be.fulfilled;
+        truffleAssert.eventEmitted(tx, "ActionsSubmission", (event) => {
+            return event.seed == seed1 && event.submissionTime.toNumber() == now.toNumber() && event.timeZone.toNumber() == timeZoneToUpdate[0].toNumber();
+        });
+        await updates.submitActionsRoot(actionsRoot =  web3.utils.keccak256("hiboy")).should.be.rejected;
+    });
+
+    it('update Timezone', async () =>  {
+        timeZoneToUpdateBefore = await updates.nextTimeZoneToUpdate().should.be.fulfilled;
+        seed0 = await updates.getCurrentVerseSeed().should.be.fulfilled;
+        now = await updates.getNow().should.be.fulfilled;
+        nextTime = await updates.nextVerseTimestamp().should.be.fulfilled;
+        (nextTime-now > 0).should.be.equal(true);
+        await timeTravel.advanceTime(nextTime-now);
+        await timeTravel.advanceBlock().should.be.fulfilled;
+        await updates.updateTZ(root =  web3.utils.keccak256("hiboyz")).should.be.rejected;
+        await updates.submitActionsRoot(actionsRoot =  web3.utils.keccak256("hiboy")).should.be.rejected;
+        await timeTravel.advanceTime(1);
+        await timeTravel.advanceBlock().should.be.fulfilled;
+        await updates.updateTZ(root =  web3.utils.keccak256("hiboyz")).should.be.rejected;
+        await updates.submitActionsRoot(actionsRoot =  web3.utils.keccak256("hiboy")).should.be.fulfilled;
+        now = await updates.getNow().should.be.fulfilled;
+        await updates.updateTZ(root =  web3.utils.keccak256("hiboyz")).should.be.fulfilled;
+        submissionTime = await assets.getLastActionsSubmissionTime(timeZoneToUpdateBefore[0].toNumber()).should.be.fulfilled;
+        timeZoneToUpdateAfter = await updates.nextTimeZoneToUpdate().should.be.fulfilled;
+        timeZoneToUpdate[0].toNumber().should.be.equal(timeZoneToUpdateBefore[0].toNumber()); // tz to update does not change during the first 4 verses
+        submissionTime.should.be.bignumber.equal(now);        
+    });
+
+    return;
+    
+    it('timeZoneToUpdate selected edge choices', async () =>  {
+        result = await updates._timeZoneToUpdatePure.call(verse = 0, TZ1 = 1).should.be.fulfilled;
+        result.timeZone.toNumber().should.be.equal(1)
+        result.day.toNumber().should.be.equal(1);
+        result.turnInDay.toNumber().should.be.equal(0);
+        result = await updates._timeZoneToUpdatePure.call(verse = 95, TZ1 = 4).should.be.fulfilled;
+        result.timeZone.toNumber().should.be.equal(3);
+        result.day.toNumber().should.be.equal(1);
+        result.turnInDay.toNumber().should.be.equal(3);
+        result = await updates._timeZoneToUpdatePure.call(verse = 96, TZ1 = 2).should.be.fulfilled;
+        result.timeZone.toNumber().should.be.equal(0);
+        result = await updates._timeZoneToUpdatePure.call(verse = 97, TZ1 = 24).should.be.fulfilled;
+        result.timeZone.toNumber().should.be.equal(24);
+        result.day.toNumber().should.be.equal(2);
+        result.turnInDay.toNumber().should.be.equal(0);
+        result = await updates._timeZoneToUpdatePure.call(verse = 1535, TZ1 = 24).should.be.fulfilled;
+        result.timeZone.toNumber().should.be.equal(23);
+        result.day.toNumber().should.be.equal(16);
+        result.turnInDay.toNumber().should.be.equal(2);
+    });
+
+    
+    it('timeZoneToUpdate exhaustive', async () =>  {
+        TZ1 = 3;
+        // for day 1
+        for (verse = 0; verse < VERSES_PER_DAY; verse+=5){
+            result = await updates._timeZoneToUpdatePure.call(verse, TZ1).should.be.fulfilled;
+            result.timeZone.toNumber().should.be.equal(1+(TZ1 - 1 + Math.floor(verse / 4))%24)
+            result.day.toNumber().should.be.equal(1);
+            result.turnInDay.toNumber().should.be.equal(verse % 4)
+        } 
+        // there is an empty spot at the end of day 1
+        result = await updates._timeZoneToUpdatePure(VERSES_PER_DAY, TZ1).should.be.fulfilled;
+        result.timeZone.toNumber().should.be.equal(0)
+        // beyond day 1
+        for (verse = VERSES_PER_DAY + 1; verse < VERSES_PER_ROUND; verse+=7){
+            result = await updates._timeZoneToUpdatePure.call(verse, TZ1).should.be.fulfilled;
+            result.timeZone.toNumber().should.be.equal(1+(TZ1 - 1 + Math.floor((verse-1) / 4))%24);
+            result.day.toNumber().should.be.equal(1 + Math.floor((verse - 1) / VERSES_PER_DAY));
+            result.turnInDay.toNumber().should.be.equal( (verse-1) % 4);
+        } 
+    });
+
+
+
+});
