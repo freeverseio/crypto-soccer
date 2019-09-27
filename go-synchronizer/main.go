3c8e5f487aca23bfa5a9a36c5f3983d131c625ff
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-27 15:58:05 +0200

    [sync] added updates contract binding

--- go-synchronizer/main.go
+++ go-synchronizer/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/config"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/assets"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/market"
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/updates"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/process"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
 )
@@ -42,9 +43,20 @@ func main() {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
+	log.Info("Creating Assets bindings to: ", config.AssetsContractAddress)
+	assetsContract, err := assets.NewAssets(common.HexToAddress(config.AssetsContractAddress), client)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
 	log.Info("Creating Market bindings to: ", config.MarketContractAddress)
 	marketContract, err := market.NewMarket(common.HexToAddress(config.MarketContractAddress), client)
-	assetsContract, err := assets.NewAssets(common.HexToAddress(config.AssetsContractAddress), client)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	log.Info("Creating Updates bindings to: ", config.UpdatesContractAddress)
+	_, err = updates.NewUpdates(common.HexToAddress(config.UpdatesContractAddress), client)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
