72f3c97b11c32043609af20f2da3b7a398220018
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-29 07:07:50 +0200

    [relay] removed config file

--- relay/nodejs-api/src/index.js
+++ relay/nodejs-api/src/index.js
@@ -10,25 +10,17 @@ const version = require("../package.json").version;
 // Parsing command line arguments
 program
   .version(version)
-  .option("-c, --config <path>", "set config path. defaults to config.json")
-  .parse(process.argv);
+  .option("-p, --providerUrl <url>", "Ethereum node url", "https://devnet.busyverse.com/web3")
+  .option("-k, --privateKey <pk>", "private key", "3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54")
+  .option("-a, --assetsContractAddress <address>", "assets contract address")
+  .option("-i, --interval <sec>", "interval in sec", "5")
+  .parse(process.argv)
 
-let configFile = "../";
-if (typeof program.config !== "undefined") configFile += program.config;
-else configFile += "config.json";
+const { providerUrl, privateKey, assetsContractAddress } = program;
 
-console.log("Configuration file: " + configFile);
-const config = require(configFile);
-const {
-  providerUrl,
-  address,
-  privateKey,
-  assetsContractAddress,
-} = config;
 
 console.log("--------------------------------------------------------");
 console.log("providerUrl       : ", providerUrl);
-console.log("account           : ", address);
 console.log("🔥  account p.k.   : ", privateKey);
 console.log("assets address    : ", assetsContractAddress);
 console.log("--------------------------------------------------------");
@@ -36,10 +28,11 @@ console.log("--------------------------------------------------------");
 const provider = new HDWalletProvider(privateKey, providerUrl);
 const web3 = new Web3(provider, null, {});
 const assets = new web3.eth.Contract(assetsJSON.abi, assetsContractAddress);
+const from = provider.addresses[0];
 
 const resolvers = new Resolvers({
   assets,
-  from: address
+  from
 });
 
 const server = new ApolloServer({
