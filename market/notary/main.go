5772d6af4137f04d60c63c5808d89646aa2ff5f9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-09-29 13:11:35 +0200

    [deploy] using different pk to avoid nonce prob

--- market/notary/main.go
+++ market/notary/main.go
@@ -19,6 +19,7 @@ func main() {
 	postgresURL := flag.String("postgres", "postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable", "postgres url")
 	ethereumClient := flag.String("ethereum_client", "https://devnet.busyverse.com/web3", "ethereum node")
 	marketContractAddress := flag.String("market_address", "", "market contract address")
+	privateKeyHex := flag.String("private_key", "3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54", "private key")
 	debug := flag.Bool("debug", false, "print debug logs")
 	flag.Parse()
 
@@ -27,6 +28,7 @@ func main() {
 	log.Infof("[PARAM] ethereum_client   : %v", *ethereumClient)
 	log.Infof("[PARAM] market_address    : %v", *marketContractAddress)
 	log.Infof("[PARAM] debug             : %v", *debug)
+	log.Infof("[PARAM] privatekey        : %v", *privateKeyHex)
 	log.Infof("-------------------------------------------------------------------")
 
 	if *debug {
@@ -58,7 +60,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	privateKey, err := crypto.HexToECDSA("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54")
+	privateKey, err := crypto.HexToECDSA(*privateKeyHex)
 	if err != nil {
 		log.Fatal(err)
 	}
