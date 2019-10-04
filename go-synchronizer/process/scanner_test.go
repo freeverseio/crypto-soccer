d6747323e52d0d189ba746feb91423390db618e0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-04 15:58:06 +0200

    [sync] test added for init timezone

--- go-synchronizer/process/scanner_test.go
+++ go-synchronizer/process/scanner_test.go
@@ -3,6 +3,7 @@ package process_test
 import (
 	"testing"
 
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/leagues"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/process"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/testutils"
 )
@@ -36,5 +37,22 @@ func TestScanningIniting(t *testing.T) {
 	err = scanner.Process()
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	events := scanner.Events
+	if len(events) != 24 {
+		t.Fatalf("Expected 24 received %v", len(events))
+	}
+	for i := 0; i < 24; i++ {
+		switch event := events[i].Value.(type) {
+		case leagues.LeaguesDivisionCreation:
+			expected := uint8(i + 1)
+			if event.Timezone != expected {
+				t.Fatalf("Expected %v recived %v", expected, event.Timezone)
+			}
+			break
+		default:
+			t.Fatalf("Wrong type %v", event)
+		}
 	}
 }
