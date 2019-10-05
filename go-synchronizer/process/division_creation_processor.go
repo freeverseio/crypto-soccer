0598ea4ef775a6a0fea2d19bacc5cf162061aaff
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-05 07:15:09 +0200

    [sync] refactoring

--- go-synchronizer/process/division_creation_processor.go
+++ go-synchronizer/process/division_creation_processor.go
@@ -7,13 +7,23 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/leagues"
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
+	"github.com/freeverseio/crypto-soccer/go-synchronizer/utils"
 	log "github.com/sirupsen/logrus"
 )
 
-func (p *EventProcessor) StoreDivisionCreation(event leagues.LeaguesDivisionCreation) error {
+type DivisionCreationProcessor struct {
+	db      *storage.Storage
+	leagues *leagues.Leagues
+}
+
+func NewDivisionCreationProcessor(db *storage.Storage, leagues *leagues.Leagues) *DivisionCreationProcessor {
+	return &DivisionCreationProcessor{db, leagues}
+}
+
+func (b *DivisionCreationProcessor) StoreDivisionCreation(event leagues.LeaguesDivisionCreation) error {
 	log.Infof("Division Creation: timezoneIdx: %v, countryIdx %v, divisionIdx %v", event.Timezone, event.CountryIdxInTZ.Uint64(), event.DivisionIdxInCountry.Uint64())
 	if event.CountryIdxInTZ.Uint64() == 0 {
-		if err := p.db.TimezoneCreate(storage.Timezone{event.Timezone}); err != nil {
+		if err := b.db.TimezoneCreate(storage.Timezone{event.Timezone}); err != nil {
 			return err
 		}
 	}
@@ -22,45 +32,45 @@ func (p *EventProcessor) StoreDivisionCreation(event leagues.LeaguesDivisionCrea
 		if countryIdx > 65535 {
 			return errors.New("Cannot cast country idx to uint16: value too large")
 		}
-		if err := p.db.CountryCreate(storage.Country{event.Timezone, uint32(countryIdx)}); err != nil {
+		if err := b.db.CountryCreate(storage.Country{event.Timezone, uint32(countryIdx)}); err != nil {
 			return err
 		}
-		if err := p.storeTeamsForNewDivision(event.Timezone, event.CountryIdxInTZ, event.DivisionIdxInCountry); err != nil {
+		if err := b.storeTeamsForNewDivision(event.Timezone, event.CountryIdxInTZ, event.DivisionIdxInCountry); err != nil {
 			return err
 		}
 	}
 	return nil
 }
-func (p *EventProcessor) storeTeamsForNewDivision(timezone uint8, countryIdx *big.Int, divisionIdxInCountry *big.Int) error {
+func (b *DivisionCreationProcessor) storeTeamsForNewDivision(timezone uint8, countryIdx *big.Int, divisionIdxInCountry *big.Int) error {
 	opts := &bind.CallOpts{}
-	calendarProcessor, err := NewCalendar(p.leagues, p.db)
+	calendarProcessor, err := NewCalendar(b.leagues, b.db)
 	if err != nil {
 		return err
 	}
 
-	LEAGUES_PER_DIV, err := p.leagues.LEAGUESPERDIV(opts)
+	LEAGUES_PER_DIV, err := b.leagues.LEAGUESPERDIV(opts)
 	if err != nil {
 		return err
 	}
 	leagueIdxBegin := divisionIdxInCountry.Int64() * int64(LEAGUES_PER_DIV)
 	leagueIdxEnd := leagueIdxBegin + int64(LEAGUES_PER_DIV)
 
-	TEAMS_PER_LEAGUE, err := p.leagues.TEAMSPERLEAGUE(opts)
+	TEAMS_PER_LEAGUE, err := b.leagues.TEAMSPERLEAGUE(opts)
 	if err != nil {
 		return err
 	}
 
 	for leagueIdx := leagueIdxBegin; leagueIdx < leagueIdxEnd; leagueIdx++ {
-		if err := p.db.LeagueCreate(storage.League{timezone, uint32(countryIdx.Uint64()), uint32(leagueIdx)}); err != nil {
+		if err := b.db.LeagueCreate(storage.League{timezone, uint32(countryIdx.Uint64()), uint32(leagueIdx)}); err != nil {
 			return err
 		}
 		teamIdxBegin := leagueIdx * int64(TEAMS_PER_LEAGUE)
 		teamIdxEnd := teamIdxBegin + int64(TEAMS_PER_LEAGUE)
 		for teamIdxInLeague, teamIdx := uint32(0), teamIdxBegin; teamIdx < teamIdxEnd; teamIdx, teamIdxInLeague = teamIdx+1, teamIdxInLeague+1 {
-			if teamId, err := p.leagues.EncodeTZCountryAndVal(opts, timezone, countryIdx, big.NewInt(teamIdx)); err != nil {
+			if teamId, err := b.leagues.EncodeTZCountryAndVal(opts, timezone, countryIdx, big.NewInt(teamIdx)); err != nil {
 				return err
 			} else {
-				if err := p.db.TeamCreate(
+				if err := b.db.TeamCreate(
 					storage.Team{
 						teamId,
 						timezone,
@@ -68,7 +78,7 @@ func (p *EventProcessor) storeTeamsForNewDivision(timezone uint8, countryIdx *bi
 						storage.TeamState{"0x0", uint32(leagueIdx), teamIdxInLeague, 0, 0, 0, 0, 0, 0}},
 				); err != nil {
 					return err
-				} else if err := p.storeVirtualPlayersForTeam(opts, teamId, timezone, countryIdx, teamIdx); err != nil {
+				} else if err := b.storeVirtualPlayersForTeam(opts, teamId, timezone, countryIdx, teamIdx); err != nil {
 					return err
 				}
 			}
@@ -84,4 +94,103 @@ func (p *EventProcessor) storeTeamsForNewDivision(timezone uint8, countryIdx *bi
 		}
 	}
 	return err
+}
+
+func (b *DivisionCreationProcessor) storeVirtualPlayersForTeam(opts *bind.CallOpts, teamId *big.Int, timezone uint8, countryIdx *big.Int, teamIdxInCountry int64) error {
+	PLAYERS_PER_TEAM_INIT, err := b.leagues.PLAYERSPERTEAMINIT(opts)
+	if err != nil {
+		return err
+	}
+	begin := teamIdxInCountry * int64(PLAYERS_PER_TEAM_INIT)
+	end := begin + int64(PLAYERS_PER_TEAM_INIT)
+
+	SK_SHO := uint8(0)
+	SK_SPE := uint8(0)
+	SK_PAS := uint8(0)
+	SK_DEF := uint8(0)
+	SK_END := uint8(0)
+
+	SK_SHO, err = b.leagues.SKSHO(opts)
+	if err != nil {
+		return err
+	}
+	SK_SPE, err = b.leagues.SKSPE(opts)
+	if err != nil {
+		return err
+	}
+	SK_PAS, err = b.leagues.SKPAS(opts)
+	if err != nil {
+		return err
+	}
+	SK_DEF, err = b.leagues.SKDEF(opts)
+	if err != nil {
+		return err
+	}
+	SK_END, err = b.leagues.SKEND(opts)
+	if err != nil {
+		return err
+	}
+
+	for i := begin; i < end; i++ {
+		if playerId, err := b.leagues.EncodeTZCountryAndVal(opts, timezone, countryIdx, big.NewInt(i)); err != nil {
+			return err
+		} else if skills, err := b.getPlayerSkillsAtBirth(opts, playerId); err != nil {
+			return err
+		} else if preferredPosition, err := b.getPlayerPreferredPosition(opts, playerId); err != nil {
+			return err
+		} else if shirtNumber, err := b.getShirtNumber(opts, playerId); err != nil {
+			return err
+		} else if err := b.db.PlayerCreate(
+			storage.Player{
+				playerId,
+				preferredPosition,
+				storage.PlayerState{ // TODO: storage should use same skill ordering as BC
+					TeamId:      teamId,
+					Defence:     uint64(skills[SK_DEF]), // TODO: type should be uint16
+					Speed:       uint64(skills[SK_SPE]),
+					Pass:        uint64(skills[SK_PAS]),
+					Shoot:       uint64(skills[SK_SHO]),
+					Endurance:   uint64(skills[SK_END]),
+					ShirtNumber: shirtNumber,
+				}},
+		); err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (p *DivisionCreationProcessor) getPlayerSkillsAtBirth(opts *bind.CallOpts, playerId *big.Int) ([5]uint16, error) {
+	if skills, err := p.leagues.GetPlayerSkillsAtBirth(opts, playerId); err != nil {
+		return [5]uint16{0, 0, 0, 0, 0}, err
+	} else {
+		return p.leagues.GetSkillsVec(opts, skills)
+	}
+}
+
+func (p *DivisionCreationProcessor) getShirtNumber(opts *bind.CallOpts, playerId *big.Int) (uint8, error) {
+	if playerState, err := p.leagues.GetPlayerState(opts, playerId); err != nil {
+		return 0, err
+	} else if shirtNumber, err := p.leagues.GetCurrentShirtNum(opts, playerState); err != nil {
+		return 0, err
+	} else {
+		return uint8(shirtNumber.Uint64()), nil
+	}
+}
+
+func (p *DivisionCreationProcessor) getPlayerPreferredPosition(opts *bind.CallOpts, playerId *big.Int) (string, error) {
+	if encodedSkills, err := p.leagues.GetPlayerSkillsAtBirth(opts, playerId); err != nil {
+		return "", err
+	} else if forwardness, err := p.leagues.GetForwardness(opts, encodedSkills); err != nil {
+		return "", err
+	} else if leftishness, err := p.leagues.GetLeftishness(opts, encodedSkills); err != nil {
+		return "", err
+	} else {
+		if forwardness.Uint64() > 255 {
+			return "", errors.New("Cannot cast forwardness to uint8: value too large")
+		} else if leftishness.Uint64() > 255 {
+			return "", errors.New("Cannot cast leftishness to uint8: value too large")
+		}
+		return utils.PreferredPosition(uint8(forwardness.Uint64()), uint8(leftishness.Uint64()))
+	}
 }
--- go-synchronizer/process/process.go
+++ go-synchronizer/process/process.go
@@ -10,7 +10,6 @@ import (
 
 	"fmt"
 	"math"
-	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -19,17 +18,17 @@ import (
 	//"github.com/freeverseio/crypto-soccer/go-synchronizer/contracts/leagues"
 
 	"github.com/freeverseio/crypto-soccer/go-synchronizer/storage"
-	"github.com/freeverseio/crypto-soccer/go-synchronizer/utils"
 	log "github.com/sirupsen/logrus"
 )
 
 type EventProcessor struct {
-	usesGanache bool
-	client      *ethclient.Client
-	db          *storage.Storage
-	engine      *engine.Engine
-	leagues     *leagues.Leagues
-	updates     *updates.Updates
+	usesGanache               bool
+	client                    *ethclient.Client
+	db                        *storage.Storage
+	engine                    *engine.Engine
+	leagues                   *leagues.Leagues
+	updates                   *updates.Updates
+	divisionCreationProcessor *DivisionCreationProcessor
 }
 
 // *****************************************************************************
@@ -38,12 +37,20 @@ type EventProcessor struct {
 
 // NewEventProcessor creates a new struct for scanning and storing crypto soccer events
 func NewEventProcessor(client *ethclient.Client, db *storage.Storage, engine *engine.Engine, leagues *leagues.Leagues, updates *updates.Updates) *EventProcessor {
-	return &EventProcessor{false, client, db, engine, leagues, updates}
+	return &EventProcessor{
+		false,
+		client,
+		db,
+		engine,
+		leagues,
+		updates,
+		NewDivisionCreationProcessor(db, leagues),
+	}
 }
 
 // NewGanacheEventProcessor creates a new struct for scanning and storing crypto soccer events from a ganache client
 func NewGanacheEventProcessor(client *ethclient.Client, db *storage.Storage, engine *engine.Engine, leagues *leagues.Leagues, updates *updates.Updates) *EventProcessor {
-	return &EventProcessor{true, client, db, engine, leagues, updates}
+	return &EventProcessor{true, client, db, engine, leagues, updates, NewDivisionCreationProcessor(db, leagues)}
 }
 
 // Process processes all scanned events and stores them into the database db
@@ -127,7 +134,7 @@ func (p *EventProcessor) dispatch(e *AbstractEvent) error {
 	switch v := e.Value.(type) {
 	case leagues.LeaguesDivisionCreation:
 		log.Info("[processor] Dispatching LeaguesDivisionCreation event")
-		return p.StoreDivisionCreation(v)
+		return p.divisionCreationProcessor.StoreDivisionCreation(v)
 	case leagues.LeaguesTeamTransfer:
 		log.Info("[processor] dispatching LeaguesTeamTransfer event")
 		teamID := v.TeamId
@@ -226,103 +233,4 @@ func (p *EventProcessor) getTimeOfEvent(eventRaw types.Log) (uint64, uint64, err
 		return 0, 0, err
 	}
 	return block.Time(), eventRaw.BlockNumber, nil
-}
-
-func (p *EventProcessor) storeVirtualPlayersForTeam(opts *bind.CallOpts, teamId *big.Int, timezone uint8, countryIdx *big.Int, teamIdxInCountry int64) error {
-	PLAYERS_PER_TEAM_INIT, err := p.leagues.PLAYERSPERTEAMINIT(opts)
-	if err != nil {
-		return err
-	}
-	begin := teamIdxInCountry * int64(PLAYERS_PER_TEAM_INIT)
-	end := begin + int64(PLAYERS_PER_TEAM_INIT)
-
-	SK_SHO := uint8(0)
-	SK_SPE := uint8(0)
-	SK_PAS := uint8(0)
-	SK_DEF := uint8(0)
-	SK_END := uint8(0)
-
-	SK_SHO, err = p.leagues.SKSHO(opts)
-	if err != nil {
-		return err
-	}
-	SK_SPE, err = p.leagues.SKSPE(opts)
-	if err != nil {
-		return err
-	}
-	SK_PAS, err = p.leagues.SKPAS(opts)
-	if err != nil {
-		return err
-	}
-	SK_DEF, err = p.leagues.SKDEF(opts)
-	if err != nil {
-		return err
-	}
-	SK_END, err = p.leagues.SKEND(opts)
-	if err != nil {
-		return err
-	}
-
-	for i := begin; i < end; i++ {
-		if playerId, err := p.leagues.EncodeTZCountryAndVal(opts, timezone, countryIdx, big.NewInt(i)); err != nil {
-			return err
-		} else if skills, err := p.getPlayerSkillsAtBirth(opts, playerId); err != nil {
-			return err
-		} else if preferredPosition, err := p.getPlayerPreferredPosition(opts, playerId); err != nil {
-			return err
-		} else if shirtNumber, err := p.getShirtNumber(opts, playerId); err != nil {
-			return err
-		} else if err := p.db.PlayerCreate(
-			storage.Player{
-				playerId,
-				preferredPosition,
-				storage.PlayerState{ // TODO: storage should use same skill ordering as BC
-					TeamId:      teamId,
-					Defence:     uint64(skills[SK_DEF]), // TODO: type should be uint16
-					Speed:       uint64(skills[SK_SPE]),
-					Pass:        uint64(skills[SK_PAS]),
-					Shoot:       uint64(skills[SK_SHO]),
-					Endurance:   uint64(skills[SK_END]),
-					ShirtNumber: shirtNumber,
-				}},
-		); err != nil {
-			return err
-		}
-	}
-	return err
-}
-
-func (p *EventProcessor) getPlayerSkillsAtBirth(opts *bind.CallOpts, playerId *big.Int) ([5]uint16, error) {
-	if skills, err := p.leagues.GetPlayerSkillsAtBirth(opts, playerId); err != nil {
-		return [5]uint16{0, 0, 0, 0, 0}, err
-	} else {
-		return p.leagues.GetSkillsVec(opts, skills)
-	}
-}
-
-func (p *EventProcessor) getShirtNumber(opts *bind.CallOpts, playerId *big.Int) (uint8, error) {
-	if playerState, err := p.leagues.GetPlayerState(opts, playerId); err != nil {
-		return 0, err
-	} else if shirtNumber, err := p.leagues.GetCurrentShirtNum(opts, playerState); err != nil {
-		return 0, err
-	} else {
-		return uint8(shirtNumber.Uint64()), nil
-	}
-}
-
-func (p *EventProcessor) getPlayerPreferredPosition(opts *bind.CallOpts, playerId *big.Int) (string, error) {
-	if encodedSkills, err := p.leagues.GetPlayerSkillsAtBirth(opts, playerId); err != nil {
-		return "", err
-	} else if forwardness, err := p.leagues.GetForwardness(opts, encodedSkills); err != nil {
-		return "", err
-	} else if leftishness, err := p.leagues.GetLeftishness(opts, encodedSkills); err != nil {
-		return "", err
-	} else {
-		if forwardness.Uint64() > 255 {
-			return "", errors.New("Cannot cast forwardness to uint8: value too large")
-		} else if leftishness.Uint64() > 255 {
-			return "", errors.New("Cannot cast leftishness to uint8: value too large")
-		}
-		return utils.PreferredPosition(uint8(forwardness.Uint64()), uint8(leftishness.Uint64()))
-	}
 }
