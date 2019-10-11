bf78270cf4e351247798bb854f8f2c5f4849e762
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-11 16:43:33 +0200

    [relay.db] added tactics table

--- relay/db/00_schema.sql
+++ relay/db/00_schema.sql
@@ -1,4 +1,15 @@
-CREATE TABLE actions (
-    id TEXT NOT NULL,
-    PRIMARY KEY(id)
+CREATE TABLE tactics (
+    teamId TEXT NOT NULL,
+    shirt_player_position_0 INT,
+    shirt_player_position_1 INT,
+    shirt_player_position_2 INT,
+    shirt_player_position_3 INT,
+    shirt_player_position_4 INT,
+    shirt_player_position_5 INT,
+    shirt_player_position_6 INT,
+    shirt_player_position_7 INT,
+    shirt_player_position_8 INT,
+    shirt_player_position_9 INT,
+    shirt_player_position_10 INT,
+    PRIMARY KEY(teamId)
 )
