041f54a876d4fdf5c8ea44f17e787177c9f54de2
Toni <toni.mateos@gmail.com>
2019-10-15 16:05:16 +0200

    correcting bug

--- truffle-core/contracts/Engine.sol
+++ truffle-core/contracts/Engine.sol
@@ -166,9 +166,7 @@ contract Engine is EngineLib, Sort{
         returns (uint256[PLAYERS_PER_TEAM_MAX] memory outStates, bool[10] memory extraAttack, uint8[9] memory playersPerZone, uint256) 
     {
         uint8 tacticsId;
-        // (substitutions, subsRounds, lineup, extraAttack, tacticsId) = decodeTactics(tactics);
-        // outStates = getLinedUpStates(matchLog, lineup, substitutions, states, is2ndHalf);
-        outStates = getLinedUpStates(matchLog, tactics, states, is2ndHalf);
+        (outStates, tacticsId, extraAttack) = getLinedUpStates(matchLog, tactics, states, is2ndHalf);
         matchLog = _precomp.computeExceptionalEvents(matchLog, outStates, is2ndHalf, seed);
         return (outStates, extraAttack, getPlayersPerZone(tacticsId), matchLog);
     }
@@ -181,13 +179,16 @@ contract Engine is EngineLib, Sort{
     )
         private 
         pure 
-        returns (uint256[PLAYERS_PER_TEAM_MAX] memory outStates) 
+        returns 
+    (
+        uint256[PLAYERS_PER_TEAM_MAX] memory outStates,
+        uint8 tacticsId,
+        bool[10] memory extraAttack
+    ) 
     {
         uint8[14] memory lineup;
         uint8[3] memory substitutions;
         uint8[3] memory subsRounds;
-        bool[10] memory extraAttack;
-        uint8 tacticsId;
         (substitutions, subsRounds, lineup, extraAttack, tacticsId) = decodeTactics(tactics);
         uint8 changes;
         if (is2ndHalf) {
