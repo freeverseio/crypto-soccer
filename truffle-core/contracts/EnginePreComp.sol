ea8d9846d6557b040a042e431b5682a5ecd0712d
Toni <toni.mateos@gmail.com>
2019-10-15 10:33:08 +0200

    splitting EngineLib

--- truffle-core/contracts/EnginePreComp.sol
+++ truffle-core/contracts/EnginePreComp.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
 
 import "./EncodingSkills.sol";
+import "./EngineLib.sol";
 
-contract EnginePreComp is EncodingSkills {
+contract EnginePreComp is EncodingSkills, EngineLib {
     uint8 private constant BITS_PER_RND     = 36;   // Number of bits allowed for random numbers inside match decisisons
     uint256 public constant MAX_RND         = 68719476735; // Max random number allowed inside match decisions: 2^36-1
     uint256 private constant ONE256       = 1; 
@@ -68,62 +69,12 @@ contract EnginePreComp is EncodingSkills {
         return matchLog;
     }
 
-    /// @dev Throws a dice that returns 0 with probability weight0/(weight0+weight1), and 1 otherwise.
-    /// @dev So, returning 0 has semantics: "the responsible for weight0 is selected".
-    /// @dev We return a uint8, not bool, to allow the return to be used as an idx in an array by the callee.
-    /// @dev The formula is derived as follows. Throw a random number R in the range [0,maxR].
-    /// @dev Then, w0 wins if (w0+w1)*(R/maxR) < w0, and w1 wins otherise. 
-    /// @dev MAX_RND controls the resolution or fine-graining of the algorithm.
-    function throwDice(uint256 weight0, uint256 weight1, uint256 rndNum) public pure returns(uint8) {
-        if( ( (weight0 + weight1) * rndNum ) < ( weight0 * (MAX_RND-1) ) ) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-
-    /// @dev Generalization of the previous to any number of input weights
-    /// @dev It therefore throws any number of dice and returns the winner's idx.
-    function throwDiceArray(uint256[] memory weights, uint256 rndNum) public pure returns(uint8 w) {
-        uint256 uniformRndInSumOfWeights;
-        for (w = 0; w < weights.length; w++) {
-            uniformRndInSumOfWeights += weights[w];
-        }
-        uniformRndInSumOfWeights *= rndNum;
-        uint256 cumSum = 0;
-        for (w = 0; w < weights.length-1; w++) {
-            cumSum += weights[w];
-            if( uniformRndInSumOfWeights < ( cumSum * (MAX_RND-1) )) {
-                return w;
-            }
-        }
-        return w;
-    }
-
     function computeTypeOfEvent(uint256 rnd) private pure returns (uint8) {
         uint256[] memory weights = new uint256[](3);
         weights[0] = 1; // injuryHard   
         weights[1] = 2; // injuryLow
         weights[2] = 5; // redCard
         return 1 + throwDiceArray(weights, rnd);
-    }
-
-    
-    function getNRandsFromSeed(uint256 seed, uint8 nRnds) public pure returns (uint64[] memory) {
-        uint256 currentBigRnd = uint256(keccak256(abi.encode(seed)));
-        uint8 remainingBits = 255;
-        uint64[] memory rnds = new uint64[](nRnds);
-        for (uint8 n = 0; n < nRnds; n++) {
-            if (remainingBits < BITS_PER_RND) {
-                currentBigRnd = uint256(keccak256(abi.encode(seed, n)));
-                remainingBits = 255;
-            }
-            rnds[n] = uint64(currentBigRnd & MAX_RND);
-            currentBigRnd >>= BITS_PER_RND;
-            remainingBits -= BITS_PER_RND;
-        }
-        return rnds;
     }
 
     function computePenalties(
@@ -274,15 +225,6 @@ contract EnginePreComp is EncodingSkills {
         }
     }
 
-    // for each day that passes over 31 years (=11315 days), we subtract 0,0274%, so that you get 10.001% less per year
-    // on a max of 1M, this is 274 per day.
-    // so, 3649 days after 31 (ten years), he will reach penalty 0. He'll be useless when reaching 41.
-    function penaltyPerAge(uint256 playerSkills, uint256 matchStartTime) public pure returns (uint256) {
-        uint256 ageDays = (7 * matchStartTime)/SECS_IN_DAY - 7 * getBirthDay(playerSkills);
-        if (ageDays > 14964) return 0; // 3649 + 11315 (41 years)
-        return ageDays < 11316 ? 1000000 : 1000000 - 274 * (ageDays - 11315);
-    }
-
     // recall order: [MOVE2ATTACK, CREATE_SHOOT, DEFEND_SHOOT, BLOCK_SHOOT, ENDURANCE]
     // the forward modifier factors only change the first 3.
     function getExtraAttackFactors(bool extraAttack) public pure returns (uint256[3] memory fwdModFactors) {
@@ -290,18 +232,6 @@ contract EnginePreComp is EncodingSkills {
         else                {fwdModFactors = [TEN_TO_4, TEN_TO_4, TEN_TO_4];}
     }
   
-    function getNDefenders(uint8[9] memory playersPerZone) private pure returns (uint8) {
-        return 2 * playersPerZone[0] + playersPerZone[1];
-    }
-
-    function getNMidfielders(uint8[9] memory playersPerZone) private pure returns (uint8) {
-        return 2 * playersPerZone[3] + playersPerZone[4];
-    }
-
-    function getNAttackers(uint8[9] memory playersPerZone) private pure returns (uint8) {
-        return 2 * playersPerZone[6] + playersPerZone[7];
-    }
-    
     function computeDefenderGlobSkills(
         uint256[5] memory globSkills,
         uint256 playerSkills, 
@@ -396,7 +326,6 @@ contract EnginePreComp is EncodingSkills {
             if (forwardness == IDX_M || forwardness == IDX_MD) {return 1000;}
             else if (forwardness == IDX_D) {return 2000;}
     }
-
 
 
 }
