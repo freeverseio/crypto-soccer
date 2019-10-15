a68758e283c83f44fb9e757dd2840cc20576b76f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-15 14:35:04 +0200

    [market.trader] fix create auction

--- market/trader/src/mutations.js
+++ market/trader/src/mutations.js
@@ -7,52 +7,49 @@ const MyPlugin = makeExtendSchemaPlugin(build => {
   return {
     typeDefs: gql`
        extend type Mutation {
-        createAuction(input: AuctionInput!): Auction
-        deleteAuction(uuid: UUID!): UUID
-        createBet(input: BetInput!): Bet
-        deleteBet(auction: UUID!, extra_price: BigFloat): UUID
+        createAuction(input: AuctionInput!): Boolean
       }
     `,
     resolvers: {
       Mutation: {
-        createPlayerSellOrder: async (_, { input }, context) =>  {
-          const { playerid, currencyid, price, rnd, validuntil, typeoftx, signature } = input;
-          const query = sql.query`INSERT INTO player_sell_orders (playerId, currencyId, price, rnd, validUntil, typeOfTx, signature) VALUES (
-            ${sql.value(playerid)}, 
-            ${sql.value(currencyid)}, 
+        createAuction: async (_, { input }, context) =>  {
+          const { uuid, playerId, currencyId, price, rnd, validUntil, signature } = input;
+          const query = sql.query`INSERT INTO auctions (uuid, player_id, currency_id, price, rnd, valid_until, signature) VALUES (
+            ${sql.value(uuid)},
+            ${sql.value(playerId)}, 
+            ${sql.value(currencyId)}, 
             ${sql.value(price)},
             ${sql.value(rnd)},
-            ${sql.value(validuntil)},
-            ${sql.value(typeoftx)},
+            ${sql.value(validUntil)},
             ${sql.value(signature)}
             )`;
           const {text, values} = sql.compile(query);
           await context.pgClient.query(text, values);
-          return playerid;
+          return true;
         },
-        deletePlayerSellOrder: async (_, {playerId}, context) => {
-          const query = sql.query`DELETE FROM player_sell_orders WHERE playerId=${sql.value(playerId)}`;
-          const {text, values} = sql.compile(query);
-          await context.pgClient.query(text, values);
-          return playerId;
-        },
-        createPlayerBuyOrder: async (_, {input}, context) => {
-          const { playerid, teamid, signature } = input;
-          const query = sql.query`INSERT INTO player_buy_orders (playerId, teamId, signature) VALUES (
-            ${sql.value(playerid)}, 
-            ${sql.value(teamid)}, 
-            ${sql.value(signature)}
-            )`;
-          const {text, values} = sql.compile(query);
-          await context.pgClient.query(text, values);
-          return playerid;
-        },
-        deletePlayerBuyOrder: async (_, {playerId}, context) => {
-          const query = sql.query`DELETE FROM player_buy_orders WHERE playerId=${sql.value(playerId)}`;
-          const {text, values} = sql.compile(query);
-          await context.pgClient.query(text, values);
-          return playerId;
-        },
+        // deletePlayerSellOrder: async (_, {playerId}, context) => {
+        //   const query = sql.query`DELETE FROM player_sell_orders WHERE playerId=${sql.value(playerId)}`;
+        //   const {text, values} = sql.compile(query);
+        //   await context.pgClient.query(text, values);
+        //   return playerId;
+        // },
+        // createPlayerBuyOrder: async (_, {input}, context) => {
+        //   const { playerid, teamid, signature } = input;
+        //   const query = sql.query`INSERT INTO player_buy_orders (playerId, teamId, signature) VALUES (
+        //     ${sql.value(playerid)}, 
+        //     ${sql.value(teamid)}, 
+        //     ${sql.value(signature)}
+        //     )`;
+        //   const {text, values} = sql.compile(query);
+        //   await context.pgClient.query(text, values);
+        //   return playerid;
+        // },
+        // deletePlayerBuyOrder: async (_, {playerId}, context) => {
+        //   const query = sql.query`DELETE FROM player_buy_orders WHERE playerId=${sql.value(playerId)}`;
+        //   const {text, values} = sql.compile(query);
+        //   await context.pgClient.query(text, values);
+        //   return playerId;
+        // },
       }
     }
   };
