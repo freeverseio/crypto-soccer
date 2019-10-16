6ac733a5d55bac1fb746a50acc74f5d4687c2645
Toni <toni.mateos@gmail.com>
2019-10-16 15:25:38 +0200

    more tests refactoring

--- truffle-core/test/Market.js
+++ truffle-core/test/Market.js
@@ -1187,66 +1187,16 @@ contract("Market", accounts => {
   //   ).should.be.rejected;
   // });
   
-  // // *************************************************************************
-  // // *********************************   TEST  *******************************
-  // // *************************************************************************
+  // *************************************************************************
+  // *********************************   TEST  *******************************
+  // *************************************************************************
   
   
-  // it("players: fails a MAKE_AN_OFFER via MTXs because validUntil is too large", async () => {
-  //   validUntil = now.toNumber() + 3600*24*2; // two days
-
-  //   sigSeller = await signPutAssetForSaleMTx(
-  //     currencyId,
-  //     price,
-  //     offererRnd, // he reuses the rnd provided
-  //     validUntil, 
-  //     playerId.toNumber(),
-  //     sellerAccount
-  //   );
-
-  //   // First of all, Freeverse and Buyer check the signature
-  //   // In this case, using web3:
-  //   recoveredSellerAddr = await web3.eth.accounts.recover(sigSeller);
-  //   recoveredSellerAddr.should.be.equal(sellerAccount.address);
-
-  //   // The correctness of the seller message can also be checked in the BC:
-  //   const sellerHiddenPrice = concatHash(
-  //     ["uint8", "uint256", "uint256"],
-  //     [currencyId, price, offererRnd]
-  //   );
-  //   sellerTxMsgBC = await market.buildPutAssetForSaleTxMsg(sellerHiddenPrice, validUntil, playerId).should.be.fulfilled;
-  //   sellerTxMsgBC.should.be.equal(sigSeller.message);
-
-  //   // Then, the buyer builds a message to sign
-  //   let isPlayerFrozen = await market.isPlayerFrozen(playerId).should.be.fulfilled;
-  //   isPlayerFrozen.should.be.equal(false);
-
-  //   // and send the Freeze TX. 
-  //   const sigSellerMsgRS = [
-  //     sigSeller.messageHash,
-  //     sigSeller.r,
-  //     sigSeller.s,
-  //   ];
-
-  //   // we can double-check that it would work
-  //   ok = await market.areFreezePlayerRequirementsOK(
-  //     sellerHiddenPrice,
-  //     validUntil,
-  //     playerId,
-  //     sigSellerMsgRS,
-  //     sigSeller.v
-  //   ).should.be.fulfilled;
-  //   ok.should.be.equal(false);
-    
-  //   // and finally do the freeze 
-  //   tx = await market.freezePlayer(
-  //     sellerHiddenPrice,
-  //     validUntil,
-  //     playerId,
-  //     sigSellerMsgRS,
-  //     sigSeller.v
-  //   ).should.be.rejected;
-  // });
+  it("players: fails a MAKE_AN_OFFER via MTXs because validUntil is too large", async () => {
+    tx, sellerHiddenPrice = await freezePlayer(currencyId, price, sellerRnd, validUntil, playerId, sellerAccount).should.be.fulfilled;
+    validUntil = now.toNumber() + 3600*24*2; // two days
+    tx, sellerHiddenPrice = await freezePlayer(currencyId, price, sellerRnd, validUntil, playerId, sellerAccount).should.be.rejected;
+  });
   
   
   
