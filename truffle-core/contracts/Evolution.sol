ca88e3f4375bd2847d4836206a4176361c8454eb
Toni <toni.mateos@gmail.com>
2019-10-18 11:07:04 +0200

    move legacy code outside truffle-core

--- truffle-core/contracts/Evolution.sol
+++ truffle-core/contracts/Evolution.sol
@@ -2,8 +2,21 @@ pragma solidity ^0.5.0;
 
 
 contract Evolution {
-    
 
+    uint8 constant public PLAYERS_PER_TEAM_MAX  = 25;
+    
+    function computeTrainingPoints( 
+        uint256[2] memory matchLog,
+        uint256[PLAYERS_PER_TEAM_MAX][2] memory statesHalf1,
+        uint256[PLAYERS_PER_TEAM_MAX][2] memory statesHalf2,
+        uint256[2] memory tacticsHalf1,
+        uint256[2] memory tacticsHalf2
+    )
+        public
+        pure
+        returns (uint16[2] memory points)
+    {
+    }
     
 }
 
