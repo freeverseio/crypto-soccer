0d773b5cf887eb066cc81a285022c07f0f4d5b2b
Toni <toni.mateos@gmail.com>
2019-10-18 12:56:55 +0200

    more

--- truffle-core/contracts/Evolution.sol
+++ truffle-core/contracts/Evolution.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.5.0;
 
+import "./EncodingSkills.sol";
 
-contract Evolution {
+contract Evolution is EncodingSkills {
 
     uint8 constant public PLAYERS_PER_TEAM_MAX  = 25;
     uint8 public constant NO_CARD  = 14;   // noone saw a card
     uint8 public constant RED_CARD = 3;   // noone saw a card
+    uint256 constant public FREE_PLAYER_ID  = 1; // it never corresponds to a legit playerId due to its TZ = 0
+    uint256 constant public MAX_DIFF  = 10; // beyond this diff among team qualities, it's basically infinite
 
     function computeTrainingPoints( 
         uint256[2] memory matchLog,
@@ -36,6 +39,7 @@ contract Evolution {
 
         // +4 for GK/D who saw no card at all, +1 for Mids and Atts
 
+
         uint256[2] memory pointsNeg;
         // -1 for each opponent goal
         pointsNeg[0] = nGoals1;
@@ -47,6 +51,32 @@ contract Evolution {
                 pointsNeg[team] += (((matchLog[0] >> offset) & 3) < RED_CARD) ? 3 : 0;
                 pointsNeg[team] += (((matchLog[0] >> (offset + 2)) & 15) < NO_CARD) ? 1 : 0;
                 pointsNeg[team] += (((matchLog[0] >> (offset + 6)) & 15) < NO_CARD) ? 1 : 0;
+            }
+        }
+        
+        points[0] = (points[0] > pointsNeg[0]) ? (points[0] - pointsNeg[0]) : 0;
+        points[1] = (points[1] > pointsNeg[1]) ? (points[1] - pointsNeg[1]) : 0;
+        
+        // +10% for each extra 50 points of lack of balance between teams
+        uint256 teamQuality0 = computeTeamQuality(statesHalf1[0]);
+        uint256 teamQuality1 = computeTeamQuality(statesHalf1[1]);
+
+        if (teamQuality0 > teamQuality1) {
+            points[0] = (points[0] * teamQuality1 * 3) / (teamQuality0 * 4);
+            points[1] = (points[1] * teamQuality0 * 4) / (teamQuality1 * 3);
+        } else if (teamQuality0 < teamQuality1) {
+            points[0] = (points[0] * teamQuality1 * 4) / (teamQuality0 * 3);
+            points[1] = (points[1] * teamQuality0 * 3) / (teamQuality1 * 4);
+        }
+    }
+    
+    function computeTeamQuality(uint256[PLAYERS_PER_TEAM_MAX] memory states) public pure returns (uint256 quality) {
+        uint256 state;
+        for (uint8 p = 0; p < PLAYERS_PER_TEAM_MAX; p++) {
+            state = states[p];
+            if (state != FREE_PLAYER_ID) {
+                quality +=  getShoot(state) + getSpeed(state) + getPass(state)
+                        +   getDefence(state) + getEndurance(state);
             }
         }
     }
