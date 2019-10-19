d73605768c80cf31ded289310b69bb07f95765aa
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-19 05:54:51 +0200

    [market.trader] added create bid mutation

--- market/trader/src/mutations.js
+++ market/trader/src/mutations.js
@@ -37,15 +37,17 @@ const MyPlugin = makeExtendSchemaPlugin(build => {
           return true; // TODO return something with sense
         },
         createBid: async (_, {input}, context) => {
-          const { playerid, teamid, signature } = input;
-          // const query = sql.query`INSERT INTO player_buy_orders (playerId, teamId, signature) VALUES (
-          //   ${sql.value(playerid)}, 
-          //   ${sql.value(teamid)}, 
-          //   ${sql.value(signature)}
-          //   )`;
-          // const {text, values} = sql.compile(query);
-          // await context.pgClient.query(text, values);
-          // return playerid;
+          const { auction, extraPrice, rnd, teamId, signature } = input;
+          const query = sql.query`INSERT INTO bids (auction, extra_price, rnd, team_id, signature, state) VALUES (
+            ${sql.value(auction)}, 
+            ${sql.value(extraPrice)}, 
+            ${sql.value(rnd)},
+            ${sql.value(teamId)},
+            ${sql.value(signature)},
+            ${sql.value('FILED')}
+            )`;
+          const {text, values} = sql.compile(query);
+          await context.pgClient.query(text, values);
           return true;
         },
         // deletePlayerBuyOrder: async (_, {playerId}, context) => {
