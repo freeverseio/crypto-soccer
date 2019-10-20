1577486ccfba3dab3c5a1bd8e0d228fd08d17f91
Toni <toni.mateos@gmail.com>
2019-10-20 23:20:37 +0200

    refactoring

--- truffle-core/contracts/EncodingMatchLog.sol
+++ truffle-core/contracts/EncodingMatchLog.sol
@@ -12,21 +12,42 @@ contract EncodingMatchLog {
     function addNGoals(uint256 log, uint8 goals) public pure returns (uint256) {
         return log + goals;
     }
+
+    function getNGoals(uint256 log) public pure returns (uint8) {
+        return uint8(log & 15);
+    }
+
     
     function addAssister(uint256 log, uint8 player, uint8 pos)  public pure returns (uint256) {
         return log | (uint256(player) << 4 + 4 * pos);
     }
   
+    function getAssister(uint256 log, uint8 pos) public pure returns (uint8) {
+        return uint8((log >> 4 + 4 * pos) & 15);
+    }
+
     function addShooter(uint256 log, uint8 player, uint8 pos)  public pure returns (uint256) {
         return log | (uint256(player) << 60 + 4 * pos);
     }
   
+    function getShooter(uint256 log, uint8 pos) public pure returns (uint8) {
+        return uint8((log >> 60 + 4 * pos) & 15);
+    }
+
     function addForwardPos(uint256 log, uint8 player, uint8 pos)  public pure returns (uint256) {
         return log | (uint256(player) << 116 + 2 * pos);
     }
     
+    function getForwardPos(uint256 log, uint8 pos) public pure returns (uint8) {
+        return uint8((log >> 116 + 4 * pos) & 3);
+    }
+    
     function addPenalty(uint256 log, bool penalty, uint8 pos)  public pure returns (uint256) {
         return log | (uint256(penalty ? 1 : 0) << 144 * pos);
+    }
+
+    function getPenalty(uint256 log, uint8 pos)  public pure returns (bool) {
+        return ((log >> 144 + pos) & 1) == 1;
     }
     
     function addOutOfGame(uint256 log, uint8 player, uint8 round, uint8 typeOfOutOfGame, bool is2ndHalf)  public pure returns (uint256) {
@@ -35,41 +56,83 @@ contract EncodingMatchLog {
         log |= (uint256(round) << offset + 4);
         return log | (uint256(typeOfOutOfGame) << offset + 8);
     }
-    
+
+    function getOutOfGame(uint256 log, bool is2ndHalf)  public pure 
+    returns (uint8 player, uint8 round, uint8 typeOfOutOfGame) {
+        uint8 offset = is2ndHalf ? 171 : 151;
+        player = uint8((log >> offset) & 15);
+        round  = uint8((log >> offset + 4) & 15);
+        typeOfOutOfGame = uint8((log >> offset + 8) & 3);
+    }
+
     function addYellowCard(uint256 log, uint8 player, uint8 posInHaf, bool is2ndHalf)  public pure returns (uint256) {
         uint8 offset = (is2ndHalf ? 181 : 161) + posInHaf * 4;
         return log | (uint256(player) << offset);
     }
 
+    function getYellowCard(uint256 log, uint8 posInHaf, bool is2ndHalf)  public pure returns (uint8) {
+        uint8 offset = (is2ndHalf ? 181 : 161) + posInHaf * 4;
+        return uint8((log >> offset) & 15);
+    }
+
     function setYellowedDidNotFinished1stHalf(uint256 log, uint8 posInHaf)  public pure returns (uint256) {
         return log | (uint256(ONE256) << (169 + posInHaf));
+    }
+
+    function getYellowedDidNotFinished1stHalf(uint256 log, uint8 posInHaf)  public pure returns (bool) {
+        return ((log >> (169 + posInHaf)) & 1) == 1;
     }
     
     function setInGameSubsHappened(uint256 log, uint8 happenedType, uint8 posInHalf, bool is2ndHalf) public pure returns (uint256) {
         uint8 offset = 189 + 2 * (posInHalf + (is2ndHalf ? 3 : 0));
         return (log & ~(uint256(3) << offset)) | (uint256(happenedType) << offset);
     }
+
+    function getInGameSubsHappened(uint256 log, uint8 posInHalf, bool is2ndHalf) public pure returns (uint8) {
+        uint8 offset = 189 + 2 * (posInHalf + (is2ndHalf ? 3 : 0));
+        return uint8((log >> offset) & 3);
+    }
     
     function addIsHomeStadium(uint256 log)  public pure returns (uint256) {
         return log | (uint256(ONE256) << 227);
     }
     
+    function getIsHomeStadium(uint256 log)  public pure returns (bool) {
+        return ((log >> 227) & 1) == 1;
+    }
+    
     function addHalfTimeSubs(uint256 log, uint8 player, uint8 pos)  public pure returns (uint256) {
         return log | (uint256(player) << 201 + 4 * pos);
+    }
+    
+    function getHalfTimeSubs(uint256 log, uint8 pos)  public pure returns (uint8) {
+        return uint8((log >> (201 + 4 * pos)) & 15);
     }
 
     function addNDefs(uint256 log, uint8 nDefs, bool is2ndHalf)  public pure returns (uint256) {
         return log | (uint256(nDefs) << (201 + 4 * (is2ndHalf ? 1 : 0)));
     }
 
+    function getNDefs(uint256 log, bool is2ndHalf)  public pure returns (uint8) {
+        return uint8((log >> (201 + 4 * (is2ndHalf ? 1 : 0))) & 15);
+    }
+
     function addNTot2ndHalf(uint256 log, uint8 nTot)  public pure returns (uint256) {
         return log | (uint256(nTot) << 209);
+    }
+
+    function getNTot2ndHalf(uint256 log)  public pure returns (uint8) {
+        return uint8((log >> 209) & 15);
     }
 
     function addWinner(uint256 log, uint8 winner)  public pure returns (uint256) {
         return log | (uint256(winner) << 225);
     }
-  
+
+    function getWinner(uint256 log)  public pure returns (uint8) {
+        return uint8((log >> 225) & 3);
+    }
+
     function encodeMatchLog(
         uint8 nGoals, // 4b, offset 0
         uint8[42] memory assistersShootersForwardsPos, 
--- truffle-core/test/EncodingMatchLog.js
+++ truffle-core/test/EncodingMatchLog.js
@@ -8,59 +8,72 @@ const EncodingMatchLog = artifacts.require('EncodingMatchLog');
 
 contract('EncodingMatchLog', (accounts) => {
 
-    beforeEach(async () => {
-        encoding = await EncodingMatchLog.new().should.be.fulfilled;
-    });
-    
-    it('encode and decode matchlog', async () =>  {
-        log = 0;
-        log = await encoding.addNGoals(log, nGoals = 3);
-        assistersIdx = Array.from(new Array(14), (x,i) => i);
+    async function encodeLog(encoding, nGoals, assistersIdx, shootersIdx, shooterForwardPos, penalties,
+        outOfGames, outOfGameRounds, typesOutOfGames, yellowCardedDidNotFinish1stHalf,
+        isHomeStadium, ingameSubs1, ingameSubs2, yellowCards1, yellowCards2, 
+        halfTimeSubstitutions, nDefs1, nDefs2, nTot, winner
+    ) {
+        log = await encoding.addNGoals(log, nGoals);
         for (p = 0; p <assistersIdx.length; p++) log = await encoding.addAssister(log, assistersIdx[p], p);
-        shootersIdx  = Array.from(new Array(14), (x,i) => 15-i);
         for (p = 0; p <shootersIdx.length; p++) log = await encoding.addShooter(log, shootersIdx[p], p);
-        shooterForwardPos  = Array.from(new Array(14), (x,i) => i % 4);
         for (p = 0; p <shootersIdx.length; p++) log = await encoding.addForwardPos(log, shooterForwardPos[p], p);
-        penalties  = Array.from(new Array(7), (x,i) => (i % 2 == 0));
         for (p = 0; p <penalties.length; p++) log = await encoding.addPenalty(log, penalties[p], p);
-        outOfGames = [10, 4];
-        outOfGameRounds = [7, 4];
-        typesOutOfGames = [1, 2];
         is2ndHalfs = [false, true];
         for (p = 0; p <outOfGames.length; p++) {
             log = await encoding.addOutOfGame(log, outOfGames[p], outOfGameRounds[p], typesOutOfGames[p], is2ndHalfs[p]);
         }
-        yellowCardedDidNotFinish1stHalf = [false, true];
         for (p = 0; p <penalties.length; p++) {
             if ( yellowCardedDidNotFinish1stHalf[p]) {
                 log = await encoding.setYellowedDidNotFinished1stHalf(log, p);
             }
         }
-
-        isHomeStadium = [true];
         if (isHomeStadium) log = await encoding.addIsHomeStadium(log)
-
-        ingameSubs1 = [0, 1, 2];
-        ingameSubs2 = [2, 1, 0];
-        
         for (p = 0; p <ingameSubs1.length; p++) log = await encoding.setInGameSubsHappened(log, ingameSubs1[p], p, is2nd = false);
         for (p = 0; p <ingameSubs2.length; p++) log = await encoding.setInGameSubsHappened(log, ingameSubs2[p], p, is2nd = true);
-
-        yellowCards1 = [9, 6];
-        yellowCards2 = [3, 0];
-
         for (p = 0; p <yellowCards1.length; p++) log = await encoding.addYellowCard(log, yellowCards1[p], p, is2nd = false);
         for (p = 0; p <yellowCards2.length; p++) log = await encoding.addYellowCard(log, yellowCards2[p], p, is2nd = true);
-        
-        halfTimeSubstitutions = [9, 7, 10];
-        
         for (p = 0; p <halfTimeSubstitutions.length; p++) log = await encoding.addHalfTimeSubs(log, halfTimeSubstitutions[p], p);
         
-        log = await encoding.addNDefs(log, nDefs1 = 4, is2nd = false);
-        log = await encoding.addNDefs(log, nDefs2 = 3, is2nd = true);
-        log = await encoding.addNTot2ndHalf(log, nTot = 10);
-        log = await encoding.addWinner(log, winner = 1);
+        log = await encoding.addNDefs(log, nDefs1, is2nd = false);
+        log = await encoding.addNDefs(log, nDefs2, is2nd = true);
+        log = await encoding.addNTot2ndHalf(log, nTot);
+        log = await encoding.addWinner(log, winner);
+        return log;
+    }
+    
+    beforeEach(async () => {
+        encoding = await EncodingMatchLog.new().should.be.fulfilled;
+    });
+    
+    it('encode and decode matchlog', async () =>  {
+        nGoals = 3;
+        assistersIdx = Array.from(new Array(14), (x,i) => i);
+        shootersIdx  = Array.from(new Array(14), (x,i) => 15-i);
+        shooterForwardPos  = Array.from(new Array(14), (x,i) => i % 4);
+        penalties  = Array.from(new Array(7), (x,i) => (i % 2 == 0));
+        outOfGames = [10, 4];
+        outOfGameRounds = [7, 4];
+        typesOutOfGames = [1, 2];
+        yellowCardedDidNotFinish1stHalf = [false, true];
+        isHomeStadium = [true];
+        ingameSubs1 = [0, 1, 2];
+        ingameSubs2 = [2, 1, 0];
+        yellowCards1 = [9, 6];
+        yellowCards2 = [3, 0];
+        halfTimeSubstitutions = [9, 7, 10];
+        nDefs1 = 4;
+        nDefs2 = 3;
+        nTot = 10;
+        winner = 1;
+        
+        log = await encodeLog(encoding, nGoals, assistersIdx, shootersIdx, shooterForwardPos, penalties,
+            outOfGames, outOfGameRounds, typesOutOfGames, yellowCardedDidNotFinish1stHalf,
+            isHomeStadium, ingameSubs1, ingameSubs2, yellowCards1, yellowCards2, 
+            halfTimeSubstitutions, nDefs1, nDefs2, nTot, winner);
 
+        ///////////////
+        
+        
         result = await encoding.decodeMatchLog(log);
         // let {0: nGo, 1: ass, 2: sho, 3: fwd, 4: pen, 5: outsAndYels, 6: outRounds, 7: typ, 8: yelFin, 9: halfSubs, 10: inGameSubs} = result;
         let {0: nGo, 1: assShoFwd, 2: pen, 3: outsAndYels, 4: outRounds, 5: typ, 6: yelFinHome, 7: halfSubs, 8: inGameSubs, 9: defTotWin} = result;
