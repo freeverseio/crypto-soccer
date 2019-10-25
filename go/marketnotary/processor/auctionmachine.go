02fb5694aa76ee8804107d61b4433e6ab3c5e1fc
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-25 17:26:48 +0200

    [notary] machine needs the market contract

--- go/marketnotary/processor/auctionmachine.go
+++ go/marketnotary/processor/auctionmachine.go
@@ -3,6 +3,7 @@ package processor
 import (
 	"time"
 
+	"github.com/freeverseio/crypto-soccer/go/contracts/market"
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/storage"
 )
 
@@ -10,15 +11,25 @@ type AuctionMachine struct {
 	Auction storage.Auction
 	Bids    []storage.Bid
 	current State
+	market  *market.Market
 }
 
-func NewAuctionMachine(auction storage.Auction, bids []storage.Bid) *AuctionMachine {
+func NewAuctionMachine(
+	auction storage.Auction,
+	bids []storage.Bid,
+	market *market.Market,
+) *AuctionMachine {
 	var state State
 	switch auction.State {
 	case storage.AUCTION_STARTED:
 		state = NewStarted()
 	}
-	return &AuctionMachine{auction, bids, state}
+	return &AuctionMachine{
+		auction,
+		bids,
+		state,
+		market,
+	}
 }
 
 func (b *AuctionMachine) SetCurrent(s State) {
@@ -46,4 +57,5 @@ func (b *Started) Process(m *AuctionMachine) {
 	if (len(m.Bids) == 0) && (m.Auction.ValidUntil.Int64()) < now {
 		m.Auction.State = storage.AUCTION_NO_BIDS
 	}
+
 }
--- go/marketnotary/processor/auctionmachine_test.go
+++ go/marketnotary/processor/auctionmachine_test.go
@@ -7,18 +7,32 @@ import (
 
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/processor"
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/storage"
+	"github.com/freeverseio/crypto-soccer/go/testutils"
 	"github.com/google/uuid"
 )
 
 func TestOutdatedAuction(t *testing.T) {
-	now := time.Now().Unix()
+	bc, err := testutils.NewBlockchainNode()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = bc.DeployContracts(bc.Owner)
+	if err != nil {
+		t.Fatal(err)
+	}
+	timezoneIdx := uint8(1)
+	err = bc.InitOneTimezone(timezoneIdx)
+	if err != nil {
+		t.Fatal(err)
+	}
 	auction := storage.Auction{
 		UUID:       uuid.New(),
-		ValidUntil: big.NewInt(now - 10),
+		ValidUntil: big.NewInt(time.Now().Unix() - 10),
 		State:      storage.AUCTION_STARTED,
 	}
 	bids := []storage.Bid{}
-	machine := processor.NewAuctionMachine(auction, bids)
+	machine := processor.NewAuctionMachine(auction, bids, bc.Market)
 	machine.Process()
 	if machine.Auction.State != storage.AUCTION_NO_BIDS {
 		t.Fatalf("Expected %v but %v", storage.AUCTION_NO_BIDS, auction.State)
