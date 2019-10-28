c77a474e33a30c476169fa1ea4871b67243b2518
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-28 10:40:20 +0100

    [notary] test refactoring

--- go/marketnotary/auctionmachine/auctionmachine_test.go
+++ go/marketnotary/auctionmachine/auctionmachine_test.go
@@ -7,33 +7,32 @@ import (
 
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/auctionmachine"
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/storage"
-	"github.com/freeverseio/crypto-soccer/go/testutils"
 	"github.com/google/uuid"
 )
 
 func TestStartedStateToNoBids(t *testing.T) {
-	bc, err := testutils.NewBlockchainNode()
-	if err != nil {
-		t.Fatal(err)
-	}
+	// bc, err := testutils.NewBlockchainNode()
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 
-	err = bc.DeployContracts(bc.Owner)
-	if err != nil {
-		t.Fatal(err)
-	}
-	timezoneIdx := uint8(1)
-	err = bc.InitOneTimezone(timezoneIdx)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// err = bc.DeployContracts(bc.Owner)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
+	// timezoneIdx := uint8(1)
+	// err = bc.InitOneTimezone(timezoneIdx)
+	// if err != nil {
+	// 	t.Fatal(err)
+	// }
 	auction := storage.Auction{
 		UUID:       uuid.New(),
 		ValidUntil: big.NewInt(time.Now().Unix() - 10),
 		State:      storage.AUCTION_STARTED,
 	}
 	bids := []storage.Bid{}
-	machine := auctionmachine.NewAuctionMachine(auction, bids, bc.Market, nil)
-	err = machine.Process()
+	machine := auctionmachine.NewAuctionMachine(auction, bids, nil, nil)
+	err := machine.Process()
 	if err != nil {
 		t.Fatal(err)
 	}
