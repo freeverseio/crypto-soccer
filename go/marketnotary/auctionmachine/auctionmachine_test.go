101c0257945447b0ad5aa67839d49ca9961c006e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-10-28 10:46:32 +0100

    [notary] when no bids and no outdated do nothing

--- go/marketnotary/auctionmachine/auctionmachine_test.go
+++ go/marketnotary/auctionmachine/auctionmachine_test.go
@@ -10,7 +10,28 @@ import (
 	"github.com/google/uuid"
 )
 
-func TestStartedStateToNoBids(t *testing.T) {
+func TestAuctionWithNoBids(t *testing.T) {
+	auction := storage.Auction{
+		UUID:       uuid.New(),
+		ValidUntil: big.NewInt(time.Now().Unix() + 100),
+		State:      storage.AUCTION_STARTED,
+	}
+	bids := []storage.Bid{}
+	machine := auctionmachine.NewAuctionMachine(auction, bids, nil, nil)
+	err := machine.Process()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if machine.Auction.State != storage.AUCTION_STARTED {
+		t.Fatalf("Expected %v but %v", storage.AUCTION_STARTED, machine.Auction.State)
+	}
+	err = machine.Process()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestAuctionOutdatedWithNoBids(t *testing.T) {
 	// bc, err := testutils.NewBlockchainNode()
 	// if err != nil {
 	// 	t.Fatal(err)
@@ -37,7 +58,7 @@ func TestStartedStateToNoBids(t *testing.T) {
 		t.Fatal(err)
 	}
 	if machine.Auction.State != storage.AUCTION_NO_BIDS {
-		t.Fatalf("Expected %v but %v", storage.AUCTION_NO_BIDS, auction.State)
+		t.Fatalf("Expected %v but %v", storage.AUCTION_NO_BIDS, machine.Auction.State)
 	}
 	err = machine.Process()
 	if err != nil {
--- go/marketnotary/auctionmachine/started.go
+++ go/marketnotary/auctionmachine/started.go
@@ -20,9 +20,11 @@ func (b *Started) Process(m *AuctionMachine) error {
 	}
 	now := time.Now().Unix()
 
-	if (len(m.Bids) == 0) && (m.Auction.ValidUntil.Int64()) < now {
-		m.Auction.State = storage.AUCTION_NO_BIDS
-		m.SetState(NewNoBids())
+	if len(m.Bids) == 0 {
+		if now > m.Auction.ValidUntil.Int64() {
+			m.Auction.State = storage.AUCTION_NO_BIDS
+			m.SetState(NewNoBids())
+		}
 		return nil
 	}
 
