bbacb5ae28f1c7b3475af2aaf43dda50810ea403
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-02 18:31:55 +0100

    [notary] refactroing

--- go/marketnotary/auctionmachine/paying.go
+++ go/marketnotary/auctionmachine/paying.go
@@ -2,7 +2,6 @@ package auctionmachine
 
 import (
 	"errors"
-	"time"
 
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/bidmachine"
 	"github.com/freeverseio/crypto-soccer/go/marketnotary/storage"
@@ -13,37 +12,32 @@ func (m *AuctionMachine) processPaying() error {
 		return errors.New("Paying: wrong state")
 	}
 
-	now := time.Now().Unix()
-	if (now - m.Auction.ValidUntil.Int64()) > 2 {
-		idx := bidmachine.IndexFirstAlive(m.Bids)
-		if idx == -1 {
-			return nil
-		}
-		bidMachine, err := bidmachine.New(
-			m.Auction,
-			m.Bids[idx],
-			m.market,
-			m.freeverse,
-			m.client,
-		)
-		if err != nil {
-			return err
-		}
-
-		bid, err := bidMachine.Process()
-		if err != nil {
-			return err
-		}
-		if bid.State == storage.BID_PAYING {
-			return nil
-		}
-		if bid.State == storage.BID_FAILED_TO_PAY {
-			m.Auction.State = storage.AUCTION_FAILED_TO_PAY
-		}
-
-		m.Bids[idx] = bid
-		m.Auction.State = storage.AUCTION_PAID
+	idx := bidmachine.IndexFirstAlive(m.Bids)
+	if idx == -1 {
+		return nil
 	}
+	bidMachine, err := bidmachine.New(
+		m.Auction,
+		m.Bids[idx],
+		m.market,
+		m.freeverse,
+		m.client,
+	)
+	if err != nil {
+		return err
+	}
+
+	m.Bids[idx], err = bidMachine.Process()
+	if err != nil {
+		return err
+	}
+	if m.Bids[idx].State == storage.BID_PAYING {
+		return nil
+	}
+	if m.Bids[idx].State == storage.BID_FAILED_TO_PAY {
+		m.Auction.State = storage.AUCTION_FAILED_TO_PAY
+	}
+	m.Auction.State = storage.AUCTION_PAID
 
 	return nil
 }
