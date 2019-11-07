b5bcb3f2275f018cddd83aa2eed5dbed822fa999
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-07 18:26:39 +0100

    [notary] create order tested

--- go/marketpay/marketpay.go
+++ go/marketpay/marketpay.go
@@ -3,13 +3,63 @@ package marketpay
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
+	"time"
 )
 
 type Customer struct {
-	ID int `json:"id"`
+	Data struct {
+		ID          int         `json:"id"`
+		Self        string      `json:"self"`
+		Name        interface{} `json:"name"`
+		Email       interface{} `json:"email"`
+		Prefix      string      `json:"prefix"`
+		Phone       string      `json:"phone"`
+		Tag         interface{} `json:"tag"`
+		Metadata    interface{} `json:"metadata"`
+		CreatedAt   time.Time   `json:"created_at"`
+		Connections struct {
+			Wallet       string `json:"wallet"`
+			Bankaccounts string `json:"bankaccounts"`
+		} `json:"connections"`
+	} `json:"data"`
+}
+
+type Order struct {
+	Data struct {
+		ID             int           `json:"id"`
+		Self           string        `json:"self"`
+		PublicID       string        `json:"public_id"`
+		Name           string        `json:"name"`
+		Value          string        `json:"value"`
+		Currency       string        `json:"currency"`
+		Amount         string        `json:"amount"`
+		PayinAmount    string        `json:"payin_amount"`
+		PayoutAmount   string        `json:"payout_amount"`
+		FeesAmount     string        `json:"fees_amount"`
+		BuyerLink      string        `json:"buyer_link"`
+		SellerLink     string        `json:"seller_link"`
+		Status         string        `json:"status"`
+		Sandbox        int           `json:"sandbox"`
+		StatusNicename string        `json:"status_nicename"`
+		Images         []interface{} `json:"images"`
+		Tag            interface{}   `json:"tag"`
+		Metadata       interface{}   `json:"metadata"`
+		CreatedAt      time.Time     `json:"created_at"`
+		PublishedAt    interface{}   `json:"published_at"`
+		AcceptedAt     interface{}   `json:"accepted_at"`
+		ValidatedAt    interface{}   `json:"validated_at"`
+		ReleasedAt     interface{}   `json:"released_at"`
+		Connections    struct {
+			Wallet string `json:"wallet"`
+			Buyer  string `json:"buyer"`
+			Payins string `json:"payins"`
+			Seller string `json:"seller"`
+		} `json:"connections"`
+	} `json:"data"`
 }
 
 type MarketPay struct {
@@ -25,7 +75,7 @@ func New() (*MarketPay, error) {
 func (b *MarketPay) CreateCustomer(
 	prefix string,
 	phoneNumber string,
-) (float64, error) {
+) (*Customer, error) {
 	url := b.endpoint + "/2.0/customers"
 	method := "POST"
 
@@ -35,7 +85,7 @@ func (b *MarketPay) CreateCustomer(
 	_ = writer.WriteField("phone", phoneNumber)
 	err := writer.Close()
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
 	client := &http.Client{
@@ -45,7 +95,7 @@ func (b *MarketPay) CreateCustomer(
 	}
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
 	req.Header.Add("Accept", "application/json")
@@ -54,19 +104,70 @@ func (b *MarketPay) CreateCustomer(
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 	res, err := client.Do(req)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 
-	// fmt.Println(string(body))
-	var dat map[string]interface{}
-	if err := json.Unmarshal(body, &dat); err != nil {
-		panic(err)
+	customer := &Customer{}
+	err = json.Unmarshal(body, customer)
+	if err != nil {
+		return nil, err
 	}
-	customerID := dat["data"].(map[string]interface{})["id"].(float64)
-	return customerID, nil
+	return customer, nil
+}
+
+func (b *MarketPay) CreateOrder(
+	seller *Customer,
+	buyer *Customer,
+) (*Order, error) {
+	url := b.endpoint + "/2.0/orders"
+	method := "POST"
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	_ = writer.WriteField("buyer_id", fmt.Sprintf("%d", buyer.Data.ID))
+	_ = writer.WriteField("seller_id", fmt.Sprintf("%d", seller.Data.ID))
+	_ = writer.WriteField("name", "Test Payin Wallet")
+	_ = writer.WriteField("value", "1")
+	err := writer.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client := &http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", "Bearer sk_stage_P4aQCVzTRGhub2p4k2Fl6YxQ")
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	body = bytes.TrimPrefix(body, []byte("\xef\xbb\xbf"))
+
+	order := &Order{}
+	err = json.Unmarshal(body, order)
+	if err != nil {
+		return nil, err
+	}
+	return order, nil
 }
--- go/marketpay/marketpay_test.go
+++ go/marketpay/marketpay_test.go
@@ -25,15 +25,31 @@ func TestCreateCustomer(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if customer != 817 {
+	if customer.Data.ID != 817 {
 		t.Fatalf("Expected %v", customer)
 	}
 }
 
 func TestCreateOrder(t *testing.T) {
-	// mp, err := marketpay.New()
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	mp, err := marketpay.New()
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	sellerID, err := mp.CreateCustomer("+34", "657497063")
+	if err != nil {
+		t.Fatal(err)
+	}
+	buyerID, err := mp.CreateCustomer("+34", "659853780")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	orderID, err := mp.CreateOrder(sellerID, buyerID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if orderID.Data.ID == 0 {
+		t.Fatalf("order wrong %v", orderID)
+	}
 }
