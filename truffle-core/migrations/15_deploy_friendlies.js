7f6e2090dc98b8cecc70ff6c97677249ee08b324
Toni <toni.mateos@gmail.com>
2019-11-07 16:26:37 +0100

    adding tests for friendlies

--- truffle-core/migrations/15_deploy_friendlies.js
+++
@@ -1,9 +0,0 @@
-const Friendlies = artifacts.require('Friendlies');
-
-module.exports = function (deployer) {
-  deployer.then(async () => {
-      await deployer.deploy(Friendlies);
-    })
-    .catch(console.error);
-};
-
