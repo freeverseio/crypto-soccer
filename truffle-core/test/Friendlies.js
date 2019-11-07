39409d847b65e9b73040394592d508256e41569d
Toni <toni.mateos@gmail.com>
2019-11-07 16:43:56 +0100

    done with tests for friendlies

--- truffle-core/test/Friendlies.js
+++ truffle-core/test/Friendlies.js
@@ -59,7 +59,28 @@ contract('Friendlies', (accounts) => {
         debug.compareArrays(nGamesPlayedByTeam, nGamesPlayedByTeamExpected, toNum = false, verbose = false);
     });     
     
-        return
+    it('getTeamsInCupMatch for 64 teams with shuffling', async () => {
+        nTeams = 64;
+        nMatchesPerDay = nTeams / 2;
+        teamsInMatches = [];
+        expected = [ 59, 45, 57, 21, 25, 51, 47, 18, 9, 13, 0, 3, 28, 55, 31, 43, 12, 63, 37, 30, 53, 24, 20, 44, 10, 17, 16, 33, 38, 2, 58, 49, 54, 8, 26, 50, 36, 52, 41, 27, 7, 34, 11, 29, 23, 35, 4, 61, 19, 62, 6, 46, 56, 32, 15, 40, 14, 60, 42, 22, 5, 1, 48, 39 ];
+        // an array containing the number of matches played by each team:
+        nGamesPlayedByTeam = Array.from(new Array(nTeams), (x,i) => 0);
+        nGamesPlayedByTeamExpected = Array.from(new Array(nTeams), (x,i) => 1);
+
+        seed = web3.utils.toBN(web3.utils.keccak256("32123"));
+
+        for (matchIdxInDay = 0; matchIdxInDay < nMatchesPerDay; matchIdxInDay++) {  
+            result = await friendlies.getTeamsInCupMatch(matchIdxInDay, nTeams, seed);
+            teamsInMatches.push(result[0]);
+            teamsInMatches.push(result[1]);
+            nGamesPlayedByTeam[result[0].toNumber()] += 1;
+            nGamesPlayedByTeam[result[1].toNumber()] += 1;
+        }
+        debug.compareArrays(teamsInMatches, expected, toNum = true, verbose = false);
+        debug.compareArrays(nGamesPlayedByTeam, nGamesPlayedByTeamExpected, toNum = false, verbose = false);
+    });     
+        
     it('getTeamsInLeagueMatch for 4 teams', async () => {
         nTeams = 4;
         nMatchdays = (nTeams - 1) * 2;
