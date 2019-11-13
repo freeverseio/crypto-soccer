416717760d3b083bd02bd2a38c0f8bffd5597412
Toni <toni.mateos@gmail.com>
2019-11-13 17:22:33 +0100

    improving parsing

--- pandora/playernames/parseMatthias.py
+++ pandora/playernames/parseMatthias.py
@@ -2,9 +2,40 @@ import csv
 
 database_name = "matthias.csv"
 
-vals = []
-nFields = 0
+IDX_NAME = 0
 IDX_GENDER = 1
+MIN_SCORE = 5
+
+def strcmp(str1, str2):
+    return str1.casefold() == str2.casefold()
+
+def belongsEnough(str, minScore = MIN_SCORE):
+    if strcmp(str, ""):
+        return False
+    return abs(int(str)) > minScore
+
+
+def getCountryIdx(fields, countryName):
+    for (f, field) in enumerate(fields):
+        if strcmp(field, countryName):
+            return f
+    assert False, "country not found"
+
+
+def getNamesFromCountry(countryName, fields, allNames):
+    names = []
+    score = MIN_SCORE
+    while len(names) < 100 and score > 1:
+        if (score < MIN_SCORE):
+            print("reducing min_score for: ", countryName)
+        names = [line[IDX_NAME] for line in allNames if belongsEnough(line[getCountryIdx(fields, countryName)], score)]
+        score -= 1
+    return names
+
+
+
+allNames = []
+nFields = 0
 with open(database_name, 'r', newline='\n') as file:
     for (l, line) in enumerate(file):
         if l == 0:
@@ -13,9 +44,13 @@ with open(database_name, 'r', newline='\n') as file:
         else:
             thisLine = line.split(";")
             if thisLine[IDX_GENDER] == "M":
-                print(l, thisLine)
-                assert (len(thisLine) == nFields, "wrong num of fields")
-                vals.append(thisLine)
+                assert len(thisLine) == nFields, "wrong num of fields"
+                allNames.append(thisLine)
+
+print(fields)
+for field in fields[2:-1]:
+    namesInCountry = getNamesFromCountry(field, fields, allNames)
+    print(field, len(namesInCountry))
 
 
 a = 2
