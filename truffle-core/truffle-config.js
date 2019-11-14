57cca4caf94e705f8e7f5a00e53f2e1407eefda0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-14 16:01:25 +0100

    [core] not using development as identify of dev net

--- truffle-core/truffle-config.js
+++ truffle-core/truffle-config.js
@@ -7,14 +7,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545
     },
-    development: { // 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
+    dev: { // 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
       provider: new HDWalletProvider(
         "FE058D4CE3446218A7B4E522D9666DF5042CF582A44A9ED64A531A81E7494A85",
         "http://localhost:8545"
       ),
       network_id: 63819
     },
-    production: { // 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
+    prod: { // 0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7
       provider: new HDWalletProvider(
         "FE058D4CE3446218A7B4E522D9666DF5042CF582A44A9ED64A531A81E7494A85",
         "http://localhost:8545"
