cab8968622810337a67215163c2ccd6d2fce8c51
Toni <toni.mateos@gmail.com>
2019-11-15 13:40:28 +0100

    stating to go to go

--- go/names/generator.go
+++ go/names/generator.go
@@ -1,17 +1,56 @@
 package names
 
 import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"io/ioutil"
 	"math/big"
-
-	"github.com/Pallinder/sillyname-go"
+	"os"
 )
+
+type Generator struct {
+	db *sql.DB
+}
+
+func New() (*Generator, error) {
+	var err error
+	generator := Generator{}
+	generator.db, err = sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		return nil, err
+	}
+	if err := generator.db.Ping(); err != nil {
+		return nil, err
+	}
+	_, err = generator.db.Exec("PRAGMA foreign_keys = ON;")
+	if err != nil {
+		return nil, err
+	}
+	file, err := os.Open("./sql/00_goalRev.sql")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	script, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	_, err = generator.db.Exec(string(script))
+	if err != nil {
+		return nil, err
+	}
+	return &generator, nil
+}
 
 func GeneratePlayerName(playerId *big.Int) string {
 	_ = playerId
-	return sillyname.GenerateStupidName()
+	return "s"
+	//	return sillyname.GenerateStupidName()
 }
 
 func GenerateTeamName(teamId *big.Int) string {
 	_ = teamId
-	return sillyname.GenerateStupidName()
+	return "s"
+	//	return sillyname.GenerateStupidName()
 }
