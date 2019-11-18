bfbf627ca42386b2a160e6f01495e8a6f120f891
Toni <toni.mateos@gmail.com>
2019-11-18 12:04:34 +0100

    generates random

--- go/names/generator.go
+++ go/names/generator.go
@@ -3,6 +3,7 @@ package names
 import (
 	"database/sql"
 	"errors"
+	"hash/fnv"
 	"math/big"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -13,9 +14,14 @@ type Generator struct {
 	db *sql.DB
 }
 
+func int_hash(s string) uint64 {
+	h := fnv.New64a()
+	h.Write([]byte(s))
+	return h.Sum64()
+}
+
 func New() (*Generator, error) {
 	var err error
-	log.Info("new")
 	generator := Generator{}
 	generator.db, err = sql.Open("sqlite3", "./sql/00_goalRev.db")
 	if err != nil {
@@ -47,10 +53,19 @@ func (b *Generator) NamesCount(teamId *big.Int) (uint64, error) {
 	return count, nil
 }
 
+func (b *Generator) GenerateRnd(seed *big.Int, max_val uint64, layers int) uint64 {
+	var iterated_seed uint64
+	iterated_seed = int_hash(seed.String())
+	for i := 1; i < layers; i++ {
+		iterated_seed = int_hash(big.NewInt(int64(iterated_seed)).String())
+	}
+	return iterated_seed % max_val
+}
+
 func (b *Generator) GeneratePlayerName(playerId *big.Int, teamId *big.Int) (string, error) {
 	_ = playerId
 	log.Debugf("[NAMES] GeneratePlayerName of playerId %v", playerId)
-	_, err := b.NamesCount(teamId)
+	num_names, err := b.NamesCount(teamId)
 	if err != nil {
 		return "", err
 	}
@@ -59,9 +74,14 @@ func (b *Generator) GeneratePlayerName(playerId *big.Int, teamId *big.Int) (stri
 		return "", err
 	}
 	defer rows.Close()
-	if !rows.Next() {
-		return "", errors.New("Unexistent playerId")
+	var selected_player uint64 = b.GenerateRnd(playerId, num_names, 1)
+	var i uint64
+	for i = 0; i < selected_player; i++ {
+		if !rows.Next() {
+			return "", errors.New("Rnd choice selected a player too too far in the database")
+		}
 	}
+
 	var name string
 	rows.Scan(&name)
 	return name, nil
