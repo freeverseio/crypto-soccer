96e2ac71cd0e54c04bbe062efcd06a528f401149
Toni <toni.mateos@gmail.com>
2019-11-18 15:22:54 +0100

    factoring and purging

--- pandora/playernames/generateGoalRevNameDB.sh
+++ pandora/playernames/generateGoalRevNameDB.sh
@@ -1,4 +1,6 @@
 python3 parseMatthias.py
 python3 parseEnglishSurnames.py
 python3 generateSQL.py
+echo "...purging Spanish names..."
+patch patch goalRevNames purgeSpanishNames.patch
 echo "...generated database: goalRev.db...DONE"
