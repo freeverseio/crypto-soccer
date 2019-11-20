fa75c0f668c8f03c116022b535fd49e5741ec8ba
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-20 18:33:17 +0100

    [notary] removed code£

--- go/notary/processor/processor.go
+++ go/notary/processor/processor.go
@@ -59,25 +59,6 @@ func (b *Processor) Process() error {
 		// update auction state if changed
 		newState := machine.Auction.State
 		if newState != auction.State {
-			// log.Infof("Auction %v: %v -> %v", auction.UUID, auction.State, newState)
-			// switch newState {
-			// case storage.AUCTION_PAYING:
-			// 	err = b.db.UpdateAuctionPaymentUrl(auction.UUID, "https://www.freeverse.io")
-			// 	if err != nil {
-			// 		return err
-			// 	}
-			// 	bid := bids[0]
-			// 	err = b.db.UpdateBidState(bid.Auction, bid.ExtraPrice, storage. BIDPAYING, "state extra")
-			// 	if err != nil {
-			// 		return err
-			// 	}
-			// 	err = b.db.UpdateBidPaymentUrl(bid.Auction, bid.ExtraPrice, "http://ninjaflex.com/")
-			// 	if err != nil {
-			// 		return err
-			// 	}
-			// 	break
-			// }
-
 			err = b.db.UpdateAuctionState(auction.UUID, newState)
 			if err != nil {
 				return err
