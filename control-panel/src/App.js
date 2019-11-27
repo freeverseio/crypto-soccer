0966ab93d48cc3e865b32c8925382f9f0e6a0447
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-27 18:19:26 +0100

    [control-panel] added connectiom to horizon

--- control-panel/src/App.js
+++ control-panel/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: 'http://165.22.66.118:4000/graphiql',
+});
+
+const createPlayer = () => {
+  client
+    .query({
+      query: gql`
+      {
+        allAuctions {
+          nodes {
+            uuid
+            state
+          }
+        }
+      }
+    `
+    })
+    .then(result => console.log(result));
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="ui divided three column grid">
+          <div class="row">
+            <div class="ui right labeled input">
+              <input type="text" placeholder="Enter weight..." />
+              <div class="ui basic label label">defence</div>
+            </div>
+          </div>
+        </div>
+        <button class="ui button" onClick={() => createPlayer()}>Create Special Player</button>
       </header>
     </div>
   );
-}
-
-export default App;
+    }
+    
+    export default App;
