10dab17af328812e83ebc78f9a2fd1b0644d6cd8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-28 11:21:35 +0100

    [control-panel] created infrastructure

--- control-panel/src/App.js
+++ control-panel/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+
+import { HashRouter as Router } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { gql } from "apollo-boost";
+import Header from './views/Header';
+import Main from './views/Main';
 
-
-import SpecialPlayer from './SpecialPlayer';
+const url = 'http://165.22.66.118:4000/graphiql';
 
 const client = new ApolloClient({
-  uri: 'http://165.22.66.118:4000/graphiql',
+  uri: url,
 });
 
 const createPlayer = () => {
@@ -31,12 +35,15 @@ const createPlayer = () => {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SpecialPlayer/>
-     </header>
-    </div>
+    <Router>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header url={url} />
+          <Main />
+        </div>
+      </ApolloProvider>
+    </Router>
   );
-    }
-    
-    export default App;
+}
+
+export default App;
