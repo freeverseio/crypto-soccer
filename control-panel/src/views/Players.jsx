8b97e5f0c7addbf3593748ce30fee4deab52e269
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-11-28 14:29:28 +0100

    [control-panel] new contracts + defaults

--- control-panel/src/views/Players.jsx
+++ control-panel/src/views/Players.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 import { Container, Form, Segment } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_PLAYERS = gql`
+{
+    allAuctions {
+        nodes {
+            uuid
+        }
+    }
+}
+`;
 
 class SpecialPlayer extends Component {
     constructor(props) {
@@ -8,12 +20,65 @@ class SpecialPlayer extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
 
         this.state = {
-            defence: '',
-            speed: ''
+            shoot: '2000',
+            speed: '2000',
+            pass: '2000',
+            defence: '2000',
+            endurance: '2000',
+            potential: '5',
+            forwardness: '3',
+            leftishness: '3',
+            aggressiveness: '2',
+            age: '17',
+            name: 'Johnnie Freeverse',
         }
     }
-    handleSubmit(event) {
-        console.log(this.state);
+
+    GetPlayers() {
+        const { loading, error, data } = useQuery(GET_PLAYERS, {
+            pollInterval: 500,
+        });
+
+        console.log("here")
+        if (loading) return null;
+        if (error) return `Error! ${error}`;
+        console.log(data);
+
+        return (
+            <div>
+                {data.allAuctions.nodes.map(auction => <div key={auction.uuid}>{auction.uuid}</div>)}
+            </div>
+        );
+    }
+
+    async handleSubmit(event) {
+        const { privileged } = this.props;
+        const { 
+            shoot, 
+            speed, 
+            pass, 
+            defence, 
+            endurance,
+            potential,
+            forwardness,
+            leftishness,
+            aggressiveness,
+            age,
+        } = this.state;
+
+        const sk = [shoot, speed, pass, defence, endurance ];
+        const traits = [potential, forwardness,  leftishness, aggressiveness];
+        const secsInYear = 365 * 24 * 3600
+        const internalId = Math.floor(Math.random()*1000000);
+        
+        const playerId = await privileged.methods.createSpecialPlayer(
+            sk,
+            age * secsInYear,
+            traits,
+            internalId
+        ).call();
+
+        console.log(playerId)
     }
 
     render() {
@@ -22,16 +87,57 @@ class SpecialPlayer extends Component {
                 <Segment>
                     <Form onSubmit={this.handleSubmit}>
                         <Form.Field>
-                            <label>Defence</label>
-                            <input placeholder='Defence' type='number' value={this.state.defence} onChange={event => this.setState({defence: event.target.value})}/>
+                            <label>Name</label>
+                            <input placeholder='Name' value={this.state.name} onChange={event => this.setState({ name: event.target.value })} />
                         </Form.Field>
-                        <Form.Field>
-                            <label>Speed</label>
-                            <input placeholder='Speed' type='number' value={this.state.speed} onChange={event => this.setState({speed: event.target.value})}/>
-                        </Form.Field>
+                        <Form.Group>
+                            <Form.Field>
+                                <label>Shoot</label>
+                                <input placeholder='Shoot' type='number' value={this.state.shoot} onChange={event => this.setState({ shoot: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Speed</label>
+                                <input placeholder='Speed' type='number' value={this.state.speed} onChange={event => this.setState({ speed: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Pass</label>
+                                <input placeholder='Pass' type='number' value={this.state.pass} onChange={event => this.setState({ pass: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Defence</label>
+                                <input placeholder='Defence' type='number' value={this.state.defence} onChange={event => this.setState({ defence: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Endurance</label>
+                                <input placeholder='Endurance' type='number' value={this.state.endurance} onChange={event => this.setState({ endurance: event.target.value })} />
+                            </Form.Field>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Field>
+                                <label>Potential</label>
+                                <input placeholder='Potential' type='number' value={this.state.potential} onChange={event => this.setState({ potential: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Forwardness</label>
+                                <input placeholder='Forwardness' type='number' value={this.state.forwardness} onChange={event => this.setState({ forwardness: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Leftishness</label>
+                                <input placeholder='Leftishness' type='number' value={this.state.leftishness} onChange={event => this.setState({ leftishness: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Aggressiveness</label>
+                                <input placeholder='Aggressiveness' type='number' value={this.state.aggressiveness} onChange={event => this.setState({ aggressiveness: event.target.value })} />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Age</label>
+                                <input placeholder='Age' type='number' value={this.state.age} onChange={event => this.setState({ age: event.target.value })} />
+                            </Form.Field>
+                        </Form.Group>
                         <Form.Button type='submit'>Create</Form.Button>
                     </Form>
                 </Segment>
+                <this.GetPlayers/>
             </Container>
         );
     }
