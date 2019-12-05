ef82a9fd2f2db46ccb24d71fb55123e8b1d827fd
Toni <toni.mateos@gmail.com>
2019-12-05 11:03:22 +0100

    generating sql

--- go/names/generator_test.go
+++ go/names/generator_test.go
@@ -143,3 +143,32 @@ func TestGenerateAcademyName(t *testing.T) {
 		t.Fatal("result of generating names not as expected")
 	}
 }
+
+func TestGenerateTeamName(t *testing.T) {
+	generator, err := names.New("./sql/names.db")
+	if err != nil {
+		t.Fatalf("error creating database for player names: %s", err)
+	}
+	// WARNING: both timezone and countryIdxInTZ are derivable from playerId
+	var timezone uint8
+	var countryIdxInTZ uint64
+	// if generation < 32, it means that it is an actual son
+	generation := uint8(33)
+	playerId := big.NewInt(int64(1))
+	timezone = 19
+	countryIdxInTZ = 0
+	name, err := generator.GeneratePlayerFullName(playerId, generation, timezone, countryIdxInTZ)
+	if err != nil {
+		t.Fatalf("error generating name for player %s: %s", playerId.String(), err)
+	}
+	fmt.Println(name)
+	if len(name) == 0 {
+		t.Fatalf("Expecting non empty player name, but got \"%v\"", name)
+	}
+	if name != "Edvin Blasco" {
+		fmt.Println("the just-obtained hash is: ")
+		fmt.Println(int_hash(name))
+		fmt.Println(name)
+		t.Fatal("result of generating names not as expected")
+	}
+}
