087a20b6ceeffaa8b107010e785ca0aaf61d444d
Toni <toni.mateos@gmail.com>
2019-12-05 10:37:30 +0100

    done with database

---
+++ pandora/teamnames/dbs/join.sh
@@ -0,0 +1,14 @@
+# Main Names
+cat constellations > main.txt
+cat animals >> main.txt
+cat gods >> main.txt
+cat drinks >> main.txt
+cat techie >> main.txt
+
+# Prefixes
+cat prefixes > firsts.txt
+cat colors >> firsts.txt
+
+# suffixes
+cat suffixes > lasts.txt
+
