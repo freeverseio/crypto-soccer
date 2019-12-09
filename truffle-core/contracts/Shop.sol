49254ffef2d6949345f443627480803b34eff91b
Toni <toni.mateos@gmail.com>
2019-12-09 10:59:31 +0100

    first test for offering item

--- truffle-core/contracts/Shop.sol
+++ truffle-core/contracts/Shop.sol
@@ -1,29 +1,59 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-// import "./EncodingSkills.sol";
-// import "./EncodingIDs.sol";
-// import "./SortIdxsAnySize.sol";
+import "./EncodingSkillsSetters.sol";
 /**
  * @title Creation of all game assets via creation of timezones, countries and divisions
  * @dev Timezones range from 1 to 24, with timeZone = 0 being null.
  */
 
-contract Shop {
+contract Shop is EncodingSkillsSetters{
 
+    event ItemOffered(uint256 itemId, uint64[] leagueIds);
+
+    uint16 constant public MAX_LEAGUES_PER_OFFER = 512;
+    uint8 constant public SK_SHO = 0;
+    uint8 constant public SK_SPE = 1;
+    uint8 constant public SK_PAS = 2;
+    uint8 constant public SK_DEF = 3;
+    uint8 constant public SK_END = 4;
+    
     struct ShopItem {
         uint256 skillsBoost;
-        uint256 duration;
-        uint16 stock;
         bytes32 championshipsHash;
+        uint16 stock;
+        uint8 matchesDuration;
         string uri;
     }        
 
-    mapping(uint256 => ShopItem) private _itemIdToItem;
+    ShopItem[] private _shopItems;
 
-    function getSkillsBoost(uint256 itemId) private view returns (uint256) { return _itemIdToItem[itemId].skillsBoost; }
-    function getDuration(uint256 itemId) private view returns (uint256) { return _itemIdToItem[itemId].duration; }
-    function getStock(uint256 itemId) private view returns (uint16) { return _itemIdToItem[itemId].stock; }
-    function getChampionshipsHash(uint256 itemId) private view returns (bytes32) { return _itemIdToItem[itemId].championshipsHash; }
-    function getUri(uint256 itemId) private view returns (string memory) { return _itemIdToItem[itemId].uri; }
+    function getSkillsBoost(uint256 itemId) private view returns (uint256) { return _shopItems[itemId].skillsBoost; }
+    function getMatchesDuration(uint256 itemId) private view returns (uint8) { return _shopItems[itemId].matchesDuration; }
+    function getStock(uint256 itemId) private view returns (uint16) { return _shopItems[itemId].stock; }
+    function getChampionshipsHash(uint256 itemId) private view returns (bytes32) { return _shopItems[itemId].championshipsHash; }
+    function getUri(uint256 itemId) private view returns (string memory) { return _shopItems[itemId].uri; }
+
+    // 1 round = 2 weeks, 1 year = 25 rounds, 100 years = 2500 rounds => 12bit
+    // leagueId: tz (5), country (10), leagueIdxInCountry (28), round(12) = 55 bit => 64 
+    function offerItem(uint256 skillsBoost, uint16 stock, uint8 matchesDuration, string memory uri, uint64[] memory leagueIds) public {
+        require(leagueIds.length < MAX_LEAGUES_PER_OFFER, "too many leafs in tree");
+        _shopItems.push(ShopItem(skillsBoost, merkleRootTemp(leagueIds), stock, matchesDuration, uri));
+        emit ItemOffered(_shopItems.length - 1, leagueIds);
+    }
+    
+    // TODO: this is a temp replacement for merkleRoot. 
+    function merkleRootTemp(uint64[] memory leafs) public pure returns (bytes32 hash) {
+        for (uint16 l = 0; l < leafs.length; l++) {
+            hash = keccak256(abi.encode(hash, leafs[l]));
+        } 
+    }
+    
+    function encodeSkillsBoost(uint16[5] memory skillsVec) public pure returns (uint256 skillsBoost) {
+        skillsBoost = setShoot(skillsBoost, skillsVec[SK_SHO]);
+        skillsBoost = setSpeed(skillsBoost, skillsVec[SK_SPE]);
+        skillsBoost = setPass(skillsBoost, skillsVec[SK_PAS]);
+        skillsBoost = setDefence(skillsBoost, skillsVec[SK_DEF]);
+        skillsBoost = setEndurance(skillsBoost, skillsVec[SK_END]);
+    }
     
 }
--- truffle-core/test/Shop.js
+++ truffle-core/test/Shop.js
@@ -19,7 +19,16 @@ contract('Shop', (accounts) => {
         shop = await Shop.new().should.be.fulfilled;
     });
 
-    it('test inits', async () => {
+    it('offer item', async () => {
+        skillsBoost = await shop.encodeSkillsBoost([5,7,8,11,23]).should.be.fulfilled;
+        leagueIds = Array.from(new Array(32), (x,i) => 312312321+i);
+        tx = await shop.offerItem(skillsBoost, stock = 32, matchesDuration = 8, "https://www.freeverse.io", leagueIds).should.be.fulfilled;
+        truffleAssert.eventEmitted(tx, "ItemOffered", async (event) => {
+            return event.itemId.toNumber().should.be.equal(0);
+            // return true;
+            // return event.playerId.should.be.bignumber.equal(playerId) && currentTeam.should.be.bignumber.equal(teamId2);
+        });
+
     });
 
 });
