4cb998ad4b2132a7d4be093f176239ee60e411fe
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-11 15:50:52 +0100

    [realy] some test on hashing

--- go/relay/storage/hasher.go
+++ go/relay/storage/hasher.go
@@ -2,35 +2,41 @@ package storage
 
 import (
 	"crypto/sha256"
-	"errors"
+	"fmt"
 )
 
 func (b *Storage) HashVerse(id int) ([]byte, error) {
 	h := sha256.New()
+	if id <= 0 {
+		return h.Sum(nil), nil
+	}
 	tacticsHash, err := b.hashVerseTactics(id)
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
 	h.Write(tacticsHash)
 	return h.Sum(nil), nil
 }
 
 func (b *Storage) hashVerseTactics(id int) ([]byte, error) {
+	if id <= 0 {
+		return nil, fmt.Errorf("Can't hash between verse %v and verse %v", id-1, id)
+	}
 	verse, err := b.GetVerse(id)
 	if err != nil {
 		return nil, err
 	}
 	if verse == nil {
-		return nil, errors.New("Unexistent verse")
+		return nil, fmt.Errorf("Unexistent verse %v", id)
 	}
 	prevVerse, err := b.GetVerse(id - 1)
 	if err != nil {
 		return nil, err
 	}
 	if prevVerse == nil {
-		return nil, errors.New("Unexistent prevVerse")
+		return nil, fmt.Errorf("Unexistent previous verse %v", id-1)
 	}
-	rows, err := b.tx.Query("SELECT * FROM TACTICS WHERE (created_at > $1) AND (created_at <= $2)", prevVerse.StartAt, verse.StartAt)
+	_, err = b.tx.Query("SELECT * FROM TACTICS WHERE (created_at > $1) AND (created_at <= $2)", prevVerse.StartAt, verse.StartAt)
 	if err != nil {
 		return nil, err
 	}
--- go/relay/storage/hasher_test.go
+++ go/relay/storage/hasher_test.go
@@ -11,11 +11,15 @@ func TestHash(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer db.Rollback()
-	hash, err := db.Hash(0)
+	hash, err := db.HashVerse(0)
 	if err != nil {
 		t.Fatal(err)
 	}
-	if hex.EncodeToString(hash) != "5df6e0e2761359d30a8275058e299fcc0381534545f55cf43e41983f5d4c9456" {
+	if hex.EncodeToString(hash) != "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" {
 		t.Fatalf("Wrong result %v", hex.EncodeToString(hash))
+	}
+	hash, err = db.HashVerse(1)
+	if err == nil {
+		t.Fatal("Expected error on hashing unexistent verse")
 	}
 }
