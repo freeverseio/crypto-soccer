ebe128b94b99d9f4aa6b189fd10e5ef1ef101a50
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-11 15:36:08 +0100

    [relay] added GetVerse

--- go/relay/storage/hasher.go
+++ go/relay/storage/hasher.go
@@ -1,10 +1,13 @@
 package storage
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"errors"
+)
 
-func (b *Storage) Hash(verse int) ([]byte, error) {
+func (b *Storage) HashVerse(id int) ([]byte, error) {
 	h := sha256.New()
-	tacticsHash, err := b.hashTactics(verse)
+	tacticsHash, err := b.hashVerseTactics(id)
 	if err != nil {
 		return nil, nil
 	}
@@ -12,7 +15,25 @@ func (b *Storage) Hash(verse int) ([]byte, error) {
 	return h.Sum(nil), nil
 }
 
-func (b *Storage) hashTactics(verse int) ([]byte, error) {
+func (b *Storage) hashVerseTactics(id int) ([]byte, error) {
+	verse, err := b.GetVerse(id)
+	if err != nil {
+		return nil, err
+	}
+	if verse == nil {
+		return nil, errors.New("Unexistent verse")
+	}
+	prevVerse, err := b.GetVerse(id - 1)
+	if err != nil {
+		return nil, err
+	}
+	if prevVerse == nil {
+		return nil, errors.New("Unexistent prevVerse")
+	}
+	rows, err := b.tx.Query("SELECT * FROM TACTICS WHERE (created_at > $1) AND (created_at <= $2)", prevVerse.StartAt, verse.StartAt)
+	if err != nil {
+		return nil, err
+	}
 	h := sha256.New()
 	return h.Sum(nil), nil
 }
