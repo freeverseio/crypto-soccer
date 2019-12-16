311c3320212acc246c840837b82eb1f068307597
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-16 22:16:40 +0100

    [universe.api] create/delete special players

--- universe.api/src/mutations_plugin.js
+++ universe.api/src/mutations_plugin.js
@@ -27,8 +27,53 @@ const MyPlugin = () => {
       }`,
       resolvers: {
         Mutation: {
-          createSpecialPlayer: (_, params) => {
-            return true;
+          createSpecialPlayer: async (_, params, context) => {
+            const { playerId, name, defence, speed, pass, shoot, endurance, preferredPosition, potential, dayOfBirth } = params;
+            const query = sql.query`INSERT INTO players (
+              name,
+              player_id,
+              team_id, 
+              defence, 
+              speed, 
+              pass, 
+              shoot, 
+              endurance, 
+              shirt_number, 
+              preferred_position, 
+              potential,
+              day_of_birth,
+              encoded_skills,
+              encoded_state,
+              frozen,
+              red_card_matches_left,
+              injury_matches_left) VALUES (
+                ${sql.value(name)},
+                ${sql.value(playerId)},
+                ${sql.value('1')},
+                ${sql.value(defence)}, 
+                ${sql.value(speed)},
+                ${sql.value(pass)},
+                ${sql.value(shoot)},
+                ${sql.value(endurance)},
+                ${sql.value(0)},
+                ${sql.value('')},
+                ${sql.value(potential)},
+                ${sql.value(dayOfBirth)},
+                ${sql.value('')},
+                ${sql.value('')},
+                ${sql.value(false)},
+                ${sql.value(0)},
+                ${sql.value(0)}
+            )`;
+            const { text, values } = sql.compile(query);
+            await context.pgClient.query(text, values);
+            return true;// TODO return something with sense
+          },
+          deleteSpecialPlayer: async (_, { playerId }, context) => {
+            const query = sql.query`DELETE FROM players WHERE team_id=${sql.value('1')} AND player_id=${sql.value(playerId)};`;
+            const { text, values } = sql.compile(query);
+            await context.pgClient.query(text, values);
+            return true;// TODO return something with sense
           },
         },
       },
