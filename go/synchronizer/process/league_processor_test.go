061069341f3cd92af018f9dc1095f4eaaeaa1f06
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-19 17:19:25 +0100

    [sync] check if ranking point are != 0

--- go/synchronizer/process/league_processor_test.go
+++ go/synchronizer/process/league_processor_test.go
@@ -247,8 +247,8 @@ func TestLeagueShuffling(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("teams in League : %v", teams)
-	if teams[0].State.RankingPoints != 24124777882976255 {
-		t.Fatalf("Wrong ranking points %v", teams[0].State.RankingPoints)
+	if teams[0].State.RankingPoints == 0 {
+		t.Fatal("Owned team has ranking point 0")
 	}
 	for _, team := range teams[1:] {
 		if team.State.RankingPoints != 0 {
