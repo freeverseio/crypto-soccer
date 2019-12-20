a9189d53077e059ecae5f9f8b4c2192575e40a0d
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-20 10:19:27 +0100

    [dashboard] fix web3 prop

--- dashboard/src/views/academy/AcademyPlayer.jsx
+++ dashboard/src/views/academy/AcademyPlayer.jsx
@@ -47,7 +47,8 @@ export default function AcademyPlayer(props) {
     const [createAuction] = useMutation(CREATE_AUCTION);
     const [deleteAcademyPlayer] = useMutation(DELETE_PLAYER);
 
-    const {player } = props;
+    const { player, web3 } = props;
+    console.log(web3)
 
     return (
         <Item>
@@ -63,41 +64,40 @@ export default function AcademyPlayer(props) {
                     </List>
                 </Item.Description>
                 <Item.Extra>
-                        <Input label='Price' type='number' value={price} onChange={event => setPrice(event.target.value)} />
-                        <Input label='Timeout' type='number' value={timeout} onChange={event => setTimeout(event.target.value)} />
+                    <Input label='Price' type='number' value={price} onChange={event => setPrice(event.target.value)} />
+                    <Input label='Timeout' type='number' value={timeout} onChange={event => setTimeout(event.target.value)} />
 
-                        <Button floated='right' basic color='green' onClick={async () => {
-                            const { web3 } = props;
-                            const rnd = Math.floor(Math.random() * 1000000);
-                            const now = new Date();
-                            const validUntil = (Math.round(now.getTime() / 1000) + timeout).toString();
-                            const sellerAccount = await web3.eth.accounts.privateKeyToAccount("0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709");
-                            const currencyId = 1;
-                            const signature = await signPutAssetForSaleMTx(web3, currencyId, price, rnd, validUntil, player.playerId, sellerAccount);
-                            const seller = sellerAccount.address;
-                            createAuction({
-                                variables: {
-                                    uuid: uuidv1(),
-                                    playerId: player.playerId,
-                                    currencyId: currencyId,
-                                    price: Number(price),
-                                    rnd: Number(rnd),
-                                    validUntil: validUntil,
-                                    signature: signature.signature,
-                                    seller: seller,
-                                }
-                            });
-                        }}>
-                            Sell
+                    <Button floated='right' basic color='green' onClick={async () => {
+                        const rnd = Math.floor(Math.random() * 1000000);
+                        const now = new Date();
+                        const validUntil = (Math.round(now.getTime() / 1000) + timeout).toString();
+                        const sellerAccount = await web3.eth.accounts.privateKeyToAccount("0x348ce564d427a3311b6536bbcff9390d69395b06ed6c486954e971d960fe8709");
+                        const currencyId = 1;
+                        const signature = await signPutAssetForSaleMTx(web3, currencyId, price, rnd, validUntil, player.playerId, sellerAccount);
+                        const seller = sellerAccount.address;
+                        createAuction({
+                            variables: {
+                                uuid: uuidv1(),
+                                playerId: player.playerId,
+                                currencyId: currencyId,
+                                price: Number(price),
+                                rnd: Number(rnd),
+                                validUntil: validUntil,
+                                signature: signature.signature,
+                                seller: seller,
+                            }
+                        });
+                    }}>
+                        Sell
                                         </Button>
-                        <Button floated='right' value='Delete' basic color='red' onClick={() => {
-                            deleteAcademyPlayer({
-                                variables: {
-                                    playerId: player.playerId,
-                                }
-                            })
-                        }
-                        }>Delete</Button>
+                    <Button floated='right' value='Delete' basic color='red' onClick={() => {
+                        deleteAcademyPlayer({
+                            variables: {
+                                playerId: player.playerId,
+                            }
+                        })
+                    }
+                    }>Delete</Button>
                 </Item.Extra>
             </Item.Content>
         </Item>
