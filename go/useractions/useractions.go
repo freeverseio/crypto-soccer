a157c03817d9d110537195494ef6b1c27d73023f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-23 00:32:00 +0100

    [useractions] fix

--- go/useractions/useractions.go
+++ go/useractions/useractions.go
@@ -15,8 +15,8 @@ import (
 )
 
 type UserActions struct {
-	Tactics   []storage.VerseTactic   `json:"tactics"`
-	Trainings []storage.VerseTraining `json:"trainings"`
+	Tactics   []storage.Tactic   `json:"tactics"`
+	Trainings []storage.Training `json:"trainings"`
 }
 
 func (b *UserActions) Equal(actions *UserActions) bool {
@@ -41,10 +41,10 @@ func (b *UserActions) Equal(actions *UserActions) bool {
 
 func (b *UserActions) PullFromStorage(tx *sql.Tx, verse uint64) error {
 	var err error
-	if b.Tactics, err = storage.VerseTacticsByVerse(tx, verse); err != nil {
+	if b.Tactics, err = storage.TacticsByVerse(tx, verse); err != nil {
 		return err
 	}
-	if b.Trainings, err = storage.VerseTrainingByVerse(tx, verse); err != nil {
+	if b.Trainings, err = storage.TrainingByVerse(tx, verse); err != nil {
 		return err
 	}
 	return nil
@@ -62,10 +62,10 @@ func (b *UserActions) Hash() ([]byte, error) {
 
 func (b *UserActions) Marshal() ([]byte, error) {
 	if b.Tactics == nil {
-		b.Tactics = make([]storage.VerseTactic, 0)
+		b.Tactics = make([]storage.Tactic, 0)
 	}
 	if b.Trainings == nil {
-		b.Trainings = make([]storage.VerseTraining, 0)
+		b.Trainings = make([]storage.Training, 0)
 	}
 	return json.Marshal(b)
 }
--- go/useractions/useractions_test.go
+++ go/useractions/useractions_test.go
@@ -16,13 +16,13 @@ func TestMarshal(t *testing.T) {
 	if string(data) != `{"tactics":[],"trainings":[]}` {
 		t.Fatalf("Wrong %v", string(data))
 	}
-	ua.Tactics = append(ua.Tactics, storage.VerseTactic{3, storage.Tactic{TeamID: "ciao"}})
-	ua.Trainings = append(ua.Trainings, storage.VerseTraining{5, storage.Training{TeamID: "pippo"}})
+	ua.Tactics = append(ua.Tactics, storage.Tactic{Verse: 3, TeamID: "ciao"})
+	ua.Trainings = append(ua.Trainings, storage.Training{Verse: 5, TeamID: "pippo"})
 	data, err = ua.Marshal()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if string(data) != `{"tactics":[{"verse":3,"tactic":{"team_id":"ciao","tactic_id":0,"shirt_0":0,"shirt_1":0,"shirt_2":0,"shirt_3":0,"shirt_4":0,"shirt_5":0,"shirt_6":0,"shirt_7":0,"shirt_8":0,"shirt_9":0,"shirt_10":0,"shirt_11":0,"shirt_12":0,"shirt_13":0,"extra_attack_1":false,"extra_attack_2":false,"extra_attack_3":false,"extra_attack_4":false,"extra_attack_5":false,"extra_attack_6":false,"extra_attack_7":false,"extra_attack_8":false,"extra_attack_9":false,"extra_attack_10":false}}],"trainings":[{"verse":5,"training":{"team_id":"pippo","special_player_shirt":0,"goalkeepers_defence":0,"goalkeepers_speed":0,"goalkeepers_pass":0,"goalkeepers_shoot":0,"goalkeepers_endurance":0,"defenders_defence":0,"defenders_speed":0,"defenders_pass":0,"defenders_shoot":0,"defenders_endurance":0,"midfielders_defence":0,"midfielders_speed":0,"midfielders_pass":0,"midfielders_shoot":0,"midfielders_endurance":0,"attackers_defence":0,"attackers_speed":0,"attackers_pass":0,"attackers_shoot":0,"attackers_endurance":0,"special_player_defence":0,"special_player_speed":0,"special_player_pass":0,"special_player_shoot":0,"special_player_endurance":0}}]}` {
+	if string(data) != `{"tactics":[{"verse":3,"team_id":"ciao","tactic_id":0,"shirt_0":0,"shirt_1":0,"shirt_2":0,"shirt_3":0,"shirt_4":0,"shirt_5":0,"shirt_6":0,"shirt_7":0,"shirt_8":0,"shirt_9":0,"shirt_10":0,"shirt_11":0,"shirt_12":0,"shirt_13":0,"extra_attack_1":false,"extra_attack_2":false,"extra_attack_3":false,"extra_attack_4":false,"extra_attack_5":false,"extra_attack_6":false,"extra_attack_7":false,"extra_attack_8":false,"extra_attack_9":false,"extra_attack_10":false}],"trainings":[{"verse":5,"team_id":"pippo","special_player_shirt":0,"goalkeepers_defence":0,"goalkeepers_speed":0,"goalkeepers_pass":0,"goalkeepers_shoot":0,"goalkeepers_endurance":0,"defenders_defence":0,"defenders_speed":0,"defenders_pass":0,"defenders_shoot":0,"defenders_endurance":0,"midfielders_defence":0,"midfielders_speed":0,"midfielders_pass":0,"midfielders_shoot":0,"midfielders_endurance":0,"attackers_defence":0,"attackers_speed":0,"attackers_pass":0,"attackers_shoot":0,"attackers_endurance":0,"special_player_defence":0,"special_player_speed":0,"special_player_pass":0,"special_player_shoot":0,"special_player_endurance":0}]}` {
 		t.Fatalf("Wrong %v", string(data))
 	}
 }
@@ -43,24 +43,24 @@ func TestUnmarshal(t *testing.T) {
 
 func TestIpfsPushAndPull(t *testing.T) {
 	var ua useractions.UserActions
-	tactic := storage.VerseTactic{}
-	tactic.Tactic.TeamID = "ciao"
+	tactic := storage.Tactic{}
+	tactic.TeamID = "ciao"
 	ua.Tactics = append(ua.Tactics, tactic)
 	cif, err := ua.PushToIpfs("localhost:5001")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if cif != "QmNuETdJchencgidaFNtgZwuneHDvNVKq1rekvN3NoZjPx" {
+	if cif != "QmS8S6a3uesR2N4sYMc18yz5yP6Wcge84L2xWxQW2EVaMJ" {
 		t.Fatalf("Wrong cif %v", cif)
 	}
-	training := storage.VerseTraining{}
-	training.Training.TeamID = "pippo"
+	training := storage.Training{}
+	training.TeamID = "pippo"
 	ua.Trainings = append(ua.Trainings, training)
 	cif, err = ua.PushToIpfs("localhost:5001")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if cif != "QmUvykKWb88kqBiSgS9G7pmh3Z5X1R5S4AE2bk9g91LunM" {
+	if cif != "QmQgu5v92T8vD9xbxVP4tfyBEexKNDiTwgLPZYLZNhH75K" {
 		t.Fatalf("Wrong cif %v", cif)
 	}
 	var ua2 useractions.UserActions
