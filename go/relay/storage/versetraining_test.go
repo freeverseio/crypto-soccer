b6755e2972b491e6c34a3777239824ff506799d1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-22 09:41:23 +0100

    [relay] added the test for verse training

---
+++ go/relay/storage/versetraining_test.go
@@ -0,0 +1,31 @@
+package storage_test
+
+import (
+	"testing"
+
+	"github.com/freeverseio/crypto-soccer/go/relay/storage"
+)
+
+func TestVerseTrainingInsert(t *testing.T) {
+	tx, err := db.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tx.Rollback()
+
+	training := storage.VerseTraining{}
+	training.Verse = 1
+	training.Training.TeamID = "4"
+	err = training.Insert(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result, err := storage.VerseTrainingByVerse(tx, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(result) != 1 {
+		t.Fatalf("Expected 1 received %v", len(result))
+	}
+}
