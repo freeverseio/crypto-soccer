b9b2f8ff69cc5abe9f18cc4ff6feb04e29d62fc0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-23 07:20:01 +0100

    [relay.db] removed update, create column verse

--- relay.db/01_schema_smart_comments.sql
+++ relay.db/01_schema_smart_comments.sql
@@ -1,2 +1,9 @@
 comment on table tactics is E'@omit delete';
+comment on column tactics.verse is E'@omit create';
+comment on column tactics.verse is E'@omit update';
+
 comment on table trainings is E'@omit delete';
+comment on column trainings.verse is E'@omit create';
+comment on column trainings.verse is E'@omit update';
+
+
