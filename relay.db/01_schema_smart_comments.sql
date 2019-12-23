5c8dbbd66df854053f4d4b3f15d04c33d3a7c8ba
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-23 07:59:14 +0100

    [relay.api] removed dafault mutations

--- relay.db/01_schema_smart_comments.sql
+++ relay.db/01_schema_smart_comments.sql
@@ -1,10 +1,6 @@
-comment on table tactics is E'@omit delete';
-comment on column tactics.verse is E'@omit create';
-comment on column tactics.verse is E'@omit update';
+-- comment on table tactics is E'@omit delete,update,create';
 
-comment on table trainings is E'@omit delete';
-comment on column trainings.verse is E'@omit create';
-comment on column trainings.verse is E'@omit update';
+-- comment on table trainings is E'@omit delete,update,create';
 
 
 
