bf12d780efedd35820e32d599128663c9addcf21
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2019-12-24 10:30:09 +0100

    [relay] create upcomings view

--- relay.db/01_schema_smart_comments.sql
+++ relay.db/01_schema_smart_comments.sql
@@ -1,6 +1,7 @@
 -- comment on table tactics is E'@omit delete,update,create';
 
 -- comment on table trainings is E'@omit delete,update,create';
+-- comment on view upcoming_trainings is E'@omit delete,update,create';
 
 
 
