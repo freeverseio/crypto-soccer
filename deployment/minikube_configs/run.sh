40a816cc2c3d33232e1a35ae3cd3c59fde9a8c9d
eaylon <eaylon@dolby.com>
2019-12-24 12:09:58 +0100

    add configuration to run ethereum image from dockerhub

--- deployment/minikube_configs/run.sh
+++ deployment/minikube_configs/run.sh
@@ -1,6 +1,11 @@
-UNIVERSE_NAMESPACE=universe
+NAMESPACE=freeverse
+DOCKER_REGISTRY_SERVER=docker.io
+DOCKER_USER=freeversedigitalocean
+DOCKER_PASSWORD=fdggreTRGDSBw45ergseth4hDGHD
 
-kubectl create ns ${UNIVERSE_NAMESPACE}
-kubectl apply -f configmap.yaml -n ${UNIVERSE_NAMESPACE}
-kubectl apply -f universedb.yaml -n ${UNIVERSE_NAMESPACE}
-kubectl apply -f universeapi.yaml -n ${UNIVERSE_NAMESPACE}
+kubectl create ns ${NAMESPACE}
+kubectl create secret docker-registry docker-secret --docker-server=${DOCKER_REGISTRY_SERVER} --docker-username=${DOCKER_USER} --docker-password=${DOCKER_PASSWORD} --docker-email=freeversedigitalocean@freeverse.io -n ${NAMESPACE}
+kubectl apply -f configmap.yaml -n ${NAMESPACE}
+kubectl apply -f universedb.yaml -n ${NAMESPACE}
+kubectl apply -f universeapi.yaml -n ${NAMESPACE}
+kubectl apply -f ethereum.yaml -n ${NAMESPACE}
