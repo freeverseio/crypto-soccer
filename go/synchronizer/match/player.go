c219e3905bd49af3867575379b5c5d2bf34fcf0a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-01-03 22:17:12 +0100

    [sync] created team_test

--- go/synchronizer/match/player.go
+++ go/synchronizer/match/player.go
@@ -6,8 +6,14 @@ type Player struct {
 	state *big.Int
 }
 
-func NewPlayer() *Player {
+func NewPlayer(state string) *Player {
 	var player Player
-	player.state = big.NewInt(0)
+	player.state, _ = new(big.Int).SetString(state, 10)
+	return &player
+}
+
+func NewPlayerDummy() *Player {
+	var player Player
+	player.state, _ = new(big.Int).SetString("3618502788679706519584493278137328010759678544985289844045583163109752700928", 10)
 	return &player
 }
