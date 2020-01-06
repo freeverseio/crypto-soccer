c64f88c19c62bb1c1689ee0b8c805495c5667f45
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-01-06 21:07:14 +0100

    [sync] test for NewPlayer

--- go/synchronizer/process/match/player_test.go
+++ go/synchronizer/process/match/player_test.go
@@ -1,6 +1,7 @@
 package match_test
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/freeverseio/crypto-soccer/go/synchronizer/process/match"
@@ -12,7 +13,7 @@ func TestDefenceOfNullPlayer(t *testing.T) {
 	p := match.NewNullPlayer()
 	defence, err := p.Defence(bc.Contracts.Assets)
 	assert.NilError(t, err)
-	assert.Equal(t, defence, uint64(0))
+	assert.Equal(t, defence, uint16(0))
 }
 
 func TestDefenceOfPlayer(t *testing.T) {
@@ -20,5 +21,54 @@ func TestDefenceOfPlayer(t *testing.T) {
 	p := match.NewPlayerFromSkills("14606253788909032162646379450304996475079674564248175")
 	defence, err := p.Defence(bc.Contracts.Assets)
 	assert.NilError(t, err)
-	assert.Equal(t, defence, uint64(955))
+	assert.Equal(t, defence, uint16(955))
+}
+
+func TestNewPlayer(t *testing.T) {
+	t.Parallel()
+	defence := uint16(50)
+	speed := uint16(50)
+	endurance := uint16(50)
+	pass := uint16(50)
+	shoot := uint16(50)
+	dayOfBirth := uint16(0)
+	generation := uint8(0)
+	playerID := big.NewInt(2132321)
+	potential := uint8(3)
+	forwardness := uint8(0)
+	leftishness := uint8(0)
+	aggressiveness := uint8(0)
+	alignedEndOfLastHalf := true
+	redCardLastGame := false
+	gamesNonStopping := uint8(0)
+	injuryWeeksLeft := uint8(0)
+	substitutedLastHalf := false
+	p, err := match.NewPlayer(
+		bc.Contracts,
+		defence,
+		speed,
+		endurance,
+		pass,
+		shoot,
+		dayOfBirth,
+		generation,
+		playerID,
+		potential,
+		forwardness,
+		leftishness,
+		aggressiveness,
+		alignedEndOfLastHalf,
+		redCardLastGame,
+		gamesNonStopping,
+		injuryWeeksLeft,
+		substitutedLastHalf,
+	)
+	assert.NilError(t, err)
+	assert.Equal(t, p.Skills().String(), "730756529746917314243487289600561848950174482825266")
+	value, err := p.Defence(bc.Contracts.Assets)
+	assert.NilError(t, err)
+	assert.Equal(t, value, defence)
+	value, err = p.Speed(bc.Contracts.Assets)
+	assert.NilError(t, err)
+	assert.Equal(t, value, speed)
 }
