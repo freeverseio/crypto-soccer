fb5be6f565dded86038832423caa6e3b80330c4d
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-01-06 18:26:37 +0100

    [sync] player has more decoded skillsW

--- go/synchronizer/process/match/player_test.go
+++ go/synchronizer/process/match/player_test.go
@@ -7,9 +7,16 @@ import (
 	"gotest.tools/assert"
 )
 
-func TestSkillsOfNullPlayer(t *testing.T) {
+func TestDefenceOfNullPlayer(t *testing.T) {
 	p := match.NewNullPlayer()
 	defence, err := p.Defence(bc.Contracts.Assets)
 	assert.NilError(t, err)
 	assert.Equal(t, defence, uint64(0))
+}
+
+func TestDefenceOfPlayer(t *testing.T) {
+	p := match.NewPlayer("14606253788909032162646379450304996475079674564248175")
+	defence, err := p.Defence(bc.Contracts.Assets)
+	assert.NilError(t, err)
+	assert.Equal(t, defence, uint64(955))
 }
