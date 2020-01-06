09d82272a6652bf6a723fa541057ccb875ea4c41
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-01-06 13:30:33 +0100

    [sync] NewNullPlayer factory function

--- go/synchronizer/process/match/player.go
+++ go/synchronizer/process/match/player.go
@@ -11,3 +11,9 @@ func NewPlayer(skills string) *Player {
 	player.skills, _ = new(big.Int).SetString(skills, 10)
 	return &player
 }
+
+func NewNullPlayer() *Player {
+	player := Player{}
+	player.skills = big.NewInt(0)
+	return &player
+}
