dd315027227ec0313ed1e2b7bd3b95296444469a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-01-10 14:48:02 +0100

    [dashboard] to prod server

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -13,11 +13,11 @@ const privilegedJSON = require("./contracts/Privileged.json");
 const marketJSON = require("./contracts/Market.json");
 
 // const web3 = new Web3(Web3.givenProvider || 'http://165.22.66.118:8545');
-const web3 = new Web3('http://dev1.gorengine.com:8545');
+const web3 = new Web3('http://prod.gorengine.com:8545');
 const privileged = new web3.eth.Contract(privilegedJSON.abi, "0x615668099Cc46D035b3c34aCdf01204Ac4A4F446");
 const market = new web3.eth.Contract(marketJSON.abi, "0xFB1436D488726D64a0441081D508b238fF756802");
 
-const url = 'http://dev1.gorengine.com:4000/graphiql';
+const url = 'http://prod.gorengine.com:4000/graphiql';
 const client = new ApolloClient({
   uri: url,
 });
