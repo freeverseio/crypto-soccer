6c1406274b8a02292b3b3dcc8f5d2a5008d10757
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-02-03 12:23:49 +0100

    [go|sync|engine] removed unuseful code

--- go/synchronizer/engine/matches.go
+++ go/synchronizer/engine/matches.go
@@ -56,48 +56,6 @@ func NewMatchesFromTimezoneIdxMatchdayIdx(
 	return &matches, nil
 }
 
-func NewMatchesFromTimezoneIdxCountryIdxLeagueIdxMatchdayIdx(
-	tx *sql.Tx,
-	timezoneIdx uint8,
-	countryIdx uint32,
-	leagueIdx uint32,
-	day uint8,
-) (*Matches, error) {
-	stoMatches, err := storage.MatchesByTimezoneIdxCountryIdxLeagueIdxMatchdayIdx(tx, timezoneIdx, countryIdx, leagueIdx, day)
-	if err != nil {
-		return nil, err
-	}
-
-	var matches Matches
-	for _, stoMatch := range stoMatches {
-		stoHomeTeam, err := storage.TeamByTeamId(tx, stoMatch.HomeTeamID)
-		if err != nil {
-			return nil, err
-		}
-		stoVisitorTeam, err := storage.TeamByTeamId(tx, stoMatch.VisitorTeamID)
-		if err != nil {
-			return nil, err
-		}
-		stoHomePlayers, err := storage.PlayersByTeamId(tx, stoMatch.HomeTeamID)
-		if err != nil {
-			return nil, err
-		}
-		stoVisitorPlayers, err := storage.PlayersByTeamId(tx, stoMatch.VisitorTeamID)
-		if err != nil {
-			return nil, err
-		}
-		match := NewMatchFromStorage(
-			stoMatch,
-			stoHomeTeam,
-			stoVisitorTeam,
-			stoHomePlayers,
-			stoVisitorPlayers,
-		)
-		matches = append(matches, *match)
-	}
-	return &matches, nil
-}
-
 func (b Matches) Play1stHalf(ctx context.Context, contracts contracts.Contracts) error {
 	for _, match := range b {
 		if err := match.Play1stHalf(contracts); err != nil {
