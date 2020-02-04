1d4d10a91d8fd2ac86c0c915d333e237810f7092
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-02-04 12:57:54 +0100

    [go|sync] better error on name creation error

--- go/synchronizer/process/division_creation_processor.go
+++ go/synchronizer/process/division_creation_processor.go
@@ -3,6 +3,7 @@ package process
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"math/big"
 
 	"github.com/freeverseio/crypto-soccer/go/names"
@@ -201,9 +202,9 @@ func (b *DivisionCreationProcessor) storeVirtualPlayersForTeam(tx *sql.Tx, opts 
 		if err != nil {
 			log.Warning(err)
 		}
-		name, err := b.namesGenerator.GeneratePlayerFullName(playerId, generation, timezone, countryIdx.Uint64())
+		name, err := b.namesGenerator.GeneratePlayerFullName(playerId, generation, timezone-1, countryIdx.Uint64())
 		if err != nil {
-			name = "Mister Random"
+			return fmt.Errorf("%s playerId: %v, generation: %v, timezone: %v, countryIdx %v", err.Error(), playerId, generation, timezone-1, countryIdx.Uint64())
 			log.Warning(err)
 		}
 		player := storage.Player{
