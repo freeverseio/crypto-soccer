dc5db46bcb941e498bf4e77e433eb95fe8b685de
Toni <toni.mateos@gmail.com>
2020-02-10 15:07:28 +0100

    typo corrected

--- truffle-core/test/Assets.js
+++ truffle-core/test/Assets.js
@@ -341,7 +341,7 @@ contract('Assets', (accounts) => {
         await assets.transferFirstBotToAddr(tz1, countryIdxInTZ1, ALICE).should.be.fulfilled;
         await assets.transferFirstBotToAddr(tz2, countryIdxInTZ2, BOB).should.be.fulfilled;
         tx = await assets.transferPlayer(playerId, teamId2).should.be.fulfilled;
-        truffleAssert.eventEmitted(tx, "PlayerStateChange", (event) => {
+        truffleAssert.eventEmitted(tx, "PlayerStateChange", async (event) => {
             const currentTeam = await assets.getCurrentTeamId(event.state).should.be.fulfilled;
             return event.playerId.should.be.bignumber.equal(playerId) && currentTeam.should.be.bignumber.equal(teamId2);
         });
