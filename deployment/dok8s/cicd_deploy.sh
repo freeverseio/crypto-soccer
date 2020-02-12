e85e9220ebed99d9ac723aaeac22f613954fcf34
eaylon <eaylon@dolby.com>
2020-02-12 20:33:30 +0100

    modify script to clean up and re-deploy everytime

--- deployment/dok8s/cicd_deploy.sh
+++ deployment/dok8s/cicd_deploy.sh
@@ -7,38 +7,55 @@ MY_DIR=`cd "$MY_DIR" ; pwd`
 
 NAMESPACE=freeverse # TODO: pass as argument so we can use the same script to deploy to testing namespace or production namespace
 
-echo -- deploying ingress,configmap,universedb,universeapi,ipsnode,marketdb,trader,notary
-kubectl apply -f ${MY_DIR}/ingress.yaml      -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/configmap.yaml    -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/universedb.yaml   -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/universeapi.yaml  -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/ipfsnode.yaml     -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/marketdb.yaml     -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/trader.yaml       -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/notary.yaml       -n ${NAMESPACE}
+clean()
+{
+    echo -- removing deployments
+    kubectl delete -f ${MY_DIR}/ingress.yaml      -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/configmap.yaml    -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/universedb.yaml   -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/universeapi.yaml  -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/ipfsnode.yaml     -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/marketdb.yaml     -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/trader.yaml       -n ${NAMESPACE}
+    kubectl delete -f ${MY_DIR}/notary.yaml       -n ${NAMESPACE}
+}
 
-echo -- waiting for pods to be ready...
-kubectl wait --for=condition=available --timeout=600s deployment/universedb -n ${NAMESPACE}
-UNIVERSEDB_POD=$(kubectl get pod -l app=universedb -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
+deploy()
+{
+    echo -- deploying ingress,configmap,universedb,universeapi,ipsnode,marketdb,trader,notary
+    kubectl apply -f ${MY_DIR}/ingress.yaml      -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/configmap.yaml    -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/universedb.yaml   -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/universeapi.yaml  -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/ipfsnode.yaml     -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/marketdb.yaml     -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/trader.yaml       -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/notary.yaml       -n ${NAMESPACE}
 
-kubectl wait --for=condition=available --timeout=600s deployment/universeapi -n ${NAMESPACE}
-UNIVERSEAPI_POD=$(kubectl get pod -l app=universeapi -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
+    echo -- waiting for pods to be ready...
+    kubectl wait --for=condition=available --timeout=600s deployment/universedb -n ${NAMESPACE}
+    UNIVERSEDB_POD=$(kubectl get pod -l app=universedb -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
 
-kubectl wait --for=condition=available --timeout=600s deployment/ipfsnode -n ${NAMESPACE}
-IPFSNODE_POD=$(kubectl get pod -l app=ipfsnode -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
+    kubectl wait --for=condition=available --timeout=600s deployment/universeapi -n ${NAMESPACE}
+    UNIVERSEAPI_POD=$(kubectl get pod -l app=universeapi -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
 
-kubectl wait --for=condition=available --timeout=600s deployment/trader -n ${NAMESPACE}
-TRADER_POD=$(kubectl get pod -l app=trader -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
+    kubectl wait --for=condition=available --timeout=600s deployment/ipfsnode -n ${NAMESPACE}
+    IPFSNODE_POD=$(kubectl get pod -l app=ipfsnode -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
 
-kubectl wait --for=condition=Ready --timeout=600s pod/${UNIVERSEDB_POD}  -n ${NAMESPACE}
-kubectl wait --for=condition=Ready --timeout=600s pod/${UNIVERSEAPI_POD} -n ${NAMESPACE}
-kubectl wait --for=condition=Ready --timeout=600s pod/${IPFSNODE_POD}    -n ${NAMESPACE}
-kubectl wait --for=condition=Ready --timeout=600s pod/${TRADER_POD}      -n ${NAMESPACE}
+    kubectl wait --for=condition=available --timeout=600s deployment/trader -n ${NAMESPACE}
+    TRADER_POD=$(kubectl get pod -l app=trader -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
+
+    kubectl wait --for=condition=Ready --timeout=600s pod/${UNIVERSEDB_POD}  -n ${NAMESPACE}
+    kubectl wait --for=condition=Ready --timeout=600s pod/${UNIVERSEAPI_POD} -n ${NAMESPACE}
+    kubectl wait --for=condition=Ready --timeout=600s pod/${IPFSNODE_POD}    -n ${NAMESPACE}
+    kubectl wait --for=condition=Ready --timeout=600s pod/${TRADER_POD}      -n ${NAMESPACE}
 
 
-echo -- deploying relayactions, synchronizer and horizon
-kubectl apply -f ${MY_DIR}/relayactions.yaml -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/synchronizer.yaml -n ${NAMESPACE}
-kubectl apply -f ${MY_DIR}/horizon.yaml      -n ${NAMESPACE}
+    echo -- deploying relayactions, synchronizer and horizon
+    kubectl apply -f ${MY_DIR}/relayactions.yaml -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/synchronizer.yaml -n ${NAMESPACE}
+    kubectl apply -f ${MY_DIR}/horizon.yaml      -n ${NAMESPACE}
+}
 
-kubectl get pods -n ${NAMESPACE}
+clean
+deploy
