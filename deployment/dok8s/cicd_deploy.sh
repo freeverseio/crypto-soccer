6459d804a2e9295bdf774bd185f83cc615c12ca1
eaylon <eaylon@dolby.com>
2020-02-12 16:57:33 +0100

    remove unneeded commands

--- deployment/dok8s/cicd_deploy.sh
+++ deployment/dok8s/cicd_deploy.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
-# you must set the following environmental variables (take values from your kube/config):
-#KUBERNETES_CLUSTER_CERTIFICATE
-#KUBERNETES_SERVER
-#KUBERNETES_TOKEN
 
 set -e
 
@@ -10,20 +6,6 @@ MY_DIR=`dirname "$0"`
 MY_DIR=`cd "$MY_DIR" ; pwd`
 
 NAMESPACE=freeverse # TODO: pass as argument so we can use the same script to deploy to testing namespace or production namespace
-#KUBERNETES_TOKEN=$(kubectl get secret -n ${NAMESPACE} $(kubectl get secret -n ${NAMESPACE} | grep cicd-token | awk '{print $1}') -o jsonpath='{.data.token}' | base64 --decode)
-
-#echo "${KUBERNETES_CLUSTER_CERTIFICATE}" | base64 --decode > cert.crt
-#echo "${KUBERNETES_CLUSTER_CERTIFICATE}" | base64 -d > cert.crt
-#KUBECTL="kubectl --kubeconfig=/dev/null --server=${KUBERNETES_SERVER} --certificate-authority=cert.crt --token=${KUBERNETES_TOKEN}"
-
-# example from https://www.digitalocean.com/community/tutorials/how-to-automate-deployments-to-digitalocean-kubernetes-with-circleci
-#envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
-#kubectl \
-#  --kubeconfig=/dev/null \
-#  --server=${KUBERNETES_SERVER} \
-#  --certificate-authority=cert.crt \
-#  --token=${KUBERNETES_TOKEN} \
-#  get pods -n ${NAMESPACE}
 
 echo -- deploying ingress,configmap,universedb,universeapi,ipsnode,marketdb,trader,notary
 kubectl apply -f ${MY_DIR}/ingress.yaml      -n ${NAMESPACE}
@@ -60,5 +42,3 @@ kubectl apply -f ${MY_DIR}/synchronizer.yaml -n ${NAMESPACE}
 kubectl apply -f ${MY_DIR}/horizon.yaml      -n ${NAMESPACE}
 
 kubectl get pods -n ${NAMESPACE}
-
-#rm cert.crt
