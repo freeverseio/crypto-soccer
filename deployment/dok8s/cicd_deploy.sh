9c847d668344576331ffcc2981125cdb568fb933
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-02-19 10:07:18 +0100

    [k8s] removed waiting for pods

--- deployment/dok8s/cicd_deploy.sh
+++ deployment/dok8s/cicd_deploy.sh
@@ -44,27 +44,6 @@ deploy()
     kubectl apply -f ${MY_DIR}/marketdb.yaml     -n ${NAMESPACE}
     kubectl apply -f ${MY_DIR}/trader.yaml       -n ${NAMESPACE}
     kubectl apply -f ${MY_DIR}/notary.yaml       -n ${NAMESPACE}
-
-    echo -- waiting for pods to be ready...
-    kubectl wait --for=condition=available --timeout=600s deployment/universedb -n ${NAMESPACE}
-    UNIVERSEDB_POD=$(kubectl get pod -l app=universedb -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
-
-    kubectl wait --for=condition=available --timeout=600s deployment/universeapi -n ${NAMESPACE}
-    UNIVERSEAPI_POD=$(kubectl get pod -l app=universeapi -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
-
-    kubectl wait --for=condition=available --timeout=600s deployment/ipfsnode -n ${NAMESPACE}
-    IPFSNODE_POD=$(kubectl get pod -l app=ipfsnode -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
-
-    kubectl wait --for=condition=available --timeout=600s deployment/trader -n ${NAMESPACE}
-    TRADER_POD=$(kubectl get pod -l app=trader -n ${NAMESPACE} -o jsonpath="{.items[0].metadata.name}")
-
-    kubectl wait --for=condition=Ready --timeout=600s pod/${UNIVERSEDB_POD}  -n ${NAMESPACE}
-    kubectl wait --for=condition=Ready --timeout=600s pod/${UNIVERSEAPI_POD} -n ${NAMESPACE}
-    kubectl wait --for=condition=Ready --timeout=600s pod/${IPFSNODE_POD}    -n ${NAMESPACE}
-    kubectl wait --for=condition=Ready --timeout=600s pod/${TRADER_POD}      -n ${NAMESPACE}
-
-
-    echo -- deploying relayactions, synchronizer and horizon
     kubectl apply -f ${MY_DIR}/relayactions.yaml -n ${NAMESPACE}
     kubectl apply -f ${MY_DIR}/synchronizer.yaml -n ${NAMESPACE}
     kubectl apply -f ${MY_DIR}/horizon.yaml      -n ${NAMESPACE}
