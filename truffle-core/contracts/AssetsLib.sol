54392aef20dedb129d6a711749fd3a8852a41c58
Toni <toni.mateos@gmail.com>
2020-03-02 15:08:09 +0100

    starting to migrate

--- truffle-core/contracts/AssetsLib.sol
+++ truffle-core/contracts/AssetsLib.sol
@@ -23,7 +23,7 @@ contract AssetsLib is Storage, EncodingSkillsGetters, EncodingIDs {
     function getOwnerTeamInCountry(uint8 timeZone, uint256 countryIdxInTZ, uint256 teamIdxInCountry) public view returns(address) {
         _assertTZExists(timeZone);
         _assertCountryInTZExists(timeZone, countryIdxInTZ);
-        return _timeZones[timeZone].countries[countryIdxInTZ].teamIdxInCountryToOwner[teamIdxInCountry];
+        return teamIdToOwner[encodeTZCountryAndVal(timeZone, countryIdxInTZ, teamIdxInCountry)];
     }
 
     function _assertCountryInTZExists(uint8 timeZone, uint256 countryIdxInTZ) internal view {
