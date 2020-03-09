32e2f17639adba3c00eedac3dfc5895881f2cdb1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-09 23:16:51 +0100

    [dashboard] prepare to release in development

--- dashboard/src/views/Header.jsx
+++ dashboard/src/views/Header.jsx
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import { Menu, Input } from 'semantic-ui-react'
+import { Menu, Icon, Header } from 'semantic-ui-react'
 
 class TopBar extends Component {
     render() {
-        const { url, onUrlChange } = this.props;
+        const { url } = this.props;
         const location = this.props.location.pathname;
 
         return (
-            <Menu pointing >
+            <Menu pointing>
                 <Menu.Item as={Link} to="/" active={location === '/'} >
                     <img src='/logo62.png' alt="not found"/>
                 </Menu.Item>
                 <Menu.Item as={Link} to="/academy" name='Academy' active={location === '/academy'} />
                 <Menu.Item as={Link} to="/shop" name='Shop' active={location === '/shop'} />
                 <Menu.Item as={Link} to="/teams" name='Teams' active={location === '/teams'} />
-                <Menu.Menu position='right'>
-                    <Menu.Item>
-                        <Input
-                            icon='database'
-                            iconPosition='left'
-                            placeholder='Horizon URL'
-                            value={url}
-                            onChange={event => onUrlChange(event.target.value)}
-                        />
-                    </Menu.Item>
-                </Menu.Menu>
+                <Menu.Item position='right'>
+                    <Icon name='database' />
+                    <Header as='h5' floated='right'>
+                        <Header.Content>{url}</Header.Content>
+                    </Header>
+                </Menu.Item>
             </Menu>
         )
     }
