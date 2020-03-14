0a0d974a51aa7429540d29292e8d55a65a8b8eb7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-14 09:43:43 +0100

    [go|sync|matchevents] starting with testing

---
+++ go/synchronizer/matchevents/matchevents_test.go
@@ -0,0 +1,13 @@
+package matchevents_test
+
+// func TestMatchEventsNewMatchEvents(t *testing.T) {
+// 	homeTeamID := "1"
+// 	visitorTeamID := "2"
+// 	homeTactics := big.NewInt("534534534")
+// 	homeTactics := big.NewInt("534534534")
+// 	events, err := matchevents.NewMatchEvents(
+// 		bc.Contracts,
+// 		homeTeamID,
+// 		visitorTeamID,
+// 	)
+// }
