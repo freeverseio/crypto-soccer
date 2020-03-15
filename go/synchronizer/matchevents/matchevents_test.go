c68804d0e5b91279bc5cfe5c87fbf61fdaca7688
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-15 13:55:12 +0100

    [go|sync|matchevents] removed test

--- go/synchronizer/matchevents/matchevents_test.go
+++ go/synchronizer/matchevents/matchevents_test.go
@@ -28,26 +28,3 @@ func TestMatchEventsNewMatchEventsNoMatchLogs(t *testing.T) {
 	)
 	assert.Error(t, err, "logAndEvents len < 2")
 }
-
-func TestMatchEventsNewMatchEventsOnlyMatchLogs(t *testing.T) {
-	verseSeed := [32]byte{0x01}
-	homeTeamID := "1"
-	visitorTeamID := "2"
-	homeTactic, _ := new(big.Int).SetString("340596594427581673436941882753025", 10)
-	visitorTactic, _ := new(big.Int).SetString("340596594427581673436941882753025", 10)
-	logsAndEvents := []*big.Int{}
-	logsAndEvents[0], _ = new(big.Int).SetString("1809252841225230840719990802576567867449345403164777349740842651283017957376", 10)
-	logsAndEvents[1], _ = new(big.Int).SetString("1809252842383666049074119856253627407002480579871841461333007538180245710739", 10)
-	is2ndHalf := false
-	_, err := matchevents.NewMatchEvents(
-		*bc.Contracts,
-		verseSeed,
-		homeTeamID,
-		visitorTeamID,
-		homeTactic,
-		visitorTactic,
-		logsAndEvents,
-		is2ndHalf,
-	)
-	assert.NilError(t, err)
-}
