652740c780db8157109840601b50b704549b6758
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-16 17:33:43 +0100

    [go|sync|matchevents] fix compilation

--- go/synchronizer/matchevents/matchevents_test.go
+++ go/synchronizer/matchevents/matchevents_test.go
@@ -14,6 +14,8 @@ func TestMatchEventsNewMatchEventsNoMatchLogs(t *testing.T) {
 	visitorTeamID := "2"
 	homeTactic, _ := new(big.Int).SetString("340596594427581673436941882753025", 10)
 	visitorTactic, _ := new(big.Int).SetString("340596594427581673436941882753025", 10)
+	decodedHomeMatchLog := [15]uint32{}
+	decodedVisitorMatchLog := [15]uint32{}
 	logsAndEvents := []*big.Int{}
 	is2ndHalf := false
 	_, err := matchevents.NewMatchEvents(
@@ -24,6 +26,8 @@ func TestMatchEventsNewMatchEventsNoMatchLogs(t *testing.T) {
 		homeTactic,
 		visitorTactic,
 		logsAndEvents,
+		decodedHomeMatchLog,
+		decodedVisitorMatchLog,
 		is2ndHalf,
 	)
 	assert.Error(t, err, "logAndEvents len < 2")
