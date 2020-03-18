ae538da204c976c4bdf66b85abc3a526d63d23fd
Toni <toni.mateos@gmail.com>
2020-03-18 18:01:28 +0100

    first test

--- truffle-core/contracts/Merkle.sol
+++ truffle-core/contracts/Merkle.sol
@@ -6,23 +6,23 @@ pragma solidity >=0.5.12 <=0.6.3;
 
 contract Merkle {
     
-    // This function will revert if nLevels = 0, unless the array is empty.
+    // This function will revert if nLevels = 0.
     // nLeafs = 2**nLevels
-    //  nLevels = 0 => 1 leafs = lev0 - root (nothing to do)
     //  nLevels = 1 => 3 leafs = lev1 - root // lev0 - 2 leafs
     //  nLevels = 2 => 7 leafs = lev2 - root // lev1 - 2 leafs // lev0 - 4 leafs
-    function merkleRoot(bytes32[] memory array, uint256 nLevels) public pure {
+    function merkleRoot(bytes32[] memory array, uint256 nLevels) public pure returns(bytes32) {
         uint256 nLeafs = 2**nLevels;
         require(array.length == nLeafs, "number of leafs is not = pow(2,nLevels)");
-        for (uint8 level = 0; level < nLevels - 1; level++) {
+        for (uint8 level = 0; level < nLevels; level++) {
             nLeafs /= 2;
-            for (uint32 pos = 0; pos < nLeafs; pos++) {
+            for (uint256 pos = 0; pos < nLeafs; pos++) {
                 array[pos] = hash_node(array[2 * pos], array[2 * pos + 1]);      
             }
         }
+        return array[0];
     }
   
-    function hash_node(bytes32 left, bytes32 right) internal pure returns (bytes32 hash) {
+    function hash_node(bytes32 left, bytes32 right) public pure returns (bytes32 hash) {
         assembly {
             mstore(0x00, left)
             mstore(0x20, right)
--- truffle-core/test/Merkle.js
+++ truffle-core/test/Merkle.js
@@ -18,12 +18,24 @@ contract('Assets', (accounts) => {
     beforeEach(async () => {
         merkle = await Merkle.new().should.be.fulfilled;
     });
-        
+
+    it('get merkle root', async () => {
+        leafs = Array.from(new Array(4), (x,i) => web3.utils.keccak256(i.toString()));
+        root = await merkle.merkleRoot(leafs, nLevels = 2).should.be.fulfilled;
+        root1 = await merkle.hash_node(leafs[0], leafs[1]).should.be.fulfilled;
+        root2 = await merkle.hash_node(leafs[2], leafs[3]).should.be.fulfilled;
+        myRoot = await merkle.hash_node(root1, root2).should.be.fulfilled;
+        myRoot.should.be.equal(root)
+    });
+
+
     it('create and verify', async () => {
         leafs = Array.from(new Array(4), (x,i) => web3.utils.keccak256(i.toString()));
-        console.log(leafs);
         root = await merkle.merkleRoot(leafs, nLevels = 2).should.be.fulfilled;
-        console.log(root);
+        leafHash = leafs[1];
+        proof2 = await merkle.hash_node(leafs[2], leafs[3]).should.be.fulfilled;
+        ok = await merkle.verify(root, [leafs[0], proof2], leafHash, 1).should.be.fulfilled;
+        ok.should.be.equal(true);
     });
 
 });
