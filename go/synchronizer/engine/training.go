7ce42b969f3923614068eae1ab3ccf327733f5a2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-19 11:26:02 +0100

    [go|sync|engine] using json format

--- go/synchronizer/engine/training.go
+++ go/synchronizer/engine/training.go
@@ -1,6 +1,10 @@
 package engine
 
-import "github.com/freeverseio/crypto-soccer/go/storage"
+import (
+	"encoding/json"
+
+	"github.com/freeverseio/crypto-soccer/go/storage"
+)
 
 type Training struct {
 	storage.Training
@@ -14,25 +18,7 @@ func NewTraining() *Training {
 
 func (b Training) ToString() string {
 	var result string
-	// result += fmt.Sprintf("seed = '0x%v';", hex.EncodeToString(b.Seed[:]))
-	// result += fmt.Sprintf("startTime = '%v';", b.StartTime)
-	// result += fmt.Sprintf("matchLog0 = '%v';", b.HomeTeam.MatchLog)
-	// result += fmt.Sprintf("teamId0 = '%v';", b.HomeTeam.TeamID)
-	// result += fmt.Sprintf("tactic0 = '%v';", b.HomeTeam.Tactic)
-	// // result += fmt.Sprintf("assignedTP0 = '%v';", b.HomeTeam.AssignedTP)
-	// result += "players0 = ["
-	// for _, player := range b.HomeTeam.Players {
-	// 	result += fmt.Sprintf("'%v',", player.EncodedSkills)
-	// }
-	// result += "];"
-	// result += fmt.Sprintf("matchLog1 = '%v';", b.VisitorTeam.MatchLog)
-	// result += fmt.Sprintf("teamId1 = '%v';", b.VisitorTeam.TeamID)
-	// result += fmt.Sprintf("tactic1 = '%v';", b.VisitorTeam.Tactic)
-	// // result += fmt.Sprintf("assignedTP1 = '%v';", b.VisitorTeam.AssignedTP)
-	// result += "players1 = ["
-	// for _, player := range b.VisitorTeam.Players {
-	// 	result += fmt.Sprintf("'%v',", player.EncodedSkills)
-	// }
-	// result += "];"
+	s, _ := json.MarshalIndent(b, "", "\t")
+	result += string(s)
 	return result
 }
