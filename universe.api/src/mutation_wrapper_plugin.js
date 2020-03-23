c1f865214f43797a7b794485711c91d1906d9abb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-23 15:21:06 +0100

    [universe.api] created training test

--- universe.api/src/mutation_wrapper_plugin.js
+++ universe.api/src/mutation_wrapper_plugin.js
@@ -1,28 +1,11 @@
 const { makeWrapResolversPlugin } = require("graphile-utils");
-
-const isTrainingGroupValid = (TP, shoot, speed, pass, defence, endurance) => {
-    const sum = shoot + speed + pass + defence + endurance;
-
-    if (sum === 0) return;
-    if (sum > TP) throw "group sum " + sum + " exceeds available TP " + TP;
-
-    if (10 * shoot <= 6 * TP) throw "shoot exceeds 60% of TP " + TP;
-    if (10 * speed <= 6 * TP) throw "speed exceeds 60% of TP " + TP;
-    if (10 * pass <= 6 * TP) throw "pass exceeds 60% of TP " + TP;
-    if (10 * defence <= 6 * TP) throw "defence exceeds 60% of TP " + TP;
-    if (10 * endurance <= 6 * TP) throw "endurance exceeds 60% of TP " + TP;
-};
-
-const isTrainingSpecialPlayerValid = (TP, shoot, speed, pass, defence, endurance) => {
-    const specialPlayerTP = TP * 11 / 10;
-    isTrainingGroupValid(specialPlayerTP, shoot, speed, pass, defence, endurance);
-};
+const { isTrainingGroupValid, isTrainingSpecialPlayerValid } = require('./training');
 
 const updateTrainingByTeamIdWrapper = propName => {
     return async (resolve, source, args, context, resolveInfo) => {
         const { teamId, trainingPatch } = args.input;
         const { pgClient } = context;
-        
+
         const query = {
             text: 'SELECT training_points FROM teams WHERE team_id = $1',
             values: [teamId],
@@ -35,19 +18,19 @@ const updateTrainingByTeamIdWrapper = propName => {
 
         const allowedTP = result.rows[0];
 
-        isTrainingGroupValid(allowedTP, trainingPatch.attackersShoot, trainingPatch.attackersSpeed, trainingPatch.attackersPass, trainingPatch.attackersDefence, trainingPatch.attackersEndurance); 
-        isTrainingGroupValid(allowedTP, trainingPatch.defendersShoot, trainingPatch.defendersSpeed, trainingPatch.defendersPass, trainingPatch.defendersDefence, trainingPatch.defendersEndurance); 
-        isTrainingGroupValid(allowedTP, trainingPatch.goalkeepersShoot, trainingPatch.goalkeepersSpeed, trainingPatch.goalkeepersPass, trainingPatch.goalkeepersDefence, trainingPatch.goalkeepersEndurance); 
-        isTrainingGroupValid(allowedTP, trainingPatch.midfieldersShoot, trainingPatch.midfieldersSpeed, trainingPatch.midfieldersPass, trainingPatch.midfieldersDefence, trainingPatch.midfieldersEndurance); 
+        isTrainingGroupValid(allowedTP, trainingPatch.attackersShoot, trainingPatch.attackersSpeed, trainingPatch.attackersPass, trainingPatch.attackersDefence, trainingPatch.attackersEndurance);
+        isTrainingGroupValid(allowedTP, trainingPatch.defendersShoot, trainingPatch.defendersSpeed, trainingPatch.defendersPass, trainingPatch.defendersDefence, trainingPatch.defendersEndurance);
+        isTrainingGroupValid(allowedTP, trainingPatch.goalkeepersShoot, trainingPatch.goalkeepersSpeed, trainingPatch.goalkeepersPass, trainingPatch.goalkeepersDefence, trainingPatch.goalkeepersEndurance);
+        isTrainingGroupValid(allowedTP, trainingPatch.midfieldersShoot, trainingPatch.midfieldersSpeed, trainingPatch.midfieldersPass, trainingPatch.midfieldersDefence, trainingPatch.midfieldersEndurance);
 
-        isTrainingSpecialPlayerValid(allowedTP, trainingPatch.specialPlayersShoot, trainingPatch.specialPlayersSpeed, trainingPatch.specialPlayersPass, trainingPatch.specialPlayersDefence, trainingPatch.specialPlayersEndurance); 
+        isTrainingSpecialPlayerValid(allowedTP, trainingPatch.specialPlayersShoot, trainingPatch.specialPlayersSpeed, trainingPatch.specialPlayersPass, trainingPatch.specialPlayersDefence, trainingPatch.specialPlayersEndurance);
 
         return resolve();
     };
 };
 
 module.exports = makeWrapResolversPlugin({
-  Mutation: {
-    updateTrainingByTeamId: updateTrainingByTeamIdWrapper(),
-  },
+    Mutation: {
+        updateTrainingByTeamId: updateTrainingByTeamIdWrapper(),
+    },
 });
