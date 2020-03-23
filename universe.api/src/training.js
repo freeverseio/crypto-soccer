a91c1012abf7816f1434700d97d3f5bbd85c9c58
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-23 15:46:56 +0100

    [universe.api] test done

--- universe.api/src/training.js
+++ universe.api/src/training.js
@@ -1,18 +1,16 @@
 const isTrainingGroupValid = (TP, shoot, speed, pass, defence, endurance) => {
     const sum = shoot + speed + pass + defence + endurance;
-
-    if (sum === 0) return;
     if (sum > TP) throw "group sum " + sum + " exceeds available TP " + TP;
 
-    if (10 * shoot <= 6 * TP) throw "shoot exceeds 60% of TP " + TP;
-    if (10 * speed <= 6 * TP) throw "speed exceeds 60% of TP " + TP;
-    if (10 * pass <= 6 * TP) throw "pass exceeds 60% of TP " + TP;
-    if (10 * defence <= 6 * TP) throw "defence exceeds 60% of TP " + TP;
-    if (10 * endurance <= 6 * TP) throw "endurance exceeds 60% of TP " + TP;
+    if (10 * shoot > 6 * TP) throw "shoot exceeds 60% of TP " + TP;
+    if (10 * speed > 6 * TP) throw "speed exceeds 60% of TP " + TP;
+    if (10 * pass > 6 * TP) throw "pass exceeds 60% of TP " + TP;
+    if (10 * defence > 6 * TP) throw "defence exceeds 60% of TP " + TP;
+    if (10 * endurance > 6 * TP) throw "endurance exceeds 60% of TP " + TP;
 };
 
 const isTrainingSpecialPlayerValid = (TP, shoot, speed, pass, defence, endurance) => {
-    const specialPlayerTP = TP * 11 / 10;
+    const specialPlayerTP = Math.floor(TP * 11 / 10);
     isTrainingGroupValid(specialPlayerTP, shoot, speed, pass, defence, endurance);
 };
 
--- universe.api/src/training.test.js
+++ universe.api/src/training.test.js
@@ -1,8 +1,67 @@
 const { isTrainingGroupValid, isTrainingSpecialPlayerValid } = require('./training');
 
 describe('training', () => {
-  test('0 group sum', () => {
-    const TP = 10;
-    expect(() => isTrainingGroupValid(TP,0,0,0,0,0)).not.toThrow();
-  });
+    describe('group', () => {
+        const TP = 10;
+
+        test('group with sum 0', () => {
+            expect(() => isTrainingGroupValid(TP, 0, 0, 0, 0, 0)).not.toThrow();
+        });
+
+        test('group with sum exceeding the TP', () => {
+            expect(() => isTrainingGroupValid(TP, 5, 5, 5, 0, 0)).toThrow("group sum 15 exceeds available TP 10");
+        });
+
+        test('each element is 60% of TP', () => {
+            expect(() => isTrainingGroupValid(TP, 6, 0, 0, 0, 0)).not.toThrow();
+            expect(() => isTrainingGroupValid(TP, 0, 6, 0, 0, 0)).not.toThrow();
+            expect(() => isTrainingGroupValid(TP, 0, 0, 6, 0, 0)).not.toThrow();
+            expect(() => isTrainingGroupValid(TP, 0, 0, 0, 6, 0)).not.toThrow();
+            expect(() => isTrainingGroupValid(TP, 0, 0, 0, 0, 6)).not.toThrow();
+        });
+
+        test('each element exceeding 60% of TP', () => {
+            expect(() => isTrainingGroupValid(TP, 7, 0, 0, 0, 0)).toThrow("shoot exceeds 60% of TP 10");
+            expect(() => isTrainingGroupValid(TP, 0, 7, 0, 0, 0)).toThrow("speed exceeds 60% of TP 10");
+            expect(() => isTrainingGroupValid(TP, 0, 0, 7, 0, 0)).toThrow("pass exceeds 60% of TP 10");
+            expect(() => isTrainingGroupValid(TP, 0, 0, 0, 7, 0)).toThrow("defence exceeds 60% of TP 10");
+            expect(() => isTrainingGroupValid(TP, 0, 0, 0, 0, 7)).toThrow("endurance exceeds 60% of TP 10");
+        });
+
+        test('group with sum < TP', () => {
+            expect(() => isTrainingGroupValid(TP, 2, 2, 2, 1, 2)).not.toThrow();
+        });
+    });
+
+    describe('special player', () => {
+        const TP = 10;
+
+        test('group with sum 0', () => {
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 0, 0, 0)).not.toThrow();
+        });
+
+        test('group with sum exceeding the TP', () => {
+            expect(() => isTrainingSpecialPlayerValid(TP, 5, 5, 5, 0, 0)).toThrow("group sum 15 exceeds available TP 11");
+        });
+
+        test('each element is 60% of TP', () => {
+            expect(() => isTrainingSpecialPlayerValid(TP, 6, 0, 0, 0, 0)).not.toThrow();
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 6, 0, 0, 0)).not.toThrow();
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 6, 0, 0)).not.toThrow();
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 0, 6, 0)).not.toThrow();
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 0, 0, 6)).not.toThrow();
+        });
+
+        test('each element exceeding 60% of TP', () => {
+            expect(() => isTrainingSpecialPlayerValid(TP, 7, 0, 0, 0, 0)).toThrow("shoot exceeds 60% of TP 11");
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 7, 0, 0, 0)).toThrow("speed exceeds 60% of TP 11");
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 7, 0, 0)).toThrow("pass exceeds 60% of TP 11");
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 0, 7, 0)).toThrow("defence exceeds 60% of TP 11");
+            expect(() => isTrainingSpecialPlayerValid(TP, 0, 0, 0, 0, 7)).toThrow("endurance exceeds 60% of TP 11");
+        });
+
+        test('group with sum < TP', () => {
+            expect(() => isTrainingSpecialPlayerValid(TP, 2, 2, 2, 1, 2)).not.toThrow();
+        });
+    });
 });
