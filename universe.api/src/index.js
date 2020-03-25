49f0edcfc15106e59cc0bae36d310f40fb0eafde
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-25 10:07:15 +0100

    [universe.api] signing into the universe.api

--- universe.api/src/index.js
+++ universe.api/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { postgraphile } = require("postgraphile");
 const Web3 = require("web3");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const assetsJSON = require("../contracts/Assets.json");
 const program = require("commander");
 const version = require("../package.json").version;
@@ -18,7 +19,7 @@ program
   .parse(process.argv)
 
 const { port, databaseUrl, ethereum, assetsContractAddress, sender } = program;
-
+const privateKey = "a98c8730d71a46bcc40fb06fc68142edbc2fdf17b89197db0fbe41d35718d5fc";
 
 console.log("--------------------------------------------------------");
 console.log("port              : ", port);
@@ -29,9 +30,12 @@ console.log("sender            : ", sender);
 console.log("--------------------------------------------------------");
 
 const app = express();
-const web3 = new Web3(ethereum);
+// const web3 = new Web3(ethereum);
+const provider = new HDWalletProvider(privateKey, ethereum);
+const web3 = new Web3(provider, null, {});
 const assets = new web3.eth.Contract(assetsJSON.abi, assetsContractAddress);
-const from = sender;
+// const from = sender;
+const from = "0xA9c0F76cA045163E28afDdFe035ec76a44f5C1F3";
 const mutationsPlugin = MutationsPlugin(assets, from);
 
 app.use(
