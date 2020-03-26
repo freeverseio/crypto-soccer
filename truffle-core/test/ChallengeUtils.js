1d1f3504c24d2b923407643e549b4c25627807a5
Toni <toni.mateos@gmail.com>
2020-03-26 10:42:03 +0100

    adding challenge utils

---
+++ truffle-core/test/ChallengeUtils.js
@@ -0,0 +1,37 @@
+const BN = require('bn.js');
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bn')(BN))
+    .should();
+const truffleAssert = require('truffle-assertions');
+const timeTravel = require('../utils/TimeTravel.js');
+const merkleUtils = require('../utils/merkleUtils.js');
+const chllUtils = require('../utils/challengeUtils.js');
+
+const ConstantsGetters = artifacts.require('ConstantsGetters');
+
+
+
+contract('Updates', (accounts) => {
+    const nullHash = web3.eth.abi.encodeParameter('bytes32','0x0');
+    
+    const it2 = async(text, f) => {};
+    
+    const moveToNextVerse = async (updates, extraSecs = 0) => {
+        now = await updates.getNow().should.be.fulfilled;
+        nextTime = await updates.getNextVerseTimestamp().should.be.fulfilled;
+        await timeTravel.advanceTime(nextTime - now + extraSecs);
+        await timeTravel.advanceBlock().should.be.fulfilled;
+    };
+
+    beforeEach(async () => {
+        constants = await ConstantsGetters.new().should.be.fulfilled;
+    });
+
+    it('test that cannot initialize updates twice', async () =>  {
+        var {0: activeTeams, 1: orgMap} = chllUtils.createUniverse(10);
+        console.log(activeTeams)
+        console.log(orgMap)
+    });
+
+});
--- truffle-core/utils/challengeUtils.js
+++ truffle-core/utils/challengeUtils.js
@@ -2,13 +2,26 @@ const NULL_BYTES32 = web3.eth.abi.encodeParameter('bytes32','0x0');
 
 const merkleUtils = require('../utils/merkleUtils.js');
 
-// returns, for a given TZ, [nActiveCountry0,... 1023]
-function createActiveTeams(nLevels) {
-  // activeTeams
-  activeTeams = Array.from(new Array(1024), (x,i) => 0);
+function sortDec(array) { 
+  arr = [...array];
+  return arr.sort(function(a, b){return b-a});
+}
 
+// returns, for a given TZ, [nActiveCountry0,... 1023]
+function createUniverse(nLevels) {
+  nCountries = 1024;
+  nTeamsInLeague = 8;
+  activeLeagues = Array.from(new Array(nCountries), (x,i) => 2 * ((i+1) % 3));
+  activeTeams = Array.from(activeLeagues, (x,i) => x * nTeamsInLeague);
+  orgMap = [];
+  for (c = 0; c < 1024; c++) {
+    teamIdxs = Array.from(new Array(activeTeams[c]), (x,i) => i);
+    if (c % 2 == 0) { orgMap = orgMap.concat(teamIdxs); }
+    else  { orgMap = orgMap.concat(sortDec(teamIdxs)); }
+  }
+  return [activeTeams, orgMap];
 }
 
   module.exports = {
-    test,
+    createUniverse,
   }
