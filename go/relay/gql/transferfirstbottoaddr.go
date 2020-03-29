facd41971ab84e55a3980fa0d84cc44cb7e19f61
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-29 17:03:40 +0200

    [relay] removed overengineered

--- go/relay/gql/transferfirstbottoaddr.go
+++ go/relay/gql/transferfirstbottoaddr.go
@@ -1,5 +1,11 @@
 package gql
 
+type TransferFirstBotToAddrInput struct {
+	Timezone             int32
+	CountryIdxInTimezone string
+	Address              string
+}
+
 func (b *Resolver) TransferFirstBotToAddr(input TransferFirstBotToAddrInput) bool {
 	if b.c != nil {
 		b.c <- input
--- go/relay/gql/transferfirstbottoaddrinput.go
+++
@@ -1,7 +0,0 @@
-package gql
-
-type TransferFirstBotToAddrInput struct {
-	Timezone             int32
-	CountryIdxInTimezone string
-	Address              string
-}
