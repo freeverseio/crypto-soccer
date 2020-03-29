2274cd6e85f2f7dcb9b49dba85bfc6e708ad4913
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-03-29 17:28:49 +0200

    [relay] warning if channel is full

--- go/relay/gql/transferfirstbottoaddr.go
+++ go/relay/gql/transferfirstbottoaddr.go
@@ -1,15 +1,25 @@
 package gql
 
+import (
+	"errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
 type TransferFirstBotToAddrInput struct {
 	Timezone             int32
 	CountryIdxInTimezone string
 	Address              string
 }
 
-func (b *Resolver) TransferFirstBotToAddr(input TransferFirstBotToAddrInput) bool {
+func (b *Resolver) TransferFirstBotToAddr(input TransferFirstBotToAddrInput) (bool, error) {
 	if b.c != nil {
-		b.c <- input
+		select {
+		case b.c <- input:
+		default:
+			log.Warning("TransferFirstBotToAddr: channel is full, discarding value")
+			return false, errors.New("channel is full, discarding value")
+		}
 	}
-
-	return true
+	return true, nil
 }
