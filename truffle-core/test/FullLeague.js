6819c1376b48a233cbb5f54122152c99c82f5d0e
Toni <toni.mateos@gmail.com>
2020-04-06 14:31:19 +0200

    everything a strings now

--- truffle-core/test/FullLeague.js
+++ truffle-core/test/FullLeague.js
@@ -323,6 +323,11 @@ contract('Evolution', (accounts) => {
         return zeroPadToLength(leafs, nLeafs);
     }
 
+    function vec2str(y) {
+        yStr = [...y];
+        for (i = 0; i < y.length; i++) yStr[i] = y[i].toString();
+        return yStr;
+    }
     
     beforeEach(async () => {
         training = await TrainingPoints.new().should.be.fulfilled;
@@ -371,8 +376,8 @@ contract('Evolution', (accounts) => {
     });
   
     // leafsLeague[128] = [Points[team=0,..,7], ML[team = 0,1; matchInDay = 0,1,2,3; matchDay = 0,..13], 0,...]
-    it2('create real data for an entire league', async () => {
-        mode = JUST_CHECK_AGAINST_EXPECTED_RESULTS; // JUST_CHECK_AGAINST_EXPECTED_RESULTS for testing, 1 WRITE_NEW_EXPECTED_RESULTS
+    it('create real data for an entire league', async () => {
+        mode = WRITE_NEW_EXPECTED_RESULTS; // JUST_CHECK_AGAINST_EXPECTED_RESULTS for testing, 1 WRITE_NEW_EXPECTED_RESULTS
         // prepare a training that is not identical to the bignumber(0), but which works irrespective of the previously earned TP
         // => all assingments to 0, but with a special player chosen
 
@@ -393,13 +398,14 @@ contract('Evolution', (accounts) => {
         // a non-null value, sorting because of all tied, which would depend on a seed.
         // we don't have that seed before a match starts, so we set all points to 0.
 
-        leagueData.seeds = Array.from(new Array(2 * nMatchdays), (x,i) => web3.utils.keccak256(i.toString()));
+        leagueData.seeds = Array.from(new Array(2 * nMatchdays), (x,i) => web3.utils.keccak256(i.toString()).toString());
         leagueData.startTimes = Array.from(new Array(2 * nMatchdays), (x,i) => now + i * secsBetweenMatches);
         allMatchLogs = Array.from(new Array(nTeamsInLeague), (x,i) => 0);
         leagueData.matchLogs.push([...allMatchLogs]);
         teamState442 = await createTeamState442(engine, forceSkills= [1000,1000,1000,1000,1000]).should.be.fulfilled;
-        allTeamsStates = Array.from(new Array(nTeamsInLeague), (x,i) => teamState442);
-        leagueData.teamStates.push([...allTeamsStates]);
+        teamState442 = vec2str(teamState442);
+        allTeamsSkills = Array.from(new Array(nTeamsInLeague), (x,i) => teamState442);
+        leagueData.teamStates.push([...allTeamsSkills]);
         // nosub = [NO_SUBST, NO_SUBST, NO_SUBST];
         // tact = await engine.encodeTactics(nosub , ro = [0, 0, 0], setNoSubstInLineUp(lineupConsecutive, nosub), extraAttackNull, tacticsId = 0).should.be.fulfilled;
         teamId = await assets.encodeTZCountryAndVal(tz = 1, countryIdxInTZ = 0, teamIdxInCountry = 0);
@@ -410,10 +416,10 @@ contract('Evolution', (accounts) => {
         leagueData.tactics.push(Array.from(new Array(nTeamsInLeague), (x,i) => 0));
         leagueData.trainings.push(Array.from(new Array(nTeamsInLeague), (x,i) => 0));
         // same tactics and trainings for all matchdays:
-        tact = tactics442NoChanges;
+        tact = tactics442NoChanges.toString();
         for (day = 0; day < 2 * nMatchdays; day++) {
             leagueData.tactics.push(Array.from(new Array(nTeamsInLeague), (x,i) => tact));
-            leagueData.trainings.push(Array.from(new Array(nTeamsInLeague), (x,i) => almostNullTraning));
+            leagueData.trainings.push(Array.from(new Array(nTeamsInLeague), (x,i) => almostNullTraning.toString()));
         }
 
         // we just need to build, across the league: teamStates, points, teamIds
@@ -427,19 +433,19 @@ contract('Evolution', (accounts) => {
                 console.log("day:", day, ", matchIdxInDay:", matchIdxInDay, ", half 0,  teams:", t0, t1);
                 var {0: newSkills, 1: newLogs} =  await play.play1stHalfAndEvolve(
                     leagueData.seeds[2 * day], leagueData.startTimes[2 * day], 
-                    [allTeamsStates[t0], allTeamsStates[t1]], 
+                    [allTeamsSkills[t0], allTeamsSkills[t1]], 
                     [leagueData.teamIds[t0], leagueData.teamIds[t1]], 
                     [leagueData.tactics[2 * day + 1][t0], leagueData.tactics[2 * day + 1][t1]], 
                     [allMatchLogs[t0], allMatchLogs[t1]],
                     [is2nd = false, isHom = true, isPlay = false],
                     [tp = 0, tp = 0]
                 ).should.be.fulfilled;
-                allTeamsStates[t0] = newSkills[0];
-                allTeamsStates[t1] = newSkills[1];
-                allMatchLogs[t0] = newLogs[0];
-                allMatchLogs[t1] = newLogs[1];
+                allTeamsSkills[t0] = vec2str(newSkills[0]);
+                allTeamsSkills[t1] = vec2str(newSkills[1]);
+                allMatchLogs[t0] = newLogs[0].toString();
+                allMatchLogs[t1] = newLogs[1].toString();
             }
-            leagueData.teamStates.push([...allTeamsStates]);        
+            leagueData.teamStates.push([...allTeamsSkills]);        
             leagueData.matchLogs.push([...allMatchLogs]);        
             // 2nd half
             for (matchIdxInDay = 0; matchIdxInDay < nMatchesPerDay; matchIdxInDay++) {
@@ -449,24 +455,24 @@ contract('Evolution', (accounts) => {
                 console.log("day:", day, ", matchIdxInDay:", matchIdxInDay, ", half 1,  teams:", t0, t1);
                 var {0: newSkills, 1: newLogs} =  await play.play2ndHalfAndEvolve(
                     leagueData.seeds[2*day + 1], leagueData.startTimes[2*day + 1], 
-                    [allTeamsStates[t0], allTeamsStates[t1]], 
+                    [allTeamsSkills[t0], allTeamsSkills[t1]], 
                     [leagueData.teamIds[t0], leagueData.teamIds[t1]], 
                     [leagueData.tactics[2 * day + 1][t0], leagueData.tactics[2 * day + 1][t1]], 
                     [allMatchLogs[t0], allMatchLogs[t1]],
                     [is2nd = true, isHom = true, isPlay = false]
                 ).should.be.fulfilled;
-                allTeamsStates[t0] = newSkills[0];
-                allTeamsStates[t1] = newSkills[1];
-                allMatchLogs[t0] = newLogs[0];
-                allMatchLogs[t1] = newLogs[1]; 
+                allTeamsSkills[t0] = vec2str(newSkills[0]);
+                allTeamsSkills[t1] = vec2str(newSkills[1]);
+                allMatchLogs[t0] = newLogs[0].toString();
+                allMatchLogs[t1] = newLogs[1].toString(); 
                 goals0 = await encodeLog.getNGoals(newLogs[0]).should.be.fulfilled;
                 goals1 = await encodeLog.getNGoals(newLogs[1]).should.be.fulfilled;
                 leagueData.results[nMatchesPerDay * day + matchIdxInDay] = [goals0.toNumber(), goals1.toNumber()];
             }
-            leagueData.teamStates.push([...allTeamsStates]);        
+            leagueData.teamStates.push([...allTeamsSkills]);        
             leagueData.matchLogs.push([...allMatchLogs]);   
             var {0: rnking, 1: lPoints} = await champs.computeLeagueLeaderBoard([...leagueData.results], day, leagueData.seeds[2*day + 1]).should.be.fulfilled;
-            leagueData.points.push(lPoints);   
+            leagueData.points.push(vec2str(lPoints));   
         }
         if (mode == WRITE_NEW_EXPECTED_RESULTS) {
             fs.writeFileSync('test/testdata/fullleague.json', JSON.stringify(leagueData), function(err) {
@@ -504,7 +510,7 @@ contract('Evolution', (accounts) => {
         assert.equal(JSON.stringify(expectedLeafs), JSON.stringify(leafs), "leafs do not coincide with expected");
     });
     
-    it('test day 0, half 0', async () => {
+    it2('test day 0, half 0', async () => {
         leafs = JSON.parse(fs.readFileSync('test/testdata/leafsPerHalf.json', 'utf8'));
         assert.equal(leafs.length, nMatchdays * 2);
         assert.equal(leafs[0].length, nLeafs);
