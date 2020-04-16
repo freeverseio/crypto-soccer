60557d503174f28eb4e773b3a2cdd54226be3a4c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-04-16 19:09:18 +0200

    [go|notary] implemented bid resolver

--- go/notary/producer/gql/input/createbidinput.go
+++ go/notary/producer/gql/input/createbidinput.go
@@ -8,6 +8,9 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/freeverseio/crypto-soccer/go/contracts"
+	"github.com/freeverseio/crypto-soccer/go/notary/signer"
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
 	"github.com/graph-gophers/graphql-go"
 )
 
@@ -25,10 +28,48 @@ func (b CreateBidInput) ID() string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
-func (b CreateBidInput) Hash() (common.Hash, error) {
+func (b CreateBidInput) Hash(
+	contracts contracts.Contracts,
+	auction storage.Auction,
+) (common.Hash, error) {
 	teamId, _ := new(big.Int).SetString(b.TeamId, 10)
 	if teamId == nil {
 		return common.Hash{}, errors.New("invalid teamId")
 	}
-	return common.Hash{}, nil
+	playerId, _ := new(big.Int).SetString(auction.PlayerID, 10)
+	if playerId == nil {
+		return common.Hash{}, errors.New("invalid playerId")
+	}
+	isOffer2StartAuction := false
+	hash, err := signer.HashBidMessage(
+		contracts.Market,
+		uint8(auction.CurrencyID),
+		big.NewInt(int64(auction.Price)),
+		big.NewInt(int64(auction.Rnd)),
+		auction.ValidUntil,
+		playerId,
+		big.NewInt(int64(b.ExtraPrice)),
+		big.NewInt(int64(b.Rnd)),
+		teamId,
+		isOffer2StartAuction,
+	)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	return common.Hash(hash), nil
+}
+
+func (b CreateBidInput) VerifySignature(
+	contracts contracts.Contracts,
+	auction storage.Auction,
+) (bool, error) {
+	hash, err := b.Hash(contracts, auction)
+	if err != nil {
+		return false, err
+	}
+	sign, err := hex.DecodeString(b.Signature)
+	if err != nil {
+		return false, err
+	}
+	return signer.VerifySignature(hash[:], sign)
 }
--- go/notary/producer/gql/input/createbidinput_test.go
+++ go/notary/producer/gql/input/createbidinput_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql/input"
+
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
 	"gotest.tools/assert"
 )
 
@@ -14,10 +16,12 @@ func TestCreateBidInputID(t *testing.T) {
 
 func TestCreateBidInputHash(t *testing.T) {
 	in := input.CreateBidInput{}
-	_, err := in.Hash()
+	auction := storage.NewAuction()
+	auction.PlayerID = "2222"
+	_, err := in.Hash(*bc.Contracts, *auction)
 	assert.Error(t, err, "invalid teamId")
 	in.TeamId = "10"
-	hash, err := in.Hash()
+	hash, err := in.Hash(*bc.Contracts, *auction)
 	assert.NilError(t, err)
-	assert.Equal(t, hash.Hex(), "0x0")
+	assert.Equal(t, hash.Hex(), "0x2e873491a9bd59b403c457d71b4cdd119c3df561980c86ee0e2ea27d70e5d118")
 }
