1b505dddab3b9ef23a9a36880dd8f4c1b1039d11
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-04-21 17:33:38 +0200

    [go|notary] all tests green

--- go/notary/auctionmachine/setup_test.go
+++ go/notary/auctionmachine/setup_test.go
@@ -11,15 +11,10 @@ import (
 	"github.com/freeverseio/crypto-soccer/go/testutils"
 )
 
-// var db *sql.DB
 var bc *testutils.BlockchainNode
 
 func TestMain(m *testing.M) {
 	var err error
-	// db, err = storage.New("postgres://freeverse:freeverse@localhost:5432/cryptosoccer?sslmode=disable")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
 	bc, err = testutils.NewBlockchainNode()
 	if err != nil {
 		log.Fatal(err)
