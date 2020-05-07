54c007fbc8278c0d984a0062efc8a456bdbfd252
Toni <toni.mateos@gmail.com>
2020-05-07 15:59:11 +0200

    fixing a test

--- truffle-core/utils/debugUtils.js
+++ truffle-core/utils/debugUtils.js
@@ -1,17 +1,28 @@
-function compareArrays(result, expected, toNum = true, verbose = false, isBigNumber = false) {
-    verb = [];
-    for (i = 0; i < expected.length; i++) {
-        if (toNum) verb.push(result[i].toNumber());
-        else verb.push(result[i]);
-        if (!verbose) {
-            if (toNum) result[i].toNumber().should.be.equal(expected[i]);
-            else {
-              if (isBigNumber) result[i].should.be.bignumber.equal(expected[i]);
-              else result[i].should.be.equal(expected[i]);
-            }
-        }            
-    }
-    if (verbose) console.log(verb);
+function compareArraysInternal(result, expected, toNum, verbose, isBigNumber) {
+  verb = [];
+  for (i = 0; i < expected.length; i++) {
+      if (toNum) verb.push(result[i].toNumber());
+      else verb.push(result[i]);
+      if (!verbose) {
+          if (toNum) result[i].toNumber().should.be.equal(expected[i]);
+          else {
+            if (isBigNumber) result[i].should.be.bignumber.equal(expected[i]);
+            else result[i].should.be.equal(expected[i]);
+          }
+      }            
+  }
+  if (verbose) console.log(verb);
+}
+
+function compareArrays(result, expected, toNum = true, isBigNumber = false) {
+  try {
+    compareArraysInternal(result, expected, toNum = true, verbose = false, isBigNumber = false)
+  } 
+  catch(e) {
+    console.log(e)
+    compareArraysInternal(result, expected, toNum = true, verbose = true, isBigNumber = false)
+    throw e
+  }  
 }
   
   module.exports = {
