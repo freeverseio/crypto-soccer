c7fe2bd0135e216d016b4dbc69d966e595228c49
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-10 18:34:01 +0200

    [go|sync|staker] starting

---
+++ go/synchronizer/staker/staker.go
@@ -0,0 +1,34 @@
+package staker
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/freeverseio/crypto-soccer/go/contracts"
+	log "github.com/sirupsen/logrus"
+)
+
+type Staker struct {
+	auth *bind.TransactOpts
+}
+
+func New(privateKey *ecdsa.PrivateKey) (*Staker, error) {
+	log.Infof("[staker] created with address : %v", crypto.PubkeyToAddress(privateKey.PublicKey).Hex())
+
+	staker := Staker{}
+	staker.auth = bind.NewKeyedTransactor(privateKey)
+	staker.auth.GasPrice = big.NewInt(1000000000) // in xdai is fixed to 1 GWei
+
+	return &staker, nil
+}
+
+func (b Staker) Address() common.Address {
+	return b.auth.From
+}
+
+func (b Staker) Enroll(contracts contracts.Contracts) error {
+	return nil
+}
---
+++ go/synchronizer/staker/staker_test.go
@@ -0,0 +1,17 @@
+package staker_test
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/freeverseio/crypto-soccer/go/synchronizer/staker"
+	"gotest.tools/assert"
+)
+
+func TestStakerNew(t *testing.T) {
+	pvc, err := crypto.HexToECDSA("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54")
+	assert.NilError(t, err)
+	s, err := staker.New(pvc)
+	assert.NilError(t, err)
+	assert.Equal(t, s.Address().Hex(), "0x291081e5a1bF0b9dF6633e4868C88e1FA48900e7")
+}
