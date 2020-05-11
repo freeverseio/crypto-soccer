98778321ba48ce08570f366fd1832e9862afe492
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-11 10:51:46 +0200

    [go|notary] removed unused code

--- go/notary/producer/gql/submitplaystoreplayerpurchase_test.go
+++ go/notary/producer/gql/submitplaystoreplayerpurchase_test.go
@@ -9,32 +9,12 @@ import (
 	"gotest.tools/assert"
 )
 
-// func TestSubmitPlayStorePlayerPurchase(t *testing.T) {
-// 	// t.Skip("Rective this test to test google api")
-// 	ch := make(chan interface{}, 10)
-// 	googleCredentials, err := ioutil.ReadFile("./key.json")
-// 	assert.NilError(t, err)
-// 	r := gql.NewResolver(ch, *bc.Contracts, namesdb, googleCredentials)
-
-// 	in := input.SubmitPlayStorePlayerPurchaseInput{}
-// 	in.TeamId = "274877906944"
-// 	in.PlayerId = "274877906944"
-// 	in.PurchaseId = "GPA.3309-9448-9453-66100"
-// 	in.Signature = "91366deb26195ac3b15b9e6fff99d425b2cc0d15d44dc8ee0377779400f92c4358a57754053facbe724e8a536e240b278cd651c756c46978eaebafc47767fd781b"
-
-// 	id, err := r.SubmitPlayStorePlayerPurchase(struct {
-// 		Input input.SubmitPlayStorePlayerPurchaseInput
-// 	}{in})
-// 	assert.NilError(t, err)
-// 	assert.Equal(t, id, in.PlayerId)
-// }
-
 func TestProva(t *testing.T) {
 	t.Skip("Runme to check the purchase communication")
 	packageName := "com.freeverse.phoenix"
 	productID := "coinpack_45"
 
-	credentials, err := ioutil.ReadFile("./key.json")
+	credentials, err := ioutil.ReadFile("../../../../../key.json")
 	assert.NilError(t, err)
 
 	token := "hjjfppagdilpbmnmjaajpcgc.AO-J1Owne5VpLZzOtfFZkDY1k5T4kEXCgack0gmEYssqCgEYzlNgPtHdp72TPELzOl3T8XCYhc0k818EbCi7hiYcEgbCGNVyNCGd1I2wdz9pxGRHXs1-msWvAD9ztmd11v_hr9NqCSn1"
