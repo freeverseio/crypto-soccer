ddf3ddb22298cccbe901a21bcd08d294bb95c829
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-13 13:55:09 +0200

    [go|sync] refactroing

--- go/synchronizer/process/actionssubmission_test.go
+++ go/synchronizer/process/actionssubmission_test.go
@@ -1,1 +1,11 @@
 package process_test
+
+// func TestActionsSubmissionFirstHalfEvents(t *testing.T) {
+// 	t.Parallel()
+// 	tx, err := universedb.Begin()
+// 	assert.NilError(t, err)
+// 	defer tx.Rollback()
+
+// 	event := updates.UpdatesActionsSubmission{}
+// 	assert.NilError(t, process.ConsumeActionsSubmission(tx, bc.Contracts, ipfsURL, nil, event))
+// }
