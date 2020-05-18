f556d221b97b4032fe33eb9b54f04358c5be2f09
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-18 15:48:53 +0200

    [go|relay] SetTeamName reachs the consumer

--- go/relay/producer/gql/setteamname.go
+++ go/relay/producer/gql/setteamname.go
@@ -1,12 +1,26 @@
 package gql
 
 import (
+	"errors"
+
 	"github.com/freeverseio/crypto-soccer/go/relay/producer/gql/input"
 	"github.com/graph-gophers/graphql-go"
+	log "github.com/sirupsen/logrus"
 )
 
 func (b *Resolver) SetTeamName(args struct{ Input input.SetTeamNameInput }) (graphql.ID, error) {
 	result := graphql.ID("")
+
+	if b.ch == nil {
+		return result, errors.New("internal error: no channel")
+	}
+
+	select {
+	case b.ch <- args.Input:
+	default:
+		log.Warning("channel is full")
+		return result, errors.New("channel is full")
+	}
 
 	return result, nil
 }
