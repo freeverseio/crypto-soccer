9f4b14c029117b7763c80278e95bcbd123087588
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-19 09:14:25 +0200

    [go|notary] fix test

--- go/notary/playstore/inapppurchasedata_test.go
+++ go/notary/playstore/inapppurchasedata_test.go
@@ -3,13 +3,13 @@ package playstore_test
 import (
 	"testing"
 
-	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql"
+	"github.com/freeverseio/crypto-soccer/go/notary/playstore"
 	"gotest.tools/assert"
 )
 
 func TestInappPurchaseDataFromReceipt(t *testing.T) {
 	receipt := ""
-	data, err := gql.InappPurchaseDataFromReceipt(receipt)
+	data, err := playstore.InappPurchaseDataFromReceipt(receipt)
 	assert.NilError(t, err)
 	assert.Equal(t, data.OrderId, "")
 }
