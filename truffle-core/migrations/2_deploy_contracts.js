a521b5a5e13ed19c6b7ed226fa762d9d5dde8602
Toni <toni.mateos@gmail.com>
2020-05-21 11:06:01 +0200

    not enrolling stakers on deploy

--- truffle-core/migrations/2_deploy_contracts.js
+++ truffle-core/migrations/2_deploy_contracts.js
@@ -73,8 +73,10 @@ module.exports = function (deployer, network, accounts) {
       await stakers.acceptOwner({from: owners.superuser}).should.be.fulfilled; 
       await stakers.setCOO(owners.COO, {from: owners.superuser}).should.be.fulfilled;
       await stakers.setGameOwner(updates.address, {from: owners.COO}).should.be.fulfilled;
-      await deployUtils.addTrustedParties(stakers, owners.COO, owners.trustedParties);
-      await deployUtils.enroll(stakers, requiredStake, owners.trustedParties);
+
+      // If we want stakers signed up during deploy, uncomment this:
+      // await deployUtils.addTrustedParties(stakers, owners.COO, owners.trustedParties);
+      // await deployUtils.enroll(stakers, requiredStake, owners.trustedParties);
       if (singleTimezone != -1) {
         console.log("Init single timezone", singleTimezone);
         await assets.initSingleTZ(singleTimezone, {from: owners.COO}).should.be.fulfilled;
