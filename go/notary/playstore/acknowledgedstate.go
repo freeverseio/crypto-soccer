e20a2c4ba51f1833d2db207b9a1532425448b53d
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-21 16:51:30 +0200

    [go|notary] test for ack state

--- go/notary/playstore/acknowledgedstate.go
+++ go/notary/playstore/acknowledgedstate.go
@@ -20,22 +20,23 @@ func (b *Machine) processAcknowledged(ctx context.Context) error {
 		b.order.PurchaseToken,
 	)
 	if err != nil {
-		b.setState(storage.PlaystoreOrderFailed, err.Error())
+		b.setState(storage.PlaystoreOrderAcknowledged, err.Error())
 		return nil
 	}
 
 	validator := NewPurchaseValidator(*purchase)
 	if validator.IsTest() && !b.iapTestOn {
 		log.Warningf("[consumer|iap] received test orderId %v ... skip creating player", purchase.OrderId)
+		b.setState(storage.PlaystoreOrderComplete, "test order")
 	} else {
 		if err := b.assignAsset(); err != nil {
 			b.setState(storage.PlaystoreOrderRefunding, err.Error())
 			return nil
 		}
 		log.Infof("[consumer|iap] orderId %v playerId %v assigned to teamId %v", purchase.OrderId, b.order.PlayerId, b.order.TeamId)
+		b.setState(storage.PlaystoreOrderComplete, "")
 	}
 
-	b.setState(storage.PlaystoreOrderComplete, "")
 	return nil
 }
 
