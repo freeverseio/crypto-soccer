2207bbf9a4099f5e8ce554b2a97170ad1e014627
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-23 18:39:07 +0200

    [dashboard] removed blockchain endpoint ... using metamask

--- dashboard/src/Config.jsx
+++ dashboard/src/Config.jsx
@@ -1,6 +1,5 @@
 export default {
     polling_ms: 2000,
-    blockchain_node: 'https://xdai.poanetwork.dev',
     url: process.env.REACT_APP_API,
     directory_address: '0xfE5864A8886ed17789EBcEF2cCC775C175F27b7b',
 };
