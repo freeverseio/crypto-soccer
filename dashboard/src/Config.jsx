cb0fa28b1563368976e27ddd1aac495fa30fd021
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-23 16:14:55 +0200

    [dashboard] added real directory address

--- dashboard/src/Config.jsx
+++ dashboard/src/Config.jsx
@@ -2,5 +2,5 @@ export default {
     polling_ms: 2000,
     blockchain_node: 'https://xdai.poanetwork.dev',
     url: process.env.REACT_APP_API,
-    directory_address: 'no idea',
+    directory_address: '0xfE5864A8886ed17789EBcEF2cCC775C175F27b7b',
 };
--- dashboard/src/views/settings/Settings.jsx
+++ dashboard/src/views/settings/Settings.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Table } from 'semantic-ui-react';
 import Config from '../../Config';
 
