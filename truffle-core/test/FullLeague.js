6b61c7af637750a59b7c490ae41123fa9600966b
Toni <toni.mateos@gmail.com>
2020-05-24 12:50:56 +0200

    minor bug in a test

--- truffle-core/test/FullLeague.js
+++ truffle-core/test/FullLeague.js
@@ -299,7 +299,7 @@ contract('FullLeague', (accounts) => {
         await assets.initTZs({from: owners.COO}).should.be.fulfilled;
         
         training= await TrainingPoints.new(assets.address).should.be.fulfilled;
-        shop = await Shop.new().should.be.fulfilled;
+        shop = await Shop.new(assets.address).should.be.fulfilled;
         encodeLog = await EncodingMatchLog.new().should.be.fulfilled;
         play = await PlayAndEvolve.new(training.address, evo.address, engine.address, shop.address).should.be.fulfilled;
         
