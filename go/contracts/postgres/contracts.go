a7e1d463e91243db98caf3235201dfa3b9894fea
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-05-27 17:51:59 +0200

    [k8s] using proxy address

--- go/contracts/postgres/contracts.go
+++ go/contracts/postgres/contracts.go
@@ -19,70 +19,10 @@ func NewFromStorage(client *ethclient.Client, tx *sql.Tx) (*contracts.Contracts,
 		contractMap[param.Name] = param.Value
 	}
 
-	return contracts.New(
-		client,
-		contractMap["LEAGUES"],
-		contractMap["ASSETS"],
-		contractMap["EVOLUTION"],
-		contractMap["ENGINE"],
-		contractMap["ENGINEPRECOMP"],
-		contractMap["UPDATES"],
-		contractMap["MARKET"],
-		contractMap["UTILS"],
-		contractMap["PLAYANDEVOLVE"],
-		contractMap["SHOP"],
-		contractMap["TRAININGPOINTS"],
-		contractMap["CONSTANTSGETTERS"],
-		contractMap["PRIVILEGED"],
-		contractMap["STAKERS"],
-		contractMap["DIRECTORY"],
-		contractMap["PROXY"],
-	)
+	return contracts.NewByProxyAddress(client, contractMap["PROXY"])
 }
 
 func ToStorage(tx *sql.Tx, contracts *contracts.Contracts) error {
-	if err := (storage.Param{"LEAGUES", contracts.LeaguesAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"ASSETS", contracts.AssetsAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"EVOLUTION", contracts.EvolutionAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"ENGINE", contracts.EngineAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"ENGINEPRECOMP", contracts.EngineprecompAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"UPDATES", contracts.UpdatesAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"MARKET", contracts.MarketAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"UTILS", contracts.UtilsAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"PLAYANDEVOLVE", contracts.PlayandevolveAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"SHOP", contracts.ShopAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"TRAININGPOINTS", contracts.TrainingpointsAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"CONSTANTSGETTERS", contracts.ConstantsgettersAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"PRIVILEGED", contracts.PrivilegedAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
-	if err := (storage.Param{"STAKERS", contracts.StakersAddress}).InsertOrUpdate(tx); err != nil {
-		return err
-	}
 	if err := (storage.Param{"DIRECTORY", contracts.DirectoryAddress}).InsertOrUpdate(tx); err != nil {
 		return err
 	}
