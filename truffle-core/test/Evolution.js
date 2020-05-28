f5f20b941be92af4edcdeb22a50027158b9e16a2
Toni <toni.mateos@gmail.com>
2020-05-28 15:46:00 +0200

    adding a test from the field in truffle

--- truffle-core/test/Evolution.js
+++ truffle-core/test/Evolution.js
@@ -333,6 +333,24 @@ contract('Evolution', (accounts) => {
         events1Half = [events1Half,events1Half];
     });
   
+    
+    it('test from real usage: unknown issue', async () => {
+        m = JSONbig.parse(fs.readFileSync('test/testdata/a102d90303aafcdae29c09bc6b338a50048b9cd4d8fa1942cf315bb7e3736aac.2nd.error.json', 'utf8'));
+        skills0 = [];
+        for (player of m.HomeTeam.Players){ 
+            skills0.push(player.EncodedSkills);
+        }
+        skills1 = [];
+        for (player of m.VisitorTeam.Players){ 
+            skills1.push(player.EncodedSkills);
+        }
+        var {0: skills, 1: matchLogsAndEvents} =  await play.play2ndHalfAndEvolve(
+            m.Seed, m.StartTime, [skills0, skills1], [m.HomeTeam.TeamID, m.VisitorTeam.TeamID], 
+            [m.HomeTeam.Tactic, m.VisitorTeam.Tactic], [m.HomeTeam.MatchLog, m.VisitorTeam.MatchLog],
+            [is2nd = true, isHom = true, isPlay = false]
+        ).should.be.rejected;
+    });
+    
     it('test from real usage with more than 3 substitutions in half time', async () => {
         m = JSONbig.parse(fs.readFileSync('test/testdata/fe6e996fc594c5043f29040561cc95c02c0f68ccdc80047a30e42e74f3b402f8.2nd.error.json', 'utf8'));
         skills0 = [];
