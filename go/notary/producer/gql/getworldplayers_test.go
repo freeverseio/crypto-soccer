d2f0b52fdb961bf14c4f7438ba31dd3bc5342a64
Toni <toni.mateos@gmail.com>
2020-06-04 12:02:34 +0200

    adding tests without changing code

--- go/notary/producer/gql/getworldplayers_test.go
+++ go/notary/producer/gql/getworldplayers_test.go
@@ -44,6 +44,8 @@ func TestGetWorldPlayersDeterministicResult(t *testing.T) {
 	for i := range players0 {
 		assert.Equal(t, *players0[i], *players1[i])
 	}
+	assert.Equal(t, players0[0].Race(), "Spanish")
+	assert.Equal(t, players0[0].CountryOfBirth(), "ES")
 }
 
 func TestGetWorldPlayersOfSoldPlayer(t *testing.T) {
@@ -73,6 +75,8 @@ func TestGetWorldPlayersOfSoldPlayer(t *testing.T) {
 	)
 	assert.NilError(t, err)
 	assert.Equal(t, len(players), 30)
+	assert.Equal(t, players[0].Race(), "Spanish")
+	assert.Equal(t, players[0].CountryOfBirth(), "ES")
 
 	player0Id, _ := new(big.Int).SetString(string(players[0].PlayerId()), 10)
 	targetTeamId, _ := new(big.Int).SetString(teamId, 10)
@@ -95,6 +99,8 @@ func TestGetWorldPlayersOfSoldPlayer(t *testing.T) {
 	)
 	assert.NilError(t, err)
 	assert.Equal(t, len(players), 29)
+	assert.Equal(t, players[0].Race(), "Spanish")
+	assert.Equal(t, players[0].CountryOfBirth(), "EE")
 }
 
 func TestGetWorldPlayers(t *testing.T) {
@@ -108,6 +114,8 @@ func TestGetWorldPlayers(t *testing.T) {
 	players, err := r.GetWorldPlayers(struct{ Input input.GetWorldPlayersInput }{in})
 	assert.NilError(t, err)
 	assert.Equal(t, len(players), 30)
+	assert.Equal(t, players[0].Race(), "TwoOrMoreRegions")
+	assert.Equal(t, players[0].CountryOfBirth(), "ES")
 }
 
 func TestCreateWorldPlayerBatch(t *testing.T) {
@@ -145,6 +153,8 @@ func TestCreateWorldPlayerBatch(t *testing.T) {
 	assert.Equal(t, len(players), 30)
 	assert.Equal(t, string(players[0].PlayerId()), "25753057320981211674441424157481453821747928514686071527706372")
 	assert.Equal(t, players[0].ValidUntil(), "1554940800")
+	assert.Equal(t, players[0].Race(), "TwoOrMoreRegions")
+	assert.Equal(t, players[0].CountryOfBirth(), "ES")
 
 	players, err = gql.CreateWorldPlayerBatch(
 		*bc.Contracts,
