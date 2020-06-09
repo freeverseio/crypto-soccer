87c3c4aea8f03fc4838b86064f7459e2a5c418e9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-06-09 11:57:29 +0200

    [dashboard] basic table added

--- dashboard/src/views/iap/Iap.jsx
+++ dashboard/src/views/iap/Iap.jsx
@@ -1,17 +1,65 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
+import { Table } from 'semantic-ui-react';
+import Config from '../../Config';
 
-const GET_PROXY_ADDRESS = gql`
+const GET_PLAYSTORE_ORDERS = gql`
     {
-        paramByName(name: "PROXY"){
-            value
+        allPlaystoreOrders {
+            nodes {
+            orderId
+            packageName
+            productId
+            purchaseToken
+            playerId
+            teamId
+            state
+            stateExtra
         }
     }
+}
 `;
 
 export default () => {
+    const { loading, error, data } = useQuery(GET_PLAYSTORE_ORDERS, {
+         pollInterval: Config.iap_polling_ms,
+    });
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
+    const orders = data.allPlaystoreOrders.nodes ? data.allPlaystoreOrders.nodes : [];
+    console.log(orders);
+
     return (
-        <div/>
+        <Table sortable celled fixed>
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell>orderId</Table.HeaderCell>
+                    <Table.HeaderCell>packageName</Table.HeaderCell>
+                    <Table.HeaderCell>productId</Table.HeaderCell>
+                    <Table.HeaderCell>purchaseToken</Table.HeaderCell>
+                    <Table.HeaderCell>playerId</Table.HeaderCell>
+                    <Table.HeaderCell>teamId</Table.HeaderCell>
+                    <Table.HeaderCell>state</Table.HeaderCell>
+                    <Table.HeaderCell>stateExtra</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {orders.map(order => (
+                    <Table.Row key={order.purchaseToken}>
+                        <Table.Cell>{order.orderId}</Table.Cell>
+                        <Table.Cell>{order.packageName}</Table.Cell>
+                        <Table.Cell>{order.productId}</Table.Cell>
+                        <Table.Cell>{order.purchaseToken}</Table.Cell>
+                        <Table.Cell>{order.playerId}</Table.Cell>
+                        <Table.Cell>{order.teamId}</Table.Cell>
+                        <Table.Cell>{order.state}</Table.Cell>
+                        <Table.Cell>{order.stateExtra}</Table.Cell>
+                    </Table.Row>
+                ))}
+            </Table.Body>
+        </Table>
     )
 }
