cb7002fc1d46bfe517cc5ac9f182aa6a98ffed30
Toni <toni.mateos@gmail.com>
2020-06-09 08:15:49 +0200

    works

--- dashboard/src/views/settings/ActivateCard.jsx
+++ dashboard/src/views/settings/ActivateCard.jsx
@@ -4,13 +4,24 @@ import Config from '../../Config';
 import RoleCard from './RoleCard';
 import IntArrayCard from './IntArrayCard';
 
+function toIntArray(x) {
+    var array = [];
+    var res = x.split(",");
+    for (var elem of res) {
+        var intElem = parseInt(elem);
+        if (!intElem) return [];
+        array.push(intElem)
+    }
+    return array;
+}
+
 const ActivateCard = ({account, proxyContract}) => {
     const [activateIds, setActivateIds] = useState("");
 
     return (
         <Table.Row>
             <Table.Cell>DeActivate and Activate</Table.Cell>
-            <Table.Cell>{activateIds}</Table.Cell>
+            <Table.Cell>{toIntArray(activateIds)}</Table.Cell>
             <Table.Cell>
                 <IntArrayCard onChange={setActivateIds}/>
             </Table.Cell>
