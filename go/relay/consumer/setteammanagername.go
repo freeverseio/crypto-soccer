8f77651c5a79b439147e91bb124d47014f4727d4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-06-16 11:25:34 +0200

    [go|relay] removed double log

--- go/relay/consumer/setteammanagername.go
+++ go/relay/consumer/setteammanagername.go
@@ -3,10 +3,8 @@ package consumer
 import (
 	"github.com/freeverseio/crypto-soccer/go/relay/producer/gql/input"
 	"github.com/freeverseio/crypto-soccer/go/storage"
-	log "github.com/sirupsen/logrus"
 )
 
 func SetTeamManagerName(teamStorageService storage.TeamStorageService, ev input.SetTeamManagerNameInput) error {
-	log.Infof("[relay|consumer] set manager team %v name %v", string(ev.TeamId), ev.Name)
 	return teamStorageService.UpdateManagerName(string(ev.TeamId), ev.Name)
 }
--- go/relay/consumer/setteamname.go
+++ go/relay/consumer/setteamname.go
@@ -3,10 +3,8 @@ package consumer
 import (
 	"github.com/freeverseio/crypto-soccer/go/relay/producer/gql/input"
 	"github.com/freeverseio/crypto-soccer/go/storage"
-	log "github.com/sirupsen/logrus"
 )
 
 func SetTeamName(teamStorageService storage.TeamStorageService, ev input.SetTeamNameInput) error {
-	log.Infof("[relay|consumer] set team %v name %v", string(ev.TeamId), ev.Name)
 	return teamStorageService.UpdateName(string(ev.TeamId), ev.Name)
 }
