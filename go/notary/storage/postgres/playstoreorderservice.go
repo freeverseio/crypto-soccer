743251f90d82edfe7b451191c959c3be94472e52
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-06-26 14:39:16 +0200

    [go|notary] added code to find pending by playerid

--- go/notary/storage/postgres/playstoreorderservice.go
+++ go/notary/storage/postgres/playstoreorderservice.go
@@ -67,6 +67,45 @@ func (b PlaystoreOrderService) PendingOrders() ([]storage.PlaystoreOrder, error)
 	return orders, nil
 }
 
+func (b PlaystoreOrderService) PendingOrdersByPlayerId(playerId string) ([]storage.PlaystoreOrder, error) {
+	rows, err := b.tx.Query(`SELECT 
+	order_id,
+	package_name,
+	product_id,
+	purchase_token,
+	player_id,
+	team_id,
+	signature,
+	state,
+	state_extra 
+	FROM playstore_orders WHERE (player_id=$1) AND NOT (state='failed' OR state='refunded' OR state='complete');`, playerId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	orders := []storage.PlaystoreOrder{}
+	for rows.Next() {
+		order := storage.PlaystoreOrder{}
+		err = rows.Scan(
+			&order.OrderId,
+			&order.PackageName,
+			&order.ProductId,
+			&order.PurchaseToken,
+			&order.PlayerId,
+			&order.TeamId,
+			&order.Signature,
+			&order.State,
+			&order.StateExtra,
+		)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, order)
+	}
+	return orders, nil
+}
+
 func (b PlaystoreOrderService) Order(orderId string) (*storage.PlaystoreOrder, error) {
 	rows, err := b.tx.Query(`SELECT 
 	package_name,
