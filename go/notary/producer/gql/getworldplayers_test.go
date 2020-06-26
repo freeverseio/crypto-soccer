d943a941b68102f4b9c663f6a4ef2b2e68a105ef
Toni Mateos <tmateos@freeverse.io>
2020-06-26 15:27:05 +0200

    updating last go test

--- go/notary/producer/gql/getworldplayers_test.go
+++ go/notary/producer/gql/getworldplayers_test.go
@@ -18,7 +18,7 @@ func TestGetWorldPlayers(t *testing.T) {
 
 	players, err := r.GetWorldPlayers(struct{ Input input.GetWorldPlayersInput }{in})
 	assert.NilError(t, err)
-	assert.Equal(t, len(players), 33)
+	assert.Equal(t, len(players), 32)
 	assert.Assert(t, players[0].Race() != "")
 	assert.Assert(t, players[0].CountryOfBirth() != "")
 }
