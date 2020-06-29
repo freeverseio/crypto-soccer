eedac6438272bae9d4a700f8cb17aebb0585dbd4
Toni Mateos <tmateos@freeverse.io>
2020-06-29 12:11:21 +0200

    making timezoneshuffle work as it should, correcting previous incorrect behaviour

--- go/synchronizer/process/shuffler.go
+++ go/synchronizer/process/shuffler.go
@@ -6,16 +6,20 @@ import "errors"
 func TimezoneToReshuffle(timezoneIdx uint8, day uint8, turnInDay uint8) (uint8, error) {
 	// Reshuffle happens 1 hour before the start of a league
 	// So for each timezone, on day 0, we reset timezone + 1.
-	// Except for the first timezone (tz=1), of course, which is reset the day before (day = 13)
-	// during the match played by the last timezone (tz=24)
+	// This applies except for:
+	// - the first timezone that played ever, i.e. tz = 24, which must be reset when tz = 23 plays day = 13
+	// - and for tz = 1, which must be reset when tz = 24 at day = 13
 	if timezoneIdx < 1 || timezoneIdx > 24 {
 		return 0, errors.New("input timezone must be in [1,24]")
 	}
 	timezoneToReshuffle := uint8(0)
+	if (timezoneIdx == 23) && (day == 13) && (turnInDay == 0) {
+		timezoneToReshuffle = 24
+	}
 	if (timezoneIdx == 24) && (day == 13) && (turnInDay == 0) {
 		timezoneToReshuffle = 1
 	}
-	if (timezoneIdx < 24) && (day == 0) && (turnInDay == 0) {
+	if (timezoneIdx < 23) && (day == 0) && (turnInDay == 0) {
 		timezoneToReshuffle = timezoneIdx + 1
 	}
 	return timezoneToReshuffle, nil
