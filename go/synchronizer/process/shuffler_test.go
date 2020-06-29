2f83db590ed5d61e3e2d573ef61de41cddc5dace
Toni Mateos <tmateos@freeverse.io>
2020-06-29 11:55:31 +0200

    completing test, and returning err

--- go/synchronizer/process/shuffler_test.go
+++ go/synchronizer/process/shuffler_test.go
@@ -15,17 +15,46 @@ func TestTimezoneToReshuffle(t *testing.T) {
 		TurnInDay   uint8
 		Result      uint8
 	}{
-		{0, 0, 0, 0},
+		{1, 0, 0, 2},
+		{1, 1, 0, 0},
+		{1, 0, 1, 0},
+		{1, 13, 0, 0},
+		{24, 13, 0, 1},
 	}
 
 	for _, tc := range cases {
 		t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
-			result := process.TimezoneToReshuffle(
+			result, err := process.TimezoneToReshuffle(
 				tc.TimezoneIdx,
 				tc.Day,
 				tc.TurnInDay,
 			)
+			assert.Equal(t, err, nil)
 			assert.Equal(t, tc.Result, result)
+		})
+	}
+}
+
+func TestNonValidTZ(t *testing.T) {
+	cases := []struct {
+		TimezoneIdx uint8
+		Day         uint8
+		TurnInDay   uint8
+		Result      uint8
+	}{
+		{0, 0, 0, 2},
+		{25, 1, 0, 0},
+		{27, 0, 1, 0},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%v", tc), func(t *testing.T) {
+			_, err := process.TimezoneToReshuffle(
+				tc.TimezoneIdx,
+				tc.Day,
+				tc.TurnInDay,
+			)
+			assert.Equal(t, err == nil, false)
 		})
 	}
 }
