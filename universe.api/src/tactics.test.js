63ed30295aedc4e858cfde5391c15f576d2008e2
Toni Mateos <tmateos@freeverse.io>
2020-07-02 17:18:41 +0200

    adding more tests

--- universe.api/src/tactics.test.js
+++ universe.api/src/tactics.test.js
@@ -4,44 +4,18 @@ const { checkTactics } = require('./tactics');
 describe('tactics', () => {
     describe('description of tests', () => {
         const data = getDefaultData();
-        const tacticPatch = {
-            tacticId: 10,
-            shirt0: 0,
-            shirt1: 3,
-            shirt2: 4,
-            shirt3: 5,
-            shirt4: 6,
-            shirt5: 7,
-            shirt6: 8,
-            shirt7: 9,
-            shirt8: 10,
-            shirt9: 11,
-            shirt10: 12,
-            substitution0Shirt: 25,
-            substitution0Target: 11,
-            substitution0Minute: 0,
-            substitution1Shirt: 25,
-            substitution1Target: 10,
-            substitution1Minute: 0,
-            substitution2Shirt: 25,
-            substitution2Target: 10,
-            substitution2Minute: 0,
-            extraAttack1: false,
-            extraAttack2: false,
-            extraAttack3: false,
-            extraAttack4: false,
-            extraAttack5: false,
-            extraAttack6: false,
-            extraAttack7: false,
-            extraAttack8: false,
-            extraAttack9: false,
-            extraAttack10: false 
-        };
+        const tacticPatch = getDefaultPatch();
 
-        test('name of test', () => {
+        test('default everything OK', () => {
             nRedCards1stHalf = 0;
             expect(() => checkTactics(nRedCards1stHalf, data, tacticPatch)).not.toThrow();
         });
+
+        test('fails one red card in 1st half', () => {
+            nRedCards1stHalf = 1;
+            expect(() => checkTactics(nRedCards1stHalf, data, tacticPatch)).toThrow("too many players aligned given the 1st half redcards");
+        });
+
     });
 
 });
@@ -63,4 +37,40 @@ function getDefaultData() {
         })
     }
     return data;
+}
+
+function getDefaultPatch() {
+    return {
+        tacticId: 10,
+        shirt0: 0,
+        shirt1: 3,
+        shirt2: 4,
+        shirt3: 5,
+        shirt4: 6,
+        shirt5: 7,
+        shirt6: 8,
+        shirt7: 9,
+        shirt8: 10,
+        shirt9: 11,
+        shirt10: 12,
+        substitution0Shirt: 25,
+        substitution0Target: 11,
+        substitution0Minute: 0,
+        substitution1Shirt: 25,
+        substitution1Target: 10,
+        substitution1Minute: 0,
+        substitution2Shirt: 25,
+        substitution2Target: 10,
+        substitution2Minute: 0,
+        extraAttack1: false,
+        extraAttack2: false,
+        extraAttack3: false,
+        extraAttack4: false,
+        extraAttack5: false,
+        extraAttack6: false,
+        extraAttack7: false,
+        extraAttack8: false,
+        extraAttack9: false,
+        extraAttack10: false 
+    };
 }
