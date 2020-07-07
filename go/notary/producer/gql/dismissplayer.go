6fadfeef92826643f588ad33cbdb91fc73c5a5d5
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-07-07 13:46:32 +0200

    [go|notary] added gql scheleton for dismiss player

---
+++ go/notary/producer/gql/dismissplayer.go
@@ -0,0 +1,24 @@
+package gql
+
+import (
+	"errors"
+
+	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql/input"
+	"github.com/graph-gophers/graphql-go"
+	log "github.com/sirupsen/logrus"
+)
+
+func (b *Resolver) DismissPlayer(args struct{ Input input.DismissPlayerInput }) (graphql.ID, error) {
+	log.Debugf("DismissPlayer %v", args)
+
+	id := graphql.ID("")
+
+	select {
+	case b.ch <- args.Input:
+	default:
+		log.Warning("channel is full")
+		return id, errors.New("channel is full")
+	}
+
+	return id, nil
+}
---
+++ go/notary/producer/gql/input/dismissplayerinput.go
@@ -0,0 +1,8 @@
+package input
+
+type DismissPlayerInput struct {
+	Signature       string
+	PlayerId        string
+	ValidUntil      string
+	ReturnToAcademy bool
+}
