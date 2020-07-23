cada4c627a02111fec8f93437c9f6472f0ff4a04
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-07-23 08:20:16 +0200

    [go|storage] fix using schema without block_number initialized

--- go/storage/param_test.go
+++ go/storage/param_test.go
@@ -46,7 +46,7 @@ func TestParamParams(t *testing.T) {
 
 	params, err := storage.Params(tx)
 	assert.NilError(t, err)
-	assert.Equal(t, len(params), 1)
+	assert.Equal(t, len(params), 0)
 
 	param := storage.Param{}
 	param.Name = "prova"
@@ -57,11 +57,45 @@ func TestParamParams(t *testing.T) {
 
 	params, err = storage.Params(tx)
 	assert.NilError(t, err)
-	assert.Equal(t, len(params), 2)
+	assert.Equal(t, len(params), 1)
 
 	param.Name = "riprova"
 	assert.NilError(t, param.InsertOrUpdate(tx))
 	params, err = storage.Params(tx)
 	assert.NilError(t, err)
-	assert.Equal(t, len(params), 3)
+	assert.Equal(t, len(params), 2)
+}
+
+func TestGetBlockNumber(t *testing.T) {
+	tx, err := s.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tx.Rollback()
+	number, err := storage.GetBlockNumber(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if number != 0 {
+		t.Fatalf("Expected 0 result %v", number)
+	}
+}
+
+func TestSetBlockNumber(t *testing.T) {
+	tx, err := s.Begin()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tx.Rollback()
+	err = storage.SetBlockNumber(tx, 3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	number, err := storage.GetBlockNumber(tx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if number != 3 {
+		t.Fatalf("Expected 3 result %v", number)
+	}
 }
