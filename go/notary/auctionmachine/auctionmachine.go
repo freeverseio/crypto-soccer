644f9b66fa8fb0731d562a1305b7680b529ec5be
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-07-27 18:12:09 +0200

    [go|notary] removed silly logs

--- go/notary/auctionmachine/auctionmachine.go
+++ go/notary/auctionmachine/auctionmachine.go
@@ -40,7 +40,7 @@ func New(
 }
 
 func (b *AuctionMachine) Process(market marketpay.IMarketPay) error {
-	log.Infof("Process auction %v in state %v", b.auction.ID, b.State())
+	log.Debugf("Process auction %v in state %v", b.auction.ID, b.State())
 	switch b.auction.State {
 	case storage.AuctionStarted:
 		return b.processStarted()
