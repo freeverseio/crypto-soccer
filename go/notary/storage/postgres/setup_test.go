6dd134fd1d04e7f5fd0f0229ced3e950793d387a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-07-31 17:21:02 +0200

    [go|notary] removed storage.go file

--- go/notary/storage/postgres/setup_test.go
+++ go/notary/storage/postgres/setup_test.go
@@ -6,14 +6,26 @@ import (
 	"os"
 	"testing"
 
-	"github.com/freeverseio/crypto-soccer/go/notary/storage/postgres"
+	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
+func new(url string) (*sql.DB, error) {
+	var err error
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func TestMain(m *testing.M) {
 	var err error
-	db, err = postgres.New("postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable")
+	db, err = new("postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
