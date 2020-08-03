cffdd4c39db7d65521bc3fca8e801f2b2f62ee82
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-08-03 11:54:45 +0200

    [notary] using postgres.New function to create connection

--- go/notary/storage/postgres/setup_test.go
+++ go/notary/storage/postgres/setup_test.go
@@ -6,26 +6,14 @@ import (
 	"os"
 	"testing"
 
-	_ "github.com/lib/pq"
+	"github.com/freeverseio/crypto-soccer/go/notary/storage/postgres"
 )
 
 var db *sql.DB
 
-func connect(url string) (*sql.DB, error) {
-	var err error
-	db, err := sql.Open("postgres", url)
-	if err != nil {
-		return nil, err
-	}
-	if err = db.Ping(); err != nil {
-		return nil, err
-	}
-	return db, nil
-}
-
 func TestMain(m *testing.M) {
 	var err error
-	db, err = connect("postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable")
+	db, err = postgres.New("postgres://freeverse:freeverse@localhost:5432/market?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
--- go/notary/storage/postgres/storageservice.go
+++ go/notary/storage/postgres/storageservice.go
@@ -1,6 +1,10 @@
 package postgres
 
-import "database/sql"
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
 
 type StorageService struct {
 	db *sql.DB
@@ -14,4 +18,16 @@ func NewStorageService(db *sql.DB) *StorageService {
 
 func (b *StorageService) DB() *sql.DB {
 	return b.db
+}
+
+func New(url string) (*sql.DB, error) {
+	var err error
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
