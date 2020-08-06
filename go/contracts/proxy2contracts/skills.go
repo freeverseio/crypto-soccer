de8c50dc58b026ce6ac4a872490dee4144f389a2
Toni Mateos <tmateos@freeverse.io>
2020-08-06 17:16:11 +0300

    first implementation

--- go/contracts/proxy2contracts/skills.go
+++ go/contracts/proxy2contracts/skills.go
@@ -1,13 +1,21 @@
-package native
+package proxy2contracts
 
 import "math/big"
 
+func right(x *big.Int, n uint) *big.Int {
+	return new(big.Int).Rsh(x, n)
+}
+
+func left(x *big.Int, n uint) *big.Int {
+	return new(big.Int).Rsh(x, n)
+}
+
 // func getSkill(encodedSkills *big.Int, uint8 skillIdx) *big.Int {
-// 	return (encodedSkills >> (uint256(skillIdx) * 20)) & 1048575; /// 1048575 = 2**20 - 1
+// 	return (encodedSkills.Rsh((uint256(skillIdx) * 20)).And(1048575; /// 1048575 = 2**20 - 1
 // }
 
 // func getBirthDay(encodedSkills *big.Int) *big.Int {
-// 	return (encodedSkills >> 100) & 65535;
+// 	return (encodedSkills.Rsh(100).And(65535;
 // }
 
 // func getPlayerIdFromSkills(encodedSkills *big.Int) *big.Int {
@@ -15,61 +23,65 @@ import "math/big"
 // }
 
 // func getInternalPlayerId(encodedSkills *big.Int) *big.Int {
-// 	return big.NewInt(encodedSkills >> 129 & 8796093022207); /// 2**43 - 1 = 8796093022207
+// 	return big.NewInt(encodedSkills.Rsh(129.And(8796093022207); /// 2**43 - 1 = 8796093022207
 // }
 
 // func getPotential(encodedSkills *big.Int) *big.Int {
-// 	return big.NewInt(encodedSkills >> 116 & 15);
+// 	return big.NewInt(encodedSkills.Rsh(116.And(15);
 // }
 
+func and(x *big.Int, n int64) *big.Int {
+	return new(big.Int).And(x, big.NewInt(n))
+}
+
 func getForwardnessNat(encodedSkills *big.Int) *big.Int {
-	return big.NewInt(encodedSkills >> 120 & 7)
+	return and(right(encodedSkills, 120), 7)
 }
 
 // func getLeftishness(encodedSkills *big.Int) *big.Int {
-// 	return big.NewInt(encodedSkills >> 123 & 7);
+// 	return big.NewInt(encodedSkills.Rsh(123.And(7);
 // }
 
 // func getAggressiveness(encodedSkills *big.Int) *big.Int {
-// 	return big.NewInt(encodedSkills >> 126 & 7);
+// 	return big.NewInt(encodedSkills.Rsh(126.And(7);
 // }
 
 // func getAlignedEndOfFirstHalf(encodedSkills *big.Int) public pure returns (bool) {
-// 	return (encodedSkills >> 172 & 1) == 1;
+// 	return (encodedSkills.Rsh(172.And(1) == 1;
 // }
 
 // func getRedCardLastGame(encodedSkills *big.Int) public pure returns (bool) {
-// 	return (encodedSkills >> 173 & 1) == 1;
+// 	return (encodedSkills.Rsh(173.And(1) == 1;
 // }
 
 // func getGamesNonStopping(encodedSkills *big.Int) public pure returns (uint8) {
-// 	return uint8(encodedSkills >> 174 & 7);
+// 	return uint8(encodedSkills.Rsh(174.And(7);
 // }
 
 // func getInjuryWeeksLeft(encodedSkills *big.Int) public pure returns (uint8) {
-// 	return uint8(encodedSkills >> 177 & 7);
+// 	return uint8(encodedSkills.Rsh(177.And(7);
 // }
 
 // func getSubstitutedFirstHalf(encodedSkills *big.Int) public pure returns (bool) {
-// 	return (encodedSkills >> 180 & 1) == 1;
+// 	return (encodedSkills.Rsh(180.And(1) == 1;
 // }
 
 // func getSumOfSkills(encodedSkills *big.Int) *big.Int {
-// 	return big.NewInt(encodedSkills >> 181 & 524287); /// 2**19-1
+// 	return big.NewInt(encodedSkills.Rsh(181.And(524287); /// 2**19-1
 // }
 
 // func getIsSpecial(encodedSkills *big.Int) public pure returns (bool) {
-// 	return big.NewInt(encodedSkills >> 204 & 1) == 1;
+// 	return big.NewInt(encodedSkills.Rsh(204.And(1) == 1;
 // }
 
 // func getGeneration(encodedSkills *big.Int) *big.Int {
-// 	return (encodedSkills >> 205) & 255;
+// 	return (encodedSkills.Rsh(205).And(255;
 // }
 
 // func getOutOfGameFirstHalf(encodedSkills *big.Int) public pure returns (bool) {
-// 	return big.NewInt(encodedSkills >> 213 & 1) == 1;
+// 	return big.NewInt(encodedSkills.Rsh(213.And(1) == 1;
 // }
 
 // func getYellowCardFirstHalf(encodedSkills *big.Int) public pure returns (bool) {
-// 	return big.NewInt(encodedSkills >> 214 & 1) == 1;
+// 	return big.NewInt(encodedSkills.Rsh(214.And(1) == 1;
 // }
