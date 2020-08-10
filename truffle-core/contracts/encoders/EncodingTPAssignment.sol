fbacad45540f23674aedc5e36b64cbcf12a2b15b
Toni Mateos <tmateos@freeverse.io>
2020-08-10 13:36:53 +0300

    - solidity: making sure that several comparisons will not overflow (uint16 -> unint256) - go: adding encodeTP

--- truffle-core/contracts/encoders/EncodingTPAssignment.sol
+++ truffle-core/contracts/encoders/EncodingTPAssignment.sol
@@ -26,50 +26,51 @@ import "../gameEngine/ErrorCodes.sol";
 
 contract EncodingTPAssignment is ErrorCodes {
 
-    uint16 public constant MAX_PERCENT = 60; 
+    uint256 public constant MAX_PERCENT = 60; 
     uint8 private constant PLAYERS_PER_TEAM_MAX  = 25;
     uint8 public constant NO_PLAYER = PLAYERS_PER_TEAM_MAX; /// No player chosen
     
     function encodeTP(uint16 TP, uint16[25] memory TPperSkill, uint8 specialPlayer) public pure returns (uint256 encoded) {
         require(specialPlayer <= PLAYERS_PER_TEAM_MAX, "specialPlayer value too large");
 
-        encoded |= uint256(TP) << 225;
+        uint256 TP256 = uint256(TP);
+        encoded = TP256 << 225;
         encoded |= uint256(specialPlayer) << 234;
 
-        uint16 maxRHS = (TP < 4) ? 100 * TP : MAX_PERCENT * TP;
+        uint256 maxRHS = (TP256 < 4) ? 100 * TP256 : MAX_PERCENT * TP256;
         uint8 lastBucket = (specialPlayer == NO_PLAYER ? 4 : 5);
         for (uint8 bucket = 0; bucket < lastBucket; bucket++) {
             if (bucket == 4) {
-                TP = uint16((uint256(TP) * 11)/10);
-                maxRHS = (TP < 4) ? 100 * TP : MAX_PERCENT * TP;
+                TP256 = (TP256 * 11)/10;
+                maxRHS = (TP256 < 4) ? 100 * TP256 : MAX_PERCENT * TP256;
             }
             uint256 sum = 0;
             for (uint8 sk = 5 * bucket; sk < 5 * (bucket+1); sk++) {
-                uint16 skill = TPperSkill[sk];
+                uint256 skill = uint256(TPperSkill[sk]);
                 require(100*skill <= maxRHS, "one of the assigned TPs is too large");
                 sum += skill;
-                encoded |= uint256(skill) << 9 * sk;
+                encoded |= skill << (9 * sk);
             }
-            require(sum <= TP, "sum of Traning Points is too large");
+            require(sum <= TP256, "sum of Traning Points is too large");
         }
     } 
 
     function decodeTP(uint256 encoded) public pure returns(uint16[25] memory TPperSkill, uint8 specialPlayer, uint16 TP, uint8) {
-        TP = uint16((encoded >> 225) & 511);
-        uint16 TPtemp = TP;
+        uint256 TPtemp = (encoded >> 225) & 511;
+        TP = uint16(TPtemp);
         specialPlayer = uint8((encoded >> 234) & 31);
         if (specialPlayer > PLAYERS_PER_TEAM_MAX) return (TPperSkill, specialPlayer, TP, ERR_TRAINING_SPLAYER); // specialPlayer value too large
-        uint16 maxRHS = (TP < 4) ? 100 * TPtemp : MAX_PERCENT * TPtemp;
+        uint256 maxRHS = (TPtemp < 4) ? 100 * TPtemp : MAX_PERCENT * TPtemp;
         for (uint8 bucket = 0; bucket < 5; bucket++) {
             if (bucket == 4) {
-                TPtemp = uint16((uint256(TPtemp) * 11000)/10000);
-                maxRHS = (TP < 4) ? 100 * TPtemp : MAX_PERCENT * TPtemp;
+                TPtemp = (TPtemp * 11)/10;
+                maxRHS = (TPtemp < 4) ? 100 * TPtemp : MAX_PERCENT * TPtemp;
             }
             uint256 sum = 0;
             for (uint8 sk = 5 * bucket; sk < 5* (bucket+1); sk++) {
-                uint16 skill = uint16((encoded >> 9 * sk) & 511);
+                uint256 skill = (encoded >> (9 * sk)) & 511;
                 if (100*skill > maxRHS) return (TPperSkill, specialPlayer, TP, ERR_TRAINING_SINGLESKILL); // one of the assigned TPs is too large or too small
-                TPperSkill[sk] = skill;
+                TPperSkill[sk] = uint16(skill);
                 sum += skill;
             }
             if (sum > TPtemp) return (TPperSkill, specialPlayer, TP, ERR_TRAINING_SUMSKILLS); // sum of Traning Points is too large"
