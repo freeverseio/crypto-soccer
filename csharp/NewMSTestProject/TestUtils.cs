17ed00d953006ae9983f048b33f424653dd23065
Toni Mateos <tmateos@freeverse.io>
2020-08-12 15:29:45 +0300

    starting to test matchlog in c sharp

--- csharp/NewMSTestProject/TestUtils.cs
+++ csharp/NewMSTestProject/TestUtils.cs
@@ -4,30 +4,6 @@ using System.IO;
 using Newtonsoft.Json;
 
 public class TestUtils {  
-
-    public class Item
-    {
-        public string encodedSkills;
-        public int[] skills;
-        public int birthday;
-        public bool isSpecial;
-        public string playerIdFromSkills;
-        public string internalPlayerId;
-        public int potential;
-        public int forwardness;
-        public int leftishness;
-        public int aggressiveness;
-        public bool alignedEndOfFirstHalf;
-        public bool redCardLastGame;
-        public int gamesNonStopping;
-        public int injuryWeeksLeft;
-        public bool substitutedFirstHalf;
-        public int sumOfSkills;
-        public int generation;
-        public bool outOfGameFirstHalf;
-        public bool yellowCardFistHalf;
-    }
-
     public dynamic LoadJson(string filename)
     {
         string workingDirectory = Environment.CurrentDirectory;
@@ -46,6 +22,4 @@ public class TestUtils {
         }
         return array;
     }
-
-
 }  
--- csharp/NewMSTestProject/UnitTest1.cs
+++ csharp/NewMSTestProject/UnitTest1.cs
@@ -74,5 +74,27 @@ namespace NewMSTestProject
             }
         }  
 
+        [TestMethod]
+        public void Test_encodingMatchLog() {  
+            Serialization serial = new Serialization();
+            TestUtils tu = new TestUtils();
+            dynamic tests = tu.LoadJson("encodingMatchLogTestData.json");
+            foreach(dynamic test in tests) {
+                BigInteger encoded;
+                bool succeeded = BigInteger.TryParse((string) test.encodedLog, out encoded);
+                Assert.AreEqual(true, succeeded);  
+                Assert.AreEqual((bool) test.isHomeStadium, serial.getIsHomeStadium(encoded));  
+                Assert.AreEqual((uint) test.winner, serial.getWinner(encoded));  
+                Assert.AreEqual((uint) test.teamSumSkills, serial.getTeamSumSkills(encoded));  
+                Assert.AreEqual((uint) test.trainingPoints, serial.getTrainingPoints(encoded));  
+                Assert.AreEqual((uint) test.nGoals, serial.getNGoals(encoded));  
+                Assert.AreEqual((uint) test.changesAtHalftime, serial.getChangesAtHalfTime(encoded));  
+                // for (int i = 0; i < 10; i++) { Assert.AreEqual((bool) test.extraAttack[i], serial.getExtraAttack(encoded, i)); }
+                // for (int i = 0; i < 3; i++) { Assert.AreEqual((uint) test.substitution[i], serial.getSubstitution(encoded, i)); }
+                // for (int i = 0; i < 3; i++) { Assert.AreEqual((uint) test.subsRound[i], serial.getSubsRound(encoded, i)); }
+                // for (int i = 0; i < 14; i++) { Assert.AreEqual((uint) test.linedUp[i], serial.getLinedUp(encoded, i)); }
+            }
+        }  
+        
     }
 }
