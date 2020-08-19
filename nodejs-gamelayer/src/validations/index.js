74db34b96fdae0f0396b318689449693caa68745
ccubu <60666948+ccubu@users.noreply.github.com>
2020-08-19 15:37:21 +0200

    Remove playevalidation remnants (#930)

--- nodejs-gamelayer/src/validations/index.js
+++ nodejs-gamelayer/src/validations/index.js
@@ -1,7 +1,5 @@
 const TeamValidation = require("./TeamValidation.js");
-const PlayerValidation = require("./PlayerValidation.js");
 
 module.exports = {
     TeamValidation,
-    PlayerValidation,
 };
