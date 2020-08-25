a6ae61bd75039df524f43048cdf9703d7bf18fc0
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-08-25 13:14:48 +0200

    [go|notary] createAuction is synchronous

--- go/notary/producer/gql/createauction.go
+++ go/notary/producer/gql/createauction.go
@@ -1,10 +1,13 @@
 package gql
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql/input"
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
 	"github.com/graph-gophers/graphql-go"
 	log "github.com/sirupsen/logrus"
 )
@@ -45,5 +48,44 @@ func (b *Resolver) CreateAuction(args struct{ Input input.CreateAuctionInput }) 
 		return id, fmt.Errorf("blockchain says no")
 	}
 
-	return id, b.push(args.Input)
+	tx, err := b.service.DB().Begin()
+	if err != nil {
+		return id, err
+	}
+	if err := CreateAuction(b.service, tx, args.Input); err != nil {
+		tx.Rollback()
+		return id, err
+	}
+
+	return id, tx.Commit()
+}
+
+func CreateAuction(service storage.StorageService, tx *sql.Tx, in input.CreateAuctionInput) error {
+	auction := storage.NewAuction()
+	id, err := in.ID()
+	if err != nil {
+		return err
+	}
+	auction.ID = string(id)
+	auction.Rnd = int64(in.Rnd)
+	auction.PlayerID = in.PlayerId
+	auction.CurrencyID = int(in.CurrencyId)
+	auction.Price = int64(in.Price)
+	if auction.ValidUntil, err = strconv.ParseInt(in.ValidUntil, 10, 64); err != nil {
+		return fmt.Errorf("invalid validUntil %v", in.ValidUntil)
+	}
+	auction.Signature = in.Signature
+	auction.State = storage.AuctionStarted
+	auction.StateExtra = ""
+	auction.PaymentURL = ""
+	signerAddress, err := in.SignerAddress()
+	if err != nil {
+		return err
+	}
+	auction.Seller = signerAddress.Hex()
+	if err = service.AuctionInsert(tx, *auction); err != nil {
+		return err
+	}
+
+	return nil
 }
