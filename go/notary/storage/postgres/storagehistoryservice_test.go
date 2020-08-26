6b77fa682eb9bf6847603a756e790f1e8fb03e3f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-08-26 15:01:57 +0200

    [go|notary] test for exposing bug of unecessary entries in auction history

--- go/notary/storage/postgres/storagehistoryservice_test.go
+++ go/notary/storage/postgres/storagehistoryservice_test.go
@@ -3,11 +3,47 @@ package postgres_test
 import (
 	"testing"
 
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
 	"github.com/freeverseio/crypto-soccer/go/notary/storage/postgres"
 	"github.com/freeverseio/crypto-soccer/go/notary/storage/storagetest"
+	"gotest.tools/assert"
 )
 
-func TestStorageHistoryService(t *testing.T) {
+func TestStorageHistoryServiceStart(t *testing.T) {
 	service := postgres.NewStorageHistoryService(db)
 	storagetest.TestStorageService(t, service)
+}
+
+func TestStorageHistoryInsertAuction(t *testing.T) {
+	service := postgres.NewStorageHistoryService(db)
+
+	tx, err := service.DB().Begin()
+	assert.NilError(t, err)
+	defer tx.Rollback()
+
+	var count int
+	tx.QueryRow("SELECT count(*) FROM auctions_histories;").Scan(&count)
+	assert.Equal(t, count, 0)
+
+	auction := storage.NewAuction()
+	assert.NilError(t, service.AuctionInsert(tx, *auction))
+
+	tx.QueryRow("SELECT count(*) FROM auctions_histories;").Scan(&count)
+	assert.Equal(t, count, 1)
+}
+
+func TestStorageHistoryUpdateUnchangedAuction(t *testing.T) {
+	service := postgres.NewStorageHistoryService(db)
+
+	tx, err := service.DB().Begin()
+	assert.NilError(t, err)
+	defer tx.Rollback()
+
+	auction := storage.NewAuction()
+	assert.NilError(t, service.AuctionInsert(tx, *auction))
+	assert.NilError(t, service.AuctionUpdate(tx, *auction))
+
+	var count int
+	tx.QueryRow("SELECT count(*) FROM auctions_histories;").Scan(&count)
+	assert.Equal(t, count, 1)
 }
