596895d22bd75ebacce01dac910ded22e165249b
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-08-31 15:13:27 +0200

    [market.db] all ending states in index

--- market.db/02_teams_constaint.sql
+++ market.db/02_teams_constaint.sql
@@ -1,1 +1,1 @@
-create unique index player_already_on_auction on auctions (player_id) WHERE state not in ('ended', 'failed', 'cancelled');
+create unique index player_already_on_auction on auctions (player_id) WHERE state not in ('ended', 'failed', 'cancelled', 'withadrable_by_seller', 'withadrable_by_buyer','validation');
