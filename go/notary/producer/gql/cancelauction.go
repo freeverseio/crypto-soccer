b592285a10f3065beb2c4d7d43f21c5404680223
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-08-31 15:17:19 +0200

    [go|notary] cancelAuction is sync

--- go/notary/producer/gql/cancelauction.go
+++ go/notary/producer/gql/cancelauction.go
@@ -2,8 +2,10 @@ package gql
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql/input"
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
 	"github.com/graph-gophers/graphql-go"
 	log "github.com/sirupsen/logrus"
 )
@@ -25,5 +27,30 @@ func (b *Resolver) CancelAuction(args struct{ Input input.CancelAuctionInput }) 
 		return id, errors.New("Invalid signature")
 	}
 
-	return id, b.push(args.Input)
+	tx, err := b.service.Begin()
+	if err != nil {
+		return id, err
+	}
+	if err := cancelAuction(tx, args.Input); err != nil {
+		tx.Rollback()
+		return id, err
+	}
+
+	return id, tx.Commit()
+}
+
+func cancelAuction(service storage.Tx, in input.CancelAuctionInput) error {
+	auction, err := service.Auction(string(in.AuctionId))
+	if err != nil {
+		return err
+	}
+	if auction == nil {
+		return errors.New("trying to cancel a nil auction")
+	}
+	if auction.State != storage.AuctionStarted {
+		return fmt.Errorf("not possible to cancel an auction in state %v", auction.State)
+	}
+
+	auction.State = storage.AuctionCancelled
+	return service.AuctionUpdate(*auction)
 }
