33c0458835f8e49b1a7e93c2a61b75f4aa90917e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-09-02 11:01:32 +0200

    [go|notary] create bid is sync

--- go/notary/producer/gql/createbid.go
+++ go/notary/producer/gql/createbid.go
@@ -2,8 +2,10 @@ package gql
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql/input"
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
 	"github.com/graph-gophers/graphql-go"
 	log "github.com/sirupsen/logrus"
 )
@@ -28,5 +30,39 @@ func (b *Resolver) CreateBid(args struct{ Input input.CreateBidInput }) (graphql
 		return graphql.ID(id), errors.New("Invalid signature")
 	}
 
-	return id, b.push(args.Input)
+	tx, err := b.service.Begin()
+	if err != nil {
+		return id, err
+	}
+	if err := createBid(tx, args.Input); err != nil {
+		tx.Rollback()
+		return id, err
+	}
+
+	return id, tx.Commit()
+}
+
+func createBid(tx storage.Tx, in input.CreateBidInput) error {
+	auction, err := tx.Auction(string(in.AuctionId))
+	if err != nil {
+		return err
+	}
+
+	if auction == nil {
+		return fmt.Errorf("No auction for bid %v", in)
+	}
+
+	bid := storage.NewBid()
+	bid.AuctionID = string(in.AuctionId)
+	bid.ExtraPrice = int64(in.ExtraPrice)
+	bid.Rnd = int64(in.Rnd)
+	bid.TeamID = in.TeamId
+	bid.Signature = in.Signature
+	bid.State = storage.BidAccepted
+	bid.StateExtra = ""
+	bid.PaymentID = ""
+	bid.PaymentURL = ""
+	bid.PaymentDeadline = 0
+
+	return tx.BidInsert(*bid)
 }
