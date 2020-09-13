6be8d856595e190df37109437ca742225a179e7b
Toni Mateos <tmateos@freeverse.io>
2020-09-13 09:46:08 +0200

    fixing tests

--- go/notary/signer/recover_test.go
+++ go/notary/signer/recover_test.go
@@ -49,28 +49,32 @@ func TestVerifySignature(t *testing.T) {
 }
 
 func TestVerifyAuctionSignature(t *testing.T) {
-	validUntil := int64(2000000000)
-	playerId := big.NewInt(10)
+	validUntil := int64(235985749)
+	offerValidUntil := int64(4358487)
+	playerId := big.NewInt(11114324213423)
 	currencyId := uint8(1)
-	price := big.NewInt(41234)
-	rnd := big.NewInt(42321)
+	price := big.NewInt(345)
+	rnd := big.NewInt(1234)
 
-	hash, err := signer.HashSellMessage(
+	hash, err := signer.ComputePutAssetForSaleDigest(
 		currencyId,
 		price,
 		rnd,
 		validUntil,
+		offerValidUntil,
 		playerId,
 	)
 	assert.NilError(t, err)
-	assert.Equal(t, hash.Hex(), "0xc50d978b8a838b6c437a162a94c715f95e92e11fe680cf0f1caf054ad78cd796")
+	assert.Equal(t, hash.Hex(), "0x376b87a3db2c3ef6e1189a96303454a32fd8bf21bfe0a470e68be98e57d36495")
+	assert.Equal(t, hex.EncodeToString(hash[:]), "376b87a3db2c3ef6e1189a96303454a32fd8bf21bfe0a470e68be98e57d36495")
 
 	privateKey, err := crypto.HexToECDSA("3B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54")
 	assert.NilError(t, err)
 
+	// note that cryptp.Sign and signer.Sign differ in the last hex value! compare to signer_test, ref XX1
 	signature, err := crypto.Sign(hash.Bytes(), privateKey)
 	assert.NilError(t, err)
-	assert.Equal(t, hex.EncodeToString(signature), "075ddf60b307abf0ecf323dcdd57230fcb81b30217fb947ee5dbd683cb8bcf074a63f87c97c736f85cd3e56e95f4fcc1e9b159059817915d0be68f944f5b4e5301")
+	assert.Equal(t, hex.EncodeToString(signature), "f0e4f8fe6502bb950fa45283832d117dda9876e1bf92c29808ab9072fd717cc3756ee55cd659cc33ed2d3d0aa6f290f3f583045e9b91c32cab64747b8b43c77000")
 
 	valid, err := signer.VerifySignature(hash.Bytes(), signature)
 	assert.NilError(t, err)
