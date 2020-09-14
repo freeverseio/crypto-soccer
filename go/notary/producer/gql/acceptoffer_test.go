d4a61b8c7064295345d54b9aac469254fed8036c
Toni Mateos <tmateos@freeverse.io>
2020-09-14 10:42:07 +0200

    improved acceptoffer test

--- go/notary/producer/gql/acceptoffer_test.go
+++ go/notary/producer/gql/acceptoffer_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql"
 	"github.com/freeverseio/crypto-soccer/go/notary/producer/gql/input"
 	"github.com/freeverseio/crypto-soccer/go/notary/signer"
@@ -14,29 +16,46 @@ import (
 )
 
 func TestAcceptOfferReturnTheSignature(t *testing.T) {
+	timezoneIdx := uint8(1)
+	countryIdx := big.NewInt(0)
+	// We will here assign the next available team to offerer so she can make an offer for the players of a different team
+	// we choose that player as a player very far from the current amount of teams (x2)
+	nHumanTeams, _ := bc.Contracts.Assets.GetNHumansInCountry(&bind.CallOpts{}, timezoneIdx, countryIdx)
+	offererTeamIdx := nHumanTeams.Int64()
+	sellerTeamIdx := offererTeamIdx + 1
+	// offererTeamId, _ := bc.Contracts.Assets.EncodeTZCountryAndVal(&bind.CallOpts{}, timezoneIdx, countryIdx, big.NewInt(offererTeamIdx))
+	offerer, _ := crypto.HexToECDSA("9B878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54")
+	seller, _ := crypto.HexToECDSA("0A878F7892FBBFA30C8AED1DF317C19B853685E707C2CF0EE1927DC516060A54")
+	playerId, _ := bc.Contracts.Assets.EncodeTZCountryAndVal(&bind.CallOpts{}, timezoneIdx, countryIdx, (big.NewInt(2 + 18*sellerTeamIdx)))
+
+	bc.Contracts.Assets.TransferFirstBotToAddr(
+		bind.NewKeyedTransactor(bc.Owner),
+		timezoneIdx,
+		countryIdx,
+		crypto.PubkeyToAddress(offerer.PublicKey),
+	)
+	bc.Contracts.Assets.TransferFirstBotToAddr(
+		bind.NewKeyedTransactor(bc.Owner),
+		timezoneIdx,
+		countryIdx,
+		crypto.PubkeyToAddress(seller.PublicKey),
+	)
+
 	ch := make(chan interface{}, 10)
 	r := gql.NewResolver(ch, *bc.Contracts, namesdb, googleCredentials, service)
 
 	in := input.AcceptOfferInput{}
-	in.ValidUntil = strconv.FormatInt(time.Now().Unix()+100, 10)
-	in.PlayerId = "274877906944"
+	in.ValidUntil = strconv.FormatInt(time.Now().Unix()+1000, 10)
+	in.OfferValidUntil = strconv.FormatInt(time.Now().Unix()+100, 10)
+	in.PlayerId = playerId.String()
 	in.CurrencyId = 1
 	in.Price = 41234
 	in.Rnd = 42321
 	in.OfferId = "12abc345cd"
 
-	playerId, _ := new(big.Int).SetString(in.PlayerId, 10)
-	validUntil, err := strconv.ParseInt(in.ValidUntil, 10, 64)
+	hash, err := in.SellerDigest()
 	assert.NilError(t, err)
-	hash, err := signer.HashSellMessage(
-		uint8(in.CurrencyId),
-		big.NewInt(int64(in.Price)),
-		big.NewInt(int64(in.Rnd)),
-		validUntil,
-		playerId,
-	)
-	assert.NilError(t, err)
-	signature, err := signer.Sign(hash.Bytes(), bc.Owner)
+	signature, err := signer.Sign(hash.Bytes(), seller)
 	assert.NilError(t, err)
 	in.Signature = hex.EncodeToString(signature)
 
