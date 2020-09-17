7caf3c3aea94a829679b3b0f39a54f9cbfe61812
Christian <cristian.cuaresma@gmail.com>
2020-09-17 16:27:04 +0200

    Refactor to async/await

--- nodejs-gamelayer/src/resolvers/queryTeamByTeamId.js
+++ nodejs-gamelayer/src/resolvers/queryTeamByTeamId.js
@@ -1,31 +1,30 @@
 const { selectTeamName, selectTeamManagerName } = require('../repositories');
 
-const queryTeamByTeamId = (parent, args, context, info, schema) => {
-  return info.mergeInfo
-    .delegateToSchema({
-      schema,
-      operation: 'query',
-      fieldName: 'teamByTeamId',
-      args: {
-        teamId: args.teamId,
-      },
-      context,
-      info,
-    })
-    .then((result) => {
-      if (!result) {
-        return;
-      }
-      const teamName = selectTeamName({ teamId: args.teamId });
-      const teamManagerName = selectTeamManagerName({
-        teamId: args.teamId,
-      });
+const queryTeamByTeamId = async (parent, args, context, info, schema) => {
+  const result = await info.mergeInfo.delegateToSchema({
+    schema,
+    operation: 'query',
+    fieldName: 'teamByTeamId',
+    args: {
+      teamId: args.teamId,
+    },
+    context,
+    info,
+  });
 
-      result.name = teamName && teamName.team_name ? teamName.team_name : result.name;
-      result.managerName =
-        teamManagerName && teamManagerName.team_manager_name ? teamManagerName.team_manager_name : result.managerName;
-      return result;
-    });
+  if (!result) {
+    return;
+  }
+  const teamName = await selectTeamName({ teamId: args.teamId });
+  const teamManagerName = await selectTeamManagerName({
+    teamId: args.teamId,
+  });
+
+  result.name = teamName && teamName.team_name ? teamName.team_name : result.name;
+  result.managerName =
+    teamManagerName && teamManagerName.team_manager_name ? teamManagerName.team_manager_name : result.managerName;
+
+  return result;
 };
 
 module.exports = queryTeamByTeamId;
