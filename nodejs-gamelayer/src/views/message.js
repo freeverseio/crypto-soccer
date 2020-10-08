ac420f2f0387c6ca32417d2ce321d52f77ce9de7
Christian <cristian.cuaresma@gmail.com>
2020-10-08 12:49:13 +0200

    Some fixes to mailbox imports and view

---
+++ nodejs-gamelayer/src/views/message.js
@@ -0,0 +1,15 @@
+const message = ({ id, destinatary, category, auction_id, title, text, custom_image_url, metadata, is_read }) => {
+  return {
+    id,
+    destinatary,
+    category,
+    auctionId: auction_id,
+    title,
+    text,
+    customImageUrl: custom_image_url,
+    metadata,
+    isRead: is_read,
+  };
+};
+
+module.exports = message;
