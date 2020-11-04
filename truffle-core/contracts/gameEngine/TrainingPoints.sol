eb1b89619016416c2197c3de144a1bdc1673189d
Toni Mateos <tmateos@freeverse.io>
2020-11-04 11:39:15 +0100

    fixed extreme cases in training points due to diff among teams qualities

--- truffle-core/contracts/gameEngine/TrainingPoints.sol
+++ truffle-core/contracts/gameEngine/TrainingPoints.sol
@@ -74,14 +74,19 @@ contract TrainingPoints is EncodingMatchLog, EngineLib, EncodingTPAssignment, En
         uint256 teamSumSkills0 = getTeamSumSkills(matchLog0);
         uint256 teamSumSkills1 = getTeamSumSkills(matchLog1);
 
-        if (teamSumSkills0 != 0 && teamSumSkills1 != 0) {
+        if ((teamSumSkills0 == 0) || (teamSumSkills1 == 0)) {
+            points[0] = POINTS_FOR_HAVING_PLAYED;
+            points[1] = POINTS_FOR_HAVING_PLAYED;
+        } else {
+            if (teamSumSkills1 > 3 * teamSumSkills0) teamSumSkills1 = 3 * teamSumSkills0;
+            if (teamSumSkills0 > 3 * teamSumSkills1) teamSumSkills0 = 3 * teamSumSkills1;
             points[0] = (points[0] * teamSumSkills1) / (teamSumSkills0);
             points[1] = (points[1] * teamSumSkills0) / (teamSumSkills1);
         }
-        
+
         if (points[0] < POINTS_FOR_HAVING_PLAYED) points[0] = POINTS_FOR_HAVING_PLAYED;
         if (points[1] < POINTS_FOR_HAVING_PLAYED) points[1] = POINTS_FOR_HAVING_PLAYED;
-
+        
         matchLog0 = addTrainingPoints(matchLog0, points[0]);
         matchLog1 = addTrainingPoints(matchLog1, points[1]);
         return (matchLog0, matchLog1);
