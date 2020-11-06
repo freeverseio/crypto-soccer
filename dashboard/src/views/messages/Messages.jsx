a35305947892da92007af7aa1e6f27390301e456
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-11-06 12:37:14 +0100

    [dashboard] added the logic to message page

--- dashboard/src/views/messages/Messages.jsx
+++ dashboard/src/views/messages/Messages.jsx
@@ -1,35 +1,49 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import { Container, Form } from 'semantic-ui-react';
-import Config from '../../Config';
+import { useState } from 'react';
 
-const GET_AUCTIONS = gql`
-{
-  allAuctions {
-    nodes {
-      id
-      playerId
-      price
-      validUntil
-      seller
-      state
-      stateExtra
-      playerByPlayerId {
-        name
-      }
-    }
+const SEND_MESSADE = gql`
+  mutation SetBroadcastMessage(
+    $category: String!,
+    $title: String!,
+    $text: String!
+  )
+  {
+    setBroadcastMessage(input: {
+    category: $category,
+    auctionId:"",
+    title: $title,
+    text: $text,
+    customImageUrl:"",
+    metadata:"{}"})
   }
-}
 `;
 
 export default () => {
+  const [sendMessage] = useMutation(SEND_MESSADE);
+  const [category, setCategory] = useState("");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSubmit = () => {
+    sendMessage({
+      variables: {
+        category: category,
+        title: title,
+        text: text,
+      }
+    })
+    .catch(console.error);
+  }
+
   return (
     <Container>
-      <Form>
-        <Form.Input fluid label="Type" />
-        <Form.Input fluid label="Title" />
-        <Form.TextArea label="Body" rows="10"/>
+      <Form onSubmit={handleSubmit}>
+        <Form.Input fluid label="Category" value={category} onChange={e => setCategory(e.value)} />
+        <Form.Input fluid label="Title" value={title} onChange={e => setTitle(e.value)} />
+        <Form.TextArea label="Body" value={text} onChange={e => setText(e.value)} rows="10" />
         <Form.Button>Submit</Form.Button>
       </Form>
     </Container>
