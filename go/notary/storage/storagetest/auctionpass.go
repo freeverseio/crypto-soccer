4e3ee320ad188623e0971cf93aa323fd11a2d606
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-12-02 11:54:26 +0100

    [go|notary] added test for inset play store order

--- go/notary/storage/storagetest/auctionpass.go
+++ go/notary/storage/storagetest/auctionpass.go
@@ -4,7 +4,15 @@ import (
 	"testing"
 
 	"github.com/freeverseio/crypto-soccer/go/notary/storage"
+	"gotest.tools/assert"
 )
 
 func testAuctionPassServiceInterface(t *testing.T, service storage.StorageService) {
+	t.Run("insert Auction Pass playstore order", func(t *testing.T) {
+		tx, err := service.Begin()
+		assert.NilError(t, err)
+		defer tx.Rollback()
+		order := storage.NewAuctionPassPlaystoreOrder()
+		assert.NilError(t, tx.AuctionPassPlayStoreInsert(*order))
+	})
 }
