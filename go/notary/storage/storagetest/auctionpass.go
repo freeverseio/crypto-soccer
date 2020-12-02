0aab87b247eafcc6e5118782bb67510950da6d08
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-12-02 11:47:39 +0100

    [go|notary|storage] test for auctionPass

---
+++ go/notary/storage/storagetest/auctionpass.go
@@ -0,0 +1,10 @@
+package storagetest
+
+import (
+	"testing"
+
+	"github.com/freeverseio/crypto-soccer/go/notary/storage"
+)
+
+func testAuctionPassServiceInterface(t *testing.T, service storage.StorageService) {
+}
