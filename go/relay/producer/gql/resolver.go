50a91bc8ce1f0438a3f293078d2f8a8f8db8ee3d
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-12-04 10:55:29 +0100

    [go|relay] using db in producer

--- go/relay/producer/gql/resolver.go
+++ go/relay/producer/gql/resolver.go
@@ -1,19 +1,26 @@
 package gql
 
-import "github.com/freeverseio/crypto-soccer/go/contracts"
+import (
+	"database/sql"
+
+	"github.com/freeverseio/crypto-soccer/go/contracts"
+)
 
 type Resolver struct {
 	ch        chan interface{}
 	contracts contracts.Contracts
+	db        *sql.DB
 }
 
 func NewResolver(
 	ch chan interface{},
 	contracts contracts.Contracts,
+	db *sql.DB,
 ) *Resolver {
 	return &Resolver{
 		ch:        ch,
 		contracts: contracts,
+		db:        db,
 	}
 }
 
