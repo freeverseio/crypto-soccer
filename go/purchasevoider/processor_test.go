9dea9712b851c58f6d52cbd929918cf40b8f3749
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2020-12-10 16:20:32 +0100

    [go|pv] added tests for get tokens

--- go/purchasevoider/processor_test.go
+++ go/purchasevoider/processor_test.go
@@ -1,10 +1,13 @@
 package purchasevoider_test
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/freeverseio/crypto-soccer/go/purchasevoider"
 	"github.com/freeverseio/crypto-soccer/go/purchasevoider/mockup"
+	"google.golang.org/api/androidpublisher/v3"
 	"gotest.tools/assert"
 )
 
@@ -20,5 +23,57 @@ func TestNew(t *testing.T) {
 			&mockup.MarketService{},
 		)
 		assert.NilError(t, err)
+	})
+}
+
+func TestGetVoidedTokens(t *testing.T) {
+	t.Run("service return nil tockens", func(t *testing.T) {
+		pv, err := purchasevoider.New(
+			&mockup.VoidPurchaseService{
+				VoidedPurchasesFn: func(context.Context) ([]*androidpublisher.VoidedPurchase, error) {
+					return nil, nil
+				},
+			},
+			&mockup.UniverseService{},
+			&mockup.MarketService{},
+		)
+		assert.NilError(t, err)
+		tokens, err := pv.GetVoidedTokens()
+		assert.NilError(t, err)
+		assert.Equal(t, len(tokens), 0)
+	})
+	t.Run("service return error", func(t *testing.T) {
+		pv, err := purchasevoider.New(
+			&mockup.VoidPurchaseService{
+				VoidedPurchasesFn: func(context.Context) ([]*androidpublisher.VoidedPurchase, error) {
+					return nil, errors.New("error")
+				},
+			},
+			&mockup.UniverseService{},
+			&mockup.MarketService{},
+		)
+		assert.NilError(t, err)
+		_, err = pv.GetVoidedTokens()
+		assert.Error(t, err, "error")
+	})
+	t.Run("service return tokens", func(t *testing.T) {
+		pv, err := purchasevoider.New(
+			&mockup.VoidPurchaseService{
+				VoidedPurchasesFn: func(context.Context) ([]*androidpublisher.VoidedPurchase, error) {
+					return []*androidpublisher.VoidedPurchase{
+						&androidpublisher.VoidedPurchase{
+							PurchaseToken: "ciao",
+						},
+					}, nil
+				},
+			},
+			&mockup.UniverseService{},
+			&mockup.MarketService{},
+		)
+		assert.NilError(t, err)
+		tokens, err := pv.GetVoidedTokens()
+		assert.NilError(t, err)
+		assert.Equal(t, len(tokens), 1)
+		assert.Equal(t, tokens[0], "ciao")
 	})
 }
