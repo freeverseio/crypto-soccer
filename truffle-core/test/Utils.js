458adb93c2d6c4e91588ca77097e1c64f40a6236
Toni <>
2024-10-16 12:09:13 +0200

    adding more tests

--- truffle-core/test/Utils.js
+++ truffle-core/test/Utils.js
@@ -10,6 +10,7 @@ const Privileged = artifacts.require('Privileged');
 const Utils = artifacts.require('Utils');
 const debug = require('../utils/debugUtils.js');
 const { isBigNumber } = require('web3-utils');
+const { assert } = require('chai');
 
 
 function getLevel(skills) {
@@ -41,7 +42,7 @@ contract('Privileged', (accounts) => {
         utils = await Utils.new().should.be.fulfilled;
     });
 
-    it('creating a team works', async () =>  {
+    it('creating a team in (10,0,0) works', async () =>  {
         const timeZone = 10;
         const countryIdxInTZ = 0;
         const teamIdxInTZ = 0;
@@ -54,7 +55,30 @@ contract('Privileged', (accounts) => {
         for (let i = 0; i < 18; i++) {
             const decodedSkills = await utils.fullDecodeSkills(playerSkillsAtBirth[i]);
             assert.equal(decodedSkills.playerId.toString(), playerIds[i].toString())
-            console.log(decodedSkills.dayOfBirth.toString())
+            const decodedPlayerId = await utils.decodeTZCountryAndVal(playerIds[i]);
+            assert.equal(decodedPlayerId[0].toString(), timeZone.toString())
+            assert.equal(decodedPlayerId[1].toString(), countryIdxInTZ.toString())
+            assert.equal(decodedPlayerId[2].toString(), i.toString())
+        }
+    });
+
+    it('creating a team in (23,3,5) works', async () =>  {
+        const timeZone = 23;
+        const countryIdxInTZ = 3;
+        const teamIdxInTZ = 5;
+        const deployTimeInUnixEpochSecs = 1728813174000; // Date and time (GMT): Sunday, 13 October 2024 09:52:54
+        const divisionCreationRound = 0;
+        const {teamId, playerIds, playerSkillsAtBirth} = await utils.createTeam(timeZone, countryIdxInTZ, teamIdxInTZ, deployTimeInUnixEpochSecs, divisionCreationRound);
+        assert.equal(teamId.toString(), "6322997166085");
+        assert.equal(playerIds.length, 18);
+        assert.equal(playerSkillsAtBirth.length, 18);
+        for (let i = 0; i < 18; i++) {
+            const decodedSkills = await utils.fullDecodeSkills(playerSkillsAtBirth[i]);
+            assert.equal(decodedSkills.playerId.toString(), playerIds[i].toString())
+            const decodedPlayerId = await utils.decodeTZCountryAndVal(playerIds[i]);
+            assert.equal(decodedPlayerId[0].toString(), timeZone.toString())
+            assert.equal(decodedPlayerId[1].toString(), countryIdxInTZ.toString())
+            assert.equal(decodedPlayerId[2].toString(), (i + 5 * 18).toString())
         }
     });
 
